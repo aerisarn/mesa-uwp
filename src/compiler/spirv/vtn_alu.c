f6f9000f84db84370262472282065a8d17ed2b36
Rhys Perry <pendingchaos02@gmail.com>
2020-06-16 14:28:18 +0100

    spirv: create ffma more often
    We will not be able to combine instructions into ffma later if they are
    exact, so create them from the start. They can be lowered later if they
    are unwanted.
    
    fossil-db (GFX10.3):
    Totals from 14697 (10.05% of 146267) affected shaders:
    VGPRs: 645736 -> 614168 (-4.89%)
    CodeSize: 59312768 -> 58735352 (-0.97%); split: -0.97%, +0.00%
    MaxWaves: 372900 -> 376666 (+1.01%)
    Instrs: 11339280 -> 11120882 (-1.93%); split: -1.93%, +0.00%
    Latency: 284874519 -> 285277327 (+0.14%); split: -0.10%, +0.24%
    InvThroughput: 68791374 -> 68526739 (-0.38%); split: -0.49%, +0.10%
    
    fossil-db (GFX10):
    Totals from 11039 (7.55% of 146267) affected shaders:
    CodeSize: 54785444 -> 54785268 (-0.00%); split: -0.00%, +0.00%
    Instrs: 10401349 -> 10401396 (+0.00%); split: -0.00%, +0.00%
    Latency: 277781803 -> 278572890 (+0.28%); split: -0.00%, +0.29%
    InvThroughput: 65035902 -> 65100855 (+0.10%); split: -0.00%, +0.10%
    
    fossil-db (GFX9):
    Totals from 24055 (16.43% of 146401) affected shaders:
    SGPRs: 1790704 -> 1790640 (-0.00%)
    VGPRs: 1105736 -> 1105716 (-0.00%)
    CodeSize: 110944732 -> 110948812 (+0.00%); split: -0.00%, +0.01%
    Instrs: 21609095 -> 21610227 (+0.01%); split: -0.00%, +0.01%
    Latency: 756137596 -> 756145812 (+0.00%); split: -0.02%, +0.02%
    InvThroughput: 344103825 -> 344112245 (+0.00%); split: -0.00%, +0.01%
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8056>

--- src/compiler/spirv/vtn_alu.c
+++ src/compiler/spirv/vtn_alu.c
@@ -122,8 +122,8 @@ matrix_multiply(struct vtn_builder *b,
                      nir_channel(&b->nb, src1->elems[i]->def, src0_columns - 1));
          for (int j = src0_columns - 2; j >= 0; j--) {
             dest->elems[i]->def =
-               nir_fadd(&b->nb, nir_fmul(&b->nb, src0->elems[j]->def,
-                                         nir_channel(&b->nb, src1->elems[i]->def, j)),
+               nir_ffma(&b->nb, src0->elems[j]->def,
+                                nir_channel(&b->nb, src1->elems[i]->def, j),
                                 dest->elems[i]->def);
          }
       }
