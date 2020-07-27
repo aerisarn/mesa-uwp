f360086c301e1672c2b7c297a61c4caaa16a8fd7
Pierre Moreau <dev@pmoreau.org>
2020-07-27 09:53:08 +0200

    nv50: Mark RESOURCE_FROM_USER_MEMORY_COMPUTE_ONLY as unsupported
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Signed-off-by: Pierre Moreau <dev@pmoreau.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10711>

--- src/gallium/drivers/nouveau/nv50/nv50_screen.c
+++ src/gallium/drivers/nouveau/nv50/nv50_screen.c
@@ -318,6 +318,7 @@ nv50_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_VERTEXID_NOBASE:
    case PIPE_CAP_MULTISAMPLE_Z_RESOLVE: /* potentially supported on some hw */
    case PIPE_CAP_RESOURCE_FROM_USER_MEMORY:
+   case PIPE_CAP_RESOURCE_FROM_USER_MEMORY_COMPUTE_ONLY:
    case PIPE_CAP_DEVICE_RESET_STATUS_QUERY:
    case PIPE_CAP_MAX_SHADER_PATCH_VARYINGS:
    case PIPE_CAP_DRAW_PARAMETERS:
