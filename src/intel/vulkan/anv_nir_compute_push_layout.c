770d3312857a2ce2130dd949f3149d14068eb760
Jason Ekstrand <jason@jlekstrand.net>
2020-08-06 22:31:02 -0500

    anv: Disallow UBO pushing for bindless shaders
    They don't really have push constants.
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8637>

--- src/intel/vulkan/anv_nir_compute_push_layout.c
+++ src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -76,7 +76,8 @@ anv_nir_compute_push_layout(const struct anv_physical_device *pdevice,
 
    const bool push_ubo_ranges =
       pdevice->info.verx10 >= 75 &&
-      has_const_ubo && nir->info.stage != MESA_SHADER_COMPUTE;
+      has_const_ubo && nir->info.stage != MESA_SHADER_COMPUTE &&
+      !brw_shader_stage_is_bindless(nir->info.stage);
 
    if (push_ubo_ranges && robust_buffer_access) {
       /* We can't on-the-fly adjust our push ranges because doing so would
