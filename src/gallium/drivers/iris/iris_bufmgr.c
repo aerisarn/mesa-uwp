abace2b8a454d4aa787a20eadef353d5f6a04592
Jordan Justen <jordan.l.justen@intel.com>
2020-12-03 15:38:59 -0800

    iris: Align buffer VMA to 2MiB for XeHP
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14155>

--- src/gallium/drivers/iris/iris_bufmgr.c
+++ src/gallium/drivers/iris/iris_bufmgr.c
@@ -2314,7 +2314,9 @@ iris_bufmgr_create(struct intel_device_info *devinfo, int fd, bool bo_reuse)
    bufmgr->handle_table =
       _mesa_hash_table_create(NULL, _mesa_hash_uint, _mesa_key_uint_equal);
 
-   bufmgr->vma_min_align = devinfo->has_local_mem ? 64 * 1024 : PAGE_SIZE;
+   bufmgr->vma_min_align =
+      devinfo->verx10 >= 125 ? 2 * 1024 * 1024 :
+      (devinfo->has_local_mem ? 64 * 1024 : PAGE_SIZE);
 
    if (devinfo->has_aux_map) {
       bufmgr->aux_map_ctx = intel_aux_map_init(bufmgr, &aux_map_allocator,
