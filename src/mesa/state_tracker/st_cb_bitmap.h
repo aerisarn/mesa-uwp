6ffdca73d3796a3e94fd52905bceac4ff0750080
Eric Anholt <eric@anholt.net>
2020-12-10 11:45:39 -0800

    mesa/st: Remove TGSI-only shader lowering code.
    These are no longer called.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8044>

--- src/mesa/state_tracker/st_cb_bitmap.h
+++ src/mesa/state_tracker/st_cb_bitmap.h
@@ -46,11 +46,6 @@ st_destroy_bitmap(struct st_context *st);
 extern void
 st_flush_bitmap_cache(struct st_context *st);
 
-extern const struct tgsi_token *
-st_get_bitmap_shader(const struct tgsi_token *tokens,
-                     unsigned tex_target, unsigned sampler_index,
-                     bool use_texcoord, bool swizzle_xxxx);
-
 void st_Bitmap(struct gl_context *ctx, GLint x, GLint y,
                GLsizei width, GLsizei height,
                const struct gl_pixelstore_attrib *unpack, const GLubyte *bitmap);
