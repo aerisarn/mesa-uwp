d08e2e5a96feb53f1c343a3913ee69f6c598bbe4
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-03-08 14:05:08 +0100

    ci: Move Kernel build tasks into its own file
    Allows to reuse the Kernel build tasks everywhere it is needed.
    
    The x86_test-gl container now need a kernel image to use for the crosvm environment,
    Reuse this task there.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10862>

--- .gitlab-ci/container/x86_test-gl.sh
+++ .gitlab-ci/container/x86_test-gl.sh
@@ -9,15 +9,20 @@ export DEBIAN_FRONTEND=noninteractive
 STABLE_EPHEMERAL=" \
       autoconf \
       automake \
+      bc \
+      bison \
+      bzip2 \
       cargo \
       ccache \
       clang-11 \
       cmake \
+      flex \
       g++ \
       glslang-tools \
       libasound2-dev \
       libcap-dev \
       libclang-cpp11-dev \
+      libelf-dev \
       libfdt-dev \
       libgbm-dev \
       libgles2-mesa-dev \
@@ -66,6 +71,16 @@ apt-get install -y --no-remove \
 
 
 . .gitlab-ci/container/container_pre_build.sh
+
+############### Build kernel
+
+export DEFCONFIG="arch/x86/configs/x86_64_defconfig"
+export KERNEL_IMAGE_NAME=bzImage
+export KERNEL_ARCH=x86_64
+export DEBIAN_ARCH=amd64
+
+mkdir -p /lava-files/
+. .gitlab-ci/container/build-kernel.sh
 
 ############### Build libdrm
 
