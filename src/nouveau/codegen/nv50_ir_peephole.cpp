81828284b2787d868da2a3cd1de5fabf77780387
Pierre Moreau <dev@pmoreau.org>
2021-04-19 19:04:11 +0200

    nv50/ir: Handle non-32-bit values when cst folding SPLIT
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Signed-off-by: Pierre Moreau <dev@pmoreau.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10711>

--- src/nouveau/codegen/nv50_ir_peephole.cpp
+++ src/nouveau/codegen/nv50_ir_peephole.cpp
@@ -1070,7 +1070,8 @@ ConstantFolding::opnd(Instruction *i, ImmediateValue &imm0, int s)
          Value *def = i->getDef(d);
          assert(def->reg.size == size);
 
-         newi = bld.mkMov(def, bld.mkImm((uint32_t)(val & mask)), TYPE_U32);
+         newi = bld.mkMov(def, bld.mkImm((uint32_t)(val & mask)),
+                          typeOfSize(size));
          val >>= bitsize;
       }
       delete_Instruction(prog, i);
