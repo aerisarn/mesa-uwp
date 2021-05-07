0cfcc0602b8ec5ee2dfab302983d8159676720c8
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-07 19:08:30 -0400

    zink: set subdata hook as PIPE_MAP_ONCE
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11401>

--- src/gallium/drivers/zink/zink_resource.c
+++ src/gallium/drivers/zink/zink_resource.c
@@ -1204,7 +1204,7 @@ zink_buffer_subdata(struct pipe_context *ctx, struct pipe_resource *buffer,
    struct pipe_box box;
    uint8_t *map = NULL;
 
-   usage |= PIPE_MAP_WRITE;
+   usage |= PIPE_MAP_WRITE | PIPE_MAP_ONCE;
 
    if (!(usage & PIPE_MAP_DIRECTLY))
       usage |= PIPE_MAP_DISCARD_RANGE;
