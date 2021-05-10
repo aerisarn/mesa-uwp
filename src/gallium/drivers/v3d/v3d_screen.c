838316dd5b530444499d285c3c679a2dd421791e
Iago Toral Quiroga <itoral@igalia.com>
2021-05-10 09:08:48 +0200

    broacom/compiler: enable PIPE_SHADER_CAP_INDIRECT_OUTPUT_ADDR
    We have been able to to handle indirect offsets on GS outputs for
    a while and we have just implemented this for VS, so we can enable
    this capability and avoid the horrible if-ladder code to convert
    indirect output indices to constant indices.
    
    Reviewed-by: Juan A. Suarez <jasuarez@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10723>

--- src/gallium/drivers/v3d/v3d_screen.c
+++ src/gallium/drivers/v3d/v3d_screen.c
@@ -370,7 +370,7 @@ v3d_screen_get_shader_param(struct pipe_screen *pscreen, unsigned shader,
         case PIPE_SHADER_CAP_INDIRECT_INPUT_ADDR:
                 return 1;
         case PIPE_SHADER_CAP_INDIRECT_OUTPUT_ADDR:
-                return 0;
+                return 1;
         case PIPE_SHADER_CAP_INDIRECT_TEMP_ADDR:
                 return 1;
         case PIPE_SHADER_CAP_INDIRECT_CONST_ADDR:
