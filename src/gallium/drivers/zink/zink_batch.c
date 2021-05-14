f223ee90047dc0c6e825e188a1dea3703c5061e2
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-14 14:52:20 -0400

    zink: only flag persistent resource maps for invalidation if they aren't coherent
    coherent is already synchronized
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11688>

--- src/gallium/drivers/zink/zink_batch.c
+++ src/gallium/drivers/zink/zink_batch.c
@@ -579,7 +579,7 @@ zink_batch_resource_usage_set(struct zink_batch *batch, struct zink_resource *re
       zink_batch_usage_set(&res->obj->reads, batch->state);
    }
    /* multiple array entries are fine */
-   if (res->obj->persistent_maps)
+   if (!res->obj->coherent && res->obj->persistent_maps)
       util_dynarray_append(&batch->state->persistent_resources, struct zink_resource_object*, res->obj);
 
    batch->has_work = true;
