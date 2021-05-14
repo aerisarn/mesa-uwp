f44a6c6a5424db714e5c93e4b13d4e3f79bd5ed2
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-05-14 14:04:11 +0200

    radv: Actually return correct value for read-only DCC compressedness.
    Most stuff that depends on the value wouldn't be triggered anyway but
    ...
    
    Fixes: b5ecf0748a5 ("radv: Ensure we never decompress or FCE read-only textures.")
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10802>

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -1976,7 +1976,7 @@ radv_layout_dcc_compressed(const struct radv_device *device, const struct radv_i
 
    /* If the image is read-only, we can always just keep it compressed */
    if (!(image->usage & RADV_IMAGE_USAGE_WRITE_BITS))
-      return false;
+      return true;
 
    /* Don't compress compute transfer dst when image stores are not supported. */
    if ((layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL || layout == VK_IMAGE_LAYOUT_GENERAL) &&
