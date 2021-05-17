de2da3dadfce31671a3b863924f0bdc147dfe55d
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-17 14:40:19 -0400

    zink: use MAP_ONCE for qbo readback
    this implies that the buffer can be unmapped
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12090>

--- src/gallium/drivers/zink/zink_query.c
+++ src/gallium/drivers/zink/zink_query.c
@@ -428,7 +428,7 @@ get_query_result(struct pipe_context *pctx,
 {
    struct zink_screen *screen = zink_screen(pctx->screen);
    struct zink_query *query = (struct zink_query *)q;
-   unsigned flags = PIPE_MAP_READ;
+   unsigned flags = PIPE_MAP_READ | PIPE_MAP_ONCE;
 
    if (!wait)
       flags |= PIPE_MAP_DONTBLOCK;
