088c7c61cfa9bb8d9066f26a836dd8c08e61eaac
Andres Gomez <agomez@igalia.com>
2021-05-18 12:31:01 +0300

    ci: Uprev piglit to 9d87cc3d79e ("framework/replay: send backend's subprocess stderr to sys.stderr")
    This pulls in a fix for replayer to make the backends send their
    stderr to sys.stderr always.
    
    v2:
      - Updated piglit results for llvmpipe, softpipe, zink and
        radeonsi-stoney.
    
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com> [v1]
    Reviewed-by: Martin Peres <martin.peres@mupuf.org> [v1]
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10854>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 3351e8952c7e4e1e9db61cddc556efc60b4d8f37
+git checkout 9d87cc3d79e0c7e572273b94dcb51882f4d83d7f
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
