a9a3f55519bf7ecf19f7b2d45c4a1bbcb6e42e4e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-05-18 10:29:00 -0400

    panfrost: Mark job_index > 10000 as unlikely
    By definition this is happening <= 0.02% of the time.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10869>

--- src/gallium/drivers/panfrost/pan_context.c
+++ src/gallium/drivers/panfrost/pan_context.c
@@ -694,7 +694,7 @@ panfrost_draw_vbo(struct pipe_context *pipe,
         /* Don't add too many jobs to a single batch. Hardware has a hard limit
          * of 65536 jobs, but we choose a smaller soft limit (arbitrary) to
          * avoid the risk of timeouts. This might not be a good idea. */
-        if (batch->scoreboard.job_index > 10000)
+        if (unlikely(batch->scoreboard.job_index > 10000))
                 batch = panfrost_get_fresh_batch_for_fbo(ctx);
 
         unsigned zs_draws = ctx->depth_stencil->draws;
