b92a6edf8245e5be99da260109198d5cafb6aa01
Alyssa Rosenzweig <alyssa@collabora.com>
2021-05-25 17:57:21 -0400

    etnaviv: Use Rn_UINT instead of In_UINT for index buffers
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10990>

--- src/gallium/drivers/etnaviv/etnaviv_screen.c
+++ src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -558,8 +558,8 @@ etna_screen_is_format_supported(struct pipe_screen *pscreen,
 
    if (usage & PIPE_BIND_INDEX_BUFFER) {
       /* must be supported index format */
-      if (format == PIPE_FORMAT_I8_UINT || format == PIPE_FORMAT_I16_UINT ||
-          (format == PIPE_FORMAT_I32_UINT &&
+      if (format == PIPE_FORMAT_R8_UINT || format == PIPE_FORMAT_R16_UINT ||
+          (format == PIPE_FORMAT_R32_UINT &&
            VIV_FEATURE(screen, chipFeatures, 32_BIT_INDICES))) {
          allowed |= PIPE_BIND_INDEX_BUFFER;
       }
