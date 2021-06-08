53826932db58989ef2e479ec395e90d71a5be64b
Emma Anholt <emma@anholt.net>
2021-06-07 17:10:31 -0700

    ci: Update piglit and deqp/piglit-runner.
    This brings in some major new features in the runner:
    
    - piglit tests now include subtest reporting
    - "-t" support for quick include-filtering of tests.
    - piglit tests that crash after their result report are considered crashes.
    - throws a nice error if you try to annotate the same failure twice
      (e.g. lvp's dEQP-VK.glsl.builtin.precision.pow.highp.vec2,Fail)
    
    Since the runner catches piglit test bugs where the same subtest is run
    twice, we also uprev piglit to pull in the fixes for those.
    
    Reviewed-by: Juan A. Suarez <jasuarez@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11283>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout eee7d89611cf6dbafdd23f85b2f5f25ecc2d1ca3
+git checkout 11025faf96df23debbefd3678fe959eaa35a50f0
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
