02832f9d03c80b32a4837432e3fc18db29b05148
Yiwei Zhang <zzyiwei@chromium.org>
2021-06-10 16:26:01 +0000

    vulkan: fix back compat with Android Oreo and below
    buffer_handle_t definition was previously inside the deprecated
    system/core/include/system/window.h.
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Acked-by: Chia-I Wu <olvaffe@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11301>

--- include/vulkan/vk_android_native_buffer.h
+++ include/vulkan/vk_android_native_buffer.h
@@ -20,6 +20,12 @@
 #ifdef ANDROID
 #include <cutils/native_handle.h>
 #include <vulkan/vulkan.h>
+
+#if ANDROID_API_LEVEL < 28
+/* buffer_handle_t was defined in the deprecated system/window.h */
+typedef const native_handle_t *buffer_handle_t;
+#endif
+
 #else
 typedef void *buffer_handle_t;
 #endif
