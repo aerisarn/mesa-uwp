d23c80eaee79f464bda9afc338021cc807f352ca
Daniel Stone <daniels@collabora.com>
2021-06-10 23:19:08 +0100

    ci/lava: Generate job name from lava-submit.sh
    Just use the CI job name rather than open-coding the parameters.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11309>

--- .gitlab-ci/lava/lava-submit.sh
+++ .gitlab-ci/lava/lava-submit.sh
@@ -21,7 +21,7 @@ ci-fairy minio cp job-rootfs-overlay.tar.gz "minio://${JOB_ROOTFS_OVERLAY_PATH}"
 
 artifacts/lava/lava_job_submitter.py \
 	--template artifacts/lava/lava.yml.jinja2 \
-	--pipeline-info "$CI_PIPELINE_URL on $CI_COMMIT_REF_NAME ${CI_NODE_INDEX}/${CI_NODE_TOTAL}" \
+	--pipeline-info "$CI_JOB_NAME: $CI_PIPELINE_URL on $CI_COMMIT_REF_NAME ${CI_NODE_INDEX}/${CI_NODE_TOTAL}" \
 	--base-system-url-prefix "https://${BASE_SYSTEM_HOST_PATH}" \
 	--mesa-build-url "${FDO_HTTP_CACHE_URI:-}https://${MESA_BUILD_PATH}" \
 	--job-rootfs-overlay-url "${FDO_HTTP_CACHE_URI:-}https://${JOB_ROOTFS_OVERLAY_PATH}" \
