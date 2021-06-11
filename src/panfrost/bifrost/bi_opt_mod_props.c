0cec71d7ce0a793b35aca7c142f511417c3fd57a
Alyssa Rosenzweig <alyssa@collabora.com>
2021-06-11 12:43:41 -0400

    pan/bi: Fuse abs into FCMP/FMIN/FMAX.v2f16
    Needs to be aware of an encoding restriction here.
    
    No shader-db changes.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11327>

--- src/panfrost/bifrost/bi_opt_mod_props.c
+++ src/panfrost/bifrost/bi_opt_mod_props.c
@@ -25,14 +25,14 @@
 #include "compiler.h"
 
 static bool
-bi_takes_fabs(bi_instr *I, unsigned s)
+bi_takes_fabs(bi_instr *I, bi_index repl, unsigned s)
 {
         switch (I->op) {
         case BI_OPCODE_FCMP_V2F16:
         case BI_OPCODE_FMAX_V2F16:
         case BI_OPCODE_FMIN_V2F16:
-                /* TODO: Check count or lower */
-                return false;
+                /* Encoding restriction: can't have both abs if equal sources */
+                return !(I->src[1 - s].abs && bi_is_word_equiv(I->src[1 - s], repl));
         case BI_OPCODE_V2F32_TO_V2F16:
                 /* TODO: Needs both match or lower */
                 return false;
@@ -126,7 +126,7 @@ bi_opt_mod_prop_forward(bi_context *ctx)
                                 continue;
 
                         if (bi_is_fabsneg(mod)) {
-                                if (mod->src[0].abs && !bi_takes_fabs(I, s))
+                                if (mod->src[0].abs && !bi_takes_fabs(I, mod->src[0], s))
                                         continue;
 
                                 if (mod->src[0].neg && !bi_takes_fneg(I, s))
