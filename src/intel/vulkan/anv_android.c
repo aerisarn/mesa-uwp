a0122385f0772ab16658853091b4f71659887715
Yiwei Zhang <zzyiwei@chromium.org>
2021-06-12 23:35:19 +0000

    anv: fix Android WSI VkFence
    Fix an obvious one-liner bug.
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11344>

--- src/intel/vulkan/anv_android.c
+++ src/intel/vulkan/anv_android.c
@@ -820,7 +820,7 @@ anv_AcquireImageANDROID(
          }
       } else if (semaphore_h != VK_NULL_HANDLE) {
          semaphore_fd = nativeFenceFd;
-      } else if (fence_h == VK_NULL_HANDLE) {
+      } else if (fence_h != VK_NULL_HANDLE) {
          fence_fd = nativeFenceFd;
       } else {
          /* Nothing to import into so we have to close the file */
