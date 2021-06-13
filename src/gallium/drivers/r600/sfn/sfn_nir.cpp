5e1a7039340cdabb4fcef63a1bd24bb927031810
Gert Wollny <gert.wollny@collabora.com>
2021-06-13 14:10:19 +0200

    r600/sfn: don't designates initializers, since they are c++20
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11348>

--- src/gallium/drivers/r600/sfn/sfn_nir.cpp
+++ src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -871,10 +871,10 @@ int r600_shader_from_nir(struct r600_context *rctx,
 
    NIR_PASS_V(sel->nir, nir_lower_vars_to_ssa);
    NIR_PASS_V(sel->nir, nir_lower_regs_to_ssa);
-   nir_lower_idiv_options idiv_options = {
-      .imprecise_32bit_lowering = sel->nir->info.stage != MESA_SHADER_COMPUTE,
-      .allow_fp16 = true,
-   };
+   nir_lower_idiv_options idiv_options = {0};
+   idiv_options.imprecise_32bit_lowering = sel->nir->info.stage != MESA_SHADER_COMPUTE;
+   idiv_options.allow_fp16 = true;
+
    NIR_PASS_V(sel->nir, nir_lower_idiv, &idiv_options);
    NIR_PASS_V(sel->nir, r600_lower_alu);
    NIR_PASS_V(sel->nir, nir_lower_phis_to_scalar, false);
