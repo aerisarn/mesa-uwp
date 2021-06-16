fde7b6694b7f2b7d60d1eedafd20c9ab00ef9cb3
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-06-16 01:15:49 -0700

    zink: use alloca instead of hard-to-size vlas
    These variable-length arrays are hard to size statically, and VLAs
    aren't supported by MSVC. So let's use alloca instead.
    
    Reviewed-by: Hoe Hao Cheng <haochengho12907@gmail.com>
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11380>

--- src/gallium/drivers/zink/zink_descriptors.c
+++ src/gallium/drivers/zink/zink_descriptors.c
@@ -545,7 +545,7 @@ bool
 zink_descriptor_util_alloc_sets(struct zink_screen *screen, VkDescriptorSetLayout dsl, VkDescriptorPool pool, VkDescriptorSet *sets, unsigned num_sets)
 {
    VkDescriptorSetAllocateInfo dsai;
-   VkDescriptorSetLayout layouts[num_sets];
+   VkDescriptorSetLayout *layouts = alloca(sizeof(*layouts) * num_sets);
    memset((void *)&dsai, 0, sizeof(dsai));
    dsai.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
    dsai.pNext = NULL;
@@ -593,7 +593,7 @@ allocate_desc_set(struct zink_context *ctx, struct zink_program *pg, enum zink_d
       for (unsigned desc_factor = DESC_BUCKET_FACTOR; desc_factor < descs_used; desc_factor *= DESC_BUCKET_FACTOR)
          bucket_size = desc_factor;
    }
-   VkDescriptorSet desc_set[bucket_size];
+   VkDescriptorSet *desc_set = alloca(sizeof(*desc_set) * bucket_size);
    if (!zink_descriptor_util_alloc_sets(screen, push_set ? ctx->dd->push_dsl[is_compute]->layout : pg->dsl[type + 1], pool->descpool, desc_set, bucket_size))
       return VK_NULL_HANDLE;
 
