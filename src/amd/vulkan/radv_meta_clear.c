269795c838e7ffbd6ecef4f7dcd176e2fc4ead13
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-06-22 14:13:36 +0200

    radv: prevent fast clearing HTILE depth for unrestricted ranges
    VK_EXT_depth_range_unrestricted removes the restriction that the
    clear value must be between 0.0 and 1.0.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10035>

--- src/amd/vulkan/radv_meta_clear.c
+++ src/amd/vulkan/radv_meta_clear.c
@@ -975,6 +975,11 @@ radv_can_fast_clear_depth(struct radv_cmd_buffer *cmd_buffer, const struct radv_
    if (!view_mask && clear_rect->layerCount != iview->image->info.array_size)
       return false;
 
+   if (cmd_buffer->device->vk.enabled_extensions.EXT_depth_range_unrestricted &&
+       (aspects & VK_IMAGE_ASPECT_DEPTH_BIT) &&
+       (clear_value.depth < 0.0 || clear_value.depth > 1.0))
+      return false;
+
    if (((aspects & VK_IMAGE_ASPECT_DEPTH_BIT) && !radv_is_fast_clear_depth_allowed(clear_value)) ||
        ((aspects & VK_IMAGE_ASPECT_STENCIL_BIT) &&
         !radv_is_fast_clear_stencil_allowed(clear_value)))
