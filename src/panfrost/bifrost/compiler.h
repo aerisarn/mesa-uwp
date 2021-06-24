d9e63586acf1388f18541cb880c81ab4a0ab2b8a
Alyssa Rosenzweig <alyssa@collabora.com>
2021-06-24 14:07:43 -0400

    pan/bi: Add bi_foreach_instr_global_rev_safe helper
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11733>

--- src/panfrost/bifrost/compiler.h
+++ src/panfrost/bifrost/compiler.h
@@ -715,6 +715,10 @@ bi_node_to_index(unsigned node, unsigned node_count)
         bi_foreach_block(ctx, v_block) \
                 bi_foreach_instr_in_block_safe((bi_block *) v_block, v)
 
+#define bi_foreach_instr_global_rev_safe(ctx, v) \
+        bi_foreach_block_rev(ctx, v_block) \
+                bi_foreach_instr_in_block_rev_safe((bi_block *) v_block, v)
+
 #define bi_foreach_instr_in_tuple(tuple, v) \
         for (bi_instr *v = tuple->fma ?: tuple->add; \
                         v != NULL; \
