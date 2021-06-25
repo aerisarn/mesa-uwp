a693fbf64b84ed6364984c45dc3255b71063c6dc
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2021-06-25 17:35:12 +0200

    amdgpu/bo: update uses_secure_bos when importing buffers
    Fixes: 90b98c06493 ("amd/tmz: move uses_secure_bos to radeon_winsys")
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11449>

--- src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
@@ -1544,6 +1544,7 @@ static struct pb_buffer *amdgpu_bo_from_handle(struct radeon_winsys *rws,
        * if it can be used for scanout.
        */
       flags |= RADEON_FLAG_ENCRYPTED;
+      *((bool*)&rws->uses_secure_bos) = true;
    }
 
    /* Initialize the structure. */
