d9c7a175424308b4da7673ea885fd8a8b3edf862
Rhys Perry <pendingchaos02@gmail.com>
2021-06-30 17:03:37 +0100

    radv: enable VK_EXT_vertex_input_dynamic_state
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Tested-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4768
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11717>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -528,6 +528,7 @@ radv_physical_device_get_supported_extensions(const struct radv_physical_device 
       .EXT_texel_buffer_alignment = true,
       .EXT_transform_feedback = true,
       .EXT_vertex_attribute_divisor = true,
+      .EXT_vertex_input_dynamic_state = !device->use_llvm,
       .EXT_ycbcr_image_arrays = true,
       .AMD_buffer_marker = true,
       .AMD_device_coherent_memory = true,
