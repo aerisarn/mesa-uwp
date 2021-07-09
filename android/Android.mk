aerisarn/mesa-uwp

commit d4b482d378e34b3f17005bc6244f24bdd73e94a0

android: Drop the Android.mk build system
Android.mk files haven't really been supported by Mesa devs for a long
time.  Most of us have been willing to update Makefile.sources if we
remember and sometimes we try to blind code some Android.mk for a new
generator.  However, the reality is that it breaks regularly and ends up
being maintained by the Android community.  To address this problem
another approach was implemented in !10183 utilizing the maintained
meson build system.  The old Android.mk files are no longer required.

This commit was created with the following commands:

    git rm **/Android.mk
    git rm **/Android.*.mk
    git rm **/Makefile.sources
    git rm CleanSpec.mk

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4487
Acked-by: Roman Stratiienko <r.stratiienko@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9728>

android/Android.mk
@@ -1,154 +1,0 @@
-# Mesa 3-D graphics library
-#
-# Copyright (C) 2021 GlobalLogic Ukraine
-# Copyright (C) 2021 Roman Stratiienko (r.stratiienko@gmail.com)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-ifneq ($(filter true, $(BOARD_MESA3D_USES_MESON_BUILD)),)
-
-LOCAL_PATH := $(call my-dir)
-MESA3D_TOP := $(dir $(LOCAL_PATH))
-
-MESA_VK_LIB_SUFFIX_amd := radeon
-MESA_VK_LIB_SUFFIX_intel := intel
-MESA_VK_LIB_SUFFIX_freedreno := freedreno
-MESA_VK_LIB_SUFFIX_broadcom := broadcom
-MESA_VK_LIB_SUFFIX_virtio-experimental := virtio
-MESA_VK_LIB_SUFFIX_swrast := lvp
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := libc libdl libdrm libm liblog libcutils libz libc++ libnativewindow libsync libhardware
-LOCAL_STATIC_LIBRARIES := libexpat libarect libelf
-LOCAL_HEADER_LIBRARIES := libnativebase_headers hwvulkan_headers libbacktrace_headers
-MESON_GEN_PKGCONFIGS := backtrace cutils expat hardware libdrm:2.4.105 nativewindow sync zlib:1.2.11 libelf
-
-ifneq ($(filter swr swrast,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
-MESON_GEN_LLVM_STUB := true
-endif
-
-ifneq ($(filter zink,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libvulkan
-MESON_GEN_PKGCONFIGS += vulkan
-endif
-
-ifneq ($(filter iris,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libdrm_intel
-MESON_GEN_PKGCONFIGS += libdrm_intel:2.4.105
-endif
-
-ifneq ($(filter radeonsi amd,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
-MESON_GEN_LLVM_STUB := true
-LOCAL_CFLAGS += -DFORCE_BUILD_AMDGPU   # instructs LLVM to declare LLVMInitializeAMDGPU* functions
-LOCAL_SHARED_LIBRARIES += libdrm_amdgpu
-MESON_GEN_PKGCONFIGS += libdrm_amdgpu:2.4.105
-endif
-
-ifneq ($(filter radeonsi r300 r600,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libdrm_radeon
-MESON_GEN_PKGCONFIGS += libdrm_radeon:2.4.105
-endif
-
-ifneq ($(filter nouveau,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libdrm_nouveau
-MESON_GEN_PKGCONFIGS += libdrm_nouveau:2.4.105
-endif
-
-ifneq ($(MESON_GEN_LLVM_STUB),)
-MESON_LLVM_VERSION := 11.0.0
-# Required for swr gallium target
-MESON_LLVM_IRBUILDER_PATH := external/llvm-project/llvm/include/llvm/IR/IRBuilder.h
-LOCAL_SHARED_LIBRARIES += libLLVM11
-endif
-
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 30; echo $$?), 0)
-LOCAL_SHARED_LIBRARIES += \
-    android.hardware.graphics.mapper@4.0 \
-    libgralloctypes \
-    libhidlbase \
-    libutils
-
-MESON_GEN_PKGCONFIGS += android.hardware.graphics.mapper:4.0
-endif
-
-ifeq ($(TARGET_IS_64_BIT),true)
-LOCAL_MULTILIB := 64
-else
-LOCAL_MULTILIB := 32
-endif
-include $(LOCAL_PATH)/mesa3d_cross.mk
-
-ifdef TARGET_2ND_ARCH
-LOCAL_MULTILIB := 32
-include $(LOCAL_PATH)/mesa3d_cross.mk
-endif
-
-#-------------------------------------------------------------------------------
-
-define mesa3d-lib
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE := $1
-LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := $3
-ifdef TARGET_2ND_ARCH
-LOCAL_SRC_FILES_$(TARGET_ARCH) := $(call relative_top_path,$(LOCAL_PATH))$($4)
-LOCAL_SRC_FILES_$(TARGET_2ND_ARCH) := $(call relative_top_path,$(LOCAL_PATH))$(2ND_$4)
-LOCAL_MULTILIB := both
-else
-LOCAL_SRC_FILES := $(call relative_top_path,$(LOCAL_PATH))$($4)
-endif
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_SYMLINKS := $1$2
-include $(BUILD_PREBUILT)
-include $(CLEAR_VARS)
-endef
-
-__MY_SHARED_LIBRARIES := $(LOCAL_SHARED_LIBRARIES)
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := $(__MY_SHARED_LIBRARIES)
-
-# Module 'libgallium_dri', produces '/vendor/lib{64}/dri/libgallium_dri.so'
-# This module also trigger DRI symlinks creation process
-$(eval $(call mesa3d-lib,libgallium_dri,.so.0,dri,MESA3D_GALLIUM_DRI_BIN))
-# Module 'libglapi', produces '/vendor/lib{64}/libglapi.so'
-$(eval $(call mesa3d-lib,libglapi,.so.0,,MESA3D_LIBGLAPI_BIN))
-
-# Module 'libEGL_mesa', produces '/vendor/lib{64}/egl/libEGL_mesa.so'
-$(eval $(call mesa3d-lib,libEGL_mesa,.so.1,egl,MESA3D_LIBEGL_BIN))
-# Module 'libGLESv1_CM_mesa', produces '/vendor/lib{64}/egl/libGLESv1_CM_mesa.so'
-$(eval $(call mesa3d-lib,libGLESv1_CM_mesa,.so.1,egl,MESA3D_LIBGLESV1_BIN))
-# Module 'libGLESv2_mesa', produces '/vendor/lib{64}/egl/libGLESv2_mesa.so'
-$(eval $(call mesa3d-lib,libGLESv2_mesa,.so.2,egl,MESA3D_LIBGLESV2_BIN))
-
-# Modules 'vulkan.{driver_name}', produces '/vendor/lib{64}/hw/vulkan.{driver_name}.so' HAL
-$(foreach driver,$(BOARD_MESA3D_VULKAN_DRIVERS), \
-    $(eval $(call mesa3d-lib,vulkan.$(MESA_VK_LIB_SUFFIX_$(driver)),.so.0,hw,MESA3D_VULKAN_$(driver)_BIN)))
-
-ifneq ($(filter true, $(BOARD_MESA3D_BUILD_LIBGBM)),)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(MESA3D_TOP)/src/gbm/main
-
-# Modules 'libgbm', produces '/vendor/lib{64}/libgbm.so'
-$(eval $(call mesa3d-lib,libgbm,.so.1,,MESA3D_LIBGBM_BIN))
-endif
-
-#-------------------------------------------------------------------------------
-
-endif

Android.mk
@@ -1,132 +1,0 @@
-# Mesa 3-D graphics library
-#
-# Copyright (C) 2010-2011 Chia-I Wu <olvaffe@gmail.com>
-# Copyright (C) 2010-2011 LunarG Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-ifneq ($(filter true, $(BOARD_MESA3D_USES_MESON_BUILD)),)
-
-include $(call my-dir)/android/Android.mk
-
-else
-
-# BOARD_GPU_DRIVERS should be defined.  The valid values are
-#
-#   classic drivers: i915 i965
-#   gallium drivers: swrast freedreno i915g nouveau kmsro r300g r600g radeonsi vc4 virgl vmwgfx etnaviv iris lima panfrost
-#
-# The main target is libGLES_mesa.  For each classic driver enabled, a DRI
-# module will also be built.  DRI modules will be loaded by libGLES_mesa.
-
-MESA_TOP := $(call my-dir)
-
-MESA_ANDROID_MAJOR_VERSION := $(word 1, $(subst ., , $(PLATFORM_VERSION)))
-ifneq ($(filter 2 4, $(MESA_ANDROID_MAJOR_VERSION)),)
-$(error "Android 4.4 and earlier not supported")
-endif
-
-MESA_DRI_MODULE_REL_PATH := dri
-MESA_DRI_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/$(MESA_DRI_MODULE_REL_PATH)
-MESA_DRI_MODULE_UNSTRIPPED_PATH := $(TARGET_OUT_SHARED_LIBRARIES_UNSTRIPPED)/$(MESA_DRI_MODULE_REL_PATH)
-MESA_DRI_LDFLAGS := -Wl,--build-id=sha1
-
-MESA_COMMON_MK := $(MESA_TOP)/Android.common.mk
-MESA_PYTHON2 := python
-MESA_PYTHON3 := python3
-ifeq ($(filter 5 6 7 8 9 10, $(MESA_ANDROID_MAJOR_VERSION)),)
-MESA_LEX     := M4=$(M4) $(LEX)
-else
-MESA_LEX     := $(LEX)
-endif
-
-# Lists to convert driver names to boolean variables
-# in form of <driver name>.<boolean make variable>
-classic_drivers := i915.HAVE_I915_DRI i965.HAVE_I965_DRI
-gallium_drivers := \
-	swrast.HAVE_GALLIUM_SOFTPIPE \
-	freedreno.HAVE_GALLIUM_FREEDRENO \
-	i915g.HAVE_GALLIUM_I915 \
-	nouveau.HAVE_GALLIUM_NOUVEAU \
-	kmsro.HAVE_GALLIUM_KMSRO \
-	r300g.HAVE_GALLIUM_R300 \
-	r600g.HAVE_GALLIUM_R600 \
-	radeonsi.HAVE_GALLIUM_RADEONSI \
-	vmwgfx.HAVE_GALLIUM_VMWGFX \
-	vc4.HAVE_GALLIUM_VC4 \
-	virgl.HAVE_GALLIUM_VIRGL \
-	etnaviv.HAVE_GALLIUM_ETNAVIV \
-	iris.HAVE_GALLIUM_IRIS \
-	lima.HAVE_GALLIUM_LIMA \
-	panfrost.HAVE_GALLIUM_PANFROST
-
-ifeq ($(BOARD_GPU_DRIVERS),all)
-MESA_BUILD_CLASSIC := $(filter HAVE_%, $(subst ., , $(classic_drivers)))
-MESA_BUILD_GALLIUM := $(filter HAVE_%, $(subst ., , $(gallium_drivers)))
-else
-# Warn if we have any invalid driver names
-$(foreach d, $(BOARD_GPU_DRIVERS), \
-	$(if $(findstring $(d).,$(classic_drivers) $(gallium_drivers)), \
-		, \
-		$(warning invalid GPU driver: $(d)) \
-	) \
-)
-MESA_BUILD_CLASSIC := $(strip $(foreach d, $(BOARD_GPU_DRIVERS), $(patsubst $(d).%,%, $(filter $(d).%, $(classic_drivers)))))
-MESA_BUILD_GALLIUM := $(strip $(foreach d, $(BOARD_GPU_DRIVERS), $(patsubst $(d).%,%, $(filter $(d).%, $(gallium_drivers)))))
-endif
-ifeq ($(filter x86%,$(TARGET_ARCH)),)
-	MESA_BUILD_CLASSIC :=
-endif
-
-$(foreach d, $(MESA_BUILD_CLASSIC) $(MESA_BUILD_GALLIUM), $(eval $(d) := true))
-
-ifneq ($(filter true, $(HAVE_GALLIUM_RADEONSI)),)
-MESA_ENABLE_LLVM := true
-endif
-
-define mesa-build-with-llvm
-  $(if $(filter $(MESA_ANDROID_MAJOR_VERSION), 4 5 6 7), \
-    $(warning Unsupported LLVM version in Android $(MESA_ANDROID_MAJOR_VERSION)),) \
-  $(eval LOCAL_CFLAGS += -DLLVM_AVAILABLE -DDRAW_LLVM_AVAILABLE -DLLVM_IS_SHARED=1 -DMESA_LLVM_VERSION_STRING=\"3.9\") \
-  $(eval LOCAL_SHARED_LIBRARIES += libLLVM)
-endef
-
-# add subdirectories
-SUBDIRS := \
-	src/etnaviv \
-	src/freedreno \
-	src/gbm \
-	src/loader \
-	src/mapi \
-	src/compiler \
-	src/mesa \
-	src/util \
-	src/egl \
-	src/amd \
-	src/broadcom \
-	src/intel \
-	src/mesa/drivers/dri \
-	src/vulkan \
-	src/panfrost \
-
-INC_DIRS := $(call all-named-subdir-makefiles,$(SUBDIRS))
-INC_DIRS += $(call all-named-subdir-makefiles,src/gallium)
-include $(INC_DIRS)
-
-endif

src/amd/Android.mk
@@ -1,33 +1,0 @@
-# Copyright © 2016 Red Hat.
-# Copyright © 2016 Mauro Rossi <issor.oruam@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice (including the next
-# paragraph) shall be included in all copies or substantial portions of the
-# Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-
-LOCAL_PATH := $(call my-dir)
-
-# Import variables
-include $(LOCAL_PATH)/Makefile.sources
-
-include $(LOCAL_PATH)/Android.addrlib.mk
-include $(LOCAL_PATH)/Android.common.mk
-ifneq ($(filter radeonsi,$(BOARD_GPU_DRIVERS)),)
-include $(LOCAL_PATH)/Android.compiler.mk
-include $(LOCAL_PATH)/vulkan/Android.mk
-endif

src/amd/vulkan/Android.mk
@@ -1,152 +1,0 @@
-# Copyright © 2018 Advanced Micro Devices, Inc.
-# Copyright © 2018 Mauro Rossi issor.oruam@gmail.com
-
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice (including the next
-# paragraph) shall be included in all copies or substantial portions of the
-# Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-
-LOCAL_PATH := $(call my-dir)
-
-# get VULKAN_FILES and VULKAN_GENERATED_FILES
-include $(LOCAL_PATH)/Makefile.sources
-
-# The gallium includes are for the util/u_math.h include from main/macros.h
-
-RADV_COMMON_INCLUDES := \
-	$(MESA_TOP)/include \
-	$(MESA_TOP)/src/ \
-	$(MESA_TOP)/src/amd/vulkan \
-	$(MESA_TOP)/src/vulkan/wsi \
-	$(MESA_TOP)/src/vulkan/util \
-	$(MESA_TOP)/src/amd \
-	$(MESA_TOP)/src/mapi \
-	$(MESA_TOP)/src/mesa \
-	$(MESA_TOP)/src/mesa/drivers/dri/common \
-	$(MESA_TOP)/src/gallium/auxiliary \
-	$(MESA_TOP)/src/gallium/include \
-	frameworks/native/vulkan/include
-
-RADV_SHARED_LIBRARIES := libdrm_amdgpu
-
-ifeq ($(filter $(MESA_ANDROID_MAJOR_VERSION), 4 5 6 7),)
-RADV_SHARED_LIBRARIES += libnativewindow
-endif
-
-#
-# libmesa_radv_common
-#
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmesa_radv_common
-LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-
-intermediates := $(call local-generated-sources-dir)
-
-LOCAL_SRC_FILES := \
-	$(VULKAN_FILES)
-
-LOCAL_CFLAGS += -DFORCE_BUILD_AMDGPU   # instructs LLVM to declare LLVMInitializeAMDGPU* functions
-LOCAL_CFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
-
-$(call mesa-build-with-llvm)
-
-LOCAL_C_INCLUDES := $(RADV_COMMON_INCLUDES)
-
-LOCAL_STATIC_LIBRARIES := \
-	libmesa_aco \
-	libmesa_amd_common \
-	libmesa_nir \
-	libmesa_util \
-	libmesa_vulkan_util \
-	libmesa_git_sha1
-
-LOCAL_GENERATED_SOURCES += $(intermediates)/radv_entrypoints.c
-LOCAL_GENERATED_SOURCES += $(intermediates)/radv_entrypoints.h
-
-RADV_ENTRYPOINTS_SCRIPT := $(MESA_TOP)/src/vulkan/util/vk_entrypoints_gen.py
-
-vulkan_api_xml = $(MESA_TOP)/src/vulkan/registry/vk.xml
-
-$(intermediates)/radv_entrypoints.c: $(RADV_ENTRYPOINTS_SCRIPT) \
-					$(vulkan_api_xml)
-	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(RADV_ENTRYPOINTS_SCRIPT) \
-		--xml $(vulkan_api_xml) \
-		--proto --weak \
-		--out-c $@ \
-		--out-h $(addsuffix .h,$(basename $@)) \
-		--prefix radv --device-prefix sqtt
-
-$(intermediates)/radv_entrypoints.h: $(intermediates)/radv_entrypoints.c
-
-LOCAL_SHARED_LIBRARIES += $(RADV_SHARED_LIBRARIES)
-
-LOCAL_EXPORT_C_INCLUDE_DIRS := \
-	$(MESA_TOP)/src/amd/vulkan \
-	$(intermediates)
-
-include $(MESA_COMMON_MK)
-include $(BUILD_STATIC_LIBRARY)
-
-#
-# libvulkan_radeon
-#
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := vulkan.radv
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := hw
-
-LOCAL_LDFLAGS += -Wl,--build-id=sha1
-
-LOCAL_SRC_FILES := \
-	$(VULKAN_ANDROID_FILES)
-
-LOCAL_CFLAGS += -DFORCE_BUILD_AMDGPU   # instructs LLVM to declare LLVMInitializeAMDGPU* functions
-LOCAL_CFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
-
-$(call mesa-build-with-llvm)
-
-LOCAL_C_INCLUDES := $(RADV_COMMON_INCLUDES)
-
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-	libmesa_util \
-	libmesa_nir \
-	libmesa_glsl \
-	libmesa_compiler \
-	libmesa_amdgpu_addrlib \
-	libmesa_amd_common \
-	libmesa_radv_common \
-	libmesa_vulkan_util \
-	libmesa_aco
-
-LOCAL_SHARED_LIBRARIES += $(RADV_SHARED_LIBRARIES) libz libsync liblog libcutils
-
-# If Android version >=8 MESA should static link libexpat else should dynamic link
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 27; echo $$?), 0)
-LOCAL_STATIC_LIBRARIES := \
-	libexpat
-else
-LOCAL_SHARED_LIBRARIES += \
-	libexpat
-endif
-
-include $(MESA_COMMON_MK)
-include $(BUILD_SHARED_LIBRARY)

src/broadcom/Android.mk
@@ -1,29 +1,0 @@
-# Copyright © 2016 Intel Corporation
-# Copyright © 2016 Mauro Rossi <issor.oruam@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-#
-
-LOCAL_PATH := $(call my-dir)
-
-# Import variables
-include $(LOCAL_PATH)/Makefile.sources
-
-include $(LOCAL_PATH)/Android.genxml.mk
-include $(LOCAL_PATH)/Android.cle.mk

src/compiler/Android.mk
@@ -1,52 +1,0 @@
-# Mesa 3-D graphics library
-#
-# Copyright (C) 2015 Intel Corporation
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-LOCAL_PATH := $(call my-dir)
-
-include $(LOCAL_PATH)/Android.glsl.mk
-include $(LOCAL_PATH)/Android.nir.mk
-
-# ---------------------------------------
-# Build libmesa_compiler
-# ---------------------------------------
-
-include $(CLEAR_VARS)
-
-include $(LOCAL_PATH)/Makefile.sources
-LOCAL_SRC_FILES := $(LIBCOMPILER_FILES)
-
-LOCAL_C_INCLUDES := \
-	$(MESA_TOP)/src/mapi \
-	$(MESA_TOP)/src/mesa \
-	$(MESA_TOP)/src/gallium/include \
-	$(MESA_TOP)/src/gallium/auxiliary \
-	$(dir $(MESA_GEN_GLSL_H))
-
-LOCAL_GENERATED_SOURCES += \
-	$(MESA_GEN_GLSL_H)
-
-LOCAL_MODULE := libmesa_compiler
-
-include $(MESA_COMMON_MK)
-include $(BUILD_STATIC_LIBRARY)
-
-

commit 439322ad4c03b135bb2570b3bbdfd7fa5fc8a06d

android: Restore android/Android.mk
It was accidentally dropped as part of d4b482d378e3 but it's the one
Android makefile we want to keep.

Fixes: d4b482d378e3 "android: Drop the Android.mk build system"
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11798>

android/Android.mk
@@ -1,0 +1,154 @@
+# Mesa 3-D graphics library
+#
+# Copyright (C) 2021 GlobalLogic Ukraine
+# Copyright (C) 2021 Roman Stratiienko (r.stratiienko@gmail.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+ifneq ($(filter true, $(BOARD_MESA3D_USES_MESON_BUILD)),)
+
+LOCAL_PATH := $(call my-dir)
+MESA3D_TOP := $(dir $(LOCAL_PATH))
+
+MESA_VK_LIB_SUFFIX_amd := radeon
+MESA_VK_LIB_SUFFIX_intel := intel
+MESA_VK_LIB_SUFFIX_freedreno := freedreno
+MESA_VK_LIB_SUFFIX_broadcom := broadcom
+MESA_VK_LIB_SUFFIX_virtio-experimental := virtio
+MESA_VK_LIB_SUFFIX_swrast := lvp
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := libc libdl libdrm libm liblog libcutils libz libc++ libnativewindow libsync libhardware
+LOCAL_STATIC_LIBRARIES := libexpat libarect libelf
+LOCAL_HEADER_LIBRARIES := libnativebase_headers hwvulkan_headers libbacktrace_headers
+MESON_GEN_PKGCONFIGS := backtrace cutils expat hardware libdrm:2.4.105 nativewindow sync zlib:1.2.11 libelf
+
+ifneq ($(filter swr swrast,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
+MESON_GEN_LLVM_STUB := true
+endif
+
+ifneq ($(filter zink,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
+LOCAL_SHARED_LIBRARIES += libvulkan
+MESON_GEN_PKGCONFIGS += vulkan
+endif
+
+ifneq ($(filter iris,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
+LOCAL_SHARED_LIBRARIES += libdrm_intel
+MESON_GEN_PKGCONFIGS += libdrm_intel:2.4.105
+endif
+
+ifneq ($(filter radeonsi amd,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
+MESON_GEN_LLVM_STUB := true
+LOCAL_CFLAGS += -DFORCE_BUILD_AMDGPU   # instructs LLVM to declare LLVMInitializeAMDGPU* functions
+LOCAL_SHARED_LIBRARIES += libdrm_amdgpu
+MESON_GEN_PKGCONFIGS += libdrm_amdgpu:2.4.105
+endif
+
+ifneq ($(filter radeonsi r300 r600,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
+LOCAL_SHARED_LIBRARIES += libdrm_radeon
+MESON_GEN_PKGCONFIGS += libdrm_radeon:2.4.105
+endif
+
+ifneq ($(filter nouveau,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
+LOCAL_SHARED_LIBRARIES += libdrm_nouveau
+MESON_GEN_PKGCONFIGS += libdrm_nouveau:2.4.105
+endif
+
+ifneq ($(MESON_GEN_LLVM_STUB),)
+MESON_LLVM_VERSION := 11.0.0
+# Required for swr gallium target
+MESON_LLVM_IRBUILDER_PATH := external/llvm-project/llvm/include/llvm/IR/IRBuilder.h
+LOCAL_SHARED_LIBRARIES += libLLVM11
+endif
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 30; echo $$?), 0)
+LOCAL_SHARED_LIBRARIES += \
+    android.hardware.graphics.mapper@4.0 \
+    libgralloctypes \
+    libhidlbase \
+    libutils
+
+MESON_GEN_PKGCONFIGS += android.hardware.graphics.mapper:4.0
+endif
+
+ifeq ($(TARGET_IS_64_BIT),true)
+LOCAL_MULTILIB := 64
+else
+LOCAL_MULTILIB := 32
+endif
+include $(LOCAL_PATH)/mesa3d_cross.mk
+
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := 32
+include $(LOCAL_PATH)/mesa3d_cross.mk
+endif
+
+#-------------------------------------------------------------------------------
+
+define mesa3d-lib
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := $1
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := $3
+ifdef TARGET_2ND_ARCH
+LOCAL_SRC_FILES_$(TARGET_ARCH) := $(call relative_top_path,$(LOCAL_PATH))$($4)
+LOCAL_SRC_FILES_$(TARGET_2ND_ARCH) := $(call relative_top_path,$(LOCAL_PATH))$(2ND_$4)
+LOCAL_MULTILIB := both
+else
+LOCAL_SRC_FILES := $(call relative_top_path,$(LOCAL_PATH))$($4)
+endif
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_SYMLINKS := $1$2
+include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+endef
+
+__MY_SHARED_LIBRARIES := $(LOCAL_SHARED_LIBRARIES)
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := $(__MY_SHARED_LIBRARIES)
+
+# Module 'libgallium_dri', produces '/vendor/lib{64}/dri/libgallium_dri.so'
+# This module also trigger DRI symlinks creation process
+$(eval $(call mesa3d-lib,libgallium_dri,.so.0,dri,MESA3D_GALLIUM_DRI_BIN))
+# Module 'libglapi', produces '/vendor/lib{64}/libglapi.so'
+$(eval $(call mesa3d-lib,libglapi,.so.0,,MESA3D_LIBGLAPI_BIN))
+
+# Module 'libEGL_mesa', produces '/vendor/lib{64}/egl/libEGL_mesa.so'
+$(eval $(call mesa3d-lib,libEGL_mesa,.so.1,egl,MESA3D_LIBEGL_BIN))
+# Module 'libGLESv1_CM_mesa', produces '/vendor/lib{64}/egl/libGLESv1_CM_mesa.so'
+$(eval $(call mesa3d-lib,libGLESv1_CM_mesa,.so.1,egl,MESA3D_LIBGLESV1_BIN))
+# Module 'libGLESv2_mesa', produces '/vendor/lib{64}/egl/libGLESv2_mesa.so'
+$(eval $(call mesa3d-lib,libGLESv2_mesa,.so.2,egl,MESA3D_LIBGLESV2_BIN))
+
+# Modules 'vulkan.{driver_name}', produces '/vendor/lib{64}/hw/vulkan.{driver_name}.so' HAL
+$(foreach driver,$(BOARD_MESA3D_VULKAN_DRIVERS), \
+    $(eval $(call mesa3d-lib,vulkan.$(MESA_VK_LIB_SUFFIX_$(driver)),.so.0,hw,MESA3D_VULKAN_$(driver)_BIN)))
+
+ifneq ($(filter true, $(BOARD_MESA3D_BUILD_LIBGBM)),)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(MESA3D_TOP)/src/gbm/main
+
+# Modules 'libgbm', produces '/vendor/lib{64}/libgbm.so'
+$(eval $(call mesa3d-lib,libgbm,.so.1,,MESA3D_LIBGBM_BIN))
+endif
+
+#-------------------------------------------------------------------------------
+
+endif