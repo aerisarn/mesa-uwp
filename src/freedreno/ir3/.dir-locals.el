f69a99081b66a170f1e4dc6c4e5018edd074f150
Connor Abbott <cwabbott0@gmail.com>
2021-07-12 15:03:38 +0200

    ir3: Update .editorconfig and .dir-locals.el
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11801>

--- src/freedreno/ir3/.dir-locals.el
+++ src/freedreno/ir3/.dir-locals.el
@@ -1,9 +1,9 @@
 ((prog-mode
-  (indent-tabs-mode . t)
-  (tab-width . 4)
-  (c-basic-offset . 4)
+  (indent-tabs-mode . nil)
+  (tab-width . 3)
+  (c-basic-offset . 3)
   (c-file-style . "k&r")
-  (fill-column . 78)
+  (fill-column . 80)
   (c-file-offsets
    (arglist-cont-nonempty . ++)
    )
