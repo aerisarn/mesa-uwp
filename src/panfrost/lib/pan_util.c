38ffedc10610bc73dbc426275587337826fec780
Alyssa Rosenzweig <alyssa@collabora.com>
2021-07-12 20:27:03 -0400

    panfrost: Fix format swizzles on G72
    Careless typo. Cleaner fix suggested by Icecream95.
    
    Reported-by: macc24
    Tested-by: macc24
    Fixes: 531d80de7c7 ("panfrost: Specialize blendable_formats for v6")
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11837>

--- src/panfrost/lib/pan_util.c
+++ src/panfrost/lib/pan_util.c
@@ -75,7 +75,7 @@ unsigned
 panfrost_format_to_bifrost_blend(const struct panfrost_device *dev,
                                  enum pipe_format format)
 {
-        mali_pixel_format pixfmt = (dev->quirks & HAS_SWIZZLES) ?
+        mali_pixel_format pixfmt = (dev->arch >= 7) ?
                 panfrost_blendable_formats_v7[format].bifrost :
                 panfrost_blendable_formats_v6[format].bifrost;
 
