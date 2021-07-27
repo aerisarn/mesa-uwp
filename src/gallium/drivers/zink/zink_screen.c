1bbab346604056f2dc85f335c68003170e3bc50e
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-07-27 14:04:35 -0400

    zink: enable fbfetch pipe cap
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12603>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -314,6 +314,9 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return modes;
    }
 
+   case PIPE_CAP_FBFETCH:
+      return 1;
+
    case PIPE_CAP_QUERY_MEMORY_INFO:
    case PIPE_CAP_NPOT_TEXTURES:
    case PIPE_CAP_TGSI_TEXCOORD:
