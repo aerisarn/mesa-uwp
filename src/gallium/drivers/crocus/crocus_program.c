e9dc98ed87f3c64f5933c0c5e95a9084a43ed6d6
Marcin Ślusarz <marcin.slusarz@intel.com>
2021-08-06 15:06:28 +0200

    crocus: remove invalid nir_metadata_preserve from crocus_fix_edge_flags
    nir_metadata_preserve(NULL, ...) doesn't make any sense.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12282>

--- src/gallium/drivers/crocus/crocus_program.c
+++ src/gallium/drivers/crocus/crocus_program.c
@@ -356,8 +356,6 @@ crocus_fix_edge_flags(nir_shader *nir)
                                nir_metadata_dominance |
                                nir_metadata_live_ssa_defs |
                                nir_metadata_loop_analysis);
-      } else {
-         nir_metadata_preserve(f->impl, nir_metadata_all);
       }
    }
 
