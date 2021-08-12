48b1b159ffe1f7864641b2a52d7f07e80149fd62
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-08-12 20:50:58 +0200

    llvmpipe: take intersection with bbox for non-legacy points
    When I updated this code for multi-sampling, I missed one detail; if we
    want to be able to support pipe_rasterizer_state::point_tri_clip, we
    need to use the intersection of the bbox (clipped to the viewport
    rectangle) and the generated primitive, otherwise we won't end up doing
    x/y viewport clipping at all.
    
    Because we've adjusted some of the parts of the bbox when adjusting for
    inclusiveness/exclusiveness and fill-rule, we also need to reverse the
    adjustment.
    
    Fixes: f530e72ea05 ("llvmpipe: do not always use pixel-rounded coordinates for points")
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12350>

--- src/gallium/drivers/llvmpipe/lp_setup_point.c
+++ src/gallium/drivers/llvmpipe/lp_setup_point.c
@@ -518,22 +518,22 @@ try_setup_point( struct lp_setup_context *setup,
 
       plane[0].dcdx = ~0U << 8;
       plane[0].dcdy = 0;
-      plane[0].c = -x[0];
+      plane[0].c = -MAX2(x[0], bbox.x0 << 8);
       plane[0].eo = 1 << 8;
 
       plane[1].dcdx = 1 << 8;
       plane[1].dcdy = 0;
-      plane[1].c = x[1];
+      plane[1].c = MIN2(x[1], (bbox.x1 + 1) << 8);
       plane[1].eo = 0;
 
       plane[2].dcdx = 0;
       plane[2].dcdy = 1 << 8;
-      plane[2].c = -y[0];
+      plane[2].c = -MAX2(y[0], (bbox.y0 << 8) - adj);
       plane[2].eo = 1 << 8;
 
       plane[3].dcdx = 0;
       plane[3].dcdy = ~0U << 8;
-      plane[3].c = y[1];
+      plane[3].c = MIN2(y[1], (bbox.y1 + 1) << 8);
       plane[3].eo = 0;
 
       if (!setup->legacy_points || setup->multisample) {
