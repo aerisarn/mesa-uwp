067599f8bc74c71446a4a682649825dc7380743c
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-08-18 16:00:50 +0200

    radv: remove incorrect comment about compressed writes to HTILE on GFX10+
    This seems to be unsupported.
    COMPRESSION_EN=1 and WRITE_COMPRESS_ENABLE=1 don't update HTILE
    with image stores.
    
    Note that there is no issue because depth/stencil images will be
    decompressed for image stores, and TC-compat HTILE is disabled.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12450>

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -2047,7 +2047,6 @@ radv_layout_is_htile_compressed(const struct radv_device *device, const struct r
        */
       if (radv_image_is_tc_compat_htile(image) && queue_mask & (1u << RADV_QUEUE_GENERAL) &&
           !in_render_loop && !device->instance->disable_tc_compat_htile_in_general) {
-         /* GFX10+ supports compressed writes to HTILE. */
          return true;
       } else {
          return false;
