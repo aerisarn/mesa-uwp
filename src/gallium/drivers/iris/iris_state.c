a969ad1ddfd6b0b94cc484da45293c407203e3ac
Kenneth Graunke <kenneth@whitecape.org>
2021-08-24 18:09:53 -0700

    iris: Demote DC flush to HDC flush in cache tracker
    FLUSH_HDC is sufficient to flush things out to L3, so we'd rather
    use that where possible.  It's also emulated via DATA_CACHE_FLUSH
    on platforms where it isn't supported, so we can use it unconditionally.
    
    We still use DATA_CACHE_FLUSH for invalidating the data cache, and to
    flush the DC-tagged cachelines in L3 to be globally-observable.
    
    Reviewed-by: Rohan Garg <rohan.garg@intel.com>
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15275>

--- src/gallium/drivers/iris/iris_state.c
+++ src/gallium/drivers/iris/iris_state.c
@@ -7640,7 +7640,7 @@ batch_mark_sync_for_pipe_control(struct iris_batch *batch, uint32_t flags)
    if ((flags & PIPE_CONTROL_DEPTH_CACHE_FLUSH))
       iris_batch_mark_invalidate_sync(batch, IRIS_DOMAIN_DEPTH_WRITE);
 
-   if ((flags & PIPE_CONTROL_DATA_CACHE_FLUSH))
+   if (flags & (PIPE_CONTROL_FLUSH_HDC | PIPE_CONTROL_DATA_CACHE_FLUSH))
       iris_batch_mark_invalidate_sync(batch, IRIS_DOMAIN_DATA_WRITE);
 
    if ((flags & PIPE_CONTROL_FLUSH_ENABLE))
