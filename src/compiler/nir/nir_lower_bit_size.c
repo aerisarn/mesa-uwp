Grima04/mesa-turnip-kgsl

commit 6d792989924ce79363f181462904fa46692a99b5

nir/lower_bit_size: fix lowering of {imul,umul}_high
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4387>

src/compiler/nir/nir_lower_bit_size.c
@@ -70,8 +70,18 @@ lower_instr(nir_builder *bld, nir_alu_instr *alu, unsigned bit_size)
    }
 
    /* Emit the lowered ALU instruction */
-   nir_ssa_def *lowered_dst =
-      nir_build_alu(bld, op, srcs[0], srcs[1], srcs[2], srcs[3]);
+   nir_ssa_def *lowered_dst = NULL;
+   if (op == nir_op_imul_high || op == nir_op_umul_high) {
+      assert(dst_bit_size * 2 <= bit_size);
+      nir_ssa_def *lowered_dst = nir_imul(bld, srcs[0], srcs[1]);
+      if (nir_op_infos[op].output_type & nir_type_uint)
+         lowered_dst = nir_ushr(bld, lowered_dst, nir_imm_int(bld, dst_bit_size));
+      else
+         lowered_dst = nir_ishr(bld, lowered_dst, nir_imm_int(bld, dst_bit_size));
+   } else {
+      lowered_dst = nir_build_alu(bld, op, srcs[0], srcs[1], srcs[2], srcs[3]);
+   }
+
 
    /* Convert result back to the original bit-size */
    nir_alu_type type = nir_op_infos[op].output_type;

aerisarn/mesa-uwp

commit 9083e9a483696672514c18cec0a91ffd480f0591

nir: fix shadowed variable in nir_lower_bit_size.c
Fixes: 6d792989924 ("nir/lower_bit_size: fix lowering of {imul,umul}_high")

Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12527>

src/compiler/nir/nir_lower_bit_size.c
@@ -74,7 +74,7 @@ lower_alu_instr(nir_builder *bld, nir_alu_instr *alu, unsigned bit_size)
    nir_ssa_def *lowered_dst = NULL;
    if (op == nir_op_imul_high || op == nir_op_umul_high) {
       assert(dst_bit_size * 2 <= bit_size);
-      nir_ssa_def *lowered_dst = nir_imul(bld, srcs[0], srcs[1]);
+      lowered_dst = nir_imul(bld, srcs[0], srcs[1]);
       if (nir_op_infos[op].output_type & nir_type_uint)
          lowered_dst = nir_ushr_imm(bld, lowered_dst, dst_bit_size);
       else