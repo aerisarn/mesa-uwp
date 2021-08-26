9ffd05b41ecebc70f2b1b58f4d155a409858ae2a
Sagar Ghuge <sagar.ghuge@intel.com>
2021-08-26 10:19:59 -0700

    anv: Advertise support for shaderBufferFloat64AtomicMinMax
    v1:
    - Just check has_lsc (Jason)
    
    Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12566>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -1709,7 +1709,7 @@ void anv_GetPhysicalDeviceFeatures2(
          VkPhysicalDeviceShaderAtomicFloatFeaturesEXT *features = (void *)ext;
          features->shaderBufferFloat32Atomics =    true;
          features->shaderBufferFloat32AtomicAdd =  pdevice->info.has_lsc;
-         features->shaderBufferFloat64Atomics =    false;
+         features->shaderBufferFloat64Atomics =    pdevice->info.has_lsc;
          features->shaderBufferFloat64AtomicAdd =  false;
          features->shaderSharedFloat32Atomics =    true;
          features->shaderSharedFloat32AtomicAdd =  false;
@@ -1728,7 +1728,7 @@ void anv_GetPhysicalDeviceFeatures2(
          features->shaderBufferFloat16AtomicAdd    = false;
          features->shaderBufferFloat16AtomicMinMax = false;
          features->shaderBufferFloat32AtomicMinMax = pdevice->info.ver >= 9;
-         features->shaderBufferFloat64AtomicMinMax = false;
+         features->shaderBufferFloat64AtomicMinMax = pdevice->info.has_lsc;
          features->shaderSharedFloat16Atomics      = false;
          features->shaderSharedFloat16AtomicAdd    = false;
          features->shaderSharedFloat16AtomicMinMax = false;
