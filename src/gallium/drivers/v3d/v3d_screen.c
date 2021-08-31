8bee5d3c11a8a60cb28fd7c185d57868eb39456a
Jose Maria Casanova Crespo <jmcasanova@igalia.com>
2021-08-31 18:43:47 +0200

    v3d: Enable PIPE_CAP_PRIMITIVE_RESTART
    We disabled PIPE_CAP_PRIMITIVE_RESTART on ffc4d82438d98 ("v3d: Disable
    PIPE_CAP_PRIMITIVE_RESTART") because the HW can only support
    PIPE_CAP_PRIMITIVE_RESTART_FIXED_INDEX subset.
    
    But the introduction of PIPE_CAP_EMULATE_NONFIXED_PRIMITIVE_RESTART
    allows us to support these nonfixed primitive restarts so we can now
    re-enable NV_primitive_restart for OpenGL extensions.
    
    Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12669>

--- src/gallium/drivers/v3d/v3d_screen.c
+++ src/gallium/drivers/v3d/v3d_screen.c
@@ -123,6 +123,8 @@ v3d_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
         case PIPE_CAP_FRAGMENT_SHADER_DERIVATIVES:
         case PIPE_CAP_VERTEX_SHADER_SATURATE:
         case PIPE_CAP_PRIMITIVE_RESTART_FIXED_INDEX:
+        case PIPE_CAP_EMULATE_NONFIXED_PRIMITIVE_RESTART:
+        case PIPE_CAP_PRIMITIVE_RESTART:
         case PIPE_CAP_OCCLUSION_QUERY:
         case PIPE_CAP_POINT_SPRITE:
         case PIPE_CAP_STREAM_OUTPUT_PAUSE_RESUME:
