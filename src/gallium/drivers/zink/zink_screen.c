14e9d01aa69288f115a5f885c6bc686eec4b374b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-09-01 09:36:26 -0400

    zink: export PIPE_SHADER_CAP_FP16_CONST_BUFFERS
    this is now functional
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12823>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -817,10 +817,12 @@ zink_get_shader_param(struct pipe_screen *pscreen,
    case PIPE_SHADER_CAP_INDIRECT_TEMP_ADDR:
    case PIPE_SHADER_CAP_SUBROUTINES:
    case PIPE_SHADER_CAP_INT64_ATOMICS:
-   case PIPE_SHADER_CAP_FP16_CONST_BUFFERS:
    case PIPE_SHADER_CAP_GLSL_16BIT_CONSTS:
       return 0; /* not implemented */
 
+   case PIPE_SHADER_CAP_FP16_CONST_BUFFERS:
+      return screen->info.feats11.uniformAndStorageBuffer16BitAccess ||
+             (screen->info.have_KHR_16bit_storage && screen->info.storage_16bit_feats.uniformAndStorageBuffer16BitAccess);
    case PIPE_SHADER_CAP_FP16_DERIVATIVES:
       return screen->info.feats11.storageInputOutput16 ||
              (screen->info.have_KHR_16bit_storage && screen->info.storage_16bit_feats.storageInputOutput16);
