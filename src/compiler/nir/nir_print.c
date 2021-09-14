f769f34680d516850c6efd64669bf601efcd4425
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-09-14 15:28:24 -0400

    nir/print: print bindless info as applicable
    this is useful to know
    
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13204>

--- src/compiler/nir/nir_print.c
+++ src/compiler/nir/nir_print.c
@@ -484,14 +484,15 @@ print_var_decl(nir_variable *var, print_state *state)
 
    fprintf(fp, "decl_var ");
 
+   const char *const bindless = (var->data.bindless) ? "bindless " : "";
    const char *const cent = (var->data.centroid) ? "centroid " : "";
    const char *const samp = (var->data.sample) ? "sample " : "";
    const char *const patch = (var->data.patch) ? "patch " : "";
    const char *const inv = (var->data.invariant) ? "invariant " : "";
    const char *const per_view = (var->data.per_view) ? "per_view " : "";
    const char *const per_primitive = (var->data.per_primitive) ? "per_primitive " : "";
-   fprintf(fp, "%s%s%s%s%s%s%s %s ",
-           cent, samp, patch, inv, per_view, per_primitive,
+   fprintf(fp, "%s%s%s%s%s%s%s%s %s ",
+           bindless, cent, samp, patch, inv, per_view, per_primitive,
            get_variable_mode_str(var->data.mode, false),
            glsl_interp_mode_name(var->data.interpolation));
 
