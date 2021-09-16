a25a6abbd7f67a966c00e735af03e421ad869020
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2021-09-16 16:39:16 +0200

    radeonsi/sqtt: export wave size and scratch size
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12899>

--- src/gallium/drivers/radeonsi/si_sqtt.c
+++ src/gallium/drivers/radeonsi/si_sqtt.c
@@ -1000,6 +1000,8 @@ si_sqtt_add_code_object(struct si_context* sctx,
       record->shader_data[i].elf_symbol_offset = 0;
       record->shader_data[i].hw_stage = hw_stage;
       record->shader_data[i].is_combined = false;
+      record->shader_data[i].scratch_memory_size = shader->config.scratch_bytes_per_wave;
+      record->shader_data[i].wavefront_size = si_get_shader_wave_size(shader);
 
       record->shader_stages_mask |= (1 << i);
       record->num_shaders_combined++;
