b07372312d7053f2ef5c858ceb1fbf9ade5e7c52
Christian Gmeiner <christian.gmeiner@gmail.com>
2021-09-16 07:32:28 +0200

    Revert "nir: make tgsi_varying_semantic_to_slot(..) public"
    This reverts commit edbdd977232f0cfe8f26a44ddbda792780168f4a.
    
    As etnaviv's TGSI compiler is gone we make that function
    private again.
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Acked-by: Lucas Stach <l.stach@pengutronix.de>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12889>

--- src/gallium/auxiliary/nir/tgsi_to_nir.c
+++ src/gallium/auxiliary/nir/tgsi_to_nir.c
@@ -100,7 +100,7 @@ struct ttn_compile {
 #define ttn_channel(b, src, swiz) \
    nir_channel(b, src, TGSI_SWIZZLE_##swiz)
 
-gl_varying_slot
+static gl_varying_slot
 tgsi_varying_semantic_to_slot(unsigned semantic, unsigned index)
 {
    switch (semantic) {
