dfe432d829257dd01070ae1dcd4b663a0e96641f
Connor Abbott <cwabbott0@gmail.com>
2021-09-17 15:37:49 +0200

    ir3: Make trig replacement expression exact
    This prevents other optimizations from associating the inner multiply,
    which can add inaccuracies that can lead to discontinuities around the
    boundary of the ffract. We should use exactly the sequence that the blob
    uses to avoid problems.
    
    Since fadd + fmul cannot be combined to ffma when exact is specified, we
    have to use ffma ourselves.
    
    Fixes artifacts in PixMark Volplosion with !7458.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12912>

--- src/freedreno/ir3/ir3_nir_trig.py
+++ src/freedreno/ir3/ir3_nir_trig.py
@@ -24,8 +24,8 @@ import argparse
 import sys
 
 trig_workarounds = [
-   (('fsin', 'x@32'), ('fsin', ('fsub', ('fmul', 6.2831853, ('ffract', ('fadd', ('fmul', 0.15915494, 'x'), 0.5))), 3.14159265))),
-   (('fcos', 'x@32'), ('fcos', ('fsub', ('fmul', 6.2831853, ('ffract', ('fadd', ('fmul', 0.15915494, 'x'), 0.5))), 3.14159265))),
+   (('fsin', 'x@32'), ('fsin', ('!ffma', 6.2831853, ('ffract', ('!ffma', 0.15915494, 'x', 0.5)), -3.14159265))),
+   (('fcos', 'x@32'), ('fcos', ('!ffma', 6.2831853, ('ffract', ('!ffma', 0.15915494, 'x', 0.5)), -3.14159265))),
 ]
 
 
