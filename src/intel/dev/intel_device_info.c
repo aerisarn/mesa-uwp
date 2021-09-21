99f90750636c4e602189d739a77f05f4822e5652
Jordan Justen <jordan.l.justen@intel.com>
2021-09-21 03:39:31 -0700

    Revert "intel/dev: Add display_ver and set adl-p to 13"
    This reverts commit c81acd365ed2a965741a828504d4a1498eaa3d3a.

--- src/intel/dev/intel_device_info.c
+++ src/intel/dev/intel_device_info.c
@@ -993,7 +993,6 @@ static const struct intel_device_info intel_device_info_adl_gt1 = {
 static const struct intel_device_info intel_device_info_adl_gt2 = {
    GFX12_GT_FEATURES(2),
    .is_alderlake = true,
-   .display_ver = 13,
 };
 
 #define GFX12_DG1_SG1_FEATURES                  \
@@ -1322,9 +1321,6 @@ intel_get_device_info_from_pci_id(int pci_id,
 
    if (devinfo->verx10 == 0)
       devinfo->verx10 = devinfo->ver * 10;
-
-   if (devinfo->display_ver == 0)
-      devinfo->display_ver = devinfo->ver;
 
    update_cs_workgroup_threads(devinfo);
 
--- src/intel/dev/intel_device_info.h
+++ src/intel/dev/intel_device_info.h
@@ -51,7 +51,6 @@ struct intel_device_info
    /* Driver internal numbers used to differentiate platforms. */
    int ver;
    int verx10;
-   int display_ver;
    int revision;
    int gt;
 
