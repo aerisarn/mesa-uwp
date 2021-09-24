4c0bced1fb7ae2e6440fbf6d030c401971b8bf00
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-09-24 12:04:36 +0200

    radv: remove useless as_ngg_passthrough init when lowering NGG in NIR
    Nothing reads the value after that, except LLVM but we don't lower it
    in NIR (yet).
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13025>

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -989,7 +989,6 @@ void radv_lower_ngg(struct radv_device *device, struct nir_shader *nir,
       info->has_ngg_early_prim_export = out_conf.early_prim_export;
       info->num_lds_blocks_when_not_culling = DIV_ROUND_UP(out_conf.lds_bytes_if_culling_off, device->physical_device->rad_info.lds_encode_granularity);
       info->is_ngg_passthrough = out_conf.passthrough;
-      key->vs_common_out.as_ngg_passthrough = out_conf.passthrough;
    } else if (nir->info.stage == MESA_SHADER_GEOMETRY) {
       assert(info->is_ngg);
       ac_nir_lower_ngg_gs(
