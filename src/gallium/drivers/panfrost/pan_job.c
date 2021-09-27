f0325ca3da7217064a50d297bd362dcee832e904
Alyssa Rosenzweig <alyssa@collabora.com>
2021-09-27 19:47:22 -0400

    panfrost: Fix PAN_MESA_DEBUG=sync with INTEL_blackhole_render
    We need to ignore the "sync" in this case, or we'll crash with
    "incomplete job" since we never submitted the work. Fixes the Piglit
    intel_blackhole-draw_gles2 when run in CI.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13070>

--- src/gallium/drivers/panfrost/pan_job.c
+++ src/gallium/drivers/panfrost/pan_job.c
@@ -645,7 +645,8 @@ panfrost_batch_submit_ioctl(struct panfrost_batch *batch,
                 if (dev->debug & PAN_DBG_TRACE)
                         pandecode_jc(submit.jc, dev->gpu_id);
 
-                if (dev->debug & PAN_DBG_SYNC)
+                /* Jobs won't be complete if blackhole rendering, that's ok */
+                if (!ctx->is_noop && dev->debug & PAN_DBG_SYNC)
                         pandecode_abort_on_fault(submit.jc, dev->gpu_id);
         }
 
