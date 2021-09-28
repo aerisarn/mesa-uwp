dff90980597dd58d17269030c94c62035c0b43ef
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-09-28 10:33:58 +0300

    anv: improve readability of pipelined states
    Improving maintenance.
    
    v2: Also add TOP_OF_PIPE_BIT, HOST_BIT
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Suggested-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9045>

--- src/intel/vulkan/genX_cmd_buffer.c
+++ src/intel/vulkan/genX_cmd_buffer.c
@@ -6862,25 +6862,14 @@ void genX(CmdEndConditionalRenderingEXT)(
 }
 #endif
 
-/* Set of stage bits for which are pipelined, i.e. they get queued by the
- * command streamer for later execution.
+/* Set of stage bits for which are pipelined, i.e. they get queued
+ * by the command streamer for later execution.
  */
 #define ANV_PIPELINE_STAGE_PIPELINED_BITS \
-   (VK_PIPELINE_STAGE_VERTEX_INPUT_BIT | \
-    VK_PIPELINE_STAGE_VERTEX_SHADER_BIT | \
-    VK_PIPELINE_STAGE_TESSELLATION_CONTROL_SHADER_BIT | \
-    VK_PIPELINE_STAGE_TESSELLATION_EVALUATION_SHADER_BIT | \
-    VK_PIPELINE_STAGE_GEOMETRY_SHADER_BIT | \
-    VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | \
-    VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | \
-    VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT | \
-    VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | \
-    VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT | \
-    VK_PIPELINE_STAGE_TRANSFER_BIT | \
-    VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT | \
-    VK_PIPELINE_STAGE_ALL_GRAPHICS_BIT | \
-    VK_PIPELINE_STAGE_ALL_COMMANDS_BIT | \
-    VK_PIPELINE_STAGE_TRANSFORM_FEEDBACK_BIT_EXT)
+   ~(VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT | \
+     VK_PIPELINE_STAGE_DRAW_INDIRECT_BIT | \
+     VK_PIPELINE_STAGE_HOST_BIT | \
+     VK_PIPELINE_STAGE_CONDITIONAL_RENDERING_BIT_EXT)
 
 void genX(CmdSetEvent)(
     VkCommandBuffer                             commandBuffer,
