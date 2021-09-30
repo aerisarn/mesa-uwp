ae58894ee79925088c9745d909c5631b211aea7b
Jason Ekstrand <jason@jlekstrand.net>
2021-09-30 12:32:48 -0500

    zink: Images can live in nir_var_mem_image now
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4743>

--- src/gallium/drivers/zink/zink_compiler.c
+++ src/gallium/drivers/zink/zink_compiler.c
@@ -1064,7 +1064,7 @@ create_bindless_image(nir_shader *nir, enum glsl_sampler_dim dim)
    nir_variable *var;
 
    const struct glsl_type *image_type = glsl_image_type(dim, false, GLSL_TYPE_FLOAT);
-   var = nir_variable_create(nir, nir_var_uniform, glsl_array_type(image_type, ZINK_MAX_BINDLESS_HANDLES, 0), "bindless_image");
+   var = nir_variable_create(nir, nir_var_mem_image, glsl_array_type(image_type, ZINK_MAX_BINDLESS_HANDLES, 0), "bindless_image");
    var->data.descriptor_set = ZINK_DESCRIPTOR_BINDLESS;
    var->data.driver_location = var->data.binding = binding;
    var->data.image.format = PIPE_FORMAT_R8G8B8A8_UNORM;
@@ -1364,6 +1364,7 @@ zink_shader_create(struct zink_screen *screen, struct nir_shader *nir,
 
    foreach_list_typed_reverse_safe(nir_variable, var, node, &nir->variables) {
       if (_nir_shader_variable_has_mode(var, nir_var_uniform |
+                                        nir_var_mem_image |
                                         nir_var_mem_ubo |
                                         nir_var_mem_ssbo)) {
          enum zink_descriptor_type ztype;
@@ -1399,7 +1400,8 @@ zink_shader_create(struct zink_screen *screen, struct nir_shader *nir,
             ret->bindings[ztype][ret->num_bindings[ztype]].size = 1;
             ret->num_bindings[ztype]++;
          } else {
-            assert(var->data.mode == nir_var_uniform);
+            assert(var->data.mode == nir_var_uniform ||
+                   var->data.mode == nir_var_mem_image);
             if (var->data.bindless) {
                ret->bindless = true;
                handle_bindless_var(nir, var, type, bindless);
