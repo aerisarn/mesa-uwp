1d48022dabc0ea8c51804a2c596f6fec9f632aa1
Dave Airlie <airlied@redhat.com>
2021-10-05 11:33:49 +1000

    gallivm/format: clamp SINT conversion rather than truncate.
    KHR-GL32.packed_pixels.pbo_rectangle.r16i on zink on lavapipe
    ends up using a pbo that does an SINT image write. This was producing
    truncated rather than clamped values.
    
    Fix the calculations for 8/16-bit signed ints to clamp not truncate.
    
    Fixes: 13e5f331db77 ("gallivm/nir: fix image store conversions")
    
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13187>

--- src/gallium/auxiliary/gallivm/lp_bld_format_soa.c
+++ src/gallium/auxiliary/gallivm/lp_bld_format_soa.c
@@ -919,7 +919,15 @@ lp_build_insert_soa_chan(struct lp_build_context *bld,
     case UTIL_FORMAT_TYPE_SIGNED:
        if (chan_desc.pure_integer) {
           chan = LLVMBuildBitCast(builder, rgba, bld->int_vec_type, "");
-          chan = LLVMBuildAnd(builder, chan, lp_build_const_int_vec(gallivm, type, chan_mask), "");
+          /* clamp to SINT range for < 32-bit values */
+          if (width < 32) {
+             struct lp_build_context int_bld;
+             lp_build_context_init(&int_bld, gallivm, lp_int_type(bld->type));
+             chan = lp_build_clamp(&int_bld, chan,
+                                   lp_build_const_int_vec(gallivm, type, -(1ULL << (width - 1))),
+                                   lp_build_const_int_vec(gallivm, type, (1ULL << (width - 1)) - 1));
+             chan = LLVMBuildAnd(builder, chan, lp_build_const_int_vec(gallivm, type, chan_mask), "");
+          }
        } else if (type.floating) {
           if (chan_desc.normalized) {
              char intrin[32];
