d590515112c08684f7e76855a0ddb81eda2f9b5c
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-10-06 16:44:50 +0300

    ir3: support source modes for resinfo.b
    IBO/SSBO may have dynamic index, previously we just silently ignored
    this fact. However resinfo supports different modes.
    
    Fixes vkd3d test "test_null_uav"
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13224>

--- src/freedreno/ir3/tests/disasm.c
+++ src/freedreno/ir3/tests/disasm.c
@@ -237,6 +237,8 @@ static const struct test {
    INSTR_6XX(c0260000_0063c000, "resinfo.b.untyped.1d.u32.1.imm r0.x, 0"), /* resinfo.u32.1d.mode0.base0 r0.x, 0 */
    /* dEQP-VK.image.image_size.2d.readonly_12x34.txt */
    INSTR_6XX(c0260000_0063c300, "resinfo.b.untyped.2d.u32.1.imm.base0 r0.x, 0"), /* resinfo.u32.2d.mode4.base0 r0.x, 0 */
+   /* Custom test */
+   INSTR_6XX(c0260000_0063c382, "resinfo.b.untyped.2d.u32.1.nonuniform.base1 r0.x, r0.x"), /* resinfo.u32.2d.mode6.base1 r0.x, r0.x */
 
    /* dEQP-GLES31.functional.image_load_store.2d.image_size.readonly_writeonly_32x32.txt */
    INSTR_5XX(c3e60000_00000200, "resinfo.u32.2d r0.x, g[0]"), /* resinfo.u32.2d r0.x, 0 */
