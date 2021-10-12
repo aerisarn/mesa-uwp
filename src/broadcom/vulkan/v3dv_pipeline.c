19894bec1fdd85b394c71442854878db2a51ee71
Alejandro Piñeiro <apinheiro@igalia.com>
2021-10-12 23:33:43 +0200

    v3dv/pipeline: don't clone the nir shader at pipeline_state_create_binning
    At that point we didn't call all the v3dv lowerings. So the reference
    nir shader used to call the v3d compiler could be different.
    
    Note that at that point the nir shader is only available for internal
    shaders (like gs multiview).
    
    This specifically affected multiview tests that wrote gl_PointSize, as
    the nir shader for the geometry shader were wrongly exposing
    per_vertex_point_size as false, as we were basing our check on the
    nir_shader_info, and that was gathered calling nir_shader_gather_info
    at pipeline_lower_nir.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13325>

--- src/broadcom/vulkan/v3dv_pipeline.c
+++ src/broadcom/vulkan/v3dv_pipeline.c
@@ -1412,7 +1412,11 @@ pipeline_stage_create_binning(const struct v3dv_pipeline_stage *src,
    p_stage->stage = bin_stage;
    p_stage->entrypoint = src->entrypoint;
    p_stage->module = src->module;
-   p_stage->nir = src->nir ? nir_shader_clone(NULL, src->nir) : NULL;
+   /* For binning shaders we will clone the NIR code from the corresponding
+    * render shader later, when we call pipeline_compile_xxx_shader. This way
+    * we only have to run the relevant NIR lowerings once for render shaders
+    */
+   p_stage->nir = NULL;
    p_stage->spec_info = src->spec_info;
    p_stage->feedback = (VkPipelineCreationFeedbackEXT) { 0 };
    memcpy(p_stage->shader_sha1, src->shader_sha1, 20);
