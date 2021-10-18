103e93cbe6e228474d464e5aae14a5b86f4fec94
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-10-18 11:33:38 -0400

    zink: eliminate a hole in zink_descriptor_layout_key
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13483>

--- src/gallium/drivers/zink/zink_descriptors.h
+++ src/gallium/drivers/zink/zink_descriptors.h
@@ -92,8 +92,8 @@ struct zink_descriptor_state_key {
 
 struct zink_descriptor_layout_key {
    unsigned num_bindings;
-   VkDescriptorSetLayoutBinding *bindings;
    unsigned use_count;
+   VkDescriptorSetLayoutBinding *bindings;
 };
 
 struct zink_descriptor_layout {
