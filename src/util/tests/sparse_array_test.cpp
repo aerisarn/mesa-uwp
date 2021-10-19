abf2af64aca1d4b3ac8e63f8bc59ff4df00adf81
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2021-10-19 09:51:59 -0700

    util: Convert sparse array multithread test to use gtest
    Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
    Acked-by: Matt Turner <mattst88@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13425>

--- src/util/tests/sparse_array_test.cpp
+++ src/util/tests/sparse_array_test.cpp
@@ -21,64 +21,58 @@
  * IN THE SOFTWARE.
  */
 
-#undef NDEBUG
-
 #include "util/sparse_array.h"
 
-#include <assert.h>
-#include <stdlib.h>
 #include "c11/threads.h"
 
-#define NUM_THREADS 16
-#define NUM_RUNS 16
+#include <gtest/gtest.h>
+
 #define NUM_SETS_PER_THREAD (1 << 10)
 #define MAX_ARR_SIZE (1 << 20)
 
 static int
 test_thread(void *_state)
 {
-   struct util_sparse_array *arr = _state;
+   struct util_sparse_array *arr = (struct util_sparse_array *)_state;
    for (unsigned i = 0; i < NUM_SETS_PER_THREAD; i++) {
       uint32_t idx = rand() % MAX_ARR_SIZE;
-      uint32_t *elem = util_sparse_array_get(arr, idx);
+      uint32_t *elem = (uint32_t *)util_sparse_array_get(arr, idx);
       *elem = idx;
    }
 
    return 0;
 }
 
-static void
-run_test(unsigned run_idx)
+TEST(SparseArrayTest, Multithread)
 {
-   size_t node_size = 4 << (run_idx / 2);
+   const int num_runs = 16;
 
-   struct util_sparse_array arr;
-   util_sparse_array_init(&arr, sizeof(uint32_t), node_size);
+   for (unsigned run_idx = 0; run_idx < num_runs; run_idx++) {
+      size_t node_size = 4 << (run_idx / 2);
 
-   thrd_t threads[NUM_THREADS];
-   for (unsigned i = 0; i < NUM_THREADS; i++) {
-      int ret = thrd_create(&threads[i], test_thread, &arr);
-      assert(ret == thrd_success);
+      struct util_sparse_array arr;
+      util_sparse_array_init(&arr, sizeof(uint32_t), node_size);
+
+      thrd_t threads[16];
+      for (unsigned i = 0; i < ARRAY_SIZE(threads); i++) {
+         int ret = thrd_create(&threads[i], test_thread, &arr);
+         ASSERT_EQ(ret, thrd_success);
+      }
+
+      for (unsigned i = 0; i < ARRAY_SIZE(threads); i++) {
+         int ret = thrd_join(threads[i], NULL);
+         ASSERT_EQ(ret, thrd_success);
+      }
+
+      util_sparse_array_validate(&arr);
+
+      for (unsigned i = 0; i < MAX_ARR_SIZE; i++) {
+         uint32_t *elem = (uint32_t *)util_sparse_array_get(&arr, i);
+         ASSERT_TRUE(*elem == 0 || *elem == i)
+            << "Element at index " << i << " is " << *elem
+            << " but expected zero or " << i;
+      }
+
+      util_sparse_array_finish(&arr);
    }
-
-   for (unsigned i = 0; i < NUM_THREADS; i++) {
-      int ret = thrd_join(threads[i], NULL);
-      assert(ret == thrd_success);
-   }
-
-   util_sparse_array_validate(&arr);
-
-   for (unsigned i = 0; i < MAX_ARR_SIZE; i++) {
-      uint32_t *elem = util_sparse_array_get(&arr, i);
-      assert(*elem == 0 || *elem == i);
-   }
-
-   util_sparse_array_finish(&arr);
-}
-
-int
-main(int argc, char **argv)
-{
-   for (unsigned i = 0; i < NUM_RUNS; i++)
-      run_test(i);
 }
