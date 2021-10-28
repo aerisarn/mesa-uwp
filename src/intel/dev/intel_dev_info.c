3dda80fcf62baa106f227392d45ea6853ef7d9e9
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-10-28 11:12:16 +0300

    intel/dev: printout timestamp period
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13571>

--- src/intel/dev/intel_dev_info.c
+++ src/intel/dev/intel_dev_info.c
@@ -110,7 +110,8 @@ main(int argc, char *argv[])
       fprintf(stdout, "   max GS  threads: %u\n", devinfo.max_gs_threads);
       fprintf(stdout, "   max WM  threads: %u\n", devinfo.max_wm_threads);
       fprintf(stdout, "   max CS  threads: %u\n", devinfo.max_cs_threads);
-      fprintf(stdout, "   timestamp frequency: %" PRIu64 "\n", devinfo.timestamp_frequency);
+      fprintf(stdout, "   timestamp frequency: %" PRIu64 " / %.4f ns\n",
+              devinfo.timestamp_frequency, 1000000000.0 / devinfo.timestamp_frequency);
    }
 
    return EXIT_SUCCESS;
