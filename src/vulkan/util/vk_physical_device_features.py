a208b849f337a60f87216575bc148d8de20517b6
Jason Ekstrand <jason@jlekstrand.net>
2021-10-29 12:23:45 -0500

    vulkan: Rework mako error handling in python generators
    Suggested-by: Dylan Baker <dylan@pnwbakers.com>
    Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13156>

--- src/vulkan/util/vk_physical_device_features.py
+++ src/vulkan/util/vk_physical_device_features.py
@@ -239,12 +239,10 @@ def main():
         # to print a useful stack trace and prints it, then exits with
         # status 1, if python is run with debug; otherwise it just raises
         # the exception
-        if __debug__:
-            import sys
-            from mako import exceptions
-            sys.stderr.write(exceptions.text_error_template().render() + '\n')
-            sys.exit(1)
-        raise
+        import sys
+        from mako import exceptions
+        print(exceptions.text_error_template().render(), file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
