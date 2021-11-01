4e28962800f477219854d8fcfa6b44e37a00a5d6
Emma Anholt <emma@anholt.net>
2021-11-01 10:49:03 -0700

    ci: Uprev VK-GL-CTS to 1.2.7.2, and pull in piglit while I'm here.
    The VK-GL-CTS fixes some issues for freedreno, and almost all of LVP's
    xfails.
    
    Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13622>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 7d7dd2688c214e1b3c00f37226500cbec4a58efb
+git checkout 83bc56abf2686e2cd9024a152e121ca4aa524985
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
