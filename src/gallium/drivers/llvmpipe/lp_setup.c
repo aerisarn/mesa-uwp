27903abbb67482e2d772c508e9d00e49029b8364
Dave Airlie <airlied@redhat.com>
2021-11-01 14:12:20 +1000

    llvmpipe: fix compressed image sizes.
    VK CTS just added some new tests to write to a compressed image
    from a compute shader, which was overrunning memory.
    
    The image width/height need to be sized according to the block
    sizes to avoid overwriting memory.
    
    dEQP-VK.image.sample_texture.*bit_compressed*
    
    Cc: mesa-stable
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13618>

--- src/gallium/drivers/llvmpipe/lp_setup.c
+++ src/gallium/drivers/llvmpipe/lp_setup.c
@@ -732,7 +732,11 @@ lp_setup_set_fs_images(struct lp_setup_context *setup,
 
          if (llvmpipe_resource_is_texture(res)) {
             uint32_t mip_offset = lp_res->mip_offsets[image->u.tex.level];
+            const uint32_t bw = util_format_get_blockwidth(image->resource->format);
+            const uint32_t bh = util_format_get_blockheight(image->resource->format);
 
+            jit_image->width = DIV_ROUND_UP(jit_image->width, bw);
+            jit_image->height = DIV_ROUND_UP(jit_image->height, bh);
             jit_image->width = u_minify(jit_image->width, image->u.tex.level);
             jit_image->height = u_minify(jit_image->height, image->u.tex.level);
 
