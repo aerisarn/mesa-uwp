f57940109998ac92656e7d5509516a44b21b08fc
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-11-05 11:09:34 -0400

    aux/trace: fix vertex state tracing
    Fixes: e8cad57aa7a ("gallium/trace: add pipe_vertex_state support")
    
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13582>

--- src/gallium/auxiliary/driver_trace/tr_screen.c
+++ src/gallium/auxiliary/driver_trace/tr_screen.c
@@ -1056,7 +1056,9 @@ trace_screen_create_vertex_state(struct pipe_screen *_screen,
    trace_dump_arg(ptr, screen);
    trace_dump_arg(ptr, buffer->buffer.resource);
    trace_dump_arg(vertex_buffer, buffer);
+   trace_dump_arg_begin("elements");
    trace_dump_struct_array(vertex_element, elements, num_elements);
+   trace_dump_arg_end();
    trace_dump_arg(uint, num_elements);
    trace_dump_arg(ptr, indexbuf);
    trace_dump_arg(uint, full_velem_mask);
