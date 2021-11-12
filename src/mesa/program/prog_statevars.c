43322ceccdfa67fcd133c94f51adfaefe2d99db2
Ilia Mirkin <imirkin@alum.mit.edu>
2021-11-11 21:10:20 -0500

    mesa: add missing state to state string computation
    This is an internal state, so does not need to be made available in the
    string itself (same as the wpos_y_transform). But it needs to be listed
    to avoid the mesa_problem.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13073>

--- src/mesa/program/prog_statevars.c
+++ src/mesa/program/prog_statevars.c
@@ -1334,6 +1334,7 @@ _mesa_program_state_string(const gl_state_index16 state[STATE_LENGTH])
    case STATE_PT_BIAS:
    case STATE_FB_SIZE:
    case STATE_FB_WPOS_Y_TRANSFORM:
+   case STATE_FB_PNTC_Y_TRANSFORM:
    case STATE_TCS_PATCH_VERTICES_IN:
    case STATE_TES_PATCH_VERTICES_IN:
    case STATE_ADVANCED_BLENDING_MODE:
