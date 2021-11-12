9d9de15a020d317d4486124f97b2cf06b98ee8b6
Timothy Arceri <tarceri@itsqueeze.com>
2021-11-12 19:31:24 +1100

    mesa: fix buffer overrun in SavedObj texture obj array
    Fixes: 3be42f9ca19d ("mesa: rewrite glPushAttrib/glPopAttrib to get rid of malloc")
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5621
    
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13773>

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -5158,7 +5158,7 @@ struct gl_texture_attrib_node
    /* For saving per texture object state (wrap modes, filters, etc),
     * SavedObj[][].Target is unused, so the value is invalid.
     */
-   struct gl_texture_object SavedObj[MAX_TEXTURE_UNITS][NUM_TEXTURE_TARGETS];
+   struct gl_texture_object SavedObj[MAX_COMBINED_TEXTURE_IMAGE_UNITS][NUM_TEXTURE_TARGETS];
 };
 
 
