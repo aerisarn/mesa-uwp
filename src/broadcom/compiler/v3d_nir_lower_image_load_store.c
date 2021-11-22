29010669802ad1730bb74626d2df3453a047840f
Alejandro Piñeiro <apinheiro@igalia.com>
2021-11-22 15:08:29 +0100

    broadcom/compiler: fix indentation at v3d_nir_lower_image_load_store
    Reviewed-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20972>

--- src/broadcom/compiler/v3d_nir_lower_image_load_store.c
+++ src/broadcom/compiler/v3d_nir_lower_image_load_store.c
@@ -189,13 +189,13 @@ v3d_nir_lower_image_load(nir_builder *b, nir_intrinsic_instr *instr)
         } else if (util_format_is_pure_sint(format)) {
                 result = nir_format_unpack_sint(b, result, bits16, 4);
         } else {
-            nir_ssa_def *rg = nir_channel(b, result, 0);
-            nir_ssa_def *ba = nir_channel(b, result, 1);
-            result = nir_vec4(b,
-                              nir_unpack_half_2x16_split_x(b, rg),
-                              nir_unpack_half_2x16_split_y(b, rg),
-                              nir_unpack_half_2x16_split_x(b, ba),
-                              nir_unpack_half_2x16_split_y(b, ba));
+                nir_ssa_def *rg = nir_channel(b, result, 0);
+                nir_ssa_def *ba = nir_channel(b, result, 1);
+                result = nir_vec4(b,
+                                  nir_unpack_half_2x16_split_x(b, rg),
+                                  nir_unpack_half_2x16_split_y(b, rg),
+                                  nir_unpack_half_2x16_split_x(b, ba),
+                                  nir_unpack_half_2x16_split_y(b, ba));
         }
 
         nir_ssa_def_rewrite_uses_after(&instr->dest.ssa, result,
