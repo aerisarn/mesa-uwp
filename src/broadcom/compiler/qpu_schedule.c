1e85be415a0b3fbf04dde28d9ac2d47054b0ed21
Iago Toral Quiroga <itoral@igalia.com>
2021-11-29 13:23:11 +0100

    broadcom/compiler: lift restriction on vpmwt in last instruction for V3D 7.x
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25450>

--- src/broadcom/compiler/qpu_schedule.c
+++ src/broadcom/compiler/qpu_schedule.c
@@ -1934,7 +1934,7 @@ qpu_inst_valid_in_thrend_slot(struct v3d_compile *c,
         if (slot > 0 && qinst->uniform != ~0)
                 return false;
 
-        if (v3d_qpu_waits_vpm(inst))
+        if (c->devinfo->ver <= 42 && v3d_qpu_waits_vpm(inst))
                 return false;
 
         if (inst->sig.ldvary)
