cdf8a14bfff8a74a250de8fd9903d2380195fbf0
Guilherme Gallo <guilherme.gallo@collabora.com>
2021-12-02 10:10:26 -0300

    ci: Uprev piglit
    Bring up the piglit replay jwt-file argument feature.
    
    Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
    Reviewed-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14004>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 83bc56abf2686e2cd9024a152e121ca4aa524985
+git checkout 00b82804aaa4b27fe7fd134e784a23a2d358ebe1
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
