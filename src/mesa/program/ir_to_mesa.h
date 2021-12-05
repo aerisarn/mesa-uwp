7abe527ab4157d52dcf594dbf83357618b032f06
Timothy Arceri <tarceri@itsqueeze.com>
2021-12-05 13:32:07 +1100

    mesa/st: move _mesa_generate_parameters_list_for_uniforms() code to st
    The classic drivers that shared the code are now gone and the only user
    is the tgsi linker so here we move the code to where it is used.
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14066>

--- src/mesa/program/ir_to_mesa.h
+++ src/mesa/program/ir_to_mesa.h
@@ -40,14 +40,6 @@ struct gl_program_parameter_list;
 void _mesa_glsl_link_shader(struct gl_context *ctx, struct gl_shader_program *prog);
 
 void
-_mesa_generate_parameters_list_for_uniforms(struct gl_context *ctx,
-                                            struct gl_shader_program
-					    *shader_program,
-					    struct gl_linked_shader *sh,
-					    struct gl_program_parameter_list
-					    *params);
-
-void
 _mesa_ensure_and_associate_uniform_storage(struct gl_context *ctx,
                              struct gl_shader_program *shader_program,
                              struct gl_program *prog, unsigned required_space);
