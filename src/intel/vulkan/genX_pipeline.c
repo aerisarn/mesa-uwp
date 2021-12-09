823cffbe1cb728a832a9c30330e94ba0e173eaa0
Marcin Ślusarz <marcin.slusarz@intel.com>
2021-12-09 17:10:55 +0100

    anv: include Primitive Header in mesh shader per-primitive output
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15303>

--- src/intel/vulkan/genX_pipeline.c
+++ src/intel/vulkan/genX_pipeline.c
@@ -528,6 +528,17 @@ emit_3dstate_sbe(struct anv_graphics_pipeline *pipeline)
          assert(mue->per_primitive_header_size_dw % 8 == 0);
          sbe_mesh.PerPrimitiveURBEntryOutputReadOffset = mue->per_primitive_header_size_dw / 8;
          sbe_mesh.PerPrimitiveURBEntryOutputReadLength = DIV_ROUND_UP(mue->per_primitive_data_size_dw, 8);
+
+         /* Just like with clip distances, if Viewport Index or Layer is read
+          * back in the FS, adjust the offset and length to cover the Primitive
+          * Header, where Viewport Index & Layer are stored.
+          */
+         if (wm_prog_data->urb_setup[VARYING_SLOT_VIEWPORT] >= 0 ||
+             wm_prog_data->urb_setup[VARYING_SLOT_LAYER] >= 0) {
+            assert(sbe_mesh.PerPrimitiveURBEntryOutputReadOffset > 0);
+            sbe_mesh.PerPrimitiveURBEntryOutputReadOffset -= 1;
+            sbe_mesh.PerPrimitiveURBEntryOutputReadLength += 1;
+         }
       }
 #endif
    }
