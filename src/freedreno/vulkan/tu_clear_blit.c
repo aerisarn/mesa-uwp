ff7aee2ac9898d71204f5a71ce02202aced5e803
Thomas H.P. Andersen <phomes@gmail.com>
2021-12-28 02:24:42 +0100

    tu/clear_blit: use || when working with bools
    Fixes a warning with clang
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14315>

--- src/freedreno/vulkan/tu_clear_blit.c
+++ src/freedreno/vulkan/tu_clear_blit.c
@@ -2690,7 +2690,7 @@ tu_emit_blit(struct tu_cmd_buffer *cmd,
    tu_cs_emit_regs(cs, A6XX_RB_BLIT_INFO(
       .unk0 = !resolve,
       .gmem = !resolve,
-      .sample_0 = vk_format_is_int(attachment->format) |
+      .sample_0 = vk_format_is_int(attachment->format) ||
          vk_format_is_depth_or_stencil(attachment->format)));
 
    tu_cs_emit_pkt4(cs, REG_A6XX_RB_BLIT_DST_INFO, 4);
