4dc406c7482d92e03761f7cae33b12a2bf8b41b6
Rob Clark <robdclark@chromium.org>
2022-01-11 15:36:22 -0800

    freedreno: Update chip-ids
    Counterpoint to https://patchwork.freedesktop.org/series/98772/
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14506>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -327,7 +327,10 @@ add_gpus([
     ))
 
 add_gpus([
-        GPUId(chip_id=0xffff06030500, name="Adreno 7c Gen 3"),
+        GPUId(chip_id=0x00be06030500, name="Adreno 8c Gen 3"),
+        GPUId(chip_id=0x007506030500, name="Adreno 7c+ Gen 3"),
+        # fallback wildcard entry should be last:
+        GPUId(chip_id=0xffff06030500, name="Adreno 7c+ Gen 3"),
     ], A6xxGPUInfo(
         a6xx_gen4,
         num_sp_cores = 2,
