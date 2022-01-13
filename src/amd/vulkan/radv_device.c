852197537ec01d4b18d8c4e83f332fc63ab44dc6
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-01-13 07:55:57 +0100

    radv: add a no-op version of vkGetPhysicalDeviceToolPropertiesEXT()
    It seems the vulkan common runtime code exposes VK_EXT_tooling but
    doesn't (yet) have a fallback if the backend doesn't enable this
    extension. Implement it as a no-op for a temporary workaround.
    
    This fixes crashes with dEQP-VK.api.tooling_info.*.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14707>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -6475,3 +6475,14 @@ radv_GetPhysicalDeviceFragmentShadingRatesKHR(
 
    return vk_outarray_status(&out);
 }
+
+/* VK_EXT_tooling_info */
+VKAPI_ATTR VkResult VKAPI_CALL
+radv_GetPhysicalDeviceToolPropertiesEXT(
+   VkPhysicalDevice physicalDevice,
+   uint32_t *pToolCount,
+   VkPhysicalDeviceToolPropertiesEXT *pToolProperties)
+{
+   *pToolCount = 0;
+   return VK_SUCCESS;
+}
