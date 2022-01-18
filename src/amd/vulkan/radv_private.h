bb71d1092b4bfd026d36855a801e9ed60c263dff
Timur Kristóf <timur.kristof@gmail.com>
2022-01-18 16:39:53 +0100

    radv: Add radv_pipeline_has_task helper.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14929>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -2018,6 +2018,12 @@ radv_pipeline_has_mesh(const struct radv_pipeline *pipeline)
    return !!pipeline->shaders[MESA_SHADER_MESH];
 }
 
+static inline bool
+radv_pipeline_has_task(const struct radv_pipeline *pipeline)
+{
+   return !!pipeline->shaders[MESA_SHADER_TASK];
+}
+
 bool radv_pipeline_has_ngg_passthrough(const struct radv_pipeline *pipeline);
 
 bool radv_pipeline_has_gs_copy_shader(const struct radv_pipeline *pipeline);
