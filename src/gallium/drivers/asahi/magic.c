daab41b80b1f6c6c92382dbba8b53f0ea57ea3f5
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-01-18 19:58:21 -0500

    asahi: Identify IOGPU_ATTACHMENT::size
    Oops.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14898>

--- src/gallium/drivers/asahi/magic.c
+++ src/gallium/drivers/asahi/magic.c
@@ -141,9 +141,7 @@ demo_cmdbuf(uint64_t *buf, size_t size,
    agx_pack((map + (offset_attachments / 4) + 4), IOGPU_ATTACHMENT, cfg) {
       cfg.address = rt0;
       cfg.type = AGX_IOGPU_ATTACHMENT_TYPE_COLOUR;
-      cfg.unk_1 = 0x80000000;
-      cfg.unk_2 = 0x5;
-      cfg.bytes_per_pixel = 4;
+      cfg.size = 800*600*4;
       cfg.percent = 100;
    }
 
