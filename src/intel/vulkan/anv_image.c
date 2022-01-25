57445adc89186fcadc1e6006d1aa32768b26556b
Nanley Chery <nanley.g.chery@intel.com>
2022-01-25 12:19:33 -0500

    anv: Re-enable CCS_E on TGL+
    Commit e6147895885 ("anv: Also disallow CCS_E for multi-LOD images")
    accidentally disabled CCS_E on TGL+ because it checked for
    image->vk.mip_levels > 0 instead of image->vk.mip_levels > 1.
    
    Instead of reverting it, we remove the code which disables CCS_E for
    mipmapped or arrayed images now that we've sufficiently handled the
    clear color issue in other ways.
    
    Fixes: e6147895885 ("anv: Also disallow CCS_E for multi-LOD images")
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14723>

--- src/intel/vulkan/anv_image.c
+++ src/intel/vulkan/anv_image.c
@@ -697,20 +697,6 @@ add_aux_surface_if_supported(struct anv_device *device,
          return VK_SUCCESS;
       }
 
-      if (device->info.ver >= 12 &&
-          (image->vk.array_layers > 1 || image->vk.mip_levels)) {
-         /* HSD 14010672564: On TGL, if a block of fragment shader outputs
-          * match the surface's clear color, the HW may convert them to
-          * fast-clears. Anv only does clear color tracking for the first
-          * slice unfortunately. Disable CCS until anv gains more clear color
-          * tracking abilities.
-          */
-         anv_perf_warn(VK_LOG_OBJS(&image->vk.base),
-                       "HW may put fast-clear blocks on more slices than SW "
-                       "currently tracks. Not allocating a CCS buffer.");
-         return VK_SUCCESS;
-      }
-
       if (INTEL_DEBUG(DEBUG_NO_RBC))
          return VK_SUCCESS;
 
