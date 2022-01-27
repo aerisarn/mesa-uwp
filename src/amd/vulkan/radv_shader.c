0d0165db8ee4268fa4f539765c237b50da1a4883
Timur Kristóf <timur.kristof@gmail.com>
2022-01-27 10:08:12 +0100

    radv: Print task shader stage name before disasm.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14929>

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -2322,6 +2322,8 @@ radv_get_shader_name(const struct radv_shader_info *info, gl_shader_stage stage)
       return "Compute Shader";
    case MESA_SHADER_MESH:
       return "Mesh Shader as NGG";
+   case MESA_SHADER_TASK:
+      return "Task Shader as CS";
    default:
       return "Unknown shader";
    };
