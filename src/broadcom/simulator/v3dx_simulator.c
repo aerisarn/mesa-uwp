70a219d4a358be1d0d41af5b33203aebb19aa9ec
Melissa Wen <mwen@igalia.com>
2022-01-31 15:17:36 -0100

    broadcom/simulator: enable multisync in the simulator
    Use drmSyncobjSignal to signal out_syncobjs when a GPU job submission
    ends in the simulator. With this, we can enable multisync support in the
    simulator and keep the multisync approach to process fence by submitting
    a serialized no-op job that adds the fence to the array of out syncobjs,
    i.e.  syncobjs to be signaled in the kernel when a job completes (job
    post deps).
    
    Signed-off-by: Melissa Wen <mwen@igalia.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14768>

--- src/broadcom/simulator/v3dx_simulator.c
+++ src/broadcom/simulator/v3dx_simulator.c
@@ -270,7 +270,7 @@ v3dX(simulator_get_param_ioctl)(struct v3d_hw *v3d,
                 args->value = V3D_VERSION >= 41;
                 return 0;
         case DRM_V3D_PARAM_SUPPORTS_MULTISYNC_EXT:
-                args->value = 0;
+                args->value = 1;
                 return 0;
         }
 
