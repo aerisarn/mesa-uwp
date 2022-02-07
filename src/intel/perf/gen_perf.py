12e065ddecd50885158c724a2ccfa453c445ce74
Emma Anholt <emma@anholt.net>
2022-02-07 11:06:18 -0800

    intel/perf: Move some static blocks of C code out of the python script.
    Now my editor can help me format code as I type.
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15237>

--- src/intel/perf/gen_perf.py
+++ src/intel/perf/gen_perf.py
@@ -686,53 +686,7 @@ def main():
 
     c(textwrap.dedent("""\
         #include "perf/intel_perf.h"
-
-
-        #define MIN(a, b) ((a < b) ? (a) : (b))
-        #define MAX(a, b) ((a > b) ? (a) : (b))
-
-        static struct intel_perf_query_info *
-        intel_query_alloc(struct intel_perf_config *perf, int ncounters)
-        {
-           struct intel_perf_query_info *query = rzalloc(perf, struct intel_perf_query_info);
-           query->perf = perf;
-           query->kind = INTEL_PERF_QUERY_TYPE_OA;
-           query->n_counters = 0;
-           query->oa_metrics_set_id = 0; /* determined at runtime, via sysfs */
-           query->counters = rzalloc_array(query, struct intel_perf_query_counter, ncounters);
-           return query;
-        }
-
-        static struct intel_perf_query_info *
-        hsw_query_alloc(struct intel_perf_config *perf, int ncounters)
-        {
-           struct intel_perf_query_info *query = intel_query_alloc(perf, ncounters);
-           query->oa_format = I915_OA_FORMAT_A45_B8_C8;
-           /* Accumulation buffer offsets... */
-           query->gpu_time_offset = 0;
-           query->a_offset = query->gpu_time_offset + 1;
-           query->b_offset = query->a_offset + 45;
-           query->c_offset = query->b_offset + 8;
-           query->perfcnt_offset = query->c_offset + 8;
-           query->rpstat_offset = query->perfcnt_offset + 2;
-           return query;
-        }
-
-        static struct intel_perf_query_info *
-        bdw_query_alloc(struct intel_perf_config *perf, int ncounters)
-        {
-           struct intel_perf_query_info *query = intel_query_alloc(perf, ncounters);
-           query->oa_format = I915_OA_FORMAT_A32u40_A4u32_B8_C8;
-           /* Accumulation buffer offsets... */
-           query->gpu_time_offset = 0;
-           query->gpu_clock_offset = query->gpu_time_offset + 1;
-           query->a_offset = query->gpu_clock_offset + 1;
-           query->b_offset = query->a_offset + 36;
-           query->c_offset = query->b_offset + 8;
-           query->perfcnt_offset = query->c_offset + 8;
-           query->rpstat_offset = query->perfcnt_offset + 2;
-           return query;
-        }
+        #include "perf/intel_perf_setup.h"
         """))
 
     # Print out all equation functions.
