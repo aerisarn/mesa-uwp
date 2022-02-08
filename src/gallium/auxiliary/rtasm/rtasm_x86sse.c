55b735c51ae27a9c40b0e230543572384ec61c9b
Ilia Mirkin <imirkin@alum.mit.edu>
2022-02-07 23:36:01 -0500

    rtasm: fix printf specifier for ptrdiff_t
    In practice it's a small number, but new gcc versions complain.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Acked-by: Emma Anholt <emma@anholt.net>
    Tested-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14922>

--- src/gallium/auxiliary/rtasm/rtasm_x86sse.c
+++ src/gallium/auxiliary/rtasm/rtasm_x86sse.c
@@ -89,7 +89,7 @@ void x86_print_reg( struct x86_reg reg )
       foo++;                                    \
    if  (*foo)                                   \
       foo++;                                    \
-   debug_printf( "\n%4x %14s ", p->csr - p->store, foo );             \
+   debug_printf( "\n%4tx %14s ", p->csr - p->store, foo );             \
 } while (0)
 
 #define DUMP_I( I ) do {                        \
