0edf63de76d075d29e3ad7affa253e47804cac20
Laurent Bigonville <bigon@bigon.be>
2022-02-17 14:49:27 +0100

    Try to fix FTBFS on kfreebsd architecture
    Fixes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4081
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15058>

--- src/util/u_qsort.h
+++ src/util/u_qsort.h
@@ -57,7 +57,10 @@ util_qsort_r(void *base, size_t nmemb, size_t size,
              void *arg)
 {
 #if HAVE_QSORT_R
-#  if DETECT_OS_APPLE || DETECT_OS_BSD
+#  if defined(__GLIBC__)
+   /* GNU extension added in glibc 2.8 */
+   qsort_r(base, nmemb, size, compar, arg);
+#  elif (DETECT_OS_APPLE || DETECT_OS_BSD)
    /* BSD/macOS qsort_r takes "arg" before the comparison function and it
     * pass the "arg" before the elements.
     */
@@ -66,9 +69,6 @@ util_qsort_r(void *base, size_t nmemb, size_t size,
       arg
    };
    qsort_r(base, nmemb, size, &data, util_qsort_adapter);
-#  else
-   /* GNU extension added in glibc 2.8 */
-   qsort_r(base, nmemb, size, compar, arg);
 #  endif
 #elif HAVE_QSORT_S
 #  ifdef _WIN32
