53f1e57ee7a7a6f7906c34cf7b78a39873fd00c2
Alyssa Rosenzweig <alyssa@collabora.com>
2022-03-05 15:49:43 -0500

    pan/bi: Handle non-2D arrays
    Handle arrays generically by using the last component of the coordinate source
    as the array index. That works for both 2D arrays and cube arrays, fixing cube
    arrays. Cube arrays were already handled correctly in core Panfrost code.
    
    This code path is not tested in dEQP-GLES31 without exposing OES_cube_map_array,
    which depends on OES_geometry_shader, which we don't have. Yet we do expose
    PIPE_CAP_CUBE_ARRAY, so ARB_cube_map_array is exposed.
    
    Disabling PIPE_CAP_CUBE_ARRAY would be an easy band-aid fix, but it's easy
    enough to handle correctly.
    
    dEQP-GLES31 passes with a hack enabling OES_cube_map_array [without geometry
    shaders].
    
    Also fixes 1D arrays on Bifrost for the same reasons.
    
    Fixes: 70d6c5675d6 ("pan/bi: Emit TEXC with builder")
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15254>

--- src/panfrost/bifrost/bifrost_compile.c
+++ src/panfrost/bifrost/bifrost_compile.c
@@ -2815,6 +2815,7 @@ bi_emit_texc(bi_builder *b, nir_tex_instr *instr)
         for (unsigned i = 0; i < instr->num_srcs; ++i) {
                 bi_index index = bi_src_index(&instr->src[i].src);
                 unsigned sz = nir_src_bit_size(instr->src[i].src);
+                unsigned components = nir_src_num_components(instr->src[i].src);
                 ASSERTED nir_alu_type base = nir_tex_instr_src_type(instr, i);
                 nir_alu_type T = base | sz;
 
@@ -2823,27 +2824,25 @@ bi_emit_texc(bi_builder *b, nir_tex_instr *instr)
                         if (instr->sampler_dim == GLSL_SAMPLER_DIM_CUBE) {
                                 cx = bi_emit_texc_cube_coord(b, index, &cy);
 			} else {
-                                unsigned components = nir_src_num_components(instr->src[i].src);
-
                                 /* Copy XY (for 2D+) or XX (for 1D) */
                                 cx = index;
                                 cy = bi_word(index, MIN2(1, components - 1));
 
                                 assert(components >= 1 && components <= 3);
 
-                                if (components < 3) {
-                                        /* nothing to do */
-                                } else if (desc.array) {
-                                        /* 2D array */
-                                        dregs[BIFROST_TEX_DREG_ARRAY] =
-                                                bi_emit_texc_array_index(b,
-                                                                bi_word(index, 2), T);
-                                } else {
+                                if (components == 3 && !desc.array) {
                                         /* 3D */
                                         dregs[BIFROST_TEX_DREG_Z_COORD] =
                                                 bi_word(index, 2);
                                 }
                         }
+
+                        if (desc.array) {
+                                dregs[BIFROST_TEX_DREG_ARRAY] =
+                                                bi_emit_texc_array_index(b,
+                                                                bi_word(index, components - 1), T);
+                        }
+
                         break;
 
                 case nir_tex_src_lod:
