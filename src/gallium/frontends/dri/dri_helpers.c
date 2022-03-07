3937c47c482c742938dca70aff06d87550d6bfd6
Rob Clark <robdclark@chromium.org>
2022-03-07 15:53:59 -0800

    gallium/dri: Add missing in_fence_fd initialization
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6108
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
    Tested-by: Michel Dänzer <mdaenzer@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15272>

--- src/gallium/frontends/dri/dri_helpers.c
+++ src/gallium/frontends/dri/dri_helpers.c
@@ -314,6 +314,7 @@ dri2_create_image_from_renderbuffer2(__DRIcontext *context,
    img->dri_format = driGLFormatToImageFormat(rb->Format);
    img->loader_private = loaderPrivate;
    img->sPriv = context->driScreenPriv;
+   img->in_fence_fd = -1;
 
    pipe_resource_reference(&img->texture, tex);
 
@@ -413,6 +414,7 @@ dri2_create_from_texture(__DRIcontext *context, int target, unsigned texture,
 
    img->level = level;
    img->layer = depth;
+   img->in_fence_fd = -1;
    img->dri_format = driGLFormatToImageFormat(obj->Image[face][level]->TexFormat);
 
    img->loader_private = loaderPrivate;
