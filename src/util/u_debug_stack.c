e50eb1ce7ab3bbbb71a4cb9016a2e41d0a30e08f
Jonathan Gray <jsg@jsg.id.au>
2022-03-12 11:08:56 +1100

    util: fix msvc build
    Fix msvc build regression after 0536b691338f2759b850f9ec94634033a5d1f9e1
    reported by Prodea Alexandru-Liviu.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6137
    Fixes: 0536b691338 ("util: fix build with clang 10 on mips64")
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15355>

--- src/util/u_debug_stack.c
+++ src/util/u_debug_stack.c
@@ -256,10 +256,11 @@ debug_backtrace_capture(struct debug_stack_frame *backtrace,
    const void **frame_pointer = ((const void **)__builtin_frame_address(1));
 #pragma GCC diagnostic pop
 #elif defined(PIPE_CC_MSVC)
+   const void **frame_pointer;
    __asm {
       mov frame_pointer, ebp
    }
-   const void **frame_pointer = (const void **)frame_pointer[0];
+   frame_pointer = (const void **)frame_pointer[0];
 #else
    const void **frame_pointer = NULL;
 #endif
