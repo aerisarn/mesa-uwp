1350e7b607629e9e539970cc88007a09311a7549
Juan A. Suarez Romero <jasuarez@igalia.com>
2022-03-14 18:53:14 +0100

    radv: change MESA_GLSL_CACHE envvar reference
    This was renamed to MESA_SHADER_CACHE.
    
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15390>

--- src/amd/vulkan/radv_pipeline_cache.c
+++ src/amd/vulkan/radv_pipeline_cache.c
@@ -288,7 +288,7 @@ radv_pipeline_cache_add_entry(struct radv_pipeline_cache *cache, struct cache_en
 static bool
 radv_is_cache_disabled(struct radv_device *device)
 {
-   /* Pipeline caches can be disabled with RADV_DEBUG=nocache and with MESA_GLSL_CACHE_DISABLE=1. */
+   /* Pipeline caches can be disabled with RADV_DEBUG=nocache and with MESA_SHADER_CACHE_DISABLE=1. */
    return (device->instance->debug_flags & RADV_DEBUG_NO_CACHE);
 }
 
