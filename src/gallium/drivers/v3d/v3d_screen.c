04fb31a4204c9c850bbf7b4c22a8bcff6114e6ad
Juan A. Suarez Romero <jasuarez@igalia.com>
2022-03-17 10:29:38 +0100

    v3d: enable GL_ARB_copy_image extension
    Enable the proper capability to get activate this extension.
    
    Fixes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4588
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15693>

--- src/gallium/drivers/v3d/v3d_screen.c
+++ src/gallium/drivers/v3d/v3d_screen.c
@@ -147,6 +147,7 @@ v3d_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
         case PIPE_CAP_TEXTURE_MIRROR_CLAMP_TO_EDGE:
         case PIPE_CAP_SAMPLER_VIEW_TARGET:
         case PIPE_CAP_ANISOTROPIC_FILTER:
+        case PIPE_CAP_COPY_BETWEEN_COMPRESSED_AND_PLAIN_FORMATS:
                 return 1;
 
         case PIPE_CAP_TEXTURE_QUERY_LOD:
