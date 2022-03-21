f9390dac23c0062bff84824fe45e2c81c541c9d9
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-03-21 16:21:16 -0400

    zink: use the current compute shader, not the base one
    this is the current variant and the one that should be used
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15501>

--- src/gallium/drivers/zink/zink_pipeline.c
+++ src/gallium/drivers/zink/zink_pipeline.c
@@ -318,7 +318,7 @@ zink_create_compute_pipeline(struct zink_screen *screen, struct zink_compute_pro
    VkPipelineShaderStageCreateInfo stage = {0};
    stage.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
    stage.stage = VK_SHADER_STAGE_COMPUTE_BIT;
-   stage.module = comp->module->shader;
+   stage.module = comp->curr->shader;
    stage.pName = "main";
 
    VkSpecializationInfo sinfo = {0};
