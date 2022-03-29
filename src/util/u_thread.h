aerisarn/mesa-uwp

commit 0f1b3fc17f0e543c55d6c30d9c1fadc610219b5b

util: Fixes unused parameter warnings
The compiler warning:
```
../src/mesa/util/u_thread.h: In function 'util_thread_get_time_nano':
../src/mesa/util/u_thread.h:239:34: warning: unused parameter 'thread' [-Wunused-parameter]
  239 | util_thread_get_time_nano(thrd_t thread)
      |                           ~~~~~~~^~~~~~
```

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15553>

src/util/u_thread.h
@@ -236,7 +236,7 @@ util_set_current_thread_affinity(const uint32_t *mask,
 
 /* Return the time of a thread's CPU time clock. */
 static inline int64_t
-util_thread_get_time_nano(thrd_t thread)
+util_thread_get_time_nano(ASSERTED thrd_t thread)
 {
 #if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__)
    struct timespec ts;

commit c91e3c6a428825ef10630ac891ccb79d5116dd65

util: Should not use ASSERTED in util_thread_get_time_nano
The parameter is not ASSERTED
Fixes: 0f1b3fc17f0 ("util: Fixes unused parameter warnings")

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15655>

src/util/u_thread.h
@@ -236,7 +236,7 @@ util_set_current_thread_affinity(const uint32_t *mask,
 
 /* Return the time of a thread's CPU time clock. */
 static inline int64_t
-util_thread_get_time_nano(ASSERTED thrd_t thread)
+util_thread_get_time_nano(thrd_t thread)
 {
 #if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__)
    struct timespec ts;
@@ -246,6 +246,7 @@ util_thread_get_time_nano(ASSERTED thrd_t thread)
    clock_gettime(cid, &ts);
    return (int64_t)ts.tv_sec * 1000000000 + ts.tv_nsec;
 #else
+   (void)thread;
    return 0;
 #endif
 }