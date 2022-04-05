3baceb0ca4be3d525a1aae023df44b34f4618d7f
Alyssa Rosenzweig <alyssa@collabora.com>
2022-04-05 12:33:10 -0400

    panfrost: Hide parts of pan_encoder.h for Valhall
    These pertain to data structures that no longer exist.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15795>

--- src/panfrost/lib/pan_encoder.h
+++ src/panfrost/lib/pan_encoder.h
@@ -145,6 +145,7 @@ panfrost_flip_compare_func(enum mali_func f)
 
 }
 
+#if PAN_ARCH <= 7
 /* Compute shaders are invoked with a gl_NumWorkGroups X/Y/Z triplet. Vertex
  * shaders are invoked as (1, vertex_count, instance_count). Compute shaders
  * also have a gl_WorkGroupSize X/Y/Z triplet. These 6 values are packed
@@ -205,6 +206,7 @@ panfrost_pack_work_groups_compute(
                         MALI_SPLIT_MIN_EFFICIENT : cfg.workgroups_x_shift;
         }
 }
+#endif
 
 #if PAN_ARCH >= 5
 /* Format conversion */
