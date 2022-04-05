a57b3bb82ab66cfe1b98fc952b8a779e8fb81739
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-05 14:11:40 -0400

    zink: refactor zink_descriptor_util_image_layout_eval
    slightly more readable
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15716>

--- src/gallium/drivers/zink/zink_descriptors.c
+++ src/gallium/drivers/zink/zink_descriptors.c
@@ -567,15 +567,17 @@ zink_descriptor_util_image_layout_eval(const struct zink_resource *res, bool is_
          return VK_IMAGE_LAYOUT_GENERAL;
       return VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
    }
-   return res->image_bind_count[is_compute] ? VK_IMAGE_LAYOUT_GENERAL :
-                          res->aspect & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT) ?
-                             //Vulkan-Docs#1490
-                             //(res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_DEPTH_READ_ONLY_OPTIMAL :
-                              //res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_STENCIL_READ_ONLY_OPTIMAL :
-                             (res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
-                              res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
-                              VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL) :
-                             VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+   if (res->image_bind_count[is_compute])
+      return VK_IMAGE_LAYOUT_GENERAL;
+   if (res->aspect & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) {
+      //Vulkan-Docs#1490
+      //(res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_DEPTH_READ_ONLY_OPTIMAL :
+       //res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_STENCIL_READ_ONLY_OPTIMAL :
+      return res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
+             res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
+                                                          VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+   }
+   return VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 }
 
 static struct zink_descriptor_pool *
