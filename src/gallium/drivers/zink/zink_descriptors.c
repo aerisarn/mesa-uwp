fd9a5fed164442f2a1c77ce4f408d9f2402712af
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-05 14:12:03 -0400

    zink: remove commented code
    probably never actually going to do this since it serves no purpose
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15716>

--- src/gallium/drivers/zink/zink_descriptors.c
+++ src/gallium/drivers/zink/zink_descriptors.c
@@ -570,9 +570,6 @@ zink_descriptor_util_image_layout_eval(const struct zink_resource *res, bool is_
    if (res->image_bind_count[is_compute])
       return VK_IMAGE_LAYOUT_GENERAL;
    if (res->aspect & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) {
-      //Vulkan-Docs#1490
-      //(res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_DEPTH_READ_ONLY_OPTIMAL :
-       //res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_STENCIL_READ_ONLY_OPTIMAL :
       return res->aspect == VK_IMAGE_ASPECT_DEPTH_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
              res->aspect == VK_IMAGE_ASPECT_STENCIL_BIT ? VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL :
                                                           VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
