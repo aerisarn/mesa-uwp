ed07721d090eb0c4dceb65fba36b0ae054519d3c
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-06 16:50:37 -0400

    zink: only apply swapchain behavior in flush_resource for swapchain images
    these could otherwise be pixmaps or dmabufs
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15784>

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -3544,7 +3544,7 @@ zink_flush_resource(struct pipe_context *pctx,
 {
    struct zink_context *ctx = zink_context(pctx);
    struct zink_resource *res = zink_resource(pres);
-   if (pres->bind & PIPE_BIND_DISPLAY_TARGET) {
+   if (res->obj->dt) {
       if (res->obj->acquire) {
          zink_resource_image_barrier(ctx, res, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR, 0, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT);
          zink_batch_reference_resource_rw(&ctx->batch, res, true);
