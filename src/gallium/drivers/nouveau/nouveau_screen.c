bf02bffe156214dad942f56ee68c380d1968704f
Emma Anholt <emma@anholt.net>
2022-04-13 20:25:59 -0700

    nouveau: Enable the NIR backend by default.
    The glsl-to-tgsi code generation and GLSL IR linker is is going away
    (!8044), so we need to make the call on whether to use nir-to-tgsi (See
    !15932 and !15541), or switch over to the NIR code generator.  The NIR
    backend should reduce the compile time regression while providing more
    direct control over the IR we receive than going through NTT, while still
    providing the optimization that NIR-to-TGSI was bringing us.
    
    nv92 shader-db:
    total local in shared programs: 2048 -> 1988 (-2.93%)
    local in affected programs: 2048 -> 1988 (-2.93%)
    total gpr in shared programs: 688468 -> 724705 (5.26%)
    gpr in affected programs: 437159 -> 473396 (8.29%)
    total instructions in shared programs: 6115978 -> 5874401 (-3.95%)
    instructions in affected programs: 5038041 -> 4796464 (-4.80%)
    total loops in shared programs: 1361 -> 835 (-38.65%)
    loops in affected programs: 538 -> 12 (-97.77%)
    total bytes in shared programs: 42389752 -> 40480416 (-4.50%)
    bytes in affected programs: 36311616 -> 34402280 (-5.26%)
    LOST:   0
    GAINED: 1 (pixmark-piano)
    
    nv120 shader-db:
    total local in shared programs: 4416 -> 1988 (-54.98%)
    local in affected programs: 4416 -> 1988 (-54.98%)
    total gpr in shared programs: 870534 -> 893490 (2.64%)
    gpr in affected programs: 564210 -> 587166 (4.07%)
    total instructions in shared programs: 6379402 -> 6243210 (-2.13%)
    instructions in affected programs: 5430790 -> 5294598 (-2.51%)
    total bytes in shared programs: 68184224 -> 66729672 (-2.13%)
    bytes in affected programs: 58013544 -> 56558992 (-2.51%)
    
    Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15949>

--- src/gallium/drivers/nouveau/nouveau_screen.c
+++ src/gallium/drivers/nouveau/nouveau_screen.c
@@ -202,10 +202,7 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
    if (nv_dbg)
       nouveau_mesa_debug = atoi(nv_dbg);
 
-   if (dev->chipset < 0x140)
-      screen->prefer_nir = debug_get_bool_option("NV50_PROG_USE_NIR", false);
-   else
-      screen->prefer_nir = true;
+   screen->prefer_nir = !debug_get_bool_option("NV50_PROG_USE_TGSI", false);
 
    screen->force_enable_cl = debug_get_bool_option("NOUVEAU_ENABLE_CL", false);
    if (screen->force_enable_cl)
