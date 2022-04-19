dab745f3b4daf87902a42abd4a531ad90d7039ef
Rhys Perry <pendingchaos02@gmail.com>
2022-04-19 11:58:56 +0100

    nir/copy_prop_vars: fix non-vector shader call payloads
    Fixes RADV+Q2RTX.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Jason Ekstrand <jason.ekstrand@collabora.com>
    Fixes: ff05137c2db ("nir: introduce and use nir_component_mask")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16029>

--- src/compiler/nir/nir_opt_copy_prop_vars.c
+++ src/compiler/nir/nir_opt_copy_prop_vars.c
@@ -191,8 +191,7 @@ gather_vars_written(struct copy_prop_var_state *state,
             nir_deref_instr *payload =
                nir_src_as_deref(*nir_get_shader_call_payload_src(intrin));
 
-            nir_component_mask_t mask =
-               nir_component_mask(glsl_get_vector_elements(payload->type));
+            nir_component_mask_t mask = (1 << glsl_get_vector_elements(payload->type)) - 1;
 
             struct hash_entry *ht_entry =
                _mesa_hash_table_search(written->derefs, payload);
@@ -1150,8 +1149,7 @@ copy_prop_vars_block(struct copy_prop_var_state *state,
 
          nir_deref_and_path payload = {
             nir_src_as_deref(*nir_get_shader_call_payload_src(intrin)), NULL};
-         nir_component_mask_t full_mask =
-            nir_component_mask(glsl_get_vector_elements(payload.instr->type));
+         nir_component_mask_t full_mask = (1 << glsl_get_vector_elements(payload.instr->type)) - 1;
          kill_aliases(state, copies, &payload, full_mask);
          break;
       }
