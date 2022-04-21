aerisarn/mesa-uwp

commit e1c640c3a43d741719864b35cbf855aa82be0611

r300: stub derivatives on r300 and r400 hardware
There are three approaches for problem:
- use dummy shader (current solution)
- use software rendering
- stub

First option always gonna give bad results, second one
gonna be slideshow (r300/r400 at best are running with
old 2 cores cpu), third one can sometimes give graphical
gliches.

IMHO third one is least annoying option.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13642>

src/gallium/drivers/r300/compiler/radeon_program_alu.c
@@ -1186,6 +1186,25 @@ int r300_transform_trig_scale_vertex(struct radeon_compiler *c,
 }
 
 /**
+ * Replaces DDX/DDY instructions with MOV 0 to avoid using dummy shaders on r300/r400.
+ *
+ * @warning This explicitly changes the form of DDX and DDY!
+ */
+
+int radeonStubDeriv(struct radeon_compiler* c,
+	struct rc_instruction* inst,
+	void* unused)
+{
+	if (inst->U.I.Opcode != RC_OPCODE_DDX && inst->U.I.Opcode != RC_OPCODE_DDY)
+		return 0;
+
+	inst->U.I.Opcode = RC_OPCODE_MOV;
+	inst->U.I.SrcReg[0].Swizzle = RC_SWIZZLE_0000;
+
+	return 1;
+}
+
+/**
  * Rewrite DDX/DDY instructions to properly work with r5xx shaders.
  * The r5xx MDH/MDV instruction provides per-quad partial derivatives.
  * It takes the form A*B+C. A and C are set by setting src0. B should be -1.

src/gallium/drivers/r300/compiler/r3xx_fragprog.c
@@ -109,6 +109,7 @@ void r3xx_compile_fragment_program(struct r300_fragment_program_compiler* c)
 
 	struct radeon_program_transformation native_rewrite_r300[] = {
 		{ &radeonTransformALU, 0 },
+		{ &radeonStubDeriv, 0 },
 		{ &r300_transform_trig_simple, 0 },
 		{ 0, 0 }
 	};

src/gallium/drivers/r300/compiler/radeon_program_alu.h
@@ -55,6 +55,11 @@ int r300_transform_trig_scale_vertex(
 	struct rc_instruction *inst,
 	void*);
 
+int radeonStubDeriv(
+	struct radeon_compiler * c,
+	struct rc_instruction * inst,
+	void*);
+
 int radeonTransformDeriv(
 	struct radeon_compiler * c,
 	struct rc_instruction * inst,

commit d48c10fab38217264045064b3f48ecac0c24b7bb

r300: Print warning when stubbing derivatives
Fixes: e1c640c3a43d741719864b35cbf855aa82be0611
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14669>

src/gallium/drivers/r300/compiler/radeon_program_alu.c
@@ -38,6 +38,7 @@
 #include "radeon_compiler.h"
 #include "radeon_compiler_util.h"
 
+#include "util/log.h"
 
 static struct rc_instruction *emit1(
 	struct radeon_compiler * c, struct rc_instruction * after,
@@ -1099,6 +1100,10 @@ int radeonStubDeriv(struct radeon_compiler* c,
 
 	inst->U.I.Opcode = RC_OPCODE_MOV;
 	inst->U.I.SrcReg[0].Swizzle = RC_SWIZZLE_0000;
+
+	mesa_logw_once("r300: WARNING: Shader is trying to use derivatives, "
+					"but the hardware doesn't support it. "
+					"Expect possible misrendering (it's not a bug, do not report it).");
 
 	return 1;
 }