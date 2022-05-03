1b639a0ce5b239ff06f1dfc8256f6512cb260144
Rhys Perry <pendingchaos02@gmail.com>
2022-05-03 12:13:55 +0100

    aco/ra: fix vgpr_limit
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Fixes: b98a4d4dd7e ("aco: refactor GPR limit calculation")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16297>

--- src/amd/compiler/aco_register_allocation.cpp
+++ src/amd/compiler/aco_register_allocation.cpp
@@ -96,7 +96,7 @@ struct ra_ctx {
       phi_dummy.reset(
          create_instruction<Instruction>(aco_opcode::p_linear_phi, Format::PSEUDO, 0, 0));
       sgpr_limit = get_addr_sgpr_from_waves(program, program->min_waves);
-      vgpr_limit = get_addr_sgpr_from_waves(program, program->min_waves);
+      vgpr_limit = get_addr_vgpr_from_waves(program, program->min_waves);
    }
 };
 
