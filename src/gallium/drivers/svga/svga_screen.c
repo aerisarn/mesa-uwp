e73443b7a502c06637d2eac0e8ef1c5cf449a341
Charmaine Lee <charmainel@vmware.com>
2022-05-10 09:34:57 -0700

    svga: enable PIPE_CAP_TGSI_TEXCOORD for vgpu10 and up
    Revise commit 0d456315f95 to enable PIPE_CAP_TGSI_TEXCOORD for
    vgpu10 and up only.
    
    Reviewed-by: Neha Bhende <bhenden@vmware.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16434>

--- src/gallium/drivers/svga/svga_screen.c
+++ src/gallium/drivers/svga/svga_screen.c
@@ -466,7 +466,7 @@ svga_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_TEXTURE_HALF_FLOAT_LINEAR:
       return 1;
    case PIPE_CAP_TGSI_TEXCOORD:
-      return 1;
+      return sws->have_vgpu10 ? 1 : 0;
    case PIPE_CAP_IMAGE_STORE_FORMATTED:
       return sws->have_gl43;
    default:
