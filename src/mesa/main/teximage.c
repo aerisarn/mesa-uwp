aerisarn/mesa-uwp

commit 89c94502b6650fed222abd3588e9c927811580aa

mesa: consider the sample count when choosing a texture format
The set of supported MSAA formats can be smaller than the set of supported
non-MSAA formats.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16326>

src/mesa/main/teximage.c
@@ -2821,7 +2821,8 @@ mesa_format
 _mesa_choose_texture_format(struct gl_context *ctx,
                             struct gl_texture_object *texObj,
                             GLenum target, GLint level,
-                            GLenum internalFormat, GLenum format, GLenum type)
+                            GLenum internalFormat, GLenum format, GLenum type,
+                            unsigned samples)
 {
    mesa_format f;
 
@@ -2841,8 +2842,27 @@ _mesa_choose_texture_format(struct gl_context *ctx,
       }
    }
 
-   f = st_ChooseTextureFormat(ctx, target, internalFormat,
-                              format, type);
+   if (samples > 0) {
+      /* TODO: This somewhat duplicates the logic in st_texture_storage. */
+      /* Find msaa sample count which is actually supported.  For example,
+       * if the user requests 1x but only 4x or 8x msaa is supported, we'll
+       * choose 4x here.
+       */
+      if (ctx->Const.MaxSamples > 1 && samples == 1) {
+         /* don't try num_samples = 1 with drivers that support real msaa */
+         samples = 2;
+      }
+
+      for (; samples <= ctx->Const.MaxSamples; samples++) {
+         f = st_ChooseTextureFormat(ctx, target, internalFormat,
+                                    format, type, samples);
+         if (f != PIPE_FORMAT_NONE)
+            break;
+      }
+   } else {
+      f = st_ChooseTextureFormat(ctx, target, internalFormat,
+                                 format, type, samples);
+   }
    assert(f != MESA_FORMAT_NONE);
    return f;
 }
@@ -3073,7 +3093,7 @@ teximage(struct gl_context *ctx, GLboolean compressed, GLuint dims,
       }
 
       texFormat = _mesa_choose_texture_format(ctx, texObj, target, level,
-                                              internalFormat, format, type);
+                                              internalFormat, format, type, 0);
    }
 
    assert(texFormat != MESA_FORMAT_NONE);
@@ -4312,7 +4332,7 @@ copyteximage(struct gl_context *ctx, GLuint dims, struct gl_texture_object *texO
    assert(texObj);
 
    texFormat = _mesa_choose_texture_format(ctx, texObj, target, level,
-                                           internalFormat, GL_NONE, GL_NONE);
+                                           internalFormat, GL_NONE, GL_NONE, 0);
 
    /* First check if reallocating the texture buffer can be avoided.
     * Without the realloc the copy can be 20x faster.
@@ -6832,7 +6852,7 @@ texture_image_multisample(struct gl_context *ctx, GLuint dims,
    }
 
    texFormat = _mesa_choose_texture_format(ctx, texObj, target, 0,
-         internalformat, GL_NONE, GL_NONE);
+         internalformat, GL_NONE, GL_NONE, samples);
    assert(texFormat != MESA_FORMAT_NONE);
 
    dimensionsOK = _mesa_legal_texture_dimensions(ctx, target, 0,

src/mesa/main/formatquery.c
@@ -997,7 +997,8 @@ _mesa_GetInternalformativ(GLenum target, GLenum internalformat, GLenum pname,
        * same format-choice logic as for textures.
        */
       texformat = st_ChooseTextureFormat(ctx, target, internalformat,
-                                         GL_NONE /*format */, GL_NONE /* type */);
+                                         GL_NONE /*format */, GL_NONE /* type */,
+                                         0);
 
       if (texformat == MESA_FORMAT_NONE || baseformat <= 0)
          goto end;

src/mesa/main/teximage.h
@@ -144,7 +144,8 @@ extern mesa_format
 _mesa_choose_texture_format(struct gl_context *ctx,
                             struct gl_texture_object *texObj,
                             GLenum target, GLint level,
-                            GLenum internalFormat, GLenum format, GLenum type);
+                            GLenum internalFormat, GLenum format, GLenum type,
+                            unsigned samples);
 
 extern void
 _mesa_update_fbo_texture(struct gl_context *ctx,

src/mesa/main/texobj.c
@@ -1018,7 +1018,7 @@ _mesa_get_fallback_texture(struct gl_context *ctx, gl_texture_index tex)
 
       texFormat = st_ChooseTextureFormat(ctx, target,
                                          GL_RGBA, GL_RGBA,
-                                         GL_UNSIGNED_BYTE);
+                                         GL_UNSIGNED_BYTE, 0);
 
       /* need a loop here just for cube maps */
       for (face = 0; face < numFaces; face++) {

src/mesa/main/texstorage.c
@@ -455,7 +455,7 @@ texture_storage(struct gl_context *ctx, GLuint dims,
    }
 
    texFormat = _mesa_choose_texture_format(ctx, texObj, target, 0,
-                                           internalformat, GL_NONE, GL_NONE);
+                                           internalformat, GL_NONE, GL_NONE, 0);
 
    if (!no_error) {
       /* check that width, height, depth are legal for the mipmap level */

src/mesa/main/textureview.c
@@ -541,7 +541,7 @@ texture_view(struct gl_context *ctx, struct gl_texture_object *origTexObj,
    GLenum faceTarget;
 
    texFormat = _mesa_choose_texture_format(ctx, texObj, target, 0,
-                                           internalformat, GL_NONE, GL_NONE);
+                                           internalformat, GL_NONE, GL_NONE, 0);
    if (texFormat == MESA_FORMAT_NONE) return;
 
    newViewNumLevels = MIN2(numlevels, origTexObj->Attrib.NumLevels - minlevel);

src/mesa/state_tracker/st_cb_drawpixels.c
@@ -629,7 +629,7 @@ make_texture(struct st_context *st,
     * image to draw.
     */
    pipeFormat = st_choose_matching_format(st, PIPE_BIND_SAMPLER_VIEW,
-                                          format, type, unpack->SwapBytes);
+                                          format, type, 0, 0, unpack->SwapBytes);
 
    if (pipeFormat == PIPE_FORMAT_NONE) {
       /* Use the generic approach. */

src/mesa/state_tracker/st_cb_readpixels.c
@@ -476,7 +476,7 @@ st_ReadPixels(struct gl_context *ctx, GLint x, GLint y,
 
    /* Choose the destination format by finding the best match
     * for the format+type combo. */
-   dst_format = st_choose_matching_format(st, bind, format, type,
+   dst_format = st_choose_matching_format(st, bind, format, type, 0, 0,
                                           pack->SwapBytes);
    if (dst_format == PIPE_FORMAT_NONE) {
       goto fallback;

commit 2e3cf3a6a60fc4a067f7cfc74c76e28216cef88f

mesa: Make sure to fallback to handling the original choose texture format
It is possible for st_ChooseTextureFormat to return MESA_FORMAT_NONE when
samples is forced to 2. Always allow to fallback to the original case.

Fixes: 89c94502b6650fed222abd3588e9c927811580aa
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6441
Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16432>

src/mesa/main/teximage.c
@@ -2824,7 +2824,7 @@ _mesa_choose_texture_format(struct gl_context *ctx,
                             GLenum internalFormat, GLenum format, GLenum type,
                             unsigned samples)
 {
-   mesa_format f;
+   mesa_format f = MESA_FORMAT_NONE;
 
    /* see if we've already chosen a format for the previous level */
    if (level > 0) {
@@ -2843,6 +2843,7 @@ _mesa_choose_texture_format(struct gl_context *ctx,
    }
 
    if (samples > 0) {
+      unsigned trySamples = samples;
       /* TODO: This somewhat duplicates the logic in st_texture_storage. */
       /* Find msaa sample count which is actually supported.  For example,
        * if the user requests 1x but only 4x or 8x msaa is supported, we'll
@@ -2850,19 +2851,22 @@ _mesa_choose_texture_format(struct gl_context *ctx,
        */
       if (ctx->Const.MaxSamples > 1 && samples == 1) {
          /* don't try num_samples = 1 with drivers that support real msaa */
-         samples = 2;
+         trySamples = 2;
       }
 
-      for (; samples <= ctx->Const.MaxSamples; samples++) {
+      for (; trySamples <= ctx->Const.MaxSamples; trySamples++) {
          f = st_ChooseTextureFormat(ctx, target, internalFormat,
-                                    format, type, samples);
-         if (f != PIPE_FORMAT_NONE)
+                                    format, type, trySamples);
+         if (f != MESA_FORMAT_NONE)
             break;
       }
-   } else {
+   }
+
+   if (f == MESA_FORMAT_NONE) {
       f = st_ChooseTextureFormat(ctx, target, internalFormat,
                                  format, type, samples);
    }
+
    assert(f != MESA_FORMAT_NONE);
    return f;
 }