d51dd7527b41b89c059950b2f8cfdc9026330827
Rhys Perry <pendingchaos02@gmail.com>
2022-05-13 15:27:31 +0100

    aco/tests: fix gfx11 variants printed as gfx12
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16595>

--- src/amd/compiler/tests/framework.h
+++ src/amd/compiler/tests/framework.h
@@ -47,7 +47,7 @@ inline bool set_variant(amd_gfx_level cls, const char *rest="")
 {
    char buf[8+strlen(rest)];
    if (cls != GFX10_3) {
-      snprintf(buf, sizeof(buf), "gfx%d%s", cls - GFX6 + 6, rest);
+      snprintf(buf, sizeof(buf), "gfx%d%s", cls - GFX6 + 6 - (cls > GFX10_3), rest);
    } else {
       snprintf(buf, sizeof(buf), "gfx10_3%s", rest);
    }
