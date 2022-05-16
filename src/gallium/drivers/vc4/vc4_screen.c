77392e49833e1c5efbbe8251abef5f3f9f11b458
Timothy Arceri <tarceri@itsqueeze.com>
2022-05-16 16:41:48 +1000

    vc4: disable GLSL IR loop unrolling
    NIR loop unrolling is already enabled so just let it do its job.
    
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16543>

--- src/gallium/drivers/vc4/vc4_screen.c
+++ src/gallium/drivers/vc4/vc4_screen.c
@@ -312,7 +312,6 @@ vc4_screen_get_shader_param(struct pipe_screen *pscreen,
         case PIPE_SHADER_CAP_SUPPORTED_IRS:
                 return 1 << PIPE_SHADER_IR_NIR;
         case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
-                return 32;
         case PIPE_SHADER_CAP_MAX_SHADER_BUFFERS:
         case PIPE_SHADER_CAP_MAX_SHADER_IMAGES:
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
