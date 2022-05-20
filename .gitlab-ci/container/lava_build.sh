3d7b2f0186cca878ec0eb54caf0b54d3c0f4defe
Erico Nunes <nunes.erico@gmail.com>
2022-05-20 17:21:21 +0200

    ci: update linux kernel to v5.17
    In addition to pushing it to the current latest stable, the v5.17 kernel
    for mesa CI pulls a patch to address a regression in drm that affects at
    least the lima jobs.
    
    The dtb for sc7180-trogdor-lazor-limozeen-nots is also updated since the
    old one no longer exists in v5.17.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16641>

--- .gitlab-ci/container/lava_build.sh
+++ .gitlab-ci/container/lava_build.sh
@@ -36,7 +36,7 @@ if [[ "$DEBIAN_ARCH" = "arm64" ]]; then
     DEVICE_TREES+=" arch/arm64/boot/dts/amlogic/meson-g12b-a311d-khadas-vim3.dtb"
     DEVICE_TREES+=" arch/arm64/boot/dts/mediatek/mt8183-kukui-jacuzzi-juniper-sku16.dtb"
     DEVICE_TREES+=" arch/arm64/boot/dts/nvidia/tegra210-p3450-0000.dtb"
-    DEVICE_TREES+=" arch/arm64/boot/dts/qcom/sc7180-trogdor-lazor-limozeen-nots.dtb"
+    DEVICE_TREES+=" arch/arm64/boot/dts/qcom/sc7180-trogdor-lazor-limozeen-nots-r5.dtb"
     KERNEL_IMAGE_NAME="Image"
 
 elif [[ "$DEBIAN_ARCH" = "armhf" ]]; then
