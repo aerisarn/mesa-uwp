583a4210e5aaf4c3eeaca4f5f6aa9758407dbf50
Corentin Noël <corentin.noel@collabora.com>
2022-05-20 10:24:08 +0200

    virgl/ci: Update virglrenderer
    Update virglrenderer to the latest version on time.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16627>

--- .gitlab-ci/container/build-crosvm.sh
+++ .gitlab-ci/container/build-crosvm.sh
@@ -13,7 +13,7 @@ git submodule update --init
 cat "$SCRIPT_DIR"/.gitlab-ci/container/build-crosvm_*.patch |
     patch -p1
 
-VIRGLRENDERER_VERSION=0564c9a0c2f584e004a7d4864aee3b8ec9692105
+VIRGLRENDERER_VERSION=1c311dc0eb4a6477ebf695ff430021adabddd518
 rm -rf third_party/virglrenderer
 git clone --single-branch -b master --no-checkout https://gitlab.freedesktop.org/virgl/virglrenderer.git third_party/virglrenderer
 pushd third_party/virglrenderer
