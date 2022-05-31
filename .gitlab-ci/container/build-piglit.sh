abe4536c51ea24443542028a0817b5eac2470559
Timothy Arceri <tarceri@itsqueeze.com>
2022-05-31 13:36:34 +1000

    ci: uprev piglit 2022-05-31
    Also document additional piglit failures and passes.
    
    Multiple changes, mostly notable:
      - few new tests
      - fixed test for upcoming mesa MR
    
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16785>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -4,7 +4,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 33c7c3c313230be47decc221c57addc59ac939dc
+git checkout 5e7d4a7b1df385f1e2b08b2b81d75f039e8be64b
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
