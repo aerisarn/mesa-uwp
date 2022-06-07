5dc30f93428c47c85c8f4cba1e739ed2d7436d29
Erik Faye-Lund <erik.faye-lund@collabora.com>
2022-06-07 16:29:39 +0200

    util: sort includes
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16908>

--- src/util/ralloc.c
+++ src/util/ralloc.c
@@ -22,11 +22,11 @@
  */
 
 #include <assert.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "util/macros.h"
 #include "util/u_math.h"
