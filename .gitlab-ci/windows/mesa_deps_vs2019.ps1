d960151dc2a4dc925fdec8337955ce5b3f565135
Yonggang Luo <luoyonggang@gmail.com>
2022-06-08 11:35:59 +0800

    ci/vs2019: Split call to C:\vs_buildtools.exe into multiple lines
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16934>

--- .gitlab-ci/windows/mesa_deps_vs2019.ps1
+++ .gitlab-ci/windows/mesa_deps_vs2019.ps1
@@ -10,7 +10,22 @@ Invoke-WebRequest -Uri $msvc_2019_url -OutFile C:\vs_buildtools.exe
 
 Get-Date
 Write-Host "Installing Visual Studio 2019"
-Start-Process -NoNewWindow -Wait C:\vs_buildtools.exe -ArgumentList '--wait --quiet --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Graphics.Tools --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --includeRecommended'
+Start-Process -NoNewWindow -Wait -FilePath C:\vs_buildtools.exe `
+-ArgumentList `
+"--wait", `
+"--quiet", `
+"--norestart", `
+"--nocache", `
+"--installPath", "C:\BuildTools", `
+"--add", "Microsoft.VisualStudio.Workload.VCTools", `
+"--add", "Microsoft.VisualStudio.Workload.NativeDesktop", `
+"--add", "Microsoft.VisualStudio.Component.VC.ATL", `
+"--add", "Microsoft.VisualStudio.Component.VC.ATLMFC", `
+"--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64", `
+"--add", "Microsoft.VisualStudio.Component.Graphics.Tools", `
+"--add", "Microsoft.VisualStudio.Component.Windows10SDK.18362", `
+"--includeRecommended"
+
 if (!$?) {
   Write-Host "Failed to install Visual Studio tools"
   Exit 1
