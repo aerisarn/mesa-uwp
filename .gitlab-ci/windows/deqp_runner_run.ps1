672447be7f3d8e88b6f74afecf9f460de8d93842
Boris Brezillon <boris.brezillon@collabora.com>
2022-06-14 01:00:56 -0700

    ci/windows: Allow skipping deqp tests if we have to
    Useful to skip tests that are known to crash.
    
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16971>

--- .gitlab-ci/windows/deqp_runner_run.ps1
+++ .gitlab-ci/windows/deqp_runner_run.ps1
@@ -18,11 +18,12 @@ $deqp_module = "C:\deqp\external\vulkancts\modules\vulkan\deqp-vk.exe"
 $caselist = "C:\deqp\mustpass\vk-master.txt"
 $baseline = ".\_install\warp-fails.txt"
 $flakes = ".\_install\warp-flakes.txt"
+$skips = ".\_install\warp-skips.txt"
 $includes = @("-t", "dEQP-VK.api.*", "-t", "dEQP-VK.binding_model.*", "-t", "dEQP-VK.info.*", "-t", "dEQP-VK.draw.*", "-t", "dEQP-VK.query_pool.*", "-t", "dEQP-VK.memory.*", "-t", "dEQP-VK.pipeline.vertex_input.*")
 
 $env:DZN_DEBUG = "warp"
 $env:MESA_VK_IGNORE_CONFORMANCE_WARNING = "true"
-deqp-runner run --deqp $($deqp_module) --output $($results) --caselist $($caselist) --baseline $($baseline) --flakes $($flakes) $($includes) --testlog-to-xml C:\deqp\executor\testlog-to-xml.exe --jobs 4 --fraction 3  -- $($deqp_options)
+deqp-runner run --deqp $($deqp_module) --output $($results) --caselist $($caselist) --baseline $($baseline) --flakes $($flakes) --skips $($skips) $($includes) --testlog-to-xml C:\deqp\executor\testlog-to-xml.exe --jobs 4 --fraction 3  -- $($deqp_options)
 $deqpstatus = $?
 
 $template = "See https://$($env:CI_PROJECT_ROOT_NAMESPACE).pages.freedesktop.org/-/$($env:CI_PROJECT_NAME)/-/jobs/$($env:CI_JOB_ID)/artifacts/results/{{testcase}}.xml"
