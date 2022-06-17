e3a842d6272798d4515c61a9cc6fa2e0749ded01
Mark Janes <markjanes@swizzler.org>
2022-06-17 10:42:34 -0700

    intel/perf: fix overflow in index types
    With DG2, the number of perf groups and metrics climbs into the
    thousands.  16bit fields are not sufficient for storing metrics
    indices, and the build throws warnings when compiling the generated
    intel_perf_metrics.c
    
    Use a 32bit integer for these values.
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Ivan Briano <ivan.briano@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18893>

--- src/intel/perf/intel_perf_setup.h
+++ src/intel/perf/intel_perf_setup.h
@@ -73,10 +73,10 @@ bdw_query_alloc(struct intel_perf_config *perf, int ncounters)
 }
 
 struct intel_perf_query_counter_data {
-   uint16_t name_idx;
-   uint16_t desc_idx;
-   uint16_t symbol_name_idx;
-   uint16_t category_idx;
+   uint32_t name_idx;
+   uint32_t desc_idx;
+   uint32_t symbol_name_idx;
+   uint32_t category_idx;
    enum intel_perf_counter_type type;
    enum intel_perf_counter_data_type data_type;
    enum intel_perf_counter_units units;
