f075aa1c11c6a8630a81e7168f1ce20f42d378ec
Andres Gomez <agomez@igalia.com>
2022-06-22 14:27:18 +0300

    ci: add wrapper script for Valve's traces runner
    Contributed by Charlie Turner.
    
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Reviewed-by: Martin Roukala <martin.roukala@mupuf.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17184>

--- .gitlab-ci/prepare-artifacts.sh
+++ .gitlab-ci/prepare-artifacts.sh
@@ -34,6 +34,7 @@ cp -Rp .gitlab-ci/fossilize-runner.sh install/
 cp -Rp .gitlab-ci/crosvm-init.sh install/
 cp -Rp .gitlab-ci/*.txt install/
 cp -Rp .gitlab-ci/report-flakes.py install/
+cp -Rp .gitlab-ci/valve install/
 cp -Rp .gitlab-ci/vkd3d-proton install/
 cp -Rp .gitlab-ci/*-runner.sh install/
 find . -path \*/ci/\*.txt \
