aerisarn/mesa-uwp

commit 302cb5c90015e2d3e139d575b3c7165519a21228

aco/ra: remove some redundant code
No fossil-db changes.

Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12836>

src/amd/compiler/aco_register_allocation.cpp
@@ -2351,17 +2351,10 @@ register_allocation(Program* program, std::vector<IDSet>& live_out_per_block, ra
                   if ((*phi_it)->definitions[0].tempId() == pc.first.tempId())
                      prev_phi = phi_it->get();
                }
-               phi_it = instr_it;
-               while (!prev_phi && is_phi(*++phi_it)) {
-                  if ((*phi_it)->definitions[0].tempId() == pc.first.tempId())
-                     prev_phi = phi_it->get();
-               }
                if (prev_phi) {
                   /* if so, just update that phi's register */
-                  register_file.clear(prev_phi->definitions[0]);
                   prev_phi->definitions[0].setFixed(pc.second.physReg());
                   ctx.assignments[prev_phi->definitions[0].tempId()].set(pc.second);
-                  register_file.fill(prev_phi->definitions[0]);
                   continue;
                }
 

commit 84f04fd0800384306347277c4e39524f0b295e34

aco/ra: update register file when updating phi definition
update_renames() fills in the wrong temp id.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Fixes: 302cb5c9001 ("aco/ra: remove some redundant code")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17295>

src/amd/compiler/aco_register_allocation.cpp
@@ -2012,6 +2012,7 @@ get_reg_phi(ra_ctx& ctx, IDSet& live_in, RegisterFile& register_file,
       if (prev_phi) {
          /* if so, just update that phi's register */
          prev_phi->definitions[0].setFixed(pc.second.physReg());
+         register_file.fill(prev_phi->definitions[0]);
          ctx.assignments[prev_phi->definitions[0].tempId()] = {pc.second.physReg(),
                                                                pc.second.regClass()};
          continue;