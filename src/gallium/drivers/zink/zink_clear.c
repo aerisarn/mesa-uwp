d1db07861953e6787745175f275a2fc24a788166
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-07-05 15:27:21 -0400

    zink: remove bogus range tracking from texture clear
    too much copy/paste
    
    Acked-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17366>

--- src/gallium/drivers/zink/zink_clear.c
+++ src/gallium/drivers/zink/zink_clear.c
@@ -421,8 +421,6 @@ zink_clear_texture(struct pipe_context *pctx,
          zink_blit_begin(ctx, ZINK_BLIT_SAVE_FB | ZINK_BLIT_SAVE_FS);
          util_blitter_clear_render_target(ctx->blitter, surf, &color, box->x, box->y, box->width, box->height);
       }
-      if (res->base.b.target == PIPE_BUFFER)
-         util_range_add(&res->base.b, &res->valid_buffer_range, box->x, box->x + box->width);
    } else {
       float depth = 0.0;
       uint8_t stencil = 0;
