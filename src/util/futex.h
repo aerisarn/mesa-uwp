c002bbeb2f7a1bf2d5c020abdddcf25909c447b6
Jesse Natalie <jenatali@microsoft.com>
2022-07-08 11:16:44 -0700

    util: Add a Win32 futex impl
    This uses APIs that are not available on Win7. Since this is a build-time
    configuration, and since we can't use the SDK version as an indicator
    (since you can support Win7 via new SDKs), a new option is added to allow
    disabling it, to maintain Win7 support if desired.
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
    Reviewed-by: Yonggang Luo <luoyonggang@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17431>

--- src/util/futex.h
+++ src/util/futex.h
@@ -117,6 +117,44 @@ static inline int futex_wait(uint32_t *addr, int32_t value, const struct timespe
    return futex(addr, FUTEX_WAIT, value, &tsrel, NULL);
 }
 
+#elif defined(_WIN32) && !defined(WINDOWS_NO_FUTEX)
+#define UTIL_FUTEX_SUPPORTED 1
+
+#include <windows.h>
+#include <stdint.h>
+#include <c11/time.h>
+#include <assert.h>
+#include <errno.h>
+
+static inline int futex_wake(uint32_t *addr, int count)
+{
+   /* All current callers fall into one of these buckets, and we'll get the semantics
+    * wrong if someone tries to be more clever.
+    */
+   assert(count == 1 || count == INT_MAX);
+   if (count == 1)
+      WakeByAddressSingle(addr);
+   else
+      WakeByAddressAll(addr);
+   return count;
+}
+
+static inline int futex_wait(uint32_t *addr, int32_t value, const struct timespec *timeout)
+{
+   DWORD timeout_ms = INFINITE;
+   if (timeout != NULL) {
+      struct timespec tsnow;
+      timespec_get(&tsnow, TIME_UTC);
+
+      timeout_ms = (timeout->tv_sec - tsnow.tv_nsec) * 1000 +
+                   (timeout->tv_nsec - tsnow.tv_nsec) / 1000000;
+   }
+
+   if (WaitOnAddress(addr, &value, sizeof(value), timeout_ms))
+      return 0;
+   return GetLastError() == ERROR_TIMEOUT ? ETIMEDOUT : -1;
+}
+
 #else
 #define UTIL_FUTEX_SUPPORTED 0
 #endif
