83ccc810b41634810680765e1211259adb10f652
Konstantin Seurer <konstantin.seurer@gmail.com>
2022-07-14 13:58:54 +0200

    aco: Remove dead nir_intrinsic_load_vertex_id case
    This intrinsic is lowered in NIR.
    
    Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17539>

--- src/amd/compiler/aco_instruction_selection_setup.cpp
+++ src/amd/compiler/aco_instruction_selection_setup.cpp
@@ -626,7 +626,6 @@ init_context(isel_context* ctx, nir_shader* shader)
                case nir_intrinsic_load_input_vertex:
                case nir_intrinsic_load_per_vertex_input:
                case nir_intrinsic_load_per_vertex_output:
-               case nir_intrinsic_load_vertex_id:
                case nir_intrinsic_load_vertex_id_zero_base:
                case nir_intrinsic_load_barycentric_sample:
                case nir_intrinsic_load_barycentric_pixel:
