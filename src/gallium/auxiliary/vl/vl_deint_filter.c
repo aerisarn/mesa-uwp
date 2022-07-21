74c5d01169d24cdeef60833933df018db9baae79
Sil Vilerino <sivileri@microsoft.com>
2022-07-21 12:41:32 -0400

    vl: Replace usage of entrypoint UNKNOWN with PROCESSING for VP
    Reviewed-by: Boyuan Zhang <Boyuan.Zhang@amd.com>
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17557>

--- src/gallium/auxiliary/vl/vl_deint_filter.c
+++ src/gallium/auxiliary/vl/vl_deint_filter.c
@@ -259,7 +259,7 @@ vl_deint_filter_init(struct vl_deint_filter *filter, struct pipe_context *pipe,
    (
       pipe->screen,
       PIPE_VIDEO_PROFILE_UNKNOWN,
-      PIPE_VIDEO_ENTRYPOINT_UNKNOWN,
+      PIPE_VIDEO_ENTRYPOINT_PROCESSING,
       PIPE_VIDEO_CAP_PREFERED_FORMAT
    );
    templ.width = video_width;
