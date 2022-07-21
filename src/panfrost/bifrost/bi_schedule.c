4f8ee2c00e2b43eafee88c93762d60d281100d03
Alyssa Rosenzweig <alyssa@collabora.com>
2022-07-21 14:21:55 -0400

    pan/bi: Remove trivial dest[0] = bi_null()
    It's already NULL and will interfere with the more comprehensive src/dest
    overwork we're doing.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17794>

--- src/panfrost/bifrost/bi_schedule.c
+++ src/panfrost/bifrost/bi_schedule.c
@@ -2066,7 +2066,6 @@ bi_add_nop_for_atest(bi_context *ctx)
 
         bi_instr *I = rzalloc(ctx, bi_instr);
         I->op = BI_OPCODE_NOP;
-        I->dest[0] = bi_null();
 
         bi_clause *new_clause = ralloc(ctx, bi_clause);
         *new_clause = (bi_clause) {
