Grima04/mesa-turnip-kgsl

commit d6db4d2e08100b12fe6ac539f419768b3b412643

virgl: Add simple disk cache
Since virgl has no backend compilation, this is just a disk cache for the
frontend. As such it is very simple and only implements enough for
get_disk_shader_cache() to work.

With portal2 apitrace:
Before: 100.65 fps
After: 129.051 fps

Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10200>

src/gallium/drivers/virgl/virgl_screen.c
@@ -824,6 +824,9 @@ virgl_destroy_screen(struct pipe_screen *screen)
 
    if (vws)
       vws->destroy(vws);
+
+   disk_cache_destroy(vscreen->disk_cache);
+
    FREE(vscreen);
 }
 
@@ -881,6 +884,28 @@ static void virgl_query_memory_info(struct pipe_screen *screen, struct pipe_memo
    ctx->destroy(ctx);
 }
 
+static struct disk_cache *virgl_get_disk_shader_cache (struct pipe_screen *pscreen)
+{
+   struct virgl_screen *screen = virgl_screen(pscreen);
+
+   return screen->disk_cache;
+}
+
+static void virgl_disk_cache_create(struct virgl_screen *screen)
+{
+   const struct build_id_note *note =
+      build_id_find_nhdr_for_addr(virgl_disk_cache_create);
+   assert(note && build_id_length(note) == 20); /* sha1 */
+
+   const uint8_t *id_sha1 = build_id_data(note);
+   assert(id_sha1);
+
+   char timestamp[41];
+   _mesa_sha1_format(timestamp, id_sha1);
+
+   screen->disk_cache = disk_cache_create("virgl", timestamp, 0);
+}
+
 struct pipe_screen *
 virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *config)
 {
@@ -923,6 +948,7 @@ virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *c
    screen->base.fence_finish = virgl_fence_finish;
    screen->base.fence_get_fd = virgl_fence_get_fd;
    screen->base.query_memory_info = virgl_query_memory_info;
+   screen->base.get_disk_shader_cache = virgl_get_disk_shader_cache;
 
    virgl_init_screen_resource_functions(&screen->base);
 
@@ -937,5 +963,6 @@ virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *c
 
    slab_create_parent(&screen->transfer_pool, sizeof(struct virgl_transfer), 16);
 
+   virgl_disk_cache_create(screen);
    return &screen->base;
 }

src/gallium/drivers/virgl/virgl_screen.h
@@ -25,6 +25,7 @@
 
 #include "pipe/p_screen.h"
 #include "util/slab.h"
+#include "util/disk_cache.h"
 #include "virgl_winsys.h"
 
 enum virgl_debug_flags {
@@ -56,6 +57,8 @@ struct virgl_screen {
    bool tweak_gles_emulate_bgra;
    bool tweak_gles_apply_bgra_dest_swizzle;
    int32_t tweak_gles_tf3_value;
+
+   struct disk_cache *disk_cache;
 };
 
 

aerisarn/mesa-uwp

commit f9703ac34dc12629e9b6fbdda7adc6c297151729

virgl: honor host caps in shader cash sha evaluation
This is needed because when we switch between GLES and GL on the host,
we have to lower atomics to ssbo, and with that the shaders  can't be
pulled from the cache anymore. Likewise when we move the disk image with
a shader cache to a different host, other features might change that
will need lowering. To avoid using stale shaders in this case, merge the
caps into into the shader cache sha.

Fixes: d6db4d2e08100b12fe6ac539f419768b3b412643
    virgl: Add simple disk cache

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17798>

src/gallium/drivers/virgl/virgl_screen.c
@@ -956,6 +956,10 @@ static void virgl_disk_cache_create(struct virgl_screen *screen)
    uint32_t shader_debug_flags = virgl_debug & VIRGL_DEBUG_USE_TGSI;
    _mesa_sha1_update(&sha1_ctx, &shader_debug_flags, sizeof(shader_debug_flags));
 
+   /* When we switch the host the caps might change and then we might have to
+    * apply different lowering. */
+   _mesa_sha1_update(&sha1_ctx, &screen->caps, sizeof(screen->caps));
+
    uint8_t sha1[20];
    _mesa_sha1_final(&sha1_ctx, sha1);
    char timestamp[41];