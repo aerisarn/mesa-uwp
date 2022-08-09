f572961d6937c0adceab39dd883cb53cfa965dc2
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2022-08-10 01:18:16 +0300

    loader/dri3: simplify check for reply
    if reply == NULL, the call `free(reply)` is a no-op, so we can have a
    single check instead of two.
    
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>.
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17974>

--- src/loader/loader_dri3_helper.c
+++ src/loader/loader_dri3_helper.c
@@ -1239,10 +1239,8 @@ loader_dri3_open(xcb_connection_t *conn,
                           provider);
 
    reply = xcb_dri3_open_reply(conn, cookie, NULL);
-   if (!reply)
-      return -1;
 
-   if (reply->nfd != 1) {
+   if (!reply || reply->nfd != 1) {
       free(reply);
       return -1;
    }
