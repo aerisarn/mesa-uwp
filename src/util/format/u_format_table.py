aerisarn/mesa-uwp

commit e342081c3769214024c22fb394ac506008c1fd37

util/format: Assert that formats are valid
It should be the responsibility of the driver to make sure, that "format" is a valid pipe_format.

Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17490>

src/util/format/u_format_table.py
@@ -176,23 +176,19 @@ def write_format_table(formats):
         suffix = ""
         if type == "unpack_":
             suffix = "_generic"
-        print("const struct util_format_%sdescription *" % type)
+        print("ATTRIBUTE_RETURNS_NONNULL const struct util_format_%sdescription *" % type)
         print("util_format_%sdescription%s(enum pipe_format format)" % (type, suffix))
         print("{")
-        print("   if (format >= ARRAY_SIZE(util_format_%sdescriptions))" % (type))
-        print("      return NULL;")
-        print()
+        print("   assert(format < PIPE_FORMAT_COUNT);")
         print("   return &util_format_%sdescriptions[format];" % (type))
         print("}")
         print()
 
     def generate_function_getter(func):
-        print("util_format_%s_func_ptr" % func)
+        print("ATTRIBUTE_RETURNS_NONNULL util_format_%s_func_ptr" % func)
         print("util_format_%s_func(enum pipe_format format)" % (func))
         print("{")
-        print("   if (format >= ARRAY_SIZE(util_format_%s_table))" % (func))
-        print("      return NULL;")
-        print()
+        print("   assert(format < PIPE_FORMAT_COUNT);")
         print("   return util_format_%s_table[format];" % (func))
         print("}")
         print()

commit 075b72ea069863021c51235d46c4c89c859d698b

util: Fixes invalid assumption that return non null by function util_format_fetch_rgba_func
Fixes: e342081c ("util/format: Assert that formats are valid")
Closes #7020

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18024>

src/util/format/u_format_table.py
@@ -185,7 +185,7 @@ def write_format_table(formats):
         print()
 
     def generate_function_getter(func):
-        print("ATTRIBUTE_RETURNS_NONNULL util_format_%s_func_ptr" % func)
+        print("util_format_%s_func_ptr" % func)
         print("util_format_%s_func(enum pipe_format format)" % (func))
         print("{")
         print("   assert(format < PIPE_FORMAT_COUNT);")