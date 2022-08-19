5b7c2dffc43eb7ada61807a6e679cc2c1b3b2682
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-08-19 13:37:38 -0400

    Revert "zink: try to reuse swapchain modifier for dmabuf export"
    This reverts commit 49efa73ba11c4cacaed0052b984e1fb884cf7600.
    
    this happens to work with mesa (sometimes), but it's going to be
    illegal most of the time since swapchain images are treated as though
    they are created with OPTIMAL tiling
    
    cc: mesa-stable
    
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18148>

--- src/gallium/drivers/zink/zink_types.h
+++ src/gallium/drivers/zink/zink_types.h
@@ -1101,7 +1101,6 @@ struct zink_screen {
 
    VkFormatProperties format_props[PIPE_FORMAT_COUNT];
    struct zink_modifier_prop modifier_props[PIPE_FORMAT_COUNT];
-   uint64_t implicit_mod;
    struct {
       uint32_t image_view;
       uint32_t buffer_view;
