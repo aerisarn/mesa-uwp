aerisarn/mesa-uwp

commit 356a60bd6c593d14324dbd9256e6c3ee8be61277

anv: Do not duplicate intel_device_info memory in each logical device
Each logical device can point to its physical device intel_device_info
saving at least one intel_device_info.

This also allow us to set 'const' to avoid values in intel_device_info
being changed by mistake.

Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Jordan Justen <jordan.l.justen@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17897>

src/intel/vulkan/anv_device.c
@@ -2901,7 +2901,7 @@ anv_state_pool_emit_data(struct anv_state_pool *pool, size_t size, size_t align,
 static void
 anv_device_init_border_colors(struct anv_device *device)
 {
-   if (device->info.platform == INTEL_PLATFORM_HSW) {
+   if (device->info->platform == INTEL_PLATFORM_HSW) {
       static const struct hsw_border_color border_colors[] = {
          [VK_BORDER_COLOR_FLOAT_TRANSPARENT_BLACK] =  { .float32 = { 0.0, 0.0, 0.0, 0.0 } },
          [VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK] =       { .float32 = { 0.0, 0.0, 0.0, 1.0 } },
@@ -3271,7 +3271,7 @@ VkResult anv_CreateDevice(
       }
    }
 
-   device->info = physical_device->info;
+   device->info = &physical_device->info;
    device->isl_dev = physical_device->isl_dev;
 
    /* On Broadwell and later, we can use batch chaining to more efficiently
@@ -3279,7 +3279,7 @@ VkResult anv_CreateDevice(
     * command parser gets in the way and we have to fall back to growing
     * the batch.
     */
-   device->can_chain_batches = device->info.ver >= 8;
+   device->can_chain_batches = device->info->ver >= 8;
 
    device->robust_buffer_access = robust_buffer_access;
 
@@ -3327,7 +3327,7 @@ VkResult anv_CreateDevice(
    if (result != VK_SUCCESS)
       goto fail_general_state_pool;
 
-   if (device->info.ver >= 8) {
+   if (device->info->ver >= 8) {
       /* The border color pointer is limited to 24 bits, so we need to make
        * sure that any such color used at any point in the program doesn't
        * exceed that limit.
@@ -3352,7 +3352,7 @@ VkResult anv_CreateDevice(
    if (result != VK_SUCCESS)
       goto fail_instruction_state_pool;
 
-   if (device->info.verx10 >= 125) {
+   if (device->info->verx10 >= 125) {
       /* We're using 3DSTATE_BINDING_TABLE_POOL_ALLOC to give the binding
        * table its own base address separately from surface state base.
        */
@@ -3373,7 +3373,7 @@ VkResult anv_CreateDevice(
    if (result != VK_SUCCESS)
       goto fail_surface_state_pool;
 
-   if (device->info.has_aux_map) {
+   if (device->info->has_aux_map) {
       device->aux_map_ctx = intel_aux_map_init(device, &aux_map_allocator,
                                                &physical_device->info);
       if (!device->aux_map_ctx)
@@ -3404,7 +3404,7 @@ VkResult anv_CreateDevice(
 
    if (device->vk.enabled_extensions.KHR_ray_query) {
       uint32_t ray_queries_size =
-         align_u32(brw_rt_ray_queries_hw_stacks_size(&device->info), 4096);
+         align_u32(brw_rt_ray_queries_hw_stacks_size(device->info), 4096);
 
       result = anv_device_alloc_bo(device, "ray queries",
                                    ray_queries_size,
@@ -3419,11 +3419,11 @@ VkResult anv_CreateDevice(
    if (result != VK_SUCCESS)
       goto fail_ray_query_bo;
 
-   if (device->info.ver >= 12 &&
+   if (device->info->ver >= 12 &&
        device->vk.enabled_extensions.KHR_fragment_shading_rate) {
       uint32_t n_cps_states = 3 * 3; /* All combinaisons of X by Y CP sizes (1, 2, 4) */
 
-      if (device->info.has_coarse_pixel_primitive_and_cb)
+      if (device->info->has_coarse_pixel_primitive_and_cb)
          n_cps_states *= 5 * 5; /* 5 combiners by 2 operators */
 
       n_cps_states += 1; /* Disable CPS */
@@ -3433,12 +3433,12 @@ VkResult anv_CreateDevice(
 
       device->cps_states =
          anv_state_pool_alloc(&device->dynamic_state_pool,
-                              n_cps_states * CPS_STATE_length(&device->info) * 4,
+                              n_cps_states * CPS_STATE_length(device->info) * 4,
                               32);
       if (device->cps_states.map == NULL)
          goto fail_trivial_batch;
 
-      anv_genX(&device->info, init_cps_device_state)(device);
+      anv_genX(device->info, init_cps_device_state)(device);
    }
 
    /* Allocate a null surface state at surface state offset 0.  This makes
@@ -3458,7 +3458,7 @@ VkResult anv_CreateDevice(
    /* TODO(RT): Do we want some sort of data structure for this? */
    memset(device->rt_scratch_bos, 0, sizeof(device->rt_scratch_bos));
 
-   result = anv_genX(&device->info, init_device_state)(device);
+   result = anv_genX(device->info, init_device_state)(device);
    if (result != VK_SUCCESS)
       goto fail_trivial_batch_bo_and_scratch_pool;
 
@@ -3515,7 +3515,7 @@ VkResult anv_CreateDevice(
  fail_workaround_bo:
    anv_device_release_bo(device, device->workaround_bo);
  fail_surface_aux_map_pool:
-   if (device->info.has_aux_map) {
+   if (device->info->has_aux_map) {
       intel_aux_map_finish(device->aux_map_ctx);
       device->aux_map_ctx = NULL;
    }
@@ -3527,7 +3527,7 @@ VkResult anv_CreateDevice(
  fail_instruction_state_pool:
    anv_state_pool_finish(&device->instruction_state_pool);
  fail_dynamic_state_pool:
-   if (device->info.ver >= 8)
+   if (device->info->ver >= 8)
       anv_state_reserved_pool_finish(&device->custom_border_colors);
    anv_state_pool_finish(&device->dynamic_state_pool);
  fail_general_state_pool:
@@ -3583,7 +3583,7 @@ void anv_DestroyDevice(
    /* We only need to free these to prevent valgrind errors.  The backing
     * BO will go away in a couple of lines so we don't actually leak.
     */
-   if (device->info.ver >= 8)
+   if (device->info->ver >= 8)
       anv_state_reserved_pool_finish(&device->custom_border_colors);
    anv_state_pool_free(&device->dynamic_state_pool, device->border_colors);
    anv_state_pool_free(&device->dynamic_state_pool, device->slice_hash);
@@ -3607,7 +3607,7 @@ void anv_DestroyDevice(
    anv_device_release_bo(device, device->workaround_bo);
    anv_device_release_bo(device, device->trivial_batch_bo);
 
-   if (device->info.has_aux_map) {
+   if (device->info->has_aux_map) {
       intel_aux_map_finish(device->aux_map_ctx);
       device->aux_map_ctx = NULL;
    }
@@ -3858,7 +3858,7 @@ VkResult anv_AllocateMemory(
    }
 
    /* By default, we want all VkDeviceMemory objects to support CCS */
-   if (device->physical->has_implicit_ccs && device->info.has_aux_map)
+   if (device->physical->has_implicit_ccs && device->info->has_aux_map)
       alloc_flags |= ANV_BO_ALLOC_IMPLICIT_CCS;
 
    /* If i915 reported a mappable/non_mappable vram regions and the
@@ -4176,7 +4176,7 @@ VkResult anv_MapMemory(
 
    uint32_t gem_flags = 0;
 
-   if (!device->info.has_llc &&
+   if (!device->info->has_llc &&
        (mem->type->propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
       gem_flags |= I915_MMAP_WC;
 
@@ -4660,7 +4660,7 @@ VkResult anv_GetCalibratedTimestampsEXT(
    uint64_t                                     *pMaxDeviation)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   uint64_t timestamp_frequency = device->info.timestamp_frequency;
+   uint64_t timestamp_frequency = device->info->timestamp_frequency;
    int  ret;
    int d;
    uint64_t begin, end;

src/intel/vulkan/anv_private.h
@@ -1157,7 +1157,7 @@ struct anv_device {
     struct vk_device                            vk;
 
     struct anv_physical_device *                physical;
-    struct intel_device_info                      info;
+    const struct intel_device_info *            info;
     struct isl_device                           isl_dev;
     int                                         context_id;
     int                                         fd;
@@ -1596,7 +1596,7 @@ static inline void
 write_reloc(const struct anv_device *device, void *p, uint64_t v, bool flush)
 {
    unsigned reloc_size = 0;
-   if (device->info.ver >= 8) {
+   if (device->info->ver >= 8) {
       reloc_size = sizeof(uint64_t);
       *(uint64_t *)p = intel_canonical_address(v);
    } else {
@@ -3702,7 +3702,7 @@ anv_image_get_fast_clear_type_addr(const struct anv_device *device,
    struct anv_address addr =
       anv_image_get_clear_color_addr(device, image, aspect);
 
-   const unsigned clear_color_state_size = device->info.ver >= 10 ?
+   const unsigned clear_color_state_size = device->info->ver >= 10 ?
       device->isl_dev.ss.clear_color_state_size :
       device->isl_dev.ss.clear_value_size;
    return anv_address_add(addr, clear_color_state_size);
@@ -3800,7 +3800,7 @@ anv_image_plane_uses_aux_map(const struct anv_device *device,
                              const struct anv_image *image,
                              uint32_t plane)
 {
-   return device->info.has_aux_map &&
+   return device->info->has_aux_map &&
       isl_aux_usage_has_ccs(image->planes[plane].aux_usage);
 }
 

src/intel/common/intel_measure.c
@@ -568,7 +568,7 @@ buffered_event_count(struct intel_measure_device *device)
 static void
 print_combined_results(struct intel_measure_device *measure_device,
                        int result_count,
-                       struct intel_device_info *info)
+                       const struct intel_device_info *info)
 {
    if (result_count == 0)
       return;
@@ -619,7 +619,7 @@ print_combined_results(struct intel_measure_device *measure_device,
  */
 static void
 intel_measure_print(struct intel_measure_device *device,
-                    struct intel_device_info *info)
+                    const struct intel_device_info *info)
 {
    while (true) {
       const int events_to_combine = buffered_event_count(device);
@@ -635,7 +635,7 @@ intel_measure_print(struct intel_measure_device *device,
  */
 void
 intel_measure_gather(struct intel_measure_device *measure_device,
-                     struct intel_device_info *info)
+                     const struct intel_device_info *info)
 {
    pthread_mutex_lock(&measure_device->mutex);
 

src/intel/common/intel_measure.h
@@ -164,6 +164,6 @@ bool intel_measure_ready(struct intel_measure_batch *batch);
 
 struct intel_device_info;
 void intel_measure_gather(struct intel_measure_device *device,
-                          struct intel_device_info *info);
+                          const struct intel_device_info *info);
 
 #endif /* INTEL_MEASURE_H */

src/intel/ds/intel_driver_ds.cc
@@ -516,7 +516,7 @@ intel_driver_ds_init(void)
 
 void
 intel_ds_device_init(struct intel_ds_device *device,
-                     struct intel_device_info *devinfo,
+                     const struct intel_device_info *devinfo,
                      int drm_fd,
                      uint32_t gpu_id,
                      enum intel_ds_api api)

src/intel/ds/intel_driver_ds.h
@@ -151,7 +151,7 @@ struct intel_ds_flush_data {
 void intel_driver_ds_init(void);
 
 void intel_ds_device_init(struct intel_ds_device *device,
-                          struct intel_device_info *devinfo,
+                          const struct intel_device_info *devinfo,
                           int drm_fd,
                           uint32_t gpu_id,
                           enum intel_ds_api api);

src/intel/vulkan/anv_allocator.c
@@ -374,7 +374,7 @@ anv_block_pool_init(struct anv_block_pool *pool,
 {
    VkResult result;
 
-   if (device->info.verx10 >= 125) {
+   if (device->info->verx10 >= 125) {
       /* Make sure VMA addresses are 2MiB aligned for the block pool */
       assert(anv_is_aligned(start_address, 2 * 1024 * 1024));
       assert(anv_is_aligned(initial_size, 2 * 1024 * 1024));
@@ -845,7 +845,7 @@ anv_state_pool_init(struct anv_state_pool *pool,
    assert(start_offset < INT32_MAX - (int32_t)BLOCK_POOL_MEMFD_SIZE);
 
    uint32_t initial_size = block_size * 16;
-   if (device->info.verx10 >= 125)
+   if (device->info->verx10 >= 125)
       initial_size = MAX2(initial_size, 2 * 1024 * 1024);
 
    VkResult result = anv_block_pool_init(&pool->block_pool, device, name,
@@ -1455,7 +1455,7 @@ anv_scratch_pool_alloc(struct anv_device *device, struct anv_scratch_pool *pool,
 
    assert(stage < ARRAY_SIZE(pool->bos));
 
-   const struct intel_device_info *devinfo = &device->info;
+   const struct intel_device_info *devinfo = device->info;
 
    /* On GFX version 12.5, scratch access changed to a surface-based model.
     * Instead of each shader type having its own layout based on IDs passed
@@ -1635,13 +1635,13 @@ anv_bo_vma_alloc_or_close(struct anv_device *device,
    uint32_t align = 4096;
 
    /* Gen12 CCS surface addresses need to be 64K aligned. */
-   if (device->info.ver >= 12 && (alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS))
+   if (device->info->ver >= 12 && (alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS))
       align = 64 * 1024;
 
    /* For XeHP, lmem and smem cannot share a single PDE, which means they
     * can't live in the same 2MiB aligned region.
     */
-   if (device->info.verx10 >= 125)
+   if (device->info->verx10 >= 125)
        align = 2 * 1024 * 1024;
 
    if (alloc_flags & ANV_BO_ALLOC_FIXED_ADDRESS) {
@@ -1683,7 +1683,7 @@ anv_device_alloc_bo(struct anv_device *device,
    size = align_u64(size, 4096);
 
    uint64_t ccs_size = 0;
-   if (device->info.has_aux_map && (alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS)) {
+   if (device->info->has_aux_map && (alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS)) {
       /* Align the size up to the next multiple of 64K so we don't have any
        * AUX-TT entries pointing from a 64K page to itself.
        */
@@ -1737,7 +1737,7 @@ anv_device_alloc_bo(struct anv_device *device,
       .is_external = (alloc_flags & ANV_BO_ALLOC_EXTERNAL),
       .has_client_visible_address =
          (alloc_flags & ANV_BO_ALLOC_CLIENT_VISIBLE_ADDRESS) != 0,
-      .has_implicit_ccs = ccs_size > 0 || (device->info.verx10 >= 125 &&
+      .has_implicit_ccs = ccs_size > 0 || (device->info->verx10 >= 125 &&
          (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM)),
    };
 
@@ -1764,7 +1764,7 @@ anv_device_alloc_bo(struct anv_device *device,
        * I915_CACHING_CACHED, which on non-LLC means snooped so there's no
        * need to do this there.
        */
-      if (!device->info.has_llc) {
+      if (!device->info->has_llc) {
          anv_gem_set_caching(device, new_bo.gem_handle,
                              I915_CACHING_CACHED);
       }
@@ -1781,7 +1781,7 @@ anv_device_alloc_bo(struct anv_device *device,
    }
 
    if (new_bo._ccs_size > 0) {
-      assert(device->info.has_aux_map);
+      assert(device->info->has_aux_map);
       intel_aux_map_add_mapping(device->aux_map_ctx,
                                 intel_canonical_address(new_bo.offset),
                                 intel_canonical_address(new_bo.offset + new_bo.size),
@@ -1846,7 +1846,7 @@ anv_device_import_bo_from_host_ptr(struct anv_device *device,
                            ANV_BO_ALLOC_FIXED_ADDRESS)));
 
    assert(!(alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS) ||
-          (device->physical->has_implicit_ccs && device->info.has_aux_map));
+          (device->physical->has_implicit_ccs && device->info->has_aux_map));
 
    struct anv_bo_cache *cache = &device->bo_cache;
    const uint32_t bo_flags =
@@ -1936,7 +1936,7 @@ anv_device_import_bo(struct anv_device *device,
                            ANV_BO_ALLOC_FIXED_ADDRESS)));
 
    assert(!(alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS) ||
-          (device->physical->has_implicit_ccs && device->info.has_aux_map));
+          (device->physical->has_implicit_ccs && device->info->has_aux_map));
 
    struct anv_bo_cache *cache = &device->bo_cache;
    const uint32_t bo_flags =
@@ -2153,7 +2153,7 @@ anv_device_release_bo(struct anv_device *device,
 
    if (bo->_ccs_size > 0) {
       assert(device->physical->has_implicit_ccs);
-      assert(device->info.has_aux_map);
+      assert(device->info->has_aux_map);
       assert(bo->has_implicit_ccs);
       intel_aux_map_unmap_range(device->aux_map_ctx,
                                 intel_canonical_address(bo->offset),

src/intel/vulkan/anv_android.c
@@ -214,7 +214,7 @@ get_ahw_buffer_format_properties2(
       tiling = VK_IMAGE_TILING_LINEAR;
 
    p->formatFeatures =
-      anv_get_image_format_features2(&device->info, p->format, anv_format,
+      anv_get_image_format_features2(device->info, p->format, anv_format,
                                      tiling, NULL);
 
    /* "Images can be created with an external format even if the Android hardware
@@ -530,7 +530,7 @@ anv_image_init_from_gralloc(struct anv_device *device,
    }
    anv_info.isl_tiling_flags = 1u << tiling;
 
-   enum isl_format format = anv_get_isl_format(&device->info,
+   enum isl_format format = anv_get_isl_format(device->info,
                                                base_info->format,
                                                VK_IMAGE_ASPECT_COLOR_BIT,
                                                base_info->tiling);

commit 12bb9cba8bd3e464624cc815950e6ac358d520d8

anv: Fixes struct anv_device::info is not initialized with struct anv_physical_device
Refactoring the function anv_device_set_physical out, so that it's can be called in unittests

Fixes: 356a60bd6c5 ("anv: Do not duplicate intel_device_info memory in each logical device")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7092

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: José Roberto de Souza jose.souza@intel.com
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17928>

src/intel/vulkan/anv_device.c
@@ -3146,7 +3146,7 @@ VkResult anv_CreateDevice(
          INSTRUCTION_STATE_POOL_MIN_ADDRESS;
    }
 
-   device->physical = physical_device;
+   anv_device_set_physical(device, physical_device);
 
    /* XXX(chadv): Can we dup() physicalDevice->fd here? */
    device->fd = open(physical_device->path, O_RDWR | O_CLOEXEC);
@@ -3271,8 +3271,6 @@ VkResult anv_CreateDevice(
       }
    }
 
-   device->info = &physical_device->info;
-   device->isl_dev = physical_device->isl_dev;
 
    /* On Broadwell and later, we can use batch chaining to more efficiently
     * implement growing command buffers.  Prior to Haswell, the kernel

src/intel/vulkan/anv_private.h
@@ -1410,6 +1410,14 @@ VkResult anv_device_set_bo_tiling(struct anv_device *device,
 void anv_device_release_bo(struct anv_device *device,
                            struct anv_bo *bo);
 
+static inline void anv_device_set_physical(struct anv_device *device,
+                                           struct anv_physical_device *physical_device)
+{
+   device->physical = physical_device;
+   device->info = &physical_device->info;
+   device->isl_dev = physical_device->isl_dev;
+}
+
 static inline struct anv_bo *
 anv_device_lookup_bo(struct anv_device *device, uint32_t gem_handle)
 {

src/intel/vulkan/tests/block_pool_grow_first.c
@@ -29,9 +29,7 @@ int main(void)
    struct anv_physical_device physical_device = {
       .use_softpin = true,
    };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_block_pool pool;
 
    /* Create a pool with initial size smaller than the block allocated, so
@@ -40,6 +38,7 @@ int main(void)
    const uint32_t block_size = 16 * 1024;
    const uint32_t initial_size = block_size / 2;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
    anv_block_pool_init(&pool, &device, "test", 4096, initial_size);

src/intel/vulkan/tests/block_pool_no_free.c
@@ -113,11 +113,10 @@ static void run_test()
    struct anv_physical_device physical_device = {
       .use_relocations = true,
    };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_block_pool pool;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
    anv_block_pool_init(&pool, &device, "test", 4096, 4096);

src/intel/vulkan/tests/state_pool.c
@@ -36,11 +36,10 @@
 int main(void)
 {
    struct anv_physical_device physical_device = { };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_state_pool state_pool;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
 

src/intel/vulkan/tests/state_pool_free_list_only.c
@@ -35,11 +35,10 @@
 int main(void)
 {
    struct anv_physical_device physical_device = { };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_state_pool state_pool;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
    anv_state_pool_init(&state_pool, &device, "test", 4096, 0, 4096);

src/intel/vulkan/tests/state_pool_no_free.c
@@ -56,11 +56,10 @@ static void *alloc_states(void *_job)
 static void run_test()
 {
    struct anv_physical_device physical_device = { };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_state_pool state_pool;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
    anv_state_pool_init(&state_pool, &device, "test", 4096, 0, 64);

src/intel/vulkan/tests/state_pool_padding.c
@@ -29,11 +29,10 @@ int main(void)
    struct anv_physical_device physical_device = {
       .use_softpin = true,
    };
-   struct anv_device device = {
-      .physical = &physical_device,
-   };
+   struct anv_device device = {};
    struct anv_state_pool state_pool;
 
+   anv_device_set_physical(&device, &physical_device);
    pthread_mutex_init(&device.mutex, NULL);
    anv_bo_cache_init(&device.bo_cache, &device);
    anv_state_pool_init(&state_pool, &device, "test", 4096, 0, 4096);