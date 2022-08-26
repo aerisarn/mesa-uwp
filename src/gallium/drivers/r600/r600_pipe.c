aerisarn/mesa-uwp

commit 33765aa92aa5c150873fc210e9d6c1fe22cf8646

r600/sfn: Enable NIR for pre RG hardware
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17822>

src/gallium/drivers/r600/r600_pipe.c
@@ -245,8 +245,8 @@ fail:
 }
 
 static bool is_nir_enabled(struct r600_common_screen *screen) {
-   return ((screen->debug_flags & DBG_NIR_PREFERRED) &&
-       screen->family >= CHIP_CEDAR);
+   return (screen->debug_flags & DBG_NIR_PREFERRED); /* &&
+       screen->family >= CHIP_CEDAR);*/
 }
 
 /*
@@ -629,7 +629,7 @@ static int r600_get_shader_param(struct pipe_screen* pscreen,
 		int ir = 0;
 		if (shader == PIPE_SHADER_COMPUTE)
 			ir = 1 << PIPE_SHADER_IR_NATIVE;
-		if (rscreen->b.family >= CHIP_CEDAR) {
+		if (is_nir_enabled(&rscreen->b)) {
 			ir |= 1 << PIPE_SHADER_IR_TGSI;
 			ir |= 1 << PIPE_SHADER_IR_NIR;
 		}

commit 90f99369aeccf2a52495319930ff23b8ffa2a691

r600: Fix reporting TGSI IR support
When NIR is not explicitely enabled we still support TGSI.

Fixes: 33765aa92aa5c150873fc210e9d6c1fe22cf8646
    r600/sfn: Enable NIR for pre RG hardware

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18212>

src/gallium/drivers/r600/r600_pipe.c
@@ -629,8 +629,8 @@ static int r600_get_shader_param(struct pipe_screen* pscreen,
 		int ir = 0;
 		if (shader == PIPE_SHADER_COMPUTE)
 			ir = 1 << PIPE_SHADER_IR_NATIVE;
+		ir |= 1 << PIPE_SHADER_IR_TGSI;
 		if (is_nir_enabled(&rscreen->b)) {
-			ir |= 1 << PIPE_SHADER_IR_TGSI;
 			ir |= 1 << PIPE_SHADER_IR_NIR;
 		}
 		return ir;