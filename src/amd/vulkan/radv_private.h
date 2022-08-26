2f9b30f30c80dc5c6e60e6d023dee7a24ce4ec5a
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-08-26 12:03:11 +0200

    radv: move computing NGG info and GS info to radv_nir_shader_info_link()
    It's a link step somehow, except for VS only on GFX10+ but keep it
    there anyways.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18278>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -2804,7 +2804,7 @@ void radv_nir_shader_info_init(struct radv_shader_info *info);
 
 void radv_nir_shader_info_link(struct radv_device *device,
                                const struct radv_pipeline_key *pipeline_key,
-                               struct radv_pipeline_stage *stages,
+                               struct radv_pipeline_stage *stages, bool pipeline_has_ngg,
                                gl_shader_stage last_vgt_api_stage);
 
 bool radv_thread_trace_init(struct radv_device *device);
