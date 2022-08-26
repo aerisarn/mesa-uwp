aerisarn/mesa-uwp

commit 53445284a427f79e94607dc4ca2f8bd8ac293356

etnaviv: add linear PE support
GPUs with the LINEAR_PE feature bit have the ability to render into linear
buffers. While this decreases PE cache effectiveness and is thus slower than
rendering into a (super-)tiled buffer, it's still preferable for cases where
we would need a blit to get into linear otherwise, i.e. when importing a
linear buffer or when linear is forced on allocation by usage flags or
modifiers.

Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16615>

src/gallium/drivers/etnaviv/etnaviv_surface.c
@@ -48,10 +48,11 @@ etna_render_handle_incompatible(struct pipe_context *pctx, struct pipe_resource 
    bool need_multitiled = screen->specs.pixel_pipes > 1 && !screen->specs.single_buffer;
    bool want_supertiled = screen->specs.can_supertile;
 
-   /* Resource is compatible if it is tiled and has multi tiling when required
-    * TODO: LINEAR_PE feature means render to linear is possible ?
+   /* Resource is compatible if it is tiled or PE is able to render to linear
+    * and has multi tiling when required.
     */
-   if (res->layout != ETNA_LAYOUT_LINEAR &&
+   if ((res->layout != ETNA_LAYOUT_LINEAR ||
+        VIV_FEATURE(screen, chipMinorFeatures2, LINEAR_PE)) &&
        (!need_multitiled || (res->layout & ETNA_LAYOUT_BIT_MULTI)))
       return res;
 

src/gallium/drivers/etnaviv/etnaviv_state.c
@@ -136,6 +136,7 @@ etna_set_framebuffer_state(struct pipe_context *pctx,
    int nr_samples_color = -1;
    int nr_samples_depth = -1;
    bool target_16bpp = false;
+   bool target_linear = false;
 
    /* Set up TS as well. Warning: this state is used by both the RS and PE */
    uint32_t ts_mem_config = 0;
@@ -148,8 +149,12 @@ etna_set_framebuffer_state(struct pipe_context *pctx,
       bool color_supertiled = (res->layout & ETNA_LAYOUT_BIT_SUPER) != 0;
       uint32_t fmt = translate_pe_format(cbuf->base.format);
 
-      assert(res->layout & ETNA_LAYOUT_BIT_TILE); /* Cannot render to linear surfaces */
+      assert((res->layout & ETNA_LAYOUT_BIT_TILE) ||
+             VIV_FEATURE(screen, chipMinorFeatures2, LINEAR_PE));
       etna_update_render_resource(pctx, etna_resource(cbuf->prsc));
+
+      if (res->layout == ETNA_LAYOUT_LINEAR)
+         target_linear = true;
 
       if (fmt >= PE_FORMAT_R16F)
           cs->PE_COLOR_FORMAT = VIVS_PE_COLOR_FORMAT_FORMAT_EXT(fmt) |
@@ -366,7 +371,9 @@ etna_set_framebuffer_state(struct pipe_context *pctx,
     * one per color buffer / depth buffer. To keep the logic simple always use
     * single buffer when this feature is available.
     */
-   if (screen->specs.single_buffer)
+   if (unlikely(target_linear))
+      pe_logic_op |= VIVS_PE_LOGIC_OP_SINGLE_BUFFER(1);
+   else if (screen->specs.single_buffer)
       pe_logic_op |= VIVS_PE_LOGIC_OP_SINGLE_BUFFER(target_16bpp ? 3 : 2);
    cs->PE_LOGIC_OP = pe_logic_op;
 

commit ea8fc9592c16ce137d58a27be8f30f530efcc524

etnaviv: use linear PE rendering only on properly aligned surfaces
When linear rendering is used together with TS, the color tiles must be fully
contained in a single row of pixels. When wrapping around to the next row
TS gets confused and records wrong tile status information, leading to visual
corruption when the surface is resolved/decompressed.

The corruption can be fixed by increasing the stride alignment for linear
render targets, but that would break some existing use-cases, as some display
engines used together with Vivante GPUs currently don't support strides that
don't match the horizontal display resolution.

For now only enable linear PE rendering when the surface is properly aligned
already. This allows to use the optimization in a lot of common use-cases, but
falls back to the proven tiled rendering with subsequent resolve into linear
for the problematic cases.

CC: mesa-stable #22.2
Fixes: 53445284a42 ("etnaviv: add linear PE support")
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Tested-by: Guido Günther <agx@sigxcpu.org>
Reviewed-by: Guido Günther <agx@sigxcpu.org>
Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18232>

src/gallium/drivers/etnaviv/etnaviv_resource.c
@@ -103,7 +103,9 @@ etna_screen_resource_alloc_ts(struct pipe_screen *pscreen,
     * the size of the resource might also determine if we want to use it or not
     */
    if (VIV_FEATURE(screen, chipMinorFeatures6, CACHE128B256BPERLINE) &&
-       ts_compress_fmt >= 0)
+       ts_compress_fmt >= 0 &&
+       (rsc->layout != ETNA_LAYOUT_LINEAR ||
+        rsc->levels[0].stride % 256 == 0) )
          ts_mode = TS_MODE_256B;
 
    layers = prsc->target == PIPE_TEXTURE_3D ? prsc->depth0 : prsc->array_size;

src/gallium/drivers/etnaviv/etnaviv_surface.c
@@ -41,19 +41,24 @@
 #include "drm-uapi/drm_fourcc.h"
 
 static struct etna_resource *
-etna_render_handle_incompatible(struct pipe_context *pctx, struct pipe_resource *prsc)
+etna_render_handle_incompatible(struct pipe_context *pctx,
+                                struct pipe_resource *prsc,
+                                unsigned int level)
 {
    struct etna_context *ctx = etna_context(pctx);
    struct etna_screen *screen = ctx->screen;
    struct etna_resource *res = etna_resource(prsc);
    bool need_multitiled = screen->specs.pixel_pipes > 1 && !screen->specs.single_buffer;
    bool want_supertiled = screen->specs.can_supertile;
+   unsigned int min_tilesize = etna_screen_get_tile_size(screen, TS_MODE_128B);
 
    /* Resource is compatible if it is tiled or PE is able to render to linear
     * and has multi tiling when required.
     */
    if ((res->layout != ETNA_LAYOUT_LINEAR ||
-        VIV_FEATURE(screen, chipMinorFeatures2, LINEAR_PE)) &&
+        (VIV_FEATURE(screen, chipMinorFeatures2, LINEAR_PE) &&
+         (!VIV_FEATURE(screen, chipFeatures, FAST_CLEAR) ||
+          res->levels[level].stride % min_tilesize == 0))) &&
        (!need_multitiled || (res->layout & ETNA_LAYOUT_BIT_MULTI)))
       return res;
 
@@ -81,15 +86,15 @@ etna_create_surface(struct pipe_context *pctx, struct pipe_resource *prsc,
 {
    struct etna_context *ctx = etna_context(pctx);
    struct etna_screen *screen = ctx->screen;
-   struct etna_resource *rsc = etna_render_handle_incompatible(pctx, prsc);
+   unsigned layer = templat->u.tex.first_layer;
+   unsigned level = templat->u.tex.level;
+   struct etna_resource *rsc = etna_render_handle_incompatible(pctx, prsc, level);
    struct etna_surface *surf = CALLOC_STRUCT(etna_surface);
 
    if (!surf)
       return NULL;
 
    assert(templat->u.tex.first_layer == templat->u.tex.last_layer);
-   unsigned layer = templat->u.tex.first_layer;
-   unsigned level = templat->u.tex.level;
    assert(layer <= util_max_layer(prsc, level));
 
    surf->base.context = pctx;