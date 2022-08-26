aerisarn/mesa-uwp

commit 79ca456b4837b3bc21cf9ef3c03c505c4b4909f6

r600/sfn: rewrite NIR backend
This is a rewite of the NIR backend. it adds some optimization
and a scheduler.

v2: - replace some magic numbers by constants
    - make sure constructor is always used with new
    - use default initialization in more places
      (changes suggested by Filip Gawin)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17076>

src/gallium/drivers/r600/sfn/sfn_instr.h
@@ -1,0 +1,314 @@
+/* -*- mesa-c++  -*-
+ *
+ * Copyright (c) 2021 Collabora LTD
+ *
+ * Author: Gert Wollny <gert.wollny@collabora.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "sfn_virtualvalues.h"
+#include "sfn_alu_defines.h"
+#include "sfn_defines.h"
+#include <set>
+#include <list>
+#include <iostream>
+
+namespace r600 {
+
+class ConstInstrVisitor;
+
+class InstrVisitor;
+class AluInstr;
+class AluGroup;
+class TexInstr;
+class ExportInstr;
+class FetchInstr;
+class ControlFlowInstr;
+class IfInstr;
+class WriteScratchInstr;
+class StreamOutInstr;
+class MemRingOutInstr;
+class EmitVertexInstr;
+class GDSInstr;
+class WriteTFInstr;
+class LDSAtomicInstr;
+class LDSReadInstr;
+class RatInstr;
+
+
+int int_from_string_with_prefix(const std::string& str, const std::string& prefix);
+int sel_and_szw_from_string(const std::string& str, RegisterVec4::Swizzle& swz, bool& is_ssa);
+
+class Instr : public Allocate {
+public:
+
+   enum Flags {
+      always_keep,
+      dead,
+      scheduled,
+      vpm,
+      force_cf,
+      ack_rat_return_write,
+      nflags
+      };
+
+   Instr();
+
+   Instr(const Instr& orig) = default;
+
+   virtual ~Instr();
+
+   using Pointer = R600_POINTER_TYPE(Instr);
+
+   void print(std::ostream& os) const;
+   bool equal_to(const Instr& lhs) const;
+
+   virtual void accept(ConstInstrVisitor& visitor) const = 0;
+   virtual void accept(InstrVisitor& visitor) = 0;
+   virtual bool end_group() const { return true;}
+
+   virtual bool is_last() const;
+
+   void set_always_keep() {m_instr_flags.set(always_keep);}
+   bool set_dead();
+   virtual void set_scheduled() { m_instr_flags.set(scheduled); forward_set_scheduled();}
+   void add_use() {++m_use_count;}
+   void dec_use() {assert(m_use_count > 0); --m_use_count;}
+   bool is_dead() const {return m_instr_flags.test(dead);}
+   bool is_scheduled() const {return m_instr_flags.test(scheduled);}
+   bool keep() const {return m_instr_flags.test(always_keep);}
+   bool has_uses() const {return m_use_count > 0;}
+
+   bool has_instr_flag(Flags f) const  {return m_instr_flags.test(f);}
+   void set_instr_flag(Flags f) { m_instr_flags.set(f);}
+
+   virtual bool replace_source(PRegister old_src, PVirtualValue new_src);
+   virtual bool replace_dest(PRegister new_dest, AluInstr *move_instr);
+
+   virtual int nesting_corr() const { return 0;}
+
+   virtual bool end_block() const { return false;}
+   virtual int nesting_offset() const { return 0;}
+
+   void set_blockid(int id, int index);
+   int block_id() const {return m_block_id;}
+   int index() const { return m_index;}
+
+   void add_required_instr(Instr *instr);
+   void replace_required_instr(Instr *old_instr, Instr *new_instr);
+
+   bool ready() const;
+
+   virtual uint32_t slots() const {return 0;};
+
+   using InstrList = std::list<Instr *, Allocator<Instr *>>;
+
+   const InstrList& dependend_instr() { return m_dependend_instr;}
+
+protected:
+
+   const InstrList& required_instr() const {return m_required_instr; }
+
+private:
+   virtual void forward_set_blockid(int id, int index);
+
+   virtual bool do_ready() const = 0;
+
+   virtual void do_print(std::ostream& os) const = 0;
+   virtual bool propagate_death();
+   virtual void forward_set_scheduled() {}
+
+   InstrList m_required_instr;
+   InstrList m_dependend_instr;
+
+   int m_use_count;
+   int m_block_id;
+   int m_index;
+   std::bitset<nflags> m_instr_flags{0};
+
+};
+using PInst = Instr::Pointer;
+
+class Block : public Instr {
+public:
+
+   enum Type {
+      cf,
+      alu,
+      tex,
+      vtx,
+      gds,
+      unknown
+   };
+
+   using Instructions = std::list<Instr *, Allocator<Instr *>>;
+   using Pointer = R600_POINTER_TYPE(Block);
+   using iterator = Instructions::iterator;
+   using reverse_iterator = Instructions::reverse_iterator;
+   using const_iterator = Instructions::const_iterator;
+
+   Block(int nesting_depth, int id);
+   Block(const Block& orig) = delete;
+
+   void push_back(PInst instr);
+   iterator begin() { return m_instructions.begin(); }
+   iterator end() { return m_instructions.end(); }
+   reverse_iterator rbegin() { return m_instructions.rbegin(); }
+   reverse_iterator rend() { return m_instructions.rend(); }
+
+   const_iterator begin() const { return m_instructions.begin();}
+   const_iterator end() const { return m_instructions.end();}
+
+   bool empty() const { return m_instructions.empty();}
+
+   void erase(iterator node);
+
+   bool is_equal_to(const Block& lhs) const;
+
+   void accept(ConstInstrVisitor& visitor) const override;
+   void accept(InstrVisitor& visitor) override;
+
+   int nesting_depth() const { return m_nesting_depth;}
+
+   int id() const {return m_id;}
+
+   auto type() const {return m_blocK_type; }
+   void set_type(Type t);
+   uint32_t remaining_slots() const { return m_remaining_slots;}
+
+   bool try_reserve_kcache(const AluGroup& group);
+
+   auto last_lds_instr() {return m_last_lds_instr;}
+   void set_last_lds_instr(Instr *instr) {m_last_lds_instr = instr;}
+
+   void lds_group_start(AluInstr *alu);
+   void lds_group_end();
+   bool lds_group_active() { return m_lds_group_start != nullptr;}
+
+   size_t size() const { return m_instructions.size();}
+
+private:
+   bool try_reserve_kcache(const UniformValue& u);
+
+   bool do_ready() const override {return true;};
+   void do_print(std::ostream& os) const override;
+   Instructions m_instructions;
+   int m_nesting_depth;
+   int m_id;
+   int m_next_index;
+
+   Type m_blocK_type{unknown};
+   uint32_t m_remaining_slots{0xffff};
+
+   std::array<KCacheLine, 4> m_kcache;
+
+   Instr *m_last_lds_instr{nullptr};
+
+   int m_lds_group_requirement{0};
+   AluInstr *m_lds_group_start{nullptr};
+};
+
+class InstrWithVectorResult : public Instr {
+public:
+   InstrWithVectorResult(const RegisterVec4& dest, const RegisterVec4::Swizzle& dest_swizzle);
+
+   void set_dest_swizzle(const RegisterVec4::Swizzle& swz) {m_dest_swizzle = swz;}
+   int dest_swizzle(int i) const { return m_dest_swizzle[i];}
+   const RegisterVec4::Swizzle&  all_dest_swizzle() const { return m_dest_swizzle;}
+   const RegisterVec4& dst() const {return m_dest;}
+
+protected:
+   InstrWithVectorResult(const InstrWithVectorResult& orig);
+
+   void print_dest(std::ostream& os) const;
+   bool comp_dest(const RegisterVec4& dest, const RegisterVec4::Swizzle& dest_swizzle) const;
+
+private:
+   RegisterVec4 m_dest;
+   RegisterVec4::Swizzle m_dest_swizzle;
+};
+
+inline bool operator == (const Instr& lhs, const Instr& rhs) {
+   return lhs.equal_to(rhs);
+}
+
+inline bool operator != (const Instr& lhs, const Instr& rhs) {
+   return !(lhs == rhs);
+}
+
+inline std::ostream& operator << (std::ostream& os, const Instr& instr)
+{
+   instr.print(os);
+   return os;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_base_of_v<Instr, T>>>
+std::ostream& operator<<(std::ostream& os, const T& instr) {
+  instr.print(os);
+  return os;
+}
+
+class ConstInstrVisitor {
+public:
+   virtual void visit(const AluInstr& instr) = 0;
+   virtual void visit(const AluGroup& instr) = 0;
+   virtual void visit(const TexInstr& instr) = 0;
+   virtual void visit(const ExportInstr& instr) = 0;
+   virtual void visit(const FetchInstr& instr) = 0;
+   virtual void visit(const Block& instr) = 0;
+   virtual void visit(const ControlFlowInstr& instr) = 0;
+   virtual void visit(const IfInstr& instr) = 0;
+   virtual void visit(const WriteScratchInstr& instr) = 0;
+   virtual void visit(const StreamOutInstr& instr) = 0;
+   virtual void visit(const MemRingOutInstr& instr) = 0;
+   virtual void visit(const EmitVertexInstr& instr) = 0;
+   virtual void visit(const GDSInstr& instr) = 0;
+   virtual void visit(const WriteTFInstr& instr) = 0;
+   virtual void visit(const LDSAtomicInstr& instr) = 0;
+   virtual void visit(const LDSReadInstr& instr) = 0;
+   virtual void visit(const RatInstr& instr) = 0;
+};
+
+class InstrVisitor {
+public:
+   virtual void visit(AluInstr  *instr) = 0;
+   virtual void visit(AluGroup *instr) = 0;
+   virtual void visit(TexInstr *instr) = 0;
+   virtual void visit(ExportInstr *instr) = 0;
+   virtual void visit(FetchInstr *instr) = 0;
+   virtual void visit(Block *instr) = 0;
+   virtual void visit(ControlFlowInstr *instr) = 0;
+   virtual void visit(IfInstr *instr) = 0;
+   virtual void visit(WriteScratchInstr *instr) = 0;
+   virtual void visit(StreamOutInstr *instr) = 0;
+   virtual void visit(MemRingOutInstr *instr) = 0;
+   virtual void visit(EmitVertexInstr *instr) = 0;
+   virtual void visit(GDSInstr *instr) = 0;
+   virtual void visit(WriteTFInstr *instr) = 0;
+   virtual void visit(LDSAtomicInstr *instr) = 0;
+   virtual void visit(LDSReadInstr *instr) = 0;
+   virtual void visit(RatInstr *instr) = 0;
+};
+
+
+} // ns r600

src/gallium/drivers/r600/sfn/sfn_instr_mem.h
@@ -1,0 +1,177 @@
+#ifndef GDSINSTR_H
+#define GDSINSTR_H
+
+#include "sfn_instr.h"
+#include "sfn_valuefactory.h"
+
+namespace r600 {
+
+class Shader;
+
+class GDSInstr : public Instr {
+public:
+
+   GDSInstr(ESDOp op, Register *dest,
+            const RegisterVec4& src, int uav_base,
+            PRegister uav_id);
+
+   bool is_equal_to(const GDSInstr& lhs) const;
+
+   void accept(ConstInstrVisitor& visitor) const override;
+   void accept(InstrVisitor& visitor) override;
+
+   bool do_ready() const override;
+
+   auto opcode() const {return m_op;}
+   auto src() const { return m_src;}
+
+   const auto& dest() const { return m_dest;}
+   auto& dest() { return m_dest;}
+
+   auto uav_id() const {return m_uav_id;}
+   auto uav_base() const {return m_uav_base;}
+
+   static auto from_string(std::istream& is, ValueFactory& value_factory) -> Pointer;
+
+   static bool emit_atomic_counter(nir_intrinsic_instr *intr, Shader& shader);
+   uint32_t slots() const override {return 1;};
+
+private:
+
+   static bool emit_atomic_read(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_atomic_op2(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_atomic_inc(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_atomic_pre_dec(nir_intrinsic_instr *intr, Shader& shader);
+
+   void do_print(std::ostream& os) const override;
+
+   ESDOp m_op{DS_OP_INVALID};
+   Register *m_dest;
+
+   RegisterVec4 m_src;
+
+   int m_uav_base{0};
+   PRegister m_uav_id{nullptr};
+   std::bitset<8> m_tex_flags;
+};
+
+
+class RatInstr : public Instr {
+
+public:
+   enum ERatOp {
+      NOP,
+      STORE_TYPED,
+      STORE_RAW,
+      STORE_RAW_FDENORM,
+      CMPXCHG_INT,
+      CMPXCHG_FLT,
+      CMPXCHG_FDENORM,
+      ADD,
+      SUB,
+      RSUB,
+      MIN_INT,
+      MIN_UINT,
+      MAX_INT,
+      MAX_UINT,
+      AND,
+      OR,
+      XOR,
+      MSKOR,
+      INC_UINT,
+      DEC_UINT,
+      NOP_RTN = 32,
+      XCHG_RTN = 34,
+      XCHG_FDENORM_RTN,
+      CMPXCHG_INT_RTN,
+      CMPXCHG_FLT_RTN,
+      CMPXCHG_FDENORM_RTN,
+      ADD_RTN,
+      SUB_RTN,
+      RSUB_RTN,
+      MIN_INT_RTN,
+      MIN_UINT_RTN,
+      MAX_INT_RTN,
+      MAX_UINT_RTN,
+      AND_RTN,
+      OR_RTN,
+      XOR_RTN,
+      MSKOR_RTN,
+      UINT_RTN,
+      UNSUPPORTED
+   };
+
+   RatInstr(ECFOpCode cf_opcode, ERatOp rat_op,
+            const RegisterVec4& data, const RegisterVec4& index,
+            int rat_id, PRegister rat_id_offset,
+            int burst_count, int comp_mask, int element_size);
+
+   auto rat_id_offset() const { return m_rat_id_offset;}
+   int  rat_id() const { return m_rat_id;}
+
+   ERatOp rat_op() const {return m_rat_op;}
+
+   const auto& value() const { return m_data;}
+   auto& value() { return m_data;}
+
+   const auto& addr() const { return m_index;}
+   auto& addr() { return m_index;}
+
+   int data_gpr() const {return m_data.sel();}
+   int index_gpr() const {return m_index.sel();}
+   int elm_size() const {return m_element_size;}
+
+   int comp_mask() const {return m_comp_mask;}
+
+   bool need_ack() const {return m_need_ack;}
+   int burst_count() const {return m_burst_count;}
+
+   int data_swz(int chan) const {return m_data[chan]->chan();}
+
+   ECFOpCode cf_opcode() const { return m_cf_opcode;}
+
+   void set_ack() {m_need_ack = true; set_mark(); }
+   void set_mark() {m_need_mark = true; }
+   bool mark() {return m_need_mark;}
+
+   void accept(ConstInstrVisitor& visitor) const override;
+   void accept(InstrVisitor& visitor) override;
+
+   bool is_equal_to(const RatInstr& lhs) const;
+
+   static bool emit(nir_intrinsic_instr *intr, Shader& shader);
+
+private:
+
+   static bool emit_ssbo_load(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_ssbo_store(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_ssbo_atomic_op(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_ssbo_size(nir_intrinsic_instr *intr, Shader& shader);
+
+   static bool emit_image_store(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_image_load_or_atomic(nir_intrinsic_instr *intr, Shader& shader);
+   static bool emit_image_size(nir_intrinsic_instr *intr, Shader& shader);
+
+   bool do_ready() const override;
+   void do_print(std::ostream& os) const override;
+
+   ECFOpCode m_cf_opcode;
+   ERatOp m_rat_op;
+
+   RegisterVec4 m_data;
+   RegisterVec4 m_index;
+   PRegister m_rat_id_offset{nullptr};
+
+   int m_rat_id{0};
+   int m_burst_count{0};
+   int m_comp_mask{15};
+   int m_element_size{3};
+   bool m_need_ack{false};
+   bool m_need_mark{false};
+
+};
+
+
+}
+
+#endif // GDSINSTR_H

commit c81fe5b235c0204e9f77d13411004a3307aa6301

r600/sfn: Use a heuristic to keep SSBO setup and store close
When SSBO instructions use constant address values the address loading
is immediately ready, scheduling the address loads early increases
the register pressure, so force a new instruction block to work around
this problem.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6975

Fixes: 79ca456b4837b3bc21cf9ef3c03c505c4b4909f6
   r600/sfn: rewrite NIR backend

v2: do handling in shader block to be thread save (hinted to by Filip)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net> (v1)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18212>

src/gallium/drivers/r600/sfn/sfn_instr.h
@@ -210,6 +210,8 @@ public:
 
    bool kcache_reservation_failed() const { return m_kcache_alloc_failed;}
 
+   int inc_rat_emitted() { return  ++m_emitted_rat_instr;}
+
    static void set_chipclass(r600_chip_class chip_class);
 
 private:
@@ -234,6 +236,7 @@ private:
    int m_lds_group_requirement{0};
    AluInstr *m_lds_group_start{nullptr};
    static unsigned s_max_kcache_banks;
+   int m_emitted_rat_instr{0};
 };
 
 class InstrWithVectorResult : public Instr {

src/gallium/drivers/r600/sfn/sfn_instr_mem.cpp
@@ -559,12 +559,6 @@ bool RatInstr::emit_ssbo_load(nir_intrinsic_instr *intr, Shader& shader)
 
 bool RatInstr::emit_ssbo_store(nir_intrinsic_instr *instr, Shader& shader)
 {
-
-   /* Forche the scheduler to not move the preparation too far away, by starting
-    * a new block (TODO: better priority handling in the scheduler)*/
-   if (nir_src_num_components(instr->src[0]) > 2)
-      shader.start_new_block(0);
-
    auto &vf = shader.value_factory();
    auto orig_addr = vf.src(instr->src[2], 0);
 

src/gallium/drivers/r600/sfn/sfn_instr_mem.h
@@ -194,7 +194,6 @@ private:
    int m_element_size{3};
    bool m_need_ack{false};
    bool m_need_mark{false};
-
 };
 
 

src/gallium/drivers/r600/sfn/sfn_shader.cpp
@@ -1085,6 +1085,9 @@ void Shader::InstructionChain::visit(RatInstr *instr)
 
    if (prepare_mem_barrier)
       instr->set_ack();
+
+   if (this_shader->m_current_block->inc_rat_emitted() > 15)
+      this_shader->start_new_block(0);
 }
 
 void Shader::InstructionChain::apply(Instr *current, Instr **last) {