250c684750533d8260eb2d48abd9b3fd729f5467
David Heidelberg <david.heidelberg@collabora.com>
2022-08-30 11:40:17 +0200

    ci: uprev piglit 2022-08-30
    Acked-by: Juan A. Suarez <jasuarez@igalia.com>  # for broadcom
    Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>  # for zink
    Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18318>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -5,7 +5,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout b2c9d8f56b45d79f804f4cb5ac62520f0edd8988
+git checkout b1266fd57df3fc2574e0c88746985cd475bbecaa
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
