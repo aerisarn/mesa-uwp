aerisarn/mesa-uwp

commit 0ee24c46e0aad500a4ef90cc03cfd319e11cb342

pan/mdg: Only print 2 sources for ALU
..and assert the other sources are null. The one place this might fail in the
future is for real FMA, but we don't support that for GL.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16798>

src/panfrost/midgard/midgard_print.c
@@ -278,9 +278,15 @@ mir_print_instruction(midgard_instruction *ins)
         else
                 mir_print_src(ins, 1);
 
-        for (unsigned c = 2; c <= 3; ++c) {
-                printf(", ");
-                mir_print_src(ins, c);
+        if (is_alu) {
+                /* ALU ops are all 2-src */
+                assert(ins->src[2] == ~0);
+                assert(ins->src[3] == ~0);
+        } else {
+                for (unsigned c = 2; c <= 3; ++c) {
+                        printf(", ");
+                        mir_print_src(ins, c);
+                }
         }
 
         if (ins->no_spill)

commit 6fed6161876681920b0d2c59cb7413d7c43fda45

pan/mdg: Print 3 sources for CSEL
The third source exists logically but not architecturally. We still need to
print it. Caught by the assertion.

Fixes: 0ee24c46e0a ("pan/mdg: Only print 2 sources for ALU")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18338>

src/panfrost/midgard/midgard_print.c
@@ -293,8 +293,22 @@ mir_print_instruction(midgard_instruction *ins)
                 mir_print_src(ins, 1);
 
         if (is_alu) {
-                /* ALU ops are all 2-src */
-                assert(ins->src[2] == ~0);
+                /* ALU ops are all 2-src, though CSEL is treated like a 3-src
+                 * pseudo op with the third source scheduler lowered
+                 */
+                switch (ins->op) {
+                case midgard_alu_op_icsel:
+                case midgard_alu_op_fcsel:
+                case midgard_alu_op_icsel_v:
+                case midgard_alu_op_fcsel_v:
+                        printf(", ");
+                        mir_print_src(ins, 2);
+                        break;
+                default:
+                        assert(ins->src[2] == ~0);
+                        break;
+                }
+
                 assert(ins->src[3] == ~0);
         } else {
                 for (unsigned c = 2; c <= 3; ++c) {