aerisarn/mesa-uwp

commit 9c5a7ffbd8a696182647135ef32bddb347c277f2

venus: fix external memory ext filtering
Fixes: 390722620e1 ("venus: clean up vn_device_fix_create_info")

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17975>

src/virtio/vulkan/vn_device.c
@@ -270,15 +270,23 @@ vn_device_fix_create_info(const struct vn_device *dev,
             extra_exts[extra_count++] =
                VK_EXT_EXTERNAL_MEMORY_DMA_BUF_EXTENSION_NAME;
          }
-         FALLTHROUGH;
+         if (!app_exts->KHR_external_memory_fd) {
+            extra_exts[extra_count++] =
+               VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME;
+         }
+         break;
       case VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT:
+         if (app_exts->EXT_external_memory_dma_buf) {
+            block_exts[block_count++] =
+               VK_EXT_EXTERNAL_MEMORY_DMA_BUF_EXTENSION_NAME;
+         }
          if (!app_exts->KHR_external_memory_fd) {
             extra_exts[extra_count++] =
                VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME;
          }
          break;
       default:
-         /* TODO other handle types */
+         unreachable("unexpected external memory handle type");
          break;
       }
    }

commit 0e0e5d1f204fc7c10a8686fe77cc1c9c0fef54f5

venus: allow no external memory handle when renderers lacks support
This is to make vtest happy.

Fixes: 9c5a7ffbd8a ("venus: fix external memory ext filtering")

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18331>

src/virtio/vulkan/vn_device.c
@@ -273,7 +273,7 @@ vn_device_fix_create_info(const struct vn_device *dev,
          }
          break;
       default:
-         unreachable("unexpected external memory handle type");
+         assert(!physical_dev->instance->renderer->info.has_dma_buf_import);
          break;
       }
    }