86d6c60eab8601ca14334d268b0f55a34df1c194
David Heidelberg <david.heidelberg@collabora.com>
2022-09-05 23:00:44 +0200

    ci/lava: print set-job-env-vars.sh as other setups do
    Let's be consistent.
    Also needed for parsing device and traces yml file from logs.
    
    Reviewed-by: Guilherme Gallo <guilherme.gallo@collabora.com>
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18422>

--- .gitlab-ci/lava/lava-submit.sh
+++ .gitlab-ci/lava/lava-submit.sh
@@ -21,6 +21,8 @@ cp artifacts/ci-common/intel-gpu-freq.sh results/job-rootfs-overlay/
 # Prepare env vars for upload.
 KERNEL_IMAGE_BASE_URL="https://${BASE_SYSTEM_HOST_PATH}" \
 	artifacts/ci-common/generate-env.sh > results/job-rootfs-overlay/set-job-env-vars.sh
+echo "Variables passed through:"
+cat results/job-rootfs-overlay/set-job-env-vars.sh
 
 tar zcf job-rootfs-overlay.tar.gz -C results/job-rootfs-overlay/ .
 ci-fairy minio login --token-file "${CI_JOB_JWT_FILE}"
