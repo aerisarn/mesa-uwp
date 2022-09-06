aerisarn/mesa-uwp

commit 9027c5df4c51bcf529ab34b0ffbe763cf8fe8543

anv: remove the LOCAL_MEM allocation bit
We always want to use local memory if possible, we'll just add the
system memory heap if the buffer needs to be host visible.

v2: Drop some usages of ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17873>

src/intel/vulkan/anv_allocator.c
@@ -509,7 +509,6 @@ anv_block_pool_expand_range(struct anv_block_pool *pool,
                                             pool->name,
                                             new_bo_size,
                                             bo_alloc_flags |
-                                            ANV_BO_ALLOC_LOCAL_MEM |
                                             ANV_BO_ALLOC_FIXED_ADDRESS |
                                             ANV_BO_ALLOC_MAPPED |
                                             ANV_BO_ALLOC_SNOOPED,
@@ -1383,7 +1382,6 @@ anv_bo_pool_alloc(struct anv_bo_pool *pool, uint32_t size,
    VkResult result = anv_device_alloc_bo(pool->device,
                                          pool->name,
                                          pow2_size,
-                                         ANV_BO_ALLOC_LOCAL_MEM |
                                          ANV_BO_ALLOC_MAPPED |
                                          ANV_BO_ALLOC_SNOOPED |
                                          ANV_BO_ALLOC_CAPTURE,
@@ -1490,9 +1488,8 @@ anv_scratch_pool_alloc(struct anv_device *device, struct anv_scratch_pool *pool,
     *
     * so nothing will ever touch the top page.
     */
-   enum anv_bo_alloc_flags alloc_flags = ANV_BO_ALLOC_LOCAL_MEM;
-   if (devinfo->verx10 < 125)
-      alloc_flags |= ANV_BO_ALLOC_32BIT_ADDRESS;
+   const enum anv_bo_alloc_flags alloc_flags =
+      devinfo->verx10 < 125 ? ANV_BO_ALLOC_32BIT_ADDRESS : 0;
    VkResult result = anv_device_alloc_bo(device, "scratch", size,
                                          alloc_flags,
                                          0 /* explicit_address */,
@@ -1668,10 +1665,6 @@ anv_device_alloc_bo(struct anv_device *device,
                     uint64_t explicit_address,
                     struct anv_bo **bo_out)
 {
-   if (!(alloc_flags & ANV_BO_ALLOC_LOCAL_MEM))
-      anv_perf_warn(VK_LOG_NO_OBJS(&device->physical->instance->vk.base),
-                                   "system memory used");
-
    if (!device->physical->has_implicit_ccs)
       assert(!(alloc_flags & ANV_BO_ALLOC_IMPLICIT_CCS));
 
@@ -1702,19 +1695,21 @@ anv_device_alloc_bo(struct anv_device *device,
       struct drm_i915_gem_memory_class_instance regions[2];
       uint32_t nregions = 0;
 
-      if (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM) {
-         /* vram_non_mappable & vram_mappable actually are the same region. */
-         regions[nregions++] = device->physical->vram_non_mappable.region;
-      } else {
-         regions[nregions++] = device->physical->sys.region;
-      }
+      /* This always try to put the object in local memory. Here
+       * vram_non_mappable & vram_mappable actually are the same region.
+       */
+      regions[nregions++] = device->physical->vram_non_mappable.region;
 
+      /* If the buffer is mapped on the host, add the system memory region.
+       * This ensures that if the buffer cannot live in mappable local memory,
+       * it can be spilled to system memory.
+       */
       uint32_t flags = 0;
-      if (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE) {
-         assert(alloc_flags & ANV_BO_ALLOC_LOCAL_MEM);
-         /* We're required to add smem as a region when using mappable vram. */
+      if ((alloc_flags & ANV_BO_ALLOC_MAPPED) ||
+          (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE)) {
          regions[nregions++] = device->physical->sys.region;
-         flags |= I915_GEM_CREATE_EXT_FLAG_NEEDS_CPU_ACCESS;
+         if (device->physical->vram_non_mappable.size > 0)
+            flags |= I915_GEM_CREATE_EXT_FLAG_NEEDS_CPU_ACCESS;
       }
 
       gem_handle = anv_gem_create_regions(device, size + ccs_size,
@@ -1737,8 +1732,7 @@ anv_device_alloc_bo(struct anv_device *device,
       .is_external = (alloc_flags & ANV_BO_ALLOC_EXTERNAL),
       .has_client_visible_address =
          (alloc_flags & ANV_BO_ALLOC_CLIENT_VISIBLE_ADDRESS) != 0,
-      .has_implicit_ccs = ccs_size > 0 || (device->info->verx10 >= 125 &&
-         (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM)),
+      .has_implicit_ccs = ccs_size > 0 || device->info->verx10 >= 125,
    };
 
    if (alloc_flags & ANV_BO_ALLOC_MAPPED) {

src/intel/vulkan/anv_device.c
@@ -3407,8 +3407,7 @@ VkResult anv_CreateDevice(
 
    result = anv_device_alloc_bo(device, "workaround", 4096,
                                 ANV_BO_ALLOC_CAPTURE |
-                                ANV_BO_ALLOC_MAPPED |
-                                ANV_BO_ALLOC_LOCAL_MEM,
+                                ANV_BO_ALLOC_MAPPED,
                                 0 /* explicit_address */,
                                 &device->workaround_bo);
    if (result != VK_SUCCESS)
@@ -3433,7 +3432,7 @@ VkResult anv_CreateDevice(
 
       result = anv_device_alloc_bo(device, "ray queries",
                                    ray_queries_size,
-                                   ANV_BO_ALLOC_LOCAL_MEM,
+                                   0,
                                    0 /* explicit_address */,
                                    &device->ray_query_bo);
       if (result != VK_SUCCESS)
@@ -3996,12 +3995,6 @@ VkResult anv_AllocateMemory(
       mem->host_ptr = host_ptr_info->pHostPointer;
       goto success;
    }
-
-   /* Set ALLOC_LOCAL_MEM flag if heap has device local bit set and requested
-    * memory property flag has DEVICE_LOCAL_BIT set.
-    */
-   if (mem_type->propertyFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
-      alloc_flags |= ANV_BO_ALLOC_LOCAL_MEM;
 
    /* Regular allocate (not importing memory). */
 

src/intel/vulkan/anv_private.h
@@ -1368,11 +1368,8 @@ enum anv_bo_alloc_flags {
    /** This buffer has implicit CCS data attached to it */
    ANV_BO_ALLOC_IMPLICIT_CCS = (1 << 9),
 
-   /** This buffer is allocated from local memory */
-   ANV_BO_ALLOC_LOCAL_MEM = (1 << 10),
-
    /** This buffer is allocated from local memory and should be cpu visible */
-   ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE = (1 << 11),
+   ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE = (1 << 10),
 };
 
 VkResult anv_device_alloc_bo(struct anv_device *device,

src/intel/vulkan/anv_cmd_buffer.c
@@ -329,7 +329,7 @@ anv_cmd_buffer_set_ray_query_buffer(struct anv_cmd_buffer *cmd_buffer,
          struct anv_bo *new_bo;
          VkResult result = anv_device_alloc_bo(device, "RT queries shadow",
                                                ray_shadow_size,
-                                               ANV_BO_ALLOC_LOCAL_MEM, /* alloc_flags */
+                                               0, /* alloc_flags */
                                                0, /* explicit_address */
                                                &new_bo);
          if (result != VK_SUCCESS) {
@@ -1087,7 +1087,7 @@ void anv_CmdSetRayTracingPipelineStackSizeKHR(
       struct anv_bo *new_bo;
       VkResult result = anv_device_alloc_bo(device, "RT scratch",
                                             rt->scratch.layout.total_size,
-                                            ANV_BO_ALLOC_LOCAL_MEM,
+                                            0, /* alloc_flags */
                                             0, /* explicit_address */
                                             &new_bo);
       if (result != VK_SUCCESS) {

commit 492761ab8d6ad94660d8465a93f41e244664da6e

anv: add a new NO_LOCAL_MEM allocation flag
We found a perf regression with 9027c5df4c51b ("anv: remove the
LOCAL_MEM allocation bit") which seems to be that we over subscribe
local memory, leading i915 to swap things in/out too much.

This change avoid putting buffers in local memory if they are not
allocated from a DEVICE_LOCAL heap.

Maybe we can revisit this later if i915 is better able to deal with
more buffers in local memory.

v2: Remove implicit_css from anv_bo when not in lmem (Ivan)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 9027c5df4c51b ("anv: remove the LOCAL_MEM allocation bit")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7188
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18395>

src/intel/vulkan/anv_allocator.c
@@ -1463,15 +1463,19 @@ anv_device_alloc_bo(struct anv_device *device,
       /* This always try to put the object in local memory. Here
        * vram_non_mappable & vram_mappable actually are the same region.
        */
-      regions[nregions++] = device->physical->vram_non_mappable.region;
+      if (alloc_flags & ANV_BO_ALLOC_NO_LOCAL_MEM)
+         regions[nregions++] = device->physical->sys.region;
+      else
+         regions[nregions++] = device->physical->vram_non_mappable.region;
 
       /* If the buffer is mapped on the host, add the system memory region.
        * This ensures that if the buffer cannot live in mappable local memory,
        * it can be spilled to system memory.
        */
       uint32_t flags = 0;
-      if ((alloc_flags & ANV_BO_ALLOC_MAPPED) ||
-          (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE)) {
+      if (!(alloc_flags & ANV_BO_ALLOC_NO_LOCAL_MEM) &&
+          ((alloc_flags & ANV_BO_ALLOC_MAPPED) ||
+           (alloc_flags & ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE))) {
          regions[nregions++] = device->physical->sys.region;
          if (device->physical->vram_non_mappable.size > 0)
             flags |= I915_GEM_CREATE_EXT_FLAG_NEEDS_CPU_ACCESS;
@@ -1497,7 +1501,8 @@ anv_device_alloc_bo(struct anv_device *device,
       .is_external = (alloc_flags & ANV_BO_ALLOC_EXTERNAL),
       .has_client_visible_address =
          (alloc_flags & ANV_BO_ALLOC_CLIENT_VISIBLE_ADDRESS) != 0,
-      .has_implicit_ccs = ccs_size > 0 || device->info->verx10 >= 125,
+      .has_implicit_ccs = ccs_size > 0 ||
+                          (device->info->verx10 >= 125 && !(alloc_flags & ANV_BO_ALLOC_NO_LOCAL_MEM)),
    };
 
    if (alloc_flags & ANV_BO_ALLOC_MAPPED) {

src/intel/vulkan/anv_device.c
@@ -3852,6 +3852,9 @@ VkResult anv_AllocateMemory(
        (mem_type->propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT))
       alloc_flags |= ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE;
 
+   if (!(mem_type->propertyFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT))
+      alloc_flags |= ANV_BO_ALLOC_NO_LOCAL_MEM;
+
    if (vk_flags & VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT)
       alloc_flags |= ANV_BO_ALLOC_CLIENT_VISIBLE_ADDRESS;
 

src/intel/vulkan/anv_private.h
@@ -1237,6 +1237,9 @@ enum anv_bo_alloc_flags {
 
    /** This buffer is allocated from local memory and should be cpu visible */
    ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE = (1 << 10),
+
+   /** For non device local allocations */
+   ANV_BO_ALLOC_NO_LOCAL_MEM = (1 << 11),
 };
 
 VkResult anv_device_alloc_bo(struct anv_device *device,