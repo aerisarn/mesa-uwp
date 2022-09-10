aerisarn/mesa-uwp

commit 5b683ba19ac77d6c7dfd8de478678d0b90ba764f

virgl: Only progagate the uniform numbers if the numbers are actually right
When the field was first introduces, the numbers were reporting the number of
vec4 instead of the number of float. Do not propagate them if they are wrong.

Fixes: d92c1ca01b326d8f0ff210828830d6542f9e67f7

Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17415>

src/gallium/drivers/virgl/virgl_screen.c
@@ -421,6 +421,8 @@ virgl_get_shader_param(struct pipe_screen *screen,
       case PIPE_SHADER_CAP_MAX_CONTROL_FLOW_DEPTH:
          return 32;
       case PIPE_SHADER_CAP_MAX_CONST_BUFFER0_SIZE:
+         if (vscreen->caps.caps.v2.host_feature_check_version < 12)
+            return 4096 * sizeof(float[4]);
          return vscreen->caps.caps.v2.max_const_buffer_size[shader];
       case PIPE_SHADER_CAP_MAX_SHADER_BUFFERS:
          if (shader == PIPE_SHADER_FRAGMENT || shader == PIPE_SHADER_COMPUTE)

commit 95cea62a9b7b4b2caa3835e4f5e04d83df1de0a3

virgl: Report CONSTANT_BUFFER_SIZE according to GL_MAX_UNIFORM_BLOCK_SIZE
GL_MAX_FRAGMENT_UNIFORM_COMPONENTS may not report a size that
is useful to calculate the supported UBO size. Use the value
GL_MAX_UNIFORM_BLOCK_SIZE instead when the host supports this.

Related: https://gitlab.freedesktop.org/virgl/virglrenderer/-/issues/286
Fixes: 5b683ba19ac77d6c7dfd8de478678d0b90ba764f
  virgl: Only progagate the uniform numbers if the numbers are actually right

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18512>

src/gallium/drivers/virgl/virgl_screen.c
@@ -356,6 +356,10 @@ virgl_get_param(struct pipe_screen *screen, enum pipe_cap param)
        return vscreen->caps.caps.v2.capability_bits_v2 & VIRGL_CAP_V2_IMPLICIT_MSAA;
    case PIPE_CAP_IMAGE_STORE_FORMATTED:
       return 1;
+   case PIPE_CAP_MAX_CONSTANT_BUFFER_SIZE_UINT:
+      if (vscreen->caps.caps.v2.host_feature_check_version >= 13)
+         return vscreen->caps.caps.v2.max_uniform_block_size;
+      FALLTHROUGH;
    default:
       return u_pipe_screen_get_param_defaults(screen, param);
    }

src/virtio/virtio-gpu/virgl_hw.h
@@ -641,6 +641,7 @@ struct virgl_caps_v2 {
         uint32_t max_const_buffer_size[6]; // PIPE_SHADER_TYPES
         uint32_t num_video_caps;
         struct virgl_video_caps video_caps[32];
+        uint32_t max_uniform_block_size;
 };
 
 union virgl_caps {