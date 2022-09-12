aerisarn/mesa-uwp

commit 33e791bc02ab53d2182076ab7021b991cfb22542

venus/pipeline: Ignore basePipelineHandle
Signed-off-by: Chad Versace <chadversary@chromium.org>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16681>

src/virtio/vulkan/vn_pipeline.c
@@ -255,6 +255,7 @@ struct vn_graphics_pipeline_create_info_fix {
    bool ignore_multisample_state;
    bool ignore_depth_stencil_state;
    bool ignore_color_blend_state;
+   bool ignore_base_pipeline_handle;
 };
 
 /** Temporary storage for fixes in vkCreateGraphicsPipelines. */
@@ -483,6 +484,15 @@ vn_fix_graphics_pipeline_create_info(
          }
       }
 
+      /* Ignore basePipelineHandle?
+       *    VUID-VkGraphicsPipelineCreateInfo-flags-00722
+       */
+      if (!(info->flags & VK_PIPELINE_CREATE_DERIVATIVE_BIT) ||
+          info->basePipelineIndex != -1) {
+         fix.ignore_base_pipeline_handle = true;
+         any_fix = true;
+      }
+
       if (!any_fix)
          continue;
 
@@ -524,6 +534,9 @@ vn_fix_graphics_pipeline_create_info(
 
       if (fix.ignore_color_blend_state)
          fixes->create_infos[i].pColorBlendState = NULL;
+
+      if (fix.ignore_base_pipeline_handle)
+         fixes->create_infos[i].basePipelineHandle = VK_NULL_HANDLE;
    }
 
    if (!fixes)

commit fb756d7949578289cc8c0889c460c28f4788a0c1

venus: avoid fixing pipeline if not derivative
Fixes: 33e791bc02a ("venus/pipeline: Ignore basePipelineHandle")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Chad Versace <chadversary@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18555>

src/virtio/vulkan/vn_pipeline.c
@@ -586,9 +586,11 @@ vn_fix_graphics_pipeline_create_info(
 
       /* Ignore basePipelineHandle?
        *    VUID-VkGraphicsPipelineCreateInfo-flags-00722
+       *    VUID-VkGraphicsPipelineCreateInfo-flags-00724
+       *    VUID-VkGraphicsPipelineCreateInfo-flags-00725
        */
-      if (!(info->flags & VK_PIPELINE_CREATE_DERIVATIVE_BIT) ||
-          info->basePipelineIndex != -1) {
+      if (info->basePipelineHandle != VK_NULL_HANDLE &&
+          !(info->flags & VK_PIPELINE_CREATE_DERIVATIVE_BIT)) {
          fix.ignore_base_pipeline_handle = true;
          any_fix = true;
       }