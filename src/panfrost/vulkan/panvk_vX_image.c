42d60aa0bc904e4a5ee65cba8d119851a3b1f605
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-09-16 21:46:09 -0500

    panvk: Fix buffer views
    Instead of overwriting the BO map pointer, write into the BO map
    pointer. Drp...  Also, drop an unnecessary & accessing
    panvk_buffer_view::tex.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14780>

--- src/panfrost/vulkan/panvk_vX_image.c
+++ src/panfrost/vulkan/panvk_vX_image.c
@@ -200,11 +200,11 @@ panvk_per_arch(CreateBufferView)(VkDevice _device,
       unsigned bo_size = pan_size(SURFACE_WITH_STRIDE);
       view->bo = panfrost_bo_create(pdev, bo_size, 0, "Texture descriptor");
 
-      pan_pack(&view->bo->ptr.cpu, SURFACE_WITH_STRIDE, cfg) {
+      pan_pack(view->bo->ptr.cpu, SURFACE_WITH_STRIDE, cfg) {
          cfg.pointer = address;
       }
 
-      pan_pack(&view->descs.tex, TEXTURE, cfg) {
+      pan_pack(view->descs.tex, TEXTURE, cfg) {
          cfg.dimension = MALI_TEXTURE_DIMENSION_1D;
          cfg.format = pdev->formats[view->fmt].hw;
          cfg.width = view->elems;
