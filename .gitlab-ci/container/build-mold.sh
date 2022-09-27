bcc5cb4a1bab7ddf15edcc1b995eed12a9592aea
Eric Engestrom <eric@engestrom.ch>
2022-09-27 09:45:25 +0100

    ci: unexport local variable (and fix formatting)
    Signed-off-by: Eric Engestrom <eric@igalia.com>
    Reviewed-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18838>

--- .gitlab-ci/container/build-mold.sh
+++ .gitlab-ci/container/build-mold.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -ex
- 
-export MOLD_VERSION="1.4.1"
+
+MOLD_VERSION="1.4.1"
+
 git clone -b v"$MOLD_VERSION" --single-branch --depth 1 https://github.com/rui314/mold.git
 cd mold
 make
