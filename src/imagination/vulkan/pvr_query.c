0b694c2eb35b92f4b27d2588ca7dbbc835985177
Rajnesh Kanwal <rajnesh.kanwal@imgtec.com>
2022-09-28 18:38:02 +0100

    pvr: Fix heap type of availability_buffer allocation.
    Signed-off-by: Rajnesh Kanwal <rajnesh.kanwal@imgtec.com>
    Reviewed-by: Karmjit Mahil <Karmjit.Mahil@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18976>

--- src/imagination/vulkan/pvr_query.c
+++ src/imagination/vulkan/pvr_query.c
@@ -81,7 +81,7 @@ VkResult pvr_CreateQueryPool(VkDevice _device,
       goto err_free_pool;
 
    result = pvr_bo_alloc(device,
-                         device->heaps.vis_test_heap,
+                         device->heaps.general_heap,
                          query_size,
                          sizeof(uint32_t),
                          PVR_BO_ALLOC_FLAG_CPU_MAPPED,
