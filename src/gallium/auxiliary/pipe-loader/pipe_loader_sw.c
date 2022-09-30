aerisarn/mesa-uwp

commit d760a9151b73d1998fceff38fb1c642e4d77bf67

gallium: Learn about kopper
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14541>

src/gallium/auxiliary/pipe-loader/pipe_loader_sw.c
@@ -43,7 +43,7 @@
 #include "frontend/drisw_api.h"
 #include "frontend/sw_driver.h"
 #include "frontend/sw_winsys.h"
-
+#include "util/driconf.h"
 
 struct pipe_loader_sw_device {
    struct pipe_loader_device base;
@@ -58,10 +58,44 @@ struct pipe_loader_sw_device {
 #define pipe_loader_sw_device(dev) ((struct pipe_loader_sw_device *)dev)
 
 static const struct pipe_loader_ops pipe_loader_sw_ops;
+#ifdef HAVE_ZINK
+static const struct pipe_loader_ops pipe_loader_vk_ops;
+#endif
 
 #ifdef GALLIUM_STATIC_TARGETS
 static const struct sw_driver_descriptor driver_descriptors = {
    .create_screen = sw_screen_create_vk,
+   .winsys = {
+#ifdef HAVE_PIPE_LOADER_DRI
+      {
+         .name = "dri",
+         .create_winsys = dri_create_sw_winsys,
+      },
+#endif
+#ifdef HAVE_PIPE_LOADER_KMS
+      {
+         .name = "kms_dri",
+         .create_winsys = kms_dri_create_winsys,
+      },
+#endif
+#ifndef __ANDROID__
+      {
+         .name = "null",
+         .create_winsys = null_sw_create,
+      },
+      {
+         .name = "wrapped",
+         .create_winsys = wrapper_sw_winsys_wrap_pipe_screen,
+      },
+#endif
+      { 0 },
+   }
+};
+#endif
+
+#if defined(GALLIUM_STATIC_TARGETS) && defined(HAVE_ZINK)
+static const struct sw_driver_descriptor kopper_driver_descriptors = {
+   .create_screen = sw_screen_create_zink,
    .winsys = {
 #ifdef HAVE_PIPE_LOADER_DRI
       {
@@ -124,6 +158,42 @@ pipe_loader_sw_probe_init_common(struct pipe_loader_sw_device *sdev)
    return true;
 }
 
+#ifdef HAVE_ZINK
+static bool
+pipe_loader_vk_probe_init_common(struct pipe_loader_sw_device *sdev)
+{
+   sdev->base.type = PIPE_LOADER_DEVICE_PLATFORM;
+   sdev->base.driver_name = "kopper";
+   sdev->base.ops = &pipe_loader_vk_ops;
+   sdev->fd = -1;
+
+#ifdef GALLIUM_STATIC_TARGETS
+   sdev->dd = &kopper_driver_descriptors;
+   if (!sdev->dd)
+      return false;
+#else
+   const char *search_dir = getenv("GALLIUM_PIPE_SEARCH_DIR");
+   if (search_dir == NULL)
+      search_dir = PIPE_SEARCH_DIR;
+
+   sdev->lib = pipe_loader_find_module("swrast", search_dir);
+   if (!sdev->lib)
+      return false;
+
+   sdev->dd = (const struct sw_driver_descriptor *)
+      util_dl_get_proc_address(sdev->lib, "swrast_driver_descriptor");
+
+   if (!sdev->dd){
+      util_dl_close(sdev->lib);
+      sdev->lib = NULL;
+      return false;
+   }
+#endif
+
+   return true;
+}
+#endif
+
 static void
 pipe_loader_sw_probe_teardown_common(struct pipe_loader_sw_device *sdev)
 {
@@ -163,6 +233,37 @@ fail:
    FREE(sdev);
    return false;
 }
+#ifdef HAVE_ZINK
+bool
+pipe_loader_vk_probe_dri(struct pipe_loader_device **devs, const struct drisw_loader_funcs *drisw_lf)
+{
+   struct pipe_loader_sw_device *sdev = CALLOC_STRUCT(pipe_loader_sw_device);
+   int i;
+
+   if (!sdev)
+      return false;
+
+   if (!pipe_loader_vk_probe_init_common(sdev))
+      goto fail;
+
+   for (i = 0; sdev->dd->winsys[i].name; i++) {
+      if (strcmp(sdev->dd->winsys[i].name, "dri") == 0) {
+         sdev->ws = sdev->dd->winsys[i].create_winsys(drisw_lf);
+         break;
+      }
+   }
+   if (!sdev->ws)
+      goto fail;
+
+   *devs = &sdev->base;
+   return true;
+
+fail:
+   pipe_loader_sw_probe_teardown_common(sdev);
+   FREE(sdev);
+   return false;
+}
+#endif
 #endif
 
 #ifdef HAVE_PIPE_LOADER_KMS
@@ -303,6 +404,19 @@ pipe_loader_sw_get_driconf(struct pipe_loader_device *dev, unsigned *count)
    return NULL;
 }
 
+#ifdef HAVE_ZINK
+static const driOptionDescription zink_driconf[] = {
+      #include "zink/driinfo_zink.h"
+};
+
+static const struct driOptionDescription *
+pipe_loader_vk_get_driconf(struct pipe_loader_device *dev, unsigned *count)
+{
+   *count = ARRAY_SIZE(zink_driconf);
+   return zink_driconf;
+}
+#endif
+
 static struct pipe_screen *
 pipe_loader_sw_create_screen(struct pipe_loader_device *dev,
                              const struct pipe_screen_config *config, bool sw_vk)
@@ -310,7 +424,7 @@ pipe_loader_sw_create_screen(struct pipe_loader_device *dev,
    struct pipe_loader_sw_device *sdev = pipe_loader_sw_device(dev);
    struct pipe_screen *screen;
 
-   screen = sdev->dd->create_screen(sdev->ws, sw_vk);
+   screen = sdev->dd->create_screen(sdev->ws, config, sw_vk);
    if (!screen)
       sdev->ws->destroy(sdev->ws);
 
@@ -322,3 +436,11 @@ static const struct pipe_loader_ops pipe_loader_sw_ops = {
    .get_driconf = pipe_loader_sw_get_driconf,
    .release = pipe_loader_sw_release
 };
+
+#ifdef HAVE_ZINK
+static const struct pipe_loader_ops pipe_loader_vk_ops = {
+   .create_screen = pipe_loader_sw_create_screen,
+   .get_driconf = pipe_loader_vk_get_driconf,
+   .release = pipe_loader_sw_release
+};
+#endif

src/gallium/auxiliary/driver_trace/tr_screen.c
@@ -668,6 +668,32 @@ trace_screen_resource_create(struct pipe_screen *_screen,
 }
 
 static struct pipe_resource *
+trace_screen_resource_create_drawable(struct pipe_screen *_screen,
+                                      const struct pipe_resource *templat,
+                                      const void *loader_data)
+{
+   struct trace_screen *tr_scr = trace_screen(_screen);
+   struct pipe_screen *screen = tr_scr->screen;
+   struct pipe_resource *result;
+
+   trace_dump_call_begin("pipe_screen", "resource_create_drawable");
+
+   trace_dump_arg(ptr, screen);
+   trace_dump_arg(resource_template, templat);
+   trace_dump_arg(ptr, loader_data);
+
+   result = screen->resource_create_drawable(screen, templat, loader_data);
+
+   trace_dump_ret(ptr, result);
+
+   trace_dump_call_end();
+
+   if (result)
+      result->screen = _screen;
+   return result;
+}
+
+static struct pipe_resource *
 trace_screen_resource_create_with_modifiers(struct pipe_screen *_screen, const struct pipe_resource *templat,
                                             const uint64_t *modifiers, int modifiers_count)
 {
@@ -1283,6 +1309,7 @@ trace_screen_create(struct pipe_screen *screen)
    tr_scr->base.resource_create = trace_screen_resource_create;
    SCR_INIT(resource_create_with_modifiers);
    tr_scr->base.resource_create_unbacked = trace_screen_resource_create_unbacked;
+   SCR_INIT(resource_create_drawable);
    tr_scr->base.resource_bind_backing = trace_screen_resource_bind_backing;
    tr_scr->base.resource_from_handle = trace_screen_resource_from_handle;
    tr_scr->base.allocate_memory = trace_screen_allocate_memory;
@@ -1344,4 +1371,13 @@ trace_screen(struct pipe_screen *screen)
    assert(screen);
    assert(screen->destroy == trace_screen_destroy);
    return (struct trace_screen *)screen;
+}
+
+struct pipe_screen *
+trace_screen_unwrap(struct pipe_screen *_screen)
+{
+   if (_screen->destroy != trace_screen_destroy)
+      return _screen;
+   struct trace_screen *tr_scr = trace_screen(_screen);
+   return tr_scr->screen;
 }

src/gallium/auxiliary/driver_trace/tr_screen.h
@@ -59,6 +59,8 @@ struct trace_screen
 struct trace_screen *
 trace_screen(struct pipe_screen *screen);
 
+struct pipe_screen *
+trace_screen_unwrap(struct pipe_screen *_screen);
 
 #ifdef __cplusplus
 }

src/gallium/auxiliary/pipe-loader/meson.build
@@ -45,7 +45,7 @@ libpipe_loader_static = static_library(
   files_pipe_loader,
   include_directories : [
     inc_util, inc_loader, inc_gallium, inc_include, inc_src, inc_gallium_aux,
-    inc_gallium_winsys,
+    inc_gallium_winsys, inc_gallium_drivers,
   ],
   c_args : [libpipe_loader_defines, '-DGALLIUM_STATIC_TARGETS=1'],
   gnu_symbol_visibility : 'hidden',
@@ -59,7 +59,7 @@ libpipe_loader_dynamic = static_library(
   files_pipe_loader,
   include_directories : [
     inc_util, inc_loader, inc_gallium, inc_include, inc_src, inc_gallium_aux,
-    inc_gallium_winsys,
+    inc_gallium_winsys, inc_gallium_drivers,
   ],
   c_args : [
     libpipe_loader_defines,

src/gallium/auxiliary/pipe-loader/pipe_loader.h
@@ -144,6 +144,20 @@ pipe_loader_sw_probe_dri(struct pipe_loader_device **devs,
                          const struct drisw_loader_funcs *drisw_lf);
 
 /**
+ * Initialize vk dri device give the drisw_loader_funcs.
+ *
+ * This function is platform-specific.
+ *
+ * Function does not take ownership of the fd, but duplicates it locally.
+ * The local fd is closed during pipe_loader_release.
+ *
+ * \sa pipe_loader_probe
+ */
+bool
+pipe_loader_vk_probe_dri(struct pipe_loader_device **devs,
+                         const struct drisw_loader_funcs *drisw_lf);
+
+/**
  * Initialize a kms backed sw device given an fd.
  *
  * This function is platform-specific.

src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -3,6 +3,7 @@
 #define INLINE_SW_HELPER_H
 
 #include "pipe/p_compiler.h"
+#include "pipe/p_screen.h"
 #include "util/u_debug.h"
 #include "util/debug.h"
 #include "frontend/sw_winsys.h"
@@ -93,9 +94,6 @@ sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk)
 #if defined(GALLIUM_SOFTPIPE)
       (sw_vk ? "" : "softpipe"),
 #endif
-#if defined(GALLIUM_ZINK)
-      (sw_vk || only_sw) ? "" : "zink",
-#endif
    };
 
    for (unsigned i = 0; i < ARRAY_SIZE(drivers); i++) {
@@ -107,6 +105,16 @@ sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk)
          return NULL;
    }
    return NULL;
+}
+
+static inline struct pipe_screen *
+sw_screen_create_zink(struct sw_winsys *winsys, const struct pipe_screen_config *config, bool whatever)
+{
+#if defined(GALLIUM_ZINK)
+   return zink_create_screen(winsys, config);
+#else
+   return NULL;
+#endif
 }
 
 static inline struct pipe_screen *

src/gallium/auxiliary/target-helpers/sw_helper.h
@@ -39,7 +39,7 @@
 #endif
 
 static inline struct pipe_screen *
-sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
+sw_screen_create_named(struct sw_winsys *winsys, const struct pipe_screen_config *config, const char *driver)
 {
    struct pipe_screen *screen = NULL;
 
@@ -80,7 +80,7 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 }
 
 struct pipe_screen *
-sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk)
+sw_screen_create_vk(struct sw_winsys *winsys, const struct pipe_screen_config *config, bool sw_vk)
 {
    UNUSED bool only_sw = env_var_as_boolean("LIBGL_ALWAYS_SOFTWARE", false);
    const char *drivers[] = {
@@ -97,13 +97,10 @@ sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk)
 #if defined(GALLIUM_SOFTPIPE)
       sw_vk ? "" : "softpipe",
 #endif
-#if defined(GALLIUM_ZINK)
-      (sw_vk || only_sw) ? "" : "zink",
-#endif
    };
 
    for (unsigned i = 0; i < ARRAY_SIZE(drivers); i++) {
-      struct pipe_screen *screen = sw_screen_create_named(winsys, drivers[i]);
+      struct pipe_screen *screen = sw_screen_create_named(winsys, config, drivers[i]);
       if (screen)
          return screen;
       /* If the env var is set, don't keep trying things */
@@ -114,8 +111,18 @@ sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk)
 }
 
 struct pipe_screen *
+sw_screen_create_zink(struct sw_winsys *winsys, const struct pipe_screen_config *config, bool whatever)
+{
+#if defined(GALLIUM_ZINK)
+   return zink_create_screen(winsys);
+#else
+   return NULL;
+#endif
+}
+
+struct pipe_screen *
 sw_screen_create(struct sw_winsys *winsys)
 {
-   return sw_screen_create_vk(winsys, false);
+   return sw_screen_create_vk(winsys, NULL, false);
 }
 #endif

src/gallium/auxiliary/target-helpers/sw_helper_public.h
@@ -5,7 +5,9 @@ struct pipe_screen;
 struct sw_winsys;
 
 struct pipe_screen *
-sw_screen_create_vk(struct sw_winsys *winsys, bool sw_vk);
+sw_screen_create_vk(struct sw_winsys *winsys, const struct pipe_screen_config *config, bool sw_vk);
+struct pipe_screen *
+sw_screen_create_zink(struct sw_winsys *winsys, const struct pipe_screen_config *config, bool whatever);
 
 struct pipe_screen *
 sw_screen_create(struct sw_winsys *winsys);

commit 8da9e65500819b2e430028b96b1f68c99f220bdd

pipe-loader: fix zink driinfo header path
this feels like a compiler bug, but somehow just specifying the
shorter path ends up with a broken/partial header include

Fixes: d760a9151b7 ("gallium: Learn about kopper")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18858>

src/gallium/auxiliary/pipe-loader/pipe_loader_sw.c
@@ -404,7 +404,7 @@ pipe_loader_sw_get_driconf(struct pipe_loader_device *dev, unsigned *count)
 
 #if defined(HAVE_DRI) && defined(HAVE_ZINK)
 static const driOptionDescription zink_driconf[] = {
-      #include "zink/driinfo_zink.h"
+      #include "gallium/drivers/zink/driinfo_zink.h"
 };
 
 static const struct driOptionDescription *