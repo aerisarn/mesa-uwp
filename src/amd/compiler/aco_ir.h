0cceab788eb68f46e05c674d8fa33a7a65bc63bc
Timur Kristóf <timur.kristof@gmail.com>
2022-10-06 10:21:55 -0500

    aco: Move is_dead to aco_ir.h to allow it to get inlined.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18103>

--- src/amd/compiler/aco_ir.h
+++ src/amd/compiler/aco_ir.h
@@ -33,6 +33,7 @@
 
 #include "nir.h"
 
+#include <algorithm>
 #include <bitset>
 #include <memory>
 #include <vector>
@@ -1851,7 +1852,19 @@ is_phi(aco_ptr<Instruction>& instr)
 
 memory_sync_info get_sync_info(const Instruction* instr);
 
-bool is_dead(const std::vector<uint16_t>& uses, Instruction* instr);
+inline bool
+is_dead(const std::vector<uint16_t>& uses, const Instruction* instr)
+{
+   if (instr->definitions.empty() || instr->isBranch() ||
+       instr->opcode == aco_opcode::p_init_scratch)
+      return false;
+
+   if (std::any_of(instr->definitions.begin(), instr->definitions.end(),
+                   [&uses](const Definition& def) { return !def.isTemp() || uses[def.tempId()]; }))
+      return false;
+
+   return !(get_sync_info(instr).semantics & (semantic_volatile | semantic_acqrel));
+}
 
 bool can_use_opsel(amd_gfx_level gfx_level, aco_opcode op, int idx);
 bool instr_is_16bit(amd_gfx_level gfx_level, aco_opcode op);
