aerisarn/mesa-uwp

commit 03b959286e2c7f8e006df02d5102bd346a9d450b

intel: Make engine related functions and types not i915 dependent
There is too much i915_drm.h code spread, this patch start to fix that
by re-organizing engine related code.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18942>

src/gallium/drivers/iris/iris_batch.c
@@ -272,30 +272,29 @@ iris_create_engines_context(struct iris_context *ice, int priority)
    const struct intel_device_info *devinfo = &screen->devinfo;
    int fd = iris_bufmgr_get_fd(screen->bufmgr);
 
-   struct drm_i915_query_engine_info *engines_info =
-      intel_i915_query_alloc(fd, DRM_I915_QUERY_ENGINE_INFO, NULL);
+   struct intel_query_engine_info *engines_info = intel_engine_get_info(fd);
 
    if (!engines_info)
       return -1;
 
-   if (intel_gem_count_engines(engines_info, I915_ENGINE_CLASS_RENDER) < 1) {
+   if (intel_engines_count(engines_info, INTEL_ENGINE_CLASS_RENDER) < 1) {
       free(engines_info);
       return -1;
    }
 
    STATIC_ASSERT(IRIS_BATCH_COUNT == 3);
-   uint16_t engine_classes[IRIS_BATCH_COUNT] = {
-      [IRIS_BATCH_RENDER] = I915_ENGINE_CLASS_RENDER,
-      [IRIS_BATCH_COMPUTE] = I915_ENGINE_CLASS_RENDER,
-      [IRIS_BATCH_BLITTER] = I915_ENGINE_CLASS_COPY,
+   enum intel_engine_class engine_classes[IRIS_BATCH_COUNT] = {
+      [IRIS_BATCH_RENDER] = INTEL_ENGINE_CLASS_RENDER,
+      [IRIS_BATCH_COMPUTE] = INTEL_ENGINE_CLASS_RENDER,
+      [IRIS_BATCH_BLITTER] = INTEL_ENGINE_CLASS_COPY,
    };
 
    /* Blitter is only supported on Gfx12+ */
    unsigned num_batches = IRIS_BATCH_COUNT - (devinfo->ver >= 12 ? 0 : 1);
 
    if (env_var_as_boolean("INTEL_COMPUTE_CLASS", false) &&
-       intel_gem_count_engines(engines_info, I915_ENGINE_CLASS_COMPUTE) > 0)
-      engine_classes[IRIS_BATCH_COMPUTE] = I915_ENGINE_CLASS_COMPUTE;
+       intel_engines_count(engines_info, INTEL_ENGINE_CLASS_COMPUTE) > 0)
+      engine_classes[IRIS_BATCH_COMPUTE] = INTEL_ENGINE_CLASS_COMPUTE;
 
    int engines_ctx =
       intel_gem_create_context_engines(fd, engines_info, num_batches,

src/intel/common/intel_gem.c
@@ -59,22 +59,9 @@ intel_gem_supports_syncobj_wait(int fd)
 }
 
 int
-intel_gem_count_engines(const struct drm_i915_query_engine_info *info,
-                        enum drm_i915_gem_engine_class engine_class)
-{
-   assert(info != NULL);
-   int count = 0;
-   for (int i = 0; i < info->num_engines; i++) {
-      if (info->engines[i].engine.engine_class == engine_class)
-         count++;
-   }
-   return count;
-}
-
-int
 intel_gem_create_context_engines(int fd,
-                                 const struct drm_i915_query_engine_info *info,
-                                 int num_engines, uint16_t *engine_classes)
+                                 const struct intel_query_engine_info *info,
+                                 int num_engines, enum intel_engine_class *engine_classes)
 {
    assert(info != NULL);
    assert(num_engines <= 64);
@@ -85,31 +72,30 @@ intel_gem_create_context_engines(int fd,
     * the previous engine instance used.
     */
    int last_engine_idx[] = {
-      [I915_ENGINE_CLASS_RENDER] = -1,
-      [I915_ENGINE_CLASS_COPY] = -1,
-      [I915_ENGINE_CLASS_COMPUTE] = -1,
+      [INTEL_ENGINE_CLASS_RENDER] = -1,
+      [INTEL_ENGINE_CLASS_COPY] = -1,
+      [INTEL_ENGINE_CLASS_COMPUTE] = -1,
    };
 
-   int i915_engine_counts[] = {
-      [I915_ENGINE_CLASS_RENDER] =
-         intel_gem_count_engines(info, I915_ENGINE_CLASS_RENDER),
-      [I915_ENGINE_CLASS_COPY] =
-         intel_gem_count_engines(info, I915_ENGINE_CLASS_COPY),
-      [I915_ENGINE_CLASS_COMPUTE] =
-         intel_gem_count_engines(info, I915_ENGINE_CLASS_COMPUTE),
+   int engine_counts[] = {
+      [INTEL_ENGINE_CLASS_RENDER] =
+         intel_engines_count(info, INTEL_ENGINE_CLASS_RENDER),
+      [INTEL_ENGINE_CLASS_COPY] =
+         intel_engines_count(info, INTEL_ENGINE_CLASS_COPY),
+      [INTEL_ENGINE_CLASS_COMPUTE] =
+         intel_engines_count(info, INTEL_ENGINE_CLASS_COMPUTE),
    };
 
    /* For each queue, we look for the next instance that matches the class we
     * need.
     */
    for (int i = 0; i < num_engines; i++) {
-      uint16_t engine_class = engine_classes[i];
-      assert(engine_class == I915_ENGINE_CLASS_RENDER ||
-             engine_class == I915_ENGINE_CLASS_COPY ||
-             engine_class == I915_ENGINE_CLASS_COMPUTE);
-      if (i915_engine_counts[engine_class] <= 0) {
+      enum intel_engine_class engine_class = engine_classes[i];
+      assert(engine_class == INTEL_ENGINE_CLASS_RENDER ||
+             engine_class == INTEL_ENGINE_CLASS_COPY ||
+             engine_class == INTEL_ENGINE_CLASS_COMPUTE);
+      if (engine_counts[engine_class] <= 0)
          return -1;
-      }
 
       /* Run through the engines reported by the kernel looking for the next
        * matching instance. We loop in case we want to create multiple
@@ -120,8 +106,8 @@ intel_gem_create_context_engines(int fd,
          int *idx = &last_engine_idx[engine_class];
          if (++(*idx) >= info->num_engines)
             *idx = 0;
-         if (info->engines[*idx].engine.engine_class == engine_class) {
-            engine_instance = info->engines[*idx].engine.engine_instance;
+         if (info->engines[*idx].engine_class == engine_class) {
+            engine_instance = info->engines[*idx].engine_instance;
             break;
          }
       }
@@ -129,7 +115,7 @@ intel_gem_create_context_engines(int fd,
          return -1;
       }
 
-      engines_param.engines[i].engine_class = engine_class;
+      engines_param.engines[i].engine_class = intel_engine_class_to_i915(engine_class);
       engines_param.engines[i].engine_instance = engine_instance;
    }
 

src/intel/vulkan/anv_device.c
@@ -679,15 +679,15 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
 
    if (pdevice->engine_info) {
       int gc_count =
-         intel_gem_count_engines(pdevice->engine_info,
-                                 I915_ENGINE_CLASS_RENDER);
+         intel_engines_count(pdevice->engine_info,
+                             INTEL_ENGINE_CLASS_RENDER);
       int g_count = 0;
       int c_count = 0;
       if (env_var_as_boolean("INTEL_COMPUTE_CLASS", false))
-         c_count = intel_gem_count_engines(pdevice->engine_info,
-                                           I915_ENGINE_CLASS_COMPUTE);
+         c_count = intel_engines_count(pdevice->engine_info,
+                                       INTEL_ENGINE_CLASS_COMPUTE);
       enum drm_i915_gem_engine_class compute_class =
-         c_count < 1 ? I915_ENGINE_CLASS_RENDER : I915_ENGINE_CLASS_COMPUTE;
+         c_count < 1 ? INTEL_ENGINE_CLASS_RENDER : INTEL_ENGINE_CLASS_COMPUTE;
 
       anv_override_engine_counts(&gc_count, &g_count, &c_count);
 
@@ -697,7 +697,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
                           VK_QUEUE_COMPUTE_BIT |
                           VK_QUEUE_TRANSFER_BIT,
             .queueCount = gc_count,
-            .engine_class = I915_ENGINE_CLASS_RENDER,
+            .engine_class = INTEL_ENGINE_CLASS_RENDER,
          };
       }
       if (g_count > 0) {
@@ -705,7 +705,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
             .queueFlags = VK_QUEUE_GRAPHICS_BIT |
                           VK_QUEUE_TRANSFER_BIT,
             .queueCount = g_count,
-            .engine_class = I915_ENGINE_CLASS_RENDER,
+            .engine_class = INTEL_ENGINE_CLASS_RENDER,
          };
       }
       if (c_count > 0) {
@@ -727,7 +727,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
                        VK_QUEUE_COMPUTE_BIT |
                        VK_QUEUE_TRANSFER_BIT,
          .queueCount = 1,
-         .engine_class = I915_ENGINE_CLASS_RENDER,
+         .engine_class = INTEL_ENGINE_CLASS_RENDER,
       };
       family_count = 1;
    }
@@ -955,7 +955,7 @@ anv_physical_device_try_create(struct vk_instance *vk_instance,
    }
    device->master_fd = master_fd;
 
-   device->engine_info = anv_gem_get_engine_info(fd);
+   device->engine_info = intel_engine_get_info(fd);
    anv_physical_device_init_queue_families(device);
 
    device->local_fd = fd;
@@ -3088,7 +3088,7 @@ anv_device_setup_context(struct anv_device *device,
    if (device->physical->engine_info) {
       /* The kernel API supports at most 64 engines */
       assert(num_queues <= 64);
-      uint16_t engine_classes[64];
+      enum intel_engine_class engine_classes[64];
       int engine_count = 0;
       for (uint32_t i = 0; i < pCreateInfo->queueCreateInfoCount; i++) {
          const VkDeviceQueueCreateInfo *queueCreateInfo =

src/intel/vulkan_hasvk/anv_device.c
@@ -675,8 +675,8 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
 
    if (pdevice->engine_info) {
       int gc_count =
-         intel_gem_count_engines(pdevice->engine_info,
-                                 I915_ENGINE_CLASS_RENDER);
+         intel_engines_count(pdevice->engine_info,
+                             INTEL_ENGINE_CLASS_RENDER);
       int g_count = 0;
       int c_count = 0;
 
@@ -688,7 +688,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
                           VK_QUEUE_COMPUTE_BIT |
                           VK_QUEUE_TRANSFER_BIT,
             .queueCount = gc_count,
-            .engine_class = I915_ENGINE_CLASS_RENDER,
+            .engine_class = INTEL_ENGINE_CLASS_RENDER,
          };
       }
       if (g_count > 0) {
@@ -991,7 +991,7 @@ anv_physical_device_try_create(struct vk_instance *vk_instance,
    }
    device->master_fd = master_fd;
 
-   device->engine_info = anv_gem_get_engine_info(fd);
+   device->engine_info = intel_engine_get_info(fd);
    anv_physical_device_init_queue_families(device);
 
    device->local_fd = fd;
@@ -2879,7 +2879,7 @@ anv_device_setup_context(struct anv_device *device,
    if (device->physical->engine_info) {
       /* The kernel API supports at most 64 engines */
       assert(num_queues <= 64);
-      uint16_t engine_classes[64];
+      enum intel_engine_class engine_classes[64];
       int engine_count = 0;
       for (uint32_t i = 0; i < pCreateInfo->queueCreateInfoCount; i++) {
          const VkDeviceQueueCreateInfo *queueCreateInfo =

src/intel/vulkan_hasvk/anv_private.h
@@ -43,6 +43,7 @@
 
 #include "common/intel_clflush.h"
 #include "common/intel_decoder.h"
+#include "common/intel_engine.h"
 #include "common/intel_gem.h"
 #include "common/intel_l3_config.h"
 #include "common/intel_measure.h"
@@ -1061,7 +1062,7 @@ struct anv_physical_device {
     bool                                        has_master;
     int64_t                                     master_major;
     int64_t                                     master_minor;
-    struct drm_i915_query_engine_info *         engine_info;
+    struct intel_query_engine_info *            engine_info;
 
     void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, bool);
     struct intel_measure_device                 measure_device;
@@ -1439,7 +1440,6 @@ uint32_t anv_gem_fd_to_handle(struct anv_device *device, int fd);
 int anv_gem_set_caching(struct anv_device *device, uint32_t gem_handle, uint32_t caching);
 int anv_i915_query(int fd, uint64_t query_id, void *buffer,
                    int32_t *buffer_len);
-struct drm_i915_query_engine_info *anv_gem_get_engine_info(int fd);
 
 uint64_t anv_vma_alloc(struct anv_device *device,
                        uint64_t size, uint64_t align,

src/intel/vulkan/anv_perf.c
@@ -343,7 +343,7 @@ VkResult anv_EnumeratePhysicalDeviceQueueFamilyPerformanceQueryCountersKHR(
     */
    struct anv_queue_family *queue_family =
       &pdevice->queue.families[queueFamilyIndex];
-   if (queue_family->engine_class != I915_ENGINE_CLASS_RENDER)
+   if (queue_family->engine_class != INTEL_ENGINE_CLASS_RENDER)
       return vk_outarray_status(&out);
 
    for (int c = 0; c < (perf ? perf->n_counters : 0); c++) {

src/intel/vulkan/anv_private.h
@@ -43,6 +43,7 @@
 
 #include "common/intel_clflush.h"
 #include "common/intel_decoder.h"
+#include "common/intel_engine.h"
 #include "common/intel_gem.h"
 #include "common/intel_l3_config.h"
 #include "common/intel_measure.h"
@@ -927,8 +928,7 @@ struct anv_queue_family {
    VkQueueFlags   queueFlags;
    uint32_t       queueCount;
 
-   /* Driver internal information */
-   enum drm_i915_gem_engine_class engine_class;
+   enum intel_engine_class engine_class;
 };
 
 #define ANV_MAX_QUEUE_FAMILIES 3
@@ -1053,7 +1053,7 @@ struct anv_physical_device {
     bool                                        has_master;
     int64_t                                     master_major;
     int64_t                                     master_minor;
-    struct drm_i915_query_engine_info *         engine_info;
+    struct intel_query_engine_info *            engine_info;
 
     void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, bool);
     struct intel_measure_device                 measure_device;
@@ -1374,7 +1374,6 @@ uint32_t anv_gem_fd_to_handle(struct anv_device *device, int fd);
 int anv_gem_set_caching(struct anv_device *device, uint32_t gem_handle, uint32_t caching);
 int anv_i915_query(int fd, uint64_t query_id, void *buffer,
                    int32_t *buffer_len);
-struct drm_i915_query_engine_info *anv_gem_get_engine_info(int fd);
 
 uint64_t anv_vma_alloc(struct anv_device *device,
                        uint64_t size, uint64_t align,

src/intel/vulkan/anv_utrace.c
@@ -267,13 +267,13 @@ static const char *
 queue_family_to_name(const struct anv_queue_family *family)
 {
    switch (family->engine_class) {
-   case I915_ENGINE_CLASS_RENDER:
+   case INTEL_ENGINE_CLASS_RENDER:
       return "render";
-   case I915_ENGINE_CLASS_COPY:
+   case INTEL_ENGINE_CLASS_COPY:
       return "copy";
-   case I915_ENGINE_CLASS_VIDEO:
+   case INTEL_ENGINE_CLASS_VIDEO:
       return "video";
-   case I915_ENGINE_CLASS_VIDEO_ENHANCE:
+   case INTEL_ENGINE_CLASS_VIDEO_ENHANCE:
       return "video-enh";
    default:
       return "unknown";

commit 5269d91efcfee95930208d9b8f3273c9c97d4a6f

intel: Convert missing i915 engine types to intel
This convertions were missed due to bad rebased in my end, sorry.

Fixes: 03b959286e2c ("intel: Make engine related functions and types not i915 dependent")
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18975>

src/gallium/drivers/iris/iris_batch.c
@@ -239,7 +239,7 @@ iris_init_batch(struct iris_context *ice,
       batch->decoder.surface_base = IRIS_MEMZONE_BINDER_START;
       batch->decoder.max_vbo_decoded_lines = 32;
       if (batch->name == IRIS_BATCH_BLITTER)
-         batch->decoder.engine = I915_ENGINE_CLASS_COPY;
+         batch->decoder.engine = INTEL_ENGINE_CLASS_COPY;
    }
 
    iris_init_batch_measure(ice, batch);

src/intel/common/intel_gem.c
@@ -68,7 +68,7 @@ intel_gem_create_context_engines(int fd,
    I915_DEFINE_CONTEXT_PARAM_ENGINES(engines_param, 64);
    engines_param.extensions = 0;
 
-   /* For each type of drm_i915_gem_engine_class of interest, we keep track of
+   /* For each type of intel_engine_class of interest, we keep track of
     * the previous engine instance used.
     */
    int last_engine_idx[] = {

src/intel/dev/intel_device_info.c
@@ -1921,16 +1921,16 @@ init_max_scratch_ids(struct intel_device_info *devinfo)
 
 static unsigned
 intel_device_info_calc_engine_prefetch(const struct intel_device_info *devinfo,
-                                       enum drm_i915_gem_engine_class engine_class)
+                                       enum intel_engine_class engine_class)
 {
    if (devinfo->verx10 < 125)
       return 512;
 
    if (intel_device_info_is_mtl(devinfo)) {
       switch (engine_class) {
-      case I915_ENGINE_CLASS_RENDER:
+      case INTEL_ENGINE_CLASS_RENDER:
          return 2048;
-      case I915_ENGINE_CLASS_COMPUTE:
+      case INTEL_ENGINE_CLASS_COMPUTE:
          return 1024;
       default:
          return 512;
@@ -2063,7 +2063,7 @@ intel_get_device_info_from_fd(int fd, struct intel_device_info *devinfo)
 
    init_max_scratch_ids(devinfo);
 
-   for (enum drm_i915_gem_engine_class engine = I915_ENGINE_CLASS_RENDER;
+   for (enum intel_engine_class engine = INTEL_ENGINE_CLASS_RENDER;
         engine < ARRAY_SIZE(devinfo->engine_class_prefetch); engine++)
       devinfo->engine_class_prefetch[engine] =
             intel_device_info_calc_engine_prefetch(devinfo, engine);

src/intel/dev/intel_device_info.h
@@ -28,16 +28,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "drm-uapi/i915_drm.h"
-
 #include "util/macros.h"
 #include "compiler/shader_enums.h"
+
+#include "intel/common/intel_engine.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-struct drm_i915_query_topology_info;
 
 #define INTEL_DEVICE_MAX_NAME_SIZE        64
 #define INTEL_DEVICE_MAX_SLICES           8
@@ -351,7 +349,7 @@ struct intel_device_info
     * Size of the command streamer prefetch. This is important to know for
     * self modifying batches.
     */
-   unsigned engine_class_prefetch[I915_ENGINE_CLASS_COMPUTE + 1];
+   unsigned engine_class_prefetch[INTEL_ENGINE_CLASS_COMPUTE + 1];
 
    /**
     * For the longest time the timestamp frequency for Gen's timestamp counter

src/intel/vulkan/anv_batch_chain.c
@@ -936,7 +936,7 @@ anv_cmd_buffer_end_batch_buffer(struct anv_cmd_buffer *cmd_buffer)
           */
          if (cmd_buffer->batch_bos.next == cmd_buffer->batch_bos.prev) {
             const struct intel_device_info *devinfo = cmd_buffer->device->info;
-            const enum drm_i915_gem_engine_class engine_class = cmd_buffer->queue_family->engine_class;
+            const enum intel_engine_class engine_class = cmd_buffer->queue_family->engine_class;
             /* Careful to have everything in signed integer. */
             int32_t prefetch_len = devinfo->engine_class_prefetch[engine_class];
             int32_t batch_len = cmd_buffer->batch.next - cmd_buffer->batch.start;

src/intel/vulkan/anv_device.c
@@ -687,7 +687,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
       if (env_var_as_boolean("INTEL_COMPUTE_CLASS", false))
          c_count = intel_engines_count(pdevice->engine_info,
                                        INTEL_ENGINE_CLASS_COMPUTE);
-      enum drm_i915_gem_engine_class compute_class =
+      enum intel_engine_class compute_class =
          c_count < 1 ? INTEL_ENGINE_CLASS_RENDER : INTEL_ENGINE_CLASS_COMPUTE;
 
       anv_override_engine_counts(&gc_count, &g_count, &c_count);

src/intel/vulkan/genX_query.c
@@ -1014,7 +1014,7 @@ void genX(CmdBeginQueryIndexedEXT)(
       assert(reloc_idx == pdevice->n_perf_query_commands);
 
       const struct intel_device_info *devinfo = cmd_buffer->device->info;
-      const enum drm_i915_gem_engine_class engine_class = cmd_buffer->queue_family->engine_class;
+      const enum intel_engine_class engine_class = cmd_buffer->queue_family->engine_class;
       mi_self_mod_barrier(&b, devinfo->engine_class_prefetch[engine_class]);
 
       anv_batch_emit(&cmd_buffer->batch, GENX(PIPE_CONTROL), pc) {

src/intel/vulkan_hasvk/anv_batch_chain.c
@@ -1019,7 +1019,7 @@ anv_cmd_buffer_end_batch_buffer(struct anv_cmd_buffer *cmd_buffer)
           */
          if (cmd_buffer->batch_bos.next == cmd_buffer->batch_bos.prev) {
             const struct intel_device_info *devinfo = cmd_buffer->device->info;
-            const enum drm_i915_gem_engine_class engine_class = cmd_buffer->queue_family->engine_class;
+            const enum intel_engine_class engine_class = cmd_buffer->queue_family->engine_class;
             /* Careful to have everything in signed integer. */
             int32_t prefetch_len = devinfo->engine_class_prefetch[engine_class];
             int batch_len = cmd_buffer->batch.next - cmd_buffer->batch.start;

src/intel/vulkan_hasvk/anv_device.c
@@ -696,7 +696,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
             .queueFlags = VK_QUEUE_GRAPHICS_BIT |
                           VK_QUEUE_TRANSFER_BIT,
             .queueCount = g_count,
-            .engine_class = I915_ENGINE_CLASS_RENDER,
+            .engine_class = INTEL_ENGINE_CLASS_RENDER,
          };
       }
       if (c_count > 0) {
@@ -704,7 +704,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
             .queueFlags = VK_QUEUE_COMPUTE_BIT |
                           VK_QUEUE_TRANSFER_BIT,
             .queueCount = c_count,
-            .engine_class = I915_ENGINE_CLASS_RENDER,
+            .engine_class = INTEL_ENGINE_CLASS_RENDER,
          };
       }
       /* Increase count below when other families are added as a reminder to
@@ -718,7 +718,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
                        VK_QUEUE_COMPUTE_BIT |
                        VK_QUEUE_TRANSFER_BIT,
          .queueCount = 1,
-         .engine_class = I915_ENGINE_CLASS_RENDER,
+         .engine_class = INTEL_ENGINE_CLASS_RENDER,
       };
       family_count = 1;
    }

src/intel/vulkan_hasvk/anv_perf.c
@@ -353,7 +353,7 @@ VkResult anv_EnumeratePhysicalDeviceQueueFamilyPerformanceQueryCountersKHR(
     */
    struct anv_queue_family *queue_family =
       &pdevice->queue.families[queueFamilyIndex];
-   if (queue_family->engine_class != I915_ENGINE_CLASS_RENDER)
+   if (queue_family->engine_class != INTEL_ENGINE_CLASS_RENDER)
       return vk_outarray_status(&out);
 
    for (int c = 0; c < (perf ? perf->n_counters : 0); c++) {

src/intel/vulkan_hasvk/anv_private.h
@@ -927,7 +927,7 @@ struct anv_queue_family {
    uint32_t       queueCount;
 
    /* Driver internal information */
-   enum drm_i915_gem_engine_class engine_class;
+   enum intel_engine_class engine_class;
 };
 
 #define ANV_MAX_QUEUE_FAMILIES 3

src/intel/vulkan_hasvk/anv_utrace.c
@@ -267,13 +267,13 @@ static const char *
 queue_family_to_name(const struct anv_queue_family *family)
 {
    switch (family->engine_class) {
-   case I915_ENGINE_CLASS_RENDER:
+   case INTEL_ENGINE_CLASS_RENDER:
       return "render";
-   case I915_ENGINE_CLASS_COPY:
+   case INTEL_ENGINE_CLASS_COPY:
       return "copy";
-   case I915_ENGINE_CLASS_VIDEO:
+   case INTEL_ENGINE_CLASS_VIDEO:
       return "video";
-   case I915_ENGINE_CLASS_VIDEO_ENHANCE:
+   case INTEL_ENGINE_CLASS_VIDEO_ENHANCE:
       return "video-enh";
    default:
       return "unknown";

src/intel/vulkan_hasvk/genX_query.c
@@ -1016,7 +1016,7 @@ void genX(CmdBeginQueryIndexedEXT)(
       assert(reloc_idx == pdevice->n_perf_query_commands);
 
       const struct intel_device_info *devinfo = cmd_buffer->device->info;
-      const enum drm_i915_gem_engine_class engine_class = cmd_buffer->queue_family->engine_class;
+      const enum intel_engine_class engine_class = cmd_buffer->queue_family->engine_class;
       mi_self_mod_barrier(&b, devinfo->engine_class_prefetch[engine_class]);
 
       anv_batch_emit(&cmd_buffer->batch, GENX(PIPE_CONTROL), pc) {

src/intel/vulkan_hasvk/genX_state.c
@@ -425,7 +425,7 @@ genX(init_device_state)(struct anv_device *device)
    for (uint32_t i = 0; i < device->queue_count; i++) {
       struct anv_queue *queue = &device->queues[i];
       switch (queue->family->engine_class) {
-      case I915_ENGINE_CLASS_RENDER:
+      case INTEL_ENGINE_CLASS_RENDER:
          res = init_render_queue_state(queue);
          break;
       default:

src/intel/dev/intel_device_info_override_test.c
@@ -71,7 +71,7 @@ main(int argc, char *argv[])
       fprintf(stderr, "%u\n", devinfo.verx10);
       assert(devinfo.verx10 == verx10);
       verify_device_info(&devinfo);
-      assert(devinfo.engine_class_prefetch[I915_ENGINE_CLASS_RENDER] > 0);
+      assert(devinfo.engine_class_prefetch[INTEL_ENGINE_CLASS_RENDER] > 0);
    }
 
    return 0;