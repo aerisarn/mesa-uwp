d28d1ead9c1770dd98a397490db18d40926c0906
Karol Herbst <kherbst@redhat.com>
2022-10-15 23:00:37 +0200

    aux/trace: add set_global_binding
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19214>

--- src/gallium/auxiliary/driver_trace/tr_context.c
+++ src/gallium/auxiliary/driver_trace/tr_context.c
@@ -2276,6 +2276,30 @@ static void trace_context_make_image_handle_resident(struct pipe_context *_pipe,
    pipe->make_image_handle_resident(pipe, handle, access, resident);
 }
 
+static void trace_context_set_global_binding(struct pipe_context *_pipe,
+                                             unsigned first, unsigned count,
+                                             struct pipe_resource **resources,
+                                             uint32_t **handles)
+{
+   struct trace_context *tr_ctx = trace_context(_pipe);
+   struct pipe_context *pipe = tr_ctx->pipe;
+
+   trace_dump_call_begin("pipe_context", "set_global_binding");
+   trace_dump_arg(ptr, pipe);
+   trace_dump_arg(uint, first);
+   trace_dump_arg(uint, count);
+   trace_dump_arg_array(ptr, resources, count);
+   trace_dump_arg_array_val(uint, handles, count);
+
+   pipe->set_global_binding(pipe, first, count, resources, handles);
+
+   /* TODO: the handles are 64 bit if ADDRESS_BITS are 64, this is better than
+    * nothing though
+    */
+   trace_dump_ret_array_val(uint, handles, count);
+   trace_dump_call_end();
+}
+
 struct pipe_context *
 trace_context_create(struct trace_screen *tr_scr,
                      struct pipe_context *pipe)
@@ -2409,6 +2433,7 @@ trace_context_create(struct trace_screen *tr_scr,
    TR_CTX_INIT(invalidate_resource);
    TR_CTX_INIT(set_context_param);
    TR_CTX_INIT(set_debug_callback);
+   TR_CTX_INIT(set_global_binding);
 
 #undef TR_CTX_INIT
 
