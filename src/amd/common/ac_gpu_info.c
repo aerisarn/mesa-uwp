aerisarn/mesa-uwp

commit 0a8a9d9d638df80454417d2041f79f645193c910

ac: add radeon_info::has_vrs_ds_export_bug
According to PAL, only NAVI21 and NAVI22 are affected.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19195>

src/amd/common/ac_gpu_info.c
@@ -1101,6 +1101,12 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info)
    info->has_vgt_flush_ngg_legacy_bug = info->gfx_level == GFX10 ||
                                         info->family == CHIP_NAVI21;
 
+   /* First Navi2x chips have a hw bug that doesn't allow to write
+    * depth/stencil from a FS for multi-pixel fragments.
+    */
+   info->has_vrs_ds_export_bug = info->family == CHIP_NAVI21 ||
+                                 info->family == CHIP_NAVI22;
+
    /* HW bug workaround when CS threadgroups > 256 threads and async compute
     * isn't used, i.e. only one compute job can run at a time.  If async
     * compute is possible, the threadgroup size must be limited to 256 threads

src/amd/common/ac_gpu_info.h
@@ -117,6 +117,7 @@ struct radeon_info {
    bool never_send_perfcounter_stop;
    bool discardable_allows_big_page;
    bool has_export_conflict_bug;
+   bool has_vrs_ds_export_bug;
 
    /* Display features. */
    /* There are 2 display DCC codepaths, because display expects unaligned DCC. */

commit 1ec5b6774dd2af05abf34fcb74cac0c71692b763

ac: fix has_vrs_ds_export_bug for VanGogh
Missed it.

Fixes: 0a8a9d9d638 ("ac: add radeon_info::has_vrs_ds_export_bug")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19272>

src/amd/common/ac_gpu_info.c
@@ -1105,7 +1105,8 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info)
     * depth/stencil from a FS for multi-pixel fragments.
     */
    info->has_vrs_ds_export_bug = info->family == CHIP_NAVI21 ||
-                                 info->family == CHIP_NAVI22;
+                                 info->family == CHIP_NAVI22 ||
+                                 info->family == CHIP_VANGOGH;
 
    /* HW bug workaround when CS threadgroups > 256 threads and async compute
     * isn't used, i.e. only one compute job can run at a time.  If async