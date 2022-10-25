aerisarn/mesa-uwp

commit c305a2c9625a91e8e4bbaca5b20b2eb7d69a74de

zink: move spec constant emission to the types/consts block
spec constants are used as constants, so emitting them in the instructions
block can create mismatched ordering of the constant usage with other types

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19116>

src/gallium/drivers/zink/nir_to_spirv/spirv_builder.c
@@ -1539,7 +1539,13 @@ SpvId
 spirv_builder_spec_const_uint(struct spirv_builder *b, int width)
 {
    assert(width <= 32);
-   return spirv_builder_emit_unop(b, SpvOpSpecConstant, spirv_builder_type_uint(b, width), 0);
+   SpvId result = spirv_builder_new_id(b);
+   spirv_buffer_prepare(&b->types_const_defs, b->mem_ctx, 4);
+   spirv_buffer_emit_word(&b->types_const_defs, SpvOpSpecConstant | (4 << 16));
+   spirv_buffer_emit_word(&b->types_const_defs, spirv_builder_type_uint(b, width));
+   spirv_buffer_emit_word(&b->types_const_defs, result);
+   spirv_buffer_emit_word(&b->types_const_defs, 0);
+   return result;
 }
 
 SpvId

commit 3128a8cada2a3434ed27211a432fa8c0dd21f348

zink: fix spirv_builder_spec_const_uint
We need to create the type upfront otherwise the spir-v binary gets
corrupted.

Fixes: c305a2c9625 ("zink: move spec constant emission to the types/consts block")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19310>

src/gallium/drivers/zink/nir_to_spirv/spirv_builder.c
@@ -1547,10 +1547,11 @@ SpvId
 spirv_builder_spec_const_uint(struct spirv_builder *b, int width)
 {
    assert(width <= 32);
+   SpvId const_type = spirv_builder_type_uint(b, width);
    SpvId result = spirv_builder_new_id(b);
    spirv_buffer_prepare(&b->types_const_defs, b->mem_ctx, 4);
    spirv_buffer_emit_word(&b->types_const_defs, SpvOpSpecConstant | (4 << 16));
-   spirv_buffer_emit_word(&b->types_const_defs, spirv_builder_type_uint(b, width));
+   spirv_buffer_emit_word(&b->types_const_defs, const_type);
    spirv_buffer_emit_word(&b->types_const_defs, result);
    /* this is the default value for spec constants;
     * if any users need a different default, add a param to pass for it