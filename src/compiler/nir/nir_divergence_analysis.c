117b32a5944890a535700937f1ebf4840ea3c0db
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-10-26 13:54:57 +0300

    nir/divergence_analysis: add missing desc_set_address_intel
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: Jason Ekstrand <jason.ekstrand@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19320>

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -411,7 +411,8 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_image_deref_load_param_intel:
    case nir_intrinsic_image_load_raw_intel:
    case nir_intrinsic_get_ubo_size:
-   case nir_intrinsic_load_ssbo_address: {
+   case nir_intrinsic_load_ssbo_address:
+   case nir_intrinsic_load_desc_set_address_intel: {
       unsigned num_srcs = nir_intrinsic_infos[instr->intrinsic].num_srcs;
       for (unsigned i = 0; i < num_srcs; i++) {
          if (instr->src[i].ssa->divergent) {
