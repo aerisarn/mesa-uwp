aerisarn/mesa-uwp

commit c746bf4c5c811d7d5614ca13504e6412ac13e911

intel/dev: Add display_ver and set adl-p to 13
Ref: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12908
Cc: mesa-stable
Fixes: e435511b580 ("intel/dev: Add device info for ADL GT2")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12973>

src/intel/dev/intel_device_info.c
@@ -993,6 +993,7 @@ static const struct intel_device_info intel_device_info_adl_gt1 = {
 static const struct intel_device_info intel_device_info_adl_gt2 = {
    GFX12_GT_FEATURES(2),
    .is_alderlake = true,
+   .display_ver = 13,
 };
 
 #define GFX12_DG1_SG1_FEATURES                  \
@@ -1321,6 +1322,9 @@ intel_get_device_info_from_pci_id(int pci_id,
 
    if (devinfo->verx10 == 0)
       devinfo->verx10 = devinfo->ver * 10;
+
+   if (devinfo->display_ver == 0)
+      devinfo->display_ver = devinfo->ver;
 
    update_cs_workgroup_threads(devinfo);
 

src/intel/dev/intel_device_info.h
@@ -51,6 +51,7 @@ struct intel_device_info
    /* Driver internal numbers used to differentiate platforms. */
    int ver;
    int verx10;
+   int display_ver;
    int revision;
    int gt;
 

commit 3ef88cd0a2de26e56def43db969f98c80235f065

intel/dev: Set display_ver = 13 on all ADL/RPL/DG2
display_ver doesn't seem to be used anywhere, but if that were to
change, we'd want this to be consistent.

Fixes: c746bf4c5c8 ("intel/dev: Add display_ver and set adl-p to 13")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19354>

src/intel/dev/intel_device_info.c
@@ -996,11 +996,13 @@ static const struct intel_device_info intel_device_info_rkl_gt1 = {
 static const struct intel_device_info intel_device_info_adl_gt05 = {
    GFX12_GT05_FEATURES,
    .platform = INTEL_PLATFORM_ADL,
+   .display_ver = 13,
 };
 
 static const struct intel_device_info intel_device_info_adl_gt1 = {
    GFX12_GT_FEATURES(1),
    .platform = INTEL_PLATFORM_ADL,
+   .display_ver = 13,
 };
 
 static const struct intel_device_info intel_device_info_adl_n = {
@@ -1019,6 +1021,7 @@ static const struct intel_device_info intel_device_info_rpl = {
    GFX12_FEATURES(1, 1, 4),
    .num_subslices = dual_subslices(2),
    .platform = INTEL_PLATFORM_RPL,
+   .display_ver = 13,
 };
 
 static const struct intel_device_info intel_device_info_rpl_p = {
@@ -1055,6 +1058,7 @@ static const struct intel_device_info intel_device_info_sg1 = {
 #define DG2_FEATURES                                            \
    /* (Sub)slice info comes from the kernel topology info */    \
    XEHP_FEATURES(0, 1, 0),                                      \
+   .display_ver = 13,                                           \
    .revision = 4, /* For offline compiler */                    \
    .num_subslices = dual_subslices(1),                          \
    .has_lsc = true,                                             \