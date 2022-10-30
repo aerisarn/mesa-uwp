4770d3f0bad8162ecf5170ce3aeb9012ce8a5bbd
Yusuf Khan <yusisamerican@gmail.com>
2022-10-30 16:38:15 -0500

    r600: enable memory objects
    Support was there but the cap was not enabled
    
    Signed-off-by: Yusuf Khan <yusisamerican@gmail.com>
    Acked-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19406>

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -295,6 +295,7 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_VS_WINDOW_SPACE_POSITION:
 	case PIPE_CAP_VS_LAYER_VIEWPORT:
 	case PIPE_CAP_SAMPLE_SHADING:
+        case PIPE_CAP_MEMOBJ:
 	case PIPE_CAP_CLIP_HALFZ:
 	case PIPE_CAP_POLYGON_OFFSET_CLAMP:
 	case PIPE_CAP_CONDITIONAL_RENDER_INVERTED:
