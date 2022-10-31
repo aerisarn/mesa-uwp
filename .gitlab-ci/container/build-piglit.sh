70ce1dcacc92a816322082c8695569b6a91a1810
Guilherme Gallo <guilherme.gallo@collabora.com>
2022-10-31 11:13:16 +0100

    ci: Update piglit with s3 support
    With new S3 support, we can use JWT-only server interaction via the
    removal of `role-session` and `minio-host` arguments from PIGLIT_ARGS in
    YAML.
    This parameter change will come in a later commit.
    
    Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19076>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -5,7 +5,7 @@ set -ex
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 591c91865012de4224bea551eac5d2274acf06ad
+git checkout 1cd716180cfb6ef0c1fc54702460ef49e5115791
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
