f1ea0bc18aa6dd62554d7c556c087640dbc8a168
Sathishkumar S <sathishkumar.sundararaju@amd.com>
2022-11-01 20:15:56 +0530

    gallium/vl: return the buffer plane order for yuv444p format
    plane order is expected when trying to render yuv surfaces, update it for yuv444p
    
    Signed-off-by: Sathishkumar S <sathishkumar.sundararaju@amd.com>
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19445>

--- src/gallium/auxiliary/vl/vl_video_buffer.c
+++ src/gallium/auxiliary/vl/vl_video_buffer.c
@@ -76,6 +76,7 @@ vl_video_buffer_plane_order(enum pipe_format format)
       return const_resource_plane_order_YVU;
 
    case PIPE_FORMAT_NV12:
+   case PIPE_FORMAT_Y8_U8_V8_444_UNORM:
    case PIPE_FORMAT_R8G8B8A8_UNORM:
    case PIPE_FORMAT_R8G8B8X8_UNORM:
    case PIPE_FORMAT_B8G8R8A8_UNORM:
