119703072735080cc0244bf63525e5c88f27d43f
Ian Romanick <ian.d.romanick@intel.com>
2022-11-01 18:11:54 -0700

    glsl: Use nir_type_convert instead of nir_type_conversion_op
    In a future commit, nit_type_conversion_op won't be able to handle i2b
    (and in a much later commit f2b), so switch many users to the fully
    featured function.
    
    In gl_nir_lower_packed_varyings, all of the type conversions are between
    int32 and uint32 types.  In NIR, those are just moves, so elide them.
    
    No shader-db or fossil-db changes on any Intel platform.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15121>

--- src/compiler/glsl/glsl_to_nir.cpp
+++ src/compiler/glsl/glsl_to_nir.cpp
@@ -2016,9 +2016,8 @@ nir_visitor::visit(ir_expression *ir)
    case ir_unop_u642i64: {
       nir_alu_type src_type = nir_get_nir_type_for_glsl_base_type(types[0]);
       nir_alu_type dst_type = nir_get_nir_type_for_glsl_base_type(out_type);
-      result = nir_build_alu(&b, nir_type_conversion_op(src_type, dst_type,
-                                 nir_rounding_mode_undef),
-                                 srcs[0], NULL, NULL, NULL);
+      result = nir_type_convert(&b, srcs[0], src_type, dst_type,
+                                nir_rounding_mode_undef);
       /* b2i and b2f don't have fixed bit-size versions so the builder will
        * just assume 32 and we have to fix it up here.
        */
