3c91168c71cc8a702792df76833b356b206ebd48
Yiwei Zhang <zzyiwei@chromium.org>
2022-11-04 23:58:13 -0700

    ci/venus: specify render server path to enable server config
    The render server path is required by crosvm to enable render server.
    
    v2: updated script to package server binary properly
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Reviewed-by: Corentin Noël <corentin.noel@collabora.com> (v1)
    Acked-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19542>

--- .gitlab-ci/container/lava_build.sh
+++ .gitlab-ci/container/lava_build.sh
@@ -200,6 +200,8 @@ if [[ ${DEBIAN_ARCH} = "amd64" ]]; then
     . .gitlab-ci/container/build-crosvm.sh
     mv /usr/local/bin/crosvm /lava-files/rootfs-${DEBIAN_ARCH}/usr/bin/
     mv /usr/local/lib/$GCC_ARCH/libvirglrenderer.* /lava-files/rootfs-${DEBIAN_ARCH}/usr/lib/$GCC_ARCH/
+    mkdir -p /lava-files/rootfs-${DEBIAN_ARCH}/usr/local/libexec/
+    mv /usr/local/libexec/virgl* /lava-files/rootfs-${DEBIAN_ARCH}/usr/local/libexec/
 fi
 
 ############### Build libdrm
