ae76bba34a878dbd299f911bceaa9acdf01eb38a
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-11-11 11:31:46 +0200

    anv: bump pool bucket max allocation size
    Age of Empire IV generates a shader of ~2.3Mb on DG2 which is above
    the limit we currently have.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
    Acked-by: Jason Ekstrand <jason.ekstrand@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19669>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -739,7 +739,7 @@ struct anv_fixed_size_state_pool {
 };
 
 #define ANV_MIN_STATE_SIZE_LOG2 6
-#define ANV_MAX_STATE_SIZE_LOG2 21
+#define ANV_MAX_STATE_SIZE_LOG2 22
 
 #define ANV_STATE_BUCKETS (ANV_MAX_STATE_SIZE_LOG2 - ANV_MIN_STATE_SIZE_LOG2 + 1)
 
