4b841cfec8b502a8290cd67356d811933d318c2b
Karol Herbst <kherbst@redhat.com>
2022-11-16 11:50:51 +0100

    rusticl: fix build error with valgrind being enabled
    This bumps the meson requierement to 1.0 because it requires
    https://github.com/mesonbuild/meson/pull/11024
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7688
    Fixes: 20c90fed5a0 ("rusticl: added")
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19778>

--- .gitlab-ci/container/debian/x86_build-base.sh
+++ .gitlab-ci/container/debian/x86_build-base.sh
@@ -78,8 +78,8 @@ apt-get install -y --no-remove \
 # Needed for ci-fairy, this revision is able to upload files to MinIO
 pip3 install git+http://gitlab.freedesktop.org/freedesktop/ci-templates@ffe4d1b10aab7534489f0c4bbc4c5899df17d3f2
 
-# We need at least 0.61.4 for proper Rust; 0.62 for modern meson env2mfile
-pip3 install meson==0.63.3
+# We need at least 1.0.0 for proper Rust; 0.62 for modern meson env2mfile
+pip3 install meson==1.0.0
 
 . .gitlab-ci/container/build-rust.sh
 
