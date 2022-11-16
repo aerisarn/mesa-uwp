aerisarn/mesa-uwp

commit 79ca456b4837b3bc21cf9ef3c03c505c4b4909f6

r600/sfn: rewrite NIR backend
This is a rewite of the NIR backend. it adds some optimization
and a scheduler.

v2: - replace some magic numbers by constants
    - make sure constructor is always used with new
    - use default initialization in more places
      (changes suggested by Filip Gawin)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17076>

src/gallium/drivers/r600/sfn/sfn_shader.h
@@ -1,0 +1,365 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "sfn_instr.h"
+#include "sfn_instrfactory.h"
+#include "sfn_instr_controlflow.h"
+#include "gallium/drivers/r600/r600_shader.h"
+#include "sfn_liverangeevaluator.h"
+
+#include <bitset>
+#include <memory>
+#include <stack>
+#include <vector>
+
+struct nir_shader;
+struct nir_cf_node;
+struct nir_if;
+struct nir_block;
+struct nir_instr;
+
+namespace r600 {
+
+class ShaderIO {
+public:
+   void set_sid(int sid);
+   void override_spi_sid(int spi_sid);
+   void print(std::ostream& os) const;
+
+   int spi_sid() const { return m_spi_sid;}
+   unsigned sid() const { return m_sid;}
+
+   int location() const {return m_location;}
+   unsigned name() const { return m_name;}
+
+   int pos() const { return m_pos;}
+   void set_pos(int pos) {m_pos = pos;}
+
+   bool is_param() const { return m_is_param;}
+   void set_is_param(bool val) { m_is_param = val;}
+
+   void set_gpr(int gpr) {m_gpr = gpr;}
+   int gpr() const {return m_gpr;}
+
+protected:
+   ShaderIO(const char *type, int loc, int name);
+
+private:
+
+   virtual void do_print(std::ostream& os) const = 0;
+
+   const char *m_type;
+   int m_location{-1};
+   int m_name{-1};
+   int m_sid{0};
+   int m_spi_sid{0};
+   int m_pos{0};
+   int m_is_param{false};
+   int m_gpr{0};
+};
+
+class ShaderOutput : public ShaderIO {
+public:
+   ShaderOutput();
+   ShaderOutput(int location, int name, int writemask);
+
+   int writemask() const { return m_writemask;}
+
+private:
+   void do_print(std::ostream& os) const override;
+
+   int m_writemask{0};
+};
+
+
+class ShaderInput : public ShaderIO  {
+public:
+   ShaderInput();
+   ShaderInput(int location, int name);
+   void set_interpolator(int interp, int interp_loc, bool uses_interpolate_at_centroid);
+   void set_uses_interpolate_at_centroid();
+   void set_need_lds_pos() { m_need_lds_pos = true;}
+   int ij_index() const { return m_ij_index;}
+
+   int interpolator() const{return m_interpolator;}
+   int interpolate_loc() const {return m_interpolate_loc;}
+   bool need_lds_pos() const {return m_need_lds_pos;}
+   int lds_pos() const {return m_lds_pos;}
+   void set_lds_pos(int pos) {m_lds_pos = pos;}
+
+   int ring_offset() const {return m_ring_offset;}
+   void set_ring_offset(int offs) {m_ring_offset = offs;}
+   bool uses_interpolate_at_centroid() const {return m_uses_interpolate_at_centroid;}
+
+private:
+   void do_print(std::ostream& os) const override;
+
+   int m_interpolator{0};
+   int m_interpolate_loc{0};
+   int m_ij_index{0};
+   bool m_uses_interpolate_at_centroid{false};
+   bool m_need_lds_pos{false};
+   int m_lds_pos{0};
+   int m_ring_offset{0};
+};
+
+class Shader : public Allocate {
+public:
+   using InputIterator = std::map<int, ShaderInput>::iterator;
+   using OutputIterator = std::map<int, ShaderOutput>::iterator;
+
+   using ShaderBlocks = std::list<Block::Pointer, Allocator<Block::Pointer>>;
+
+   Shader(const Shader& orig) = delete;
+
+   virtual ~Shader() {}
+
+   bool add_info_from_string(std::istream& is);
+
+   static Shader *translate_from_nir(nir_shader *nir, const pipe_stream_output_info *so_info, r600_shader *gs_shader,
+                                     r600_shader_key& key, r600_chip_class chip_class);
+
+   bool process(nir_shader *nir);
+
+   bool process_cf_node(nir_cf_node *node);
+   bool process_if(nir_if *node);
+   bool process_loop(nir_loop *node);
+   bool process_block(nir_block *node);
+   bool process_instr(nir_instr *instr);
+   void emit_instruction(PInst instr);
+   bool emit_atomic_local_shared(nir_intrinsic_instr* instr);
+
+   void print(std::ostream& os ) const;
+   void print_header(std::ostream& os ) const;
+
+   bool process_intrinsic(nir_intrinsic_instr *intr);
+
+   virtual bool load_input(nir_intrinsic_instr *intr) = 0;
+   virtual bool store_output(nir_intrinsic_instr *intr) = 0;
+
+   bool load_uniform(nir_intrinsic_instr *intr);
+   bool load_ubo(nir_intrinsic_instr *intr);
+
+   ValueFactory& value_factory();
+
+   void add_output(const ShaderOutput& output) {
+      m_outputs[output.location()] = output;
+   }
+
+   void add_input(const ShaderInput& input) {
+      m_inputs[input.location()] = input;
+   }
+
+   void set_input_gpr(int driver_lcation, int gpr);
+
+   InputIterator find_input(int location) { return m_inputs.find(location);}
+
+   InputIterator input_not_found() {return m_inputs.end();}
+
+   OutputIterator find_output(int location);
+   OutputIterator output_not_found() {return m_outputs.end();}
+
+   ShaderBlocks& func() { return m_root; }
+   void reset_function(ShaderBlocks& new_root);
+
+   void emit_instruction_from_string(const std::string &s);
+
+   void set_info(nir_shader *nir);
+   void get_shader_info(r600_shader *sh_info);
+
+   r600_chip_class chip_class() const {return m_chip_class;};
+   void set_chip_class(r600_chip_class cls) {m_chip_class = cls;};
+
+   void start_new_block(int nesting_depth);
+
+   const ShaderOutput& output(int base) const;
+
+   LiveRangeMap prepare_live_range_map();
+
+   void set_last_txd(Instr *txd){m_last_txd = txd;}
+   Instr *last_txd(){return m_last_txd;}
+
+   // Needed for keeping the memory access in order
+   void chain_scratch_read(Instr *instr);
+   void chain_ssbo_read(Instr *instr);
+
+   virtual uint32_t enabled_stream_buffers_mask() const {return 0;};
+
+   size_t noutputs() const { return m_outputs.size();}
+   size_t ninputs() const { return m_inputs.size();}
+
+   enum Flags {
+      sh_indirect_const_file,
+      sh_needs_scratch_space,
+      sh_needs_sbo_ret_address,
+      sh_uses_atomics,
+      sh_uses_images,
+      sh_uses_tex_buffer,
+      sh_writes_memory,
+      sh_txs_cube_array_comp,
+      sh_indirect_atomic,
+      sh_mem_barrier,
+      sh_flags_count
+   };
+
+   void set_flag(Flags f) {m_flags.set(f);}
+   bool has_flag(Flags f) const {return m_flags.test(f);}
+
+   int atomic_file_count() const { return m_atomic_file_count; }
+
+   PRegister atomic_update();
+   int remap_atomic_base(int base);
+   auto evaluate_resource_offset(nir_intrinsic_instr *instr, int src_id) -> std::pair<int, PRegister>;
+   int ssbo_image_offset() const {return m_ssbo_image_offset;}
+   PRegister rat_return_address() {assert(m_rat_return_address); return m_rat_return_address;}
+
+   PRegister emit_load_to_register(PVirtualValue src);
+
+protected:
+   enum ESlots {
+      es_face,
+      es_instanceid,
+      es_invocation_id,
+      es_patch_id,
+      es_pos,
+      es_rel_patch_id,
+      es_sample_mask_in,
+      es_sample_id,
+      es_sample_pos,
+      es_tess_factor_base,
+      es_vertexid,
+      es_tess_coord,
+      es_primitive_id,
+      es_helper_invocation,
+      es_last
+   };
+
+   std::bitset<es_last> m_sv_values;
+
+   Shader(const char *type_id);
+
+   const ShaderInput& input(int base) const;
+
+   bool emit_simple_mov(nir_dest& dest, int chan, PVirtualValue src, Pin pin = pin_free);
+
+private:
+   virtual bool process_stage_intrinsic(nir_intrinsic_instr *intr) = 0;
+
+   bool allocate_registers_from_string(std::istream& is, Pin pin);
+   bool allocate_arrays_from_string(std::istream& is);
+
+   bool read_chipclass(std::istream& is);
+
+   bool load_uniform_indirect(nir_intrinsic_instr *intr, PVirtualValue addr, int offset , int buffer_id);
+
+   bool scan_shader(const nir_function *impl);
+   bool scan_uniforms(nir_variable *uniform);
+   void allocate_reserved_registers();
+
+   void allocate_local_registers(const exec_list *registers);
+
+   virtual int do_allocate_reserved_registers() = 0;
+
+   bool scan_instruction(nir_instr *instr);
+   virtual bool do_scan_instruction(nir_instr *instr) = 0;
+
+   void print_properties(std::ostream& os) const;
+   virtual void do_print_properties(std::ostream& os) const = 0;
+
+   bool read_output(std::istream& is);
+   bool read_input(std::istream& is);
+   virtual bool read_prop(std::istream& is) = 0;
+
+   bool emit_if_start(nir_if *if_stmt);
+   bool emit_control_flow(ControlFlowInstr::CFType type);
+   bool emit_store_scratch(nir_intrinsic_instr *intr);
+   bool emit_load_scratch(nir_intrinsic_instr *intr);
+   bool emit_local_store(nir_intrinsic_instr *intr);
+   bool emit_local_load(nir_intrinsic_instr* instr);
+   bool emit_load_tcs_param_base(nir_intrinsic_instr* instr, int offset);
+   bool emit_barrier(nir_intrinsic_instr* intr);
+   bool emit_shader_clock(nir_intrinsic_instr* instr);
+   bool emit_wait_ack();
+
+   bool equal_to(const Shader& other) const;
+   void finalize();
+   virtual void do_finalize();
+
+   virtual void do_get_shader_info(r600_shader *sh_info);
+
+   ShaderBlocks m_root;
+   Block::Pointer m_current_block;
+
+   InstrFactory *m_instr_factory;
+   const char *m_type_id;
+
+   template <typename T>
+   using IOMap = std::map<int, T, std::less<int>, Allocator<std::pair<const int, T>>>;
+
+   IOMap<ShaderOutput> m_outputs;
+   IOMap<ShaderInput> m_inputs;
+   r600_chip_class m_chip_class;
+
+   int m_scratch_size;
+   int m_next_block;
+   bool m_indirect_const_file{false};
+
+   Instr *m_last_txd {nullptr};
+
+   uint32_t m_indirect_files{0};      
+   std::bitset<sh_flags_count> m_flags;
+   uint32_t nhwatomic_ranges{0};
+   std::vector<r600_shader_atomic> m_atomics;
+
+   uint32_t m_nhwatomic{0};
+   uint32_t m_atomic_base{0};
+   uint32_t m_next_hwatomic_loc{0};
+   std::unordered_map<int, int> m_atomic_base_map;
+   uint32_t m_atomic_file_count{0};
+   PRegister m_atomic_update{nullptr};
+   PRegister m_rat_return_address{nullptr};
+
+   int32_t m_ssbo_image_offset{0};
+   uint32_t m_nloops{0};
+
+   class InstructionChain : public InstrVisitor {
+   public:
+      void visit(AluInstr  *instr) override {(void) instr;}
+      void visit(AluGroup *instr) override {(void) instr;}
+      void visit(TexInstr *instr) override {(void) instr;}
+      void visit(ExportInstr *instr) override {(void) instr;}
+      void visit(FetchInstr *instr) override {(void) instr;}
+      void visit(Block *instr) override {(void) instr;}
+      void visit(ControlFlowInstr *instr) override {(void) instr;}
+      void visit(IfInstr *instr) override {(void) instr;}
+      void visit(StreamOutInstr *instr) override {(void) instr;}
+      void visit(MemRingOutInstr *instr) override {(void) instr;}
+      void visit(EmitVertexInstr *instr) override {(void) instr;}
+      void visit(WriteTFInstr *instr) override {(void) instr;}
+      void visit(LDSAtomicInstr *instr) override {(void) instr;}
+      void visit(LDSReadInstr *instr) override {(void) instr;}
+
+      void visit(WriteScratchInstr *instr) override;
+      void visit(GDSInstr *instr) override;
+      void visit(RatInstr *instr) override;
+
+      void apply(Instr *current, Instr **last);
+
+      Shader *this_shader{nullptr};
+      Instr *last_scratch_instr{nullptr};
+      Instr *last_gds_instr{nullptr};
+      Instr *last_ssbo_instr{nullptr};
+      bool prepare_mem_barrier{false};
+   };
+
+   InstructionChain m_chain_instr;
+   std::vector<Instr *> m_loops;
+};
+
+
+std::pair<unsigned, unsigned>
+r600_get_varying_semantic(unsigned varying_location);
+
+}
+
+#endif // SHADER_H

src/gallium/drivers/r600/sfn/sfn_shader_cs.cpp
@@ -1,0 +1,95 @@
+#include "sfn_shader_cs.h"
+#include "sfn_instr_fetch.h"
+
+
+namespace r600 {
+
+ComputeShader::ComputeShader(UNUSED const r600_shader_key& key):
+   Shader("CS")
+{
+
+}
+
+bool ComputeShader::do_scan_instruction(UNUSED nir_instr *instr)
+{
+   return false;
+}
+
+int ComputeShader::do_allocate_reserved_registers()
+{
+   auto& vf = value_factory();
+
+   const int thread_id_sel = 0;
+   const int wg_id_sel = 1;
+
+   for (int i = 0; i < 3; ++i) {
+      m_local_invocation_id[i] = vf.allocate_pinned_register(thread_id_sel, i);
+      m_local_invocation_id[i]->pin_live_range(true);
+
+      m_workgroup_id[i] = vf.allocate_pinned_register(wg_id_sel, i);
+      m_workgroup_id[i]->pin_live_range(true);
+   }
+   return 2;
+}
+
+bool ComputeShader::process_stage_intrinsic(nir_intrinsic_instr *instr)
+{
+   switch (instr->intrinsic) {
+   case nir_intrinsic_load_local_invocation_id:
+      return emit_load_3vec(instr, m_local_invocation_id);
+   case nir_intrinsic_load_workgroup_id:
+      return emit_load_3vec(instr, m_workgroup_id);
+   case nir_intrinsic_load_num_workgroups:
+      return emit_load_num_workgroups(instr);
+   default:
+      return false;
+   }
+}
+
+void ComputeShader::do_get_shader_info(r600_shader *sh_info)
+{
+   sh_info->processor_type = PIPE_SHADER_COMPUTE;
+}
+
+bool ComputeShader::read_prop(UNUSED std::istream& is)
+{
+   return true;
+}
+
+void ComputeShader::do_print_properties(UNUSED std::ostream& os) const
+{
+
+}
+
+bool ComputeShader::emit_load_num_workgroups(nir_intrinsic_instr* instr)
+{
+   auto zero = value_factory().temp_register();
+
+   emit_instruction(new AluInstr(op1_mov, zero, value_factory().inline_const(ALU_SRC_0, 0),
+                                 AluInstr::last_write));
+   auto dest = value_factory().dest_vec4(instr->dest, pin_group);
+
+   auto ir = new LoadFromBuffer(dest, {0,1,2,7}, zero, 16,
+                                R600_BUFFER_INFO_CONST_BUFFER,
+                                nullptr, fmt_32_32_32_32);
+
+   ir->set_fetch_flag(LoadFromBuffer::srf_mode);
+   ir->reset_fetch_flag(LoadFromBuffer::format_comp_signed);
+   ir->set_num_format(vtx_nf_int);
+   emit_instruction(ir);
+   return true;
+
+}
+
+bool ComputeShader::emit_load_3vec(nir_intrinsic_instr* instr, const std::array<PRegister,3>& src)
+{
+   auto& vf = value_factory();
+
+   for (int i = 0; i < 3; ++i) {
+      auto dest = vf.dest(instr->dest, i, pin_none);
+      emit_instruction(new AluInstr(op1_mov, dest, src[i], i == 2 ? AluInstr::last_write : AluInstr::write));
+   }
+   return true;
+}
+
+}

src/gallium/drivers/r600/sfn/sfn_shader_gs.cpp
@@ -1,0 +1,370 @@
+#include "sfn_shader_gs.h"
+#include "sfn_instr_fetch.h"
+#include "sfn_debug.h"
+
+namespace r600 {
+
+GeometryShader::GeometryShader(const r600_shader_key& key):
+   Shader("GS"),
+   m_tri_strip_adj_fix(key.gs.tri_strip_adj_fix)
+{
+
+}
+
+bool GeometryShader::do_scan_instruction(nir_instr *instr)
+{
+   if (instr->type != nir_instr_type_intrinsic)
+      return false;
+
+   nir_intrinsic_instr *ii =  nir_instr_as_intrinsic(instr);
+
+   switch (ii->intrinsic) {
+   case nir_intrinsic_store_output:
+      return process_store_output(ii);
+   case nir_intrinsic_load_per_vertex_input:
+      return process_load_input(ii);
+   default:
+      return false;
+   }  
+}
+
+bool GeometryShader::process_store_output(nir_intrinsic_instr *instr)
+{
+   auto location = nir_intrinsic_io_semantics(instr).location;
+   auto index = nir_src_as_const_value(instr->src[1]);
+   assert(index);
+
+   auto driver_location = nir_intrinsic_base(instr) + index->u32;
+
+   if (location == VARYING_SLOT_COL0 ||
+       location == VARYING_SLOT_COL1 ||
+       (location >= VARYING_SLOT_VAR0 &&
+       location <= VARYING_SLOT_VAR31) ||
+       (location >= VARYING_SLOT_TEX0 &&
+       location <= VARYING_SLOT_TEX7) ||
+       location == VARYING_SLOT_BFC0 ||
+       location == VARYING_SLOT_BFC1 ||
+       location == VARYING_SLOT_PNTC ||
+       location == VARYING_SLOT_CLIP_VERTEX ||
+       location == VARYING_SLOT_CLIP_DIST0 ||
+       location == VARYING_SLOT_CLIP_DIST1 ||
+       location == VARYING_SLOT_PRIMITIVE_ID ||
+       location == VARYING_SLOT_POS ||
+       location == VARYING_SLOT_PSIZ ||
+       location == VARYING_SLOT_LAYER ||
+       location == VARYING_SLOT_VIEWPORT ||
+       location == VARYING_SLOT_FOGC) {
+
+      auto semantic = r600_get_varying_semantic(location);
+      tgsi_semantic name = (tgsi_semantic)semantic.first;
+      auto write_mask = nir_intrinsic_write_mask(instr);
+      ShaderOutput output(driver_location, name, write_mask);
+      output.set_sid(semantic.second);
+      add_output(output);
+
+      if (location == VARYING_SLOT_CLIP_DIST0 ||
+          location == VARYING_SLOT_CLIP_DIST1) {
+         m_clip_dist_mask |= 1 << (location - VARYING_SLOT_CLIP_DIST0);
+      }
+
+      if (location == VARYING_SLOT_VIEWPORT) {
+         m_out_viewport = true;
+         m_out_misc_write = true;
+
+      }
+      if (m_noutputs <= driver_location)
+         m_noutputs = driver_location + 1;
+
+      return true;
+   }
+   return false;
+}
+
+bool GeometryShader::process_load_input(nir_intrinsic_instr *instr)
+{
+   auto location = nir_intrinsic_io_semantics(instr).location;
+   auto index = nir_src_as_const_value(instr->src[1]);
+   assert(index);
+
+   auto driver_location = nir_intrinsic_base(instr) + index->u32;
+
+   if (location == VARYING_SLOT_POS ||
+       location == VARYING_SLOT_PSIZ ||
+       location == VARYING_SLOT_FOGC ||
+       location == VARYING_SLOT_CLIP_VERTEX ||
+       location == VARYING_SLOT_CLIP_DIST0 ||
+       location == VARYING_SLOT_CLIP_DIST1 ||
+       location == VARYING_SLOT_COL0 ||
+       location == VARYING_SLOT_COL1 ||
+       location == VARYING_SLOT_BFC0 ||
+       location == VARYING_SLOT_BFC1 ||
+       location == VARYING_SLOT_PNTC ||
+       (location >= VARYING_SLOT_VAR0 &&
+        location <= VARYING_SLOT_VAR31) ||
+       (location >= VARYING_SLOT_TEX0 &&
+       location <= VARYING_SLOT_TEX7)) {
+
+      uint64_t bit = 1ull << location;
+      if (!(bit & m_input_mask)) {
+         auto semantic = r600_get_varying_semantic(location);
+         ShaderInput input(driver_location, semantic.first);
+         input.set_sid(semantic.second);
+         input.set_ring_offset(16 * driver_location);
+         add_input(input);
+         m_next_input_ring_offset += 16;
+         m_input_mask |= bit;
+      }
+      return true;
+   }
+   return false;
+}
+
+int GeometryShader::do_allocate_reserved_registers()
+{
+   const int sel[6] = {0, 0 ,0, 1, 1, 1};
+   const int chan[6] = {0, 1 ,3, 0, 1, 2};
+
+   /* Reserve registers used by the shaders (should check how many
+    * components are actually used */
+   for (int i = 0; i < 6; ++i) {
+      m_per_vertex_offsets[i] = value_factory().allocate_pinned_register(sel[i], chan[i]);
+      m_per_vertex_offsets[i]->pin_live_range(true);
+   }
+
+   m_primitive_id = value_factory().allocate_pinned_register(0, 2);
+   m_primitive_id->pin_live_range(true);
+   m_invocation_id = value_factory().allocate_pinned_register(1, 3);
+   m_invocation_id->pin_live_range(true);
+
+   value_factory().set_virtual_register_base(2);
+
+   auto zero = value_factory().inline_const(ALU_SRC_0, 0);
+
+   for (int i = 0; i < 4; ++i) {
+      m_export_base[i] = value_factory().temp_register(0, false);
+      emit_instruction(new AluInstr(op1_mov, m_export_base[i], zero, AluInstr::last_write));
+   }
+
+   m_ring_item_sizes[0] = m_next_input_ring_offset;
+
+   if (m_tri_strip_adj_fix)
+      emit_adj_fix();
+
+   return value_factory().next_register_index();
+}
+
+bool GeometryShader::process_stage_intrinsic(nir_intrinsic_instr *intr)
+{
+   switch (intr->intrinsic) {
+   case nir_intrinsic_emit_vertex:
+      return emit_vertex(intr, false);
+   case nir_intrinsic_end_primitive:
+      return emit_vertex(intr, true);
+   case nir_intrinsic_load_primitive_id:
+      return emit_simple_mov(intr->dest, 0, m_primitive_id);
+   case nir_intrinsic_load_invocation_id:
+      return emit_simple_mov(intr->dest, 0, m_invocation_id);
+   case nir_intrinsic_load_per_vertex_input:
+      return emit_load_per_vertex_input(intr);
+   default:
+      ;
+   }
+   return false;
+}
+
+bool GeometryShader::emit_vertex(nir_intrinsic_instr* instr, bool cut)
+{
+   int stream = nir_intrinsic_stream_id(instr);
+   assert(stream < 4);
+
+   auto cut_instr = new EmitVertexInstr(stream, cut);
+
+   for(auto v: m_streamout_data) {
+      if (stream == 0 || v.first != VARYING_SLOT_POS) {
+         v.second->patch_ring(stream, m_export_base[stream]);
+         cut_instr->add_required_instr(v.second);
+         emit_instruction(v.second);
+      } else
+         delete v.second;
+   }
+   m_streamout_data.clear();
+
+   emit_instruction(cut_instr);
+   start_new_block(0);
+
+   if (!cut) {
+      auto ir = new AluInstr(op2_add_int, m_export_base[stream], m_export_base[stream],
+                             value_factory().literal(m_noutputs),
+                             AluInstr::last_write);
+      //ir->add_required_instr(cut_instr);
+      emit_instruction(ir);
+   }
+
+
+
+   return true;
+}
+
+bool GeometryShader::store_output(nir_intrinsic_instr* instr)
+{
+   auto location = nir_intrinsic_io_semantics(instr).location;
+   auto index = nir_src_as_const_value(instr->src[1]);
+   assert(index);
+   auto driver_location = nir_intrinsic_base(instr) + index->u32;
+
+   uint32_t write_mask = nir_intrinsic_write_mask(instr);
+   uint32_t shift = nir_intrinsic_component(instr);
+
+   RegisterVec4::Swizzle src_swz {7,7,7,7};
+   for (unsigned i = shift; i < 4; ++i) {
+      src_swz[i] = (1 << i) & (write_mask << shift) ? i  - shift: 7;
+   }
+
+   auto out_value = value_factory().src_vec4(instr->src[0], pin_group, src_swz);
+
+   AluInstr *ir = nullptr;
+   if (m_streamout_data[location]) {
+      auto value = m_streamout_data[location]->value();
+      auto tmp = value_factory().temp_vec4(pin_group);
+
+      for (unsigned i = 0; i < 4 - shift; ++i) {
+         if (!(write_mask & (1 << i)))
+            continue;
+         if (value[i]->chan() < 4) {
+            ir = new AluInstr(op1_mov, tmp[i], value[src_swz[i]], AluInstr::write);
+         } else if (out_value[i]->chan() < 4) {
+            ir = new AluInstr(op1_mov, tmp[i], out_value[i], AluInstr::write);
+         } else
+            continue;
+         emit_instruction(ir);
+      }
+      ir->set_alu_flag(alu_last_instr);
+      m_streamout_data[location] = new MemRingOutInstr(cf_mem_ring, MemRingOutInstr::mem_write_ind, tmp,
+                                                       4 * driver_location,
+                                                       instr->num_components, m_export_base[0]);
+   } else {
+
+      sfn_log << SfnLog::io << "None-streamout ";
+      bool need_copy =  shift != 0;
+      if (!need_copy) {
+         for (int i = 0; i < 4; ++i) {
+            if ((write_mask & (1 << i)) && (out_value[i]->chan() != i)) {
+               need_copy = true;
+               break;
+            }
+         }
+      }
+
+      if (need_copy) {
+         auto tmp = value_factory().temp_vec4(pin_group);
+         for (unsigned i = 0; i < 4 - shift; ++i) {
+            if (out_value[i]->chan() < 4) {
+               ir = new AluInstr(op1_mov, tmp[i], out_value[i], AluInstr::write);
+               emit_instruction(ir);
+            }
+         }
+         ir->set_alu_flag(alu_last_instr);
+         m_streamout_data[location] = new MemRingOutInstr(cf_mem_ring, MemRingOutInstr::mem_write_ind, tmp,
+                                                          4 * driver_location,
+                                                          instr->num_components, m_export_base[0]);
+      } else {
+         m_streamout_data[location] = new MemRingOutInstr(cf_mem_ring, MemRingOutInstr::mem_write_ind, out_value,
+                                                          4 * driver_location,
+                                                          instr->num_components, m_export_base[0]);
+      }
+   }
+
+
+
+
+   return true;
+}
+
+bool GeometryShader::emit_load_per_vertex_input(nir_intrinsic_instr* instr)
+{
+   auto dest = value_factory().dest_vec4(instr->dest, pin_group);
+
+   RegisterVec4::Swizzle dest_swz{7,7,7,7};
+   for (unsigned i = 0; i < nir_dest_num_components(instr->dest); ++i) {
+      dest_swz[i] = i + nir_intrinsic_component(instr);
+   }
+
+   auto literal_index = nir_src_as_const_value(instr->src[0]);
+
+   if (!literal_index) {
+      sfn_log << SfnLog::err << "GS: Indirect input addressing not (yet) supported\n";
+      return false;
+   }
+   assert(literal_index->u32 < 6);
+   assert(nir_intrinsic_io_semantics(instr).num_slots == 1);
+
+   auto addr = m_per_vertex_offsets[literal_index->u32];
+   auto fetch = new LoadFromBuffer(dest, dest_swz, addr,
+                                   16 * nir_intrinsic_base(instr),
+                                   R600_GS_RING_CONST_BUFFER, nullptr,
+                                   fmt_invalid);
+
+   fetch->set_fetch_flag(FetchInstr::use_const_field);
+   fetch->set_num_format(vtx_nf_norm);
+   fetch->reset_fetch_flag(FetchInstr::format_comp_signed);
+
+   emit_instruction(fetch);
+   return true;
+}
+
+void GeometryShader::do_finalize()
+{
+
+}
+
+void GeometryShader::do_get_shader_info(r600_shader *sh_info)
+{
+   sh_info->processor_type = PIPE_SHADER_GEOMETRY;
+   sh_info->ring_item_sizes[0] =  m_ring_item_sizes[0];
+}
+
+bool GeometryShader::read_prop(std::istream& is)
+{
+   (void)is;
+   return true;
+}
+
+void GeometryShader::do_print_properties(std::ostream& os) const
+{
+   (void)os;
+}
+
+void GeometryShader::emit_adj_fix()
+{
+   auto adjhelp0 = value_factory().temp_register();
+
+   emit_instruction(new AluInstr(op2_and_int, adjhelp0,
+                                 m_primitive_id, value_factory().one_i(),
+                                 AluInstr::last_write));
+
+   int reg_indices[6];
+   int rotate_indices[6] = {4, 5, 0, 1, 2, 3};
+
+   reg_indices[0] = reg_indices[1] = reg_indices[2] = m_export_base[1]->sel();
+   reg_indices[3] = reg_indices[4] = reg_indices[5] = m_export_base[2]->sel();
+
+   std::array<PRegister, 6> adjhelp;
+
+   AluInstr *ir = nullptr;
+   for (int i = 0; i < 6; i++) {
+      adjhelp[i] = value_factory().temp_register();
+      ir = new AluInstr(op3_cnde_int, adjhelp[i],
+                        adjhelp0, m_per_vertex_offsets[i],
+                        m_per_vertex_offsets[rotate_indices[i]],
+                        AluInstr::write);
+
+      emit_instruction(ir);
+   }
+   ir->set_alu_flag(alu_last_instr);
+
+   for (int i = 0; i < 6; i++)
+      m_per_vertex_offsets[i] = adjhelp[i];
+}
+
+
+}

commit 0ccf7ed7916bee67c8028bf251ca483b1be07d57

r600/sfn: Honor shader key w.r.t. atomic counter layout
Fixes: 79ca456b4837b3bc21cf9ef3c03c505c4b4909f6
        r600/sfn: rewrite NIR backend

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19755>

src/gallium/drivers/r600/sfn/sfn_shader.cpp
@@ -171,11 +171,12 @@ ShaderInput::set_uses_interpolate_at_centroid()
    m_uses_interpolate_at_centroid = true;
 }
 
-Shader::Shader(const char *type_id):
+Shader::Shader(const char *type_id, unsigned atomic_base):
     m_current_block(nullptr),
     m_type_id(type_id),
     m_chip_class(ISA_CC_R600),
-    m_next_block(0)
+    m_next_block(0),
+    m_atomic_base(atomic_base)
 {
    m_instr_factory = new InstrFactory();
    m_chain_instr.this_shader = this;

src/gallium/drivers/r600/sfn/sfn_shader.h
@@ -264,7 +264,7 @@ protected:
 
    std::bitset<es_last> m_sv_values;
 
-   Shader(const char *type_id);
+   Shader(const char *type_id, unsigned atomic_base);
 
    const ShaderInput& input(int base) const;
 

src/gallium/drivers/r600/sfn/sfn_shader_cs.cpp
@@ -31,7 +31,7 @@
 namespace r600 {
 
 ComputeShader::ComputeShader(UNUSED const r600_shader_key& key):
-    Shader("CS")
+    Shader("CS", 0)
 {
 }
 

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -40,7 +40,7 @@ namespace r600 {
 using std::string;
 
 FragmentShader::FragmentShader(const r600_shader_key& key):
-    Shader("FS"),
+    Shader("FS", key.ps.first_atomic_counter),
     m_dual_source_blend(key.ps.dual_source_blend),
     m_max_color_exports(MAX2(key.ps.nr_cbufs, 1)),
     m_export_highest(0),

src/gallium/drivers/r600/sfn/sfn_shader_gs.cpp
@@ -32,7 +32,7 @@
 namespace r600 {
 
 GeometryShader::GeometryShader(const r600_shader_key& key):
-    Shader("GS"),
+    Shader("GS", key.gs.first_atomic_counter),
     m_tri_strip_adj_fix(key.gs.tri_strip_adj_fix)
 {
 }

src/gallium/drivers/r600/sfn/sfn_shader_tess.cpp
@@ -36,7 +36,7 @@ namespace r600 {
 using std::string;
 
 TCSShader::TCSShader(const r600_shader_key& key):
-    Shader("TCS"),
+    Shader("TCS", key.tcs.first_atomic_counter),
     m_tcs_prim_mode(key.tcs.prim_mode)
 {
 }
@@ -177,7 +177,7 @@ TCSShader::do_print_properties(std::ostream& os) const
 TESShader::TESShader(const pipe_stream_output_info *so_info,
                      const r600_shader *gs_shader,
                      const r600_shader_key& key):
-    VertexStageShader("TES"),
+    VertexStageShader("TES", key.tes.first_atomic_counter),
     m_vs_as_gs_a(key.vs.as_gs_a),
     m_tes_as_es(key.tes.as_es)
 {

src/gallium/drivers/r600/sfn/sfn_shader_vs.cpp
@@ -414,7 +414,7 @@ VertexExportForFs::output_register(int loc) const
 VertexShader::VertexShader(const pipe_stream_output_info *so_info,
                            r600_shader *gs_shader,
                            r600_shader_key& key):
-    VertexStageShader("VS"),
+    VertexStageShader("VS", key.vs.first_atomic_counter),
     m_vs_as_gs_a(key.vs.as_gs_a)
 {
    if (key.vs.as_es)