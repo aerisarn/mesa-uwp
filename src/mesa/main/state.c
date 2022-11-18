b6e800af7d54c3fb838f1f872fb8be1bef7ba1fa
Marek Olšák <marek.olsak@amd.com>
2022-11-18 18:59:46 -0500

    mesa: don't call _mesa_set_varying_vp_inputs in set_vertex_processing_mode
    It's redundant because it's always called in _mesa_update_draw_vao.
    
    Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19953>

--- src/mesa/main/state.c
+++ src/mesa/main/state.c
@@ -684,12 +684,6 @@ set_vertex_processing_mode(struct gl_context *ctx, gl_vertex_processing_mode m)
    default:
       assert(0);
    }
-
-   /* Since we only track the varying inputs while being in fixed function
-    * vertex processing mode, we may need to update fixed-func shaders
-    * for zero-stride vertex attribs.
-    */
-   _mesa_set_varying_vp_inputs(ctx, ctx->Array._DrawVAOEnabledAttribs);
 }
 
 
