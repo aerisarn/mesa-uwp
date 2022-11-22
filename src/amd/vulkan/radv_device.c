bdb7fd69d659dee5c8bf0a05eef9fc3f330704c0
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-11-22 10:12:06 +0100

    radv: mark extendedDynamicState3SampleLocationsEnable as not required by Zink
    Only dynamic blend equation support is still required.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19924>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -1946,7 +1946,7 @@ radv_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
          features->extendedDynamicState3ColorWriteMask = true;
          features->extendedDynamicState3RasterizationSamples = true;
          features->extendedDynamicState3ColorBlendEquation = false; /* TODO: Zink */
-         features->extendedDynamicState3SampleLocationsEnable = false; /* TODO: Zink */
+         features->extendedDynamicState3SampleLocationsEnable = false; /* TODO */
          features->extendedDynamicState3LineRasterizationMode = true;
          features->extendedDynamicState3ExtraPrimitiveOverestimationSize = false;
          features->extendedDynamicState3AlphaToOneEnable = false;
