aerisarn/mesa-uwp

commit 0186e9e1c51b1de5d47ac10854c048cb83e81f4b

mesa: always support occlusion queries
Excerpt from ARB_occlusion_query.txt:
  An implementation can either set QUERY_COUNTER_BITS_ARB to the
  value 0, or to some number greater than or equal to n.  If an
  implementation returns 0 for QUERY_COUNTER_BITS_ARB, then the
  occlusion queries will always return that zero samples passed the
  occlusion test, and so an application should not use occlusion queries
  on that implementation.

This looks more sane for drivers wanting desktop gl 1.5 without real
hw support then just faking it.

Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14361>

src/mesa/main/queryobj.c
@@ -31,6 +31,8 @@
 
 #include "queryobj.h"
 #include "mtypes.h"
+#include "pipe/p_context.h"
+#include "pipe/p_screen.h"
 #include "util/u_memory.h"
 
 #include "state_tracker/st_cb_queryobj.h"
@@ -914,10 +916,16 @@ _mesa_GetQueryBufferObjectui64v(GLuint id, GLuint buffer, GLenum pname,
 void
 _mesa_init_queryobj(struct gl_context *ctx)
 {
+   struct pipe_screen *screen = ctx->pipe->screen;
+
    ctx->Query.QueryObjects = _mesa_NewHashTable();
    ctx->Query.CurrentOcclusionObject = NULL;
 
-   ctx->Const.QueryCounterBits.SamplesPassed = 64;
+   if (screen->get_param(screen, PIPE_CAP_OCCLUSION_QUERY))
+      ctx->Const.QueryCounterBits.SamplesPassed = 64;
+   else
+      ctx->Const.QueryCounterBits.SamplesPassed = 0;
+
    ctx->Const.QueryCounterBits.TimeElapsed = 64;
    ctx->Const.QueryCounterBits.Timestamp = 64;
    ctx->Const.QueryCounterBits.PrimitivesGenerated = 64;

src/mesa/state_tracker/st_context.c
@@ -933,12 +933,12 @@ st_create_context(gl_api api, struct pipe_context *pipe,
       return NULL;
    memset(ctx, 0, sizeof(*ctx));
 
+   ctx->pipe = pipe;
    if (!_mesa_initialize_context(ctx, api, visual, shareCtx, &funcs)) {
       align_free(ctx);
       return NULL;
    }
 
-   ctx->pipe = pipe;
    st_debug_init();
 
    if (pipe->screen->get_disk_shader_cache)

src/mesa/main/extensions.c
@@ -277,6 +277,7 @@ _mesa_init_extensions(struct gl_extensions *extensions)
    extensions->ARB_internalformat_query = GL_TRUE;
    extensions->ARB_internalformat_query2 = GL_TRUE;
    extensions->ARB_map_buffer_range = GL_TRUE;
+   extensions->ARB_occlusion_query = GL_TRUE;
    extensions->ARB_sync = GL_TRUE;
    extensions->ARB_texture_env_crossbar = GL_TRUE;
    extensions->ARB_vertex_program = GL_TRUE;

src/mesa/state_tracker/st_extensions.c
@@ -798,7 +798,6 @@ void st_init_extensions(struct pipe_screen *screen,
       { o(ARB_gl_spirv),                     PIPE_CAP_GL_SPIRV                         },
       { o(ARB_indirect_parameters),          PIPE_CAP_MULTI_DRAW_INDIRECT_PARAMS       },
       { o(ARB_instanced_arrays),             PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR  },
-      { o(ARB_occlusion_query),              PIPE_CAP_OCCLUSION_QUERY                  },
       { o(ARB_occlusion_query2),             PIPE_CAP_OCCLUSION_QUERY                  },
       { o(ARB_pipeline_statistics_query),    PIPE_CAP_QUERY_PIPELINE_STATISTICS        },
       { o(ARB_pipeline_statistics_query),    PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE },

commit 1b1e8873fe90e878f014140b3b6bd1e5dbfb5a4c

mesa: treat unsupported queries as dummies
It's legal in OpenGL to start a query even if the result will have zero
valid bits. It's not enough to just report zero bits, We need to also
prevent calling down into the driver with these invalid queries.

Because ARB_ES3_compatibility adds ANY_SAMPLES_PASSED and
ANY_SAMPLES_PASSED_CONSERVATIVE to the set of queries that support zero
bits, we also need to check for the corresponding indices.

Fixes: 0186e9e1c51 ("mesa: always support occlusion queries")
Reviewed-by: Soroush Kashani <soroush.kashani@imgtec.com>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19750>

src/mesa/main/queryobj.c
@@ -124,6 +124,21 @@ target_to_index(const struct gl_query_object *q)
    return 0;
 }
 
+static bool
+query_type_is_dummy(struct gl_context *ctx, unsigned type)
+{
+   struct st_context *st = st_context(ctx);
+   switch (type) {
+   case PIPE_QUERY_OCCLUSION_COUNTER:
+   case PIPE_QUERY_OCCLUSION_PREDICATE:
+   case PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE:
+      return !st->has_occlusion_query;
+   default:
+      break;
+   }
+   return false;
+}
+
 static void
 begin_query(struct gl_context *ctx, struct gl_query_object *q)
 {
@@ -198,7 +213,12 @@ begin_query(struct gl_context *ctx, struct gl_query_object *q)
       if (q->pq_begin)
          ret = pipe->end_query(pipe, q->pq_begin);
    } else {
-      if (!q->pq) {
+      if (query_type_is_dummy(ctx, type)) {
+         /* starting a dummy-query; ignore */
+         assert(!q->pq);
+         q->type = type;
+         ret = true;
+      } else if (!q->pq) {
          q->pq = pipe->create_query(pipe, type, target_to_index(q));
          q->type = type;
       }
@@ -237,7 +257,10 @@ end_query(struct gl_context *ctx, struct gl_query_object *q)
       q->type = PIPE_QUERY_TIMESTAMP;
    }
 
-   if (q->pq)
+   if (query_type_is_dummy(ctx, q->type)) {
+      /* ending a dummy-query; ignore */
+      ret = true;
+   } else if (q->pq)
       ret = pipe->end_query(pipe, q->pq);
 
    if (!ret) {
@@ -258,8 +281,10 @@ get_query_result(struct pipe_context *pipe,
    union pipe_query_result data;
 
    if (!q->pq) {
-      /* Only needed in case we failed to allocate the gallium query earlier.
-       * Return TRUE so we don't spin on this forever.
+      /* Needed in case we failed to allocate the gallium query earlier, or
+       * in the case of a dummy query.
+       *
+       * Return TRUE in either case so we don't spin on this forever.
        */
       return TRUE;
    }
@@ -430,8 +455,9 @@ store_query_result(struct gl_context *ctx, struct gl_query_object *q,
       index = 0;
    }
 
-   pipe->get_query_result_resource(pipe, q->pq, flags, result_type, index,
-                                   buf->buffer, offset);
+   if (q->pq)
+      pipe->get_query_result_resource(pipe, q->pq, flags, result_type, index,
+                                      buf->buffer, offset);
 }
 
 static struct gl_query_object **

src/mesa/state_tracker/st_context.c
@@ -625,6 +625,8 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
       screen->get_param(screen, PIPE_CAP_MULTI_DRAW_INDIRECT);
    st->has_indirect_partial_stride =
       screen->get_param(screen, PIPE_CAP_MULTI_DRAW_INDIRECT_PARTIAL_STRIDE);
+   st->has_occlusion_query =
+      screen->get_param(screen, PIPE_CAP_OCCLUSION_QUERY);
    st->has_single_pipe_stat =
       screen->get_param(screen, PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE);
    st->has_indep_blend_func =

src/mesa/state_tracker/st_context.h
@@ -156,6 +156,7 @@ struct st_context
    boolean has_half_float_packing;
    boolean has_multi_draw_indirect;
    boolean has_indirect_partial_stride;
+   boolean has_occlusion_query;
    boolean has_single_pipe_stat;
    boolean has_indep_blend_func;
    boolean needs_rgb_dst_alpha_override;