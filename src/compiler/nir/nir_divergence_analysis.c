4fb33124c32a9445a8013f61dccb0360c956efa5
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-11-29 13:52:43 -0600

    nir/divergence: Handle base_workgroup_id and workgrpu_id_zero_base
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20068>

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -203,6 +203,7 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_load_lds_ngg_scratch_base_amd:
    case nir_intrinsic_load_lds_ngg_gs_out_vertex_base_amd:
    case nir_intrinsic_load_btd_shader_type_intel:
+   case nir_intrinsic_load_base_workgroup_id:
       is_divergent = false;
       break;
 
@@ -314,6 +315,7 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
 
    case nir_intrinsic_load_workgroup_index:
    case nir_intrinsic_load_workgroup_id:
+   case nir_intrinsic_load_workgroup_id_zero_base:
       assert(gl_shader_stage_uses_workgroup(stage));
       if (stage == MESA_SHADER_COMPUTE)
          is_divergent |= (options & nir_divergence_multiple_workgroup_per_compute_subgroup);
