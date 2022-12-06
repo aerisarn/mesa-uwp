3b579a2ea85e4a7955dbd2e01a6e8f469b905f4c
Ian Romanick <ian.d.romanick@intel.com>
2022-12-06 12:16:12 -0800

    intel/compiler: Validate 3-source instruction source strides
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20527>

--- src/intel/compiler/brw_fs_validate.cpp
+++ src/intel/compiler/brw_fs_validate.cpp
@@ -54,6 +54,20 @@
       }                                                                 \
    }
 
+#define fsv_assert_ne(first, second)                                    \
+   {                                                                    \
+      unsigned f = (first);                                             \
+      unsigned s = (second);                                            \
+      if (f == s) {                                                     \
+         fprintf(stderr, "ASSERT: Scalar %s validation failed!\n", stage_abbrev); \
+         dump_instruction(inst, stderr);                                \
+         fprintf(stderr, "%s:%d: A != B failed\n", __FILE__, __LINE__); \
+         fprintf(stderr, "  A = %s = %u\n", #first, f);                 \
+         fprintf(stderr, "  B = %s = %u\n", #second, s);                \
+         abort();                                                       \
+      }                                                                 \
+   }
+
 #define fsv_assert_lte(first, second)                                   \
    {                                                                    \
       unsigned f = (first);                                             \
@@ -99,6 +113,58 @@ fs_visitor::validate()
 
          fsv_assert((integer_sources == 3 && float_sources == 0) ||
                     (integer_sources == 0 && float_sources == 3));
+
+         if (devinfo->ver >= 10) {
+            for (unsigned i = 0; i < 3; i++) {
+               if (inst->src[i].file == BRW_IMMEDIATE_VALUE)
+                  continue;
+
+               switch (inst->src[i].vstride) {
+               case BRW_VERTICAL_STRIDE_0:
+               case BRW_VERTICAL_STRIDE_4:
+               case BRW_VERTICAL_STRIDE_8:
+               case BRW_VERTICAL_STRIDE_16:
+                  break;
+
+               case BRW_VERTICAL_STRIDE_1:
+                  fsv_assert_lte(12, devinfo->ver);
+                  break;
+
+               case BRW_VERTICAL_STRIDE_2:
+                  fsv_assert_lte(devinfo->ver, 11);
+                  break;
+
+               default:
+                  fsv_assert(!"invalid vstride");
+                  break;
+               }
+            }
+         } else if (grf_used != 0) {
+            /* Only perform the pre-Gfx10 checks after register allocation has
+             * occured.
+             *
+             * Many passes (e.g., constant copy propagation) will genenerate
+             * invalid 3-source instructions with the expectation that later
+             * passes (e.g., combine constants) will fix them.
+             */
+            for (unsigned i = 0; i < 3; i++) {
+               fsv_assert_ne(inst->src[i].file, BRW_IMMEDIATE_VALUE);
+
+               /* A stride of 1 (the usual case) or 0, with a special
+                * "repctrl" bit, is allowed. The repctrl bit doesn't work for
+                * 64-bit datatypes, so if the source type is 64-bit then only
+                * a stride of 1 is allowed. From the Broadwell PRM, Volume 7
+                * "3D Media GPGPU", page 944:
+                *
+                *    This is applicable to 32b datatypes and 16b datatype. 64b
+                *    datatypes cannot use the replicate control.
+                */
+               fsv_assert_lte(inst->src[i].vstride, 1);
+
+               if (type_sz(inst->src[i].type) > 4)
+                  fsv_assert_eq(inst->src[i].vstride, 1);
+            }
+         }
       }
 
       if (inst->dst.file == VGRF) {
