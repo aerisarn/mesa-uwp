e2221a9cac9603fdd873c9b9f30696151effd442
Rebecca Mckeever <rebecca.mckeever@collabora.com>
2022-12-09 16:29:18 -0600

    nouveau/codegen: Support nir_intrinsic_load_workgroup_id_zero_base
    Signed-off-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24327>

--- src/nouveau/codegen/nv50_ir_from_nir.cpp
+++ src/nouveau/codegen/nv50_ir_from_nir.cpp
@@ -1623,6 +1623,7 @@ Converter::convert(nir_intrinsic_op intr)
    case nir_intrinsic_load_vertex_id:
       return SV_VERTEX_ID;
    case nir_intrinsic_load_workgroup_id:
+   case nir_intrinsic_load_workgroup_id_zero_base:
       return SV_CTAID;
    case nir_intrinsic_load_work_dim:
       return SV_WORK_DIM;
@@ -1919,6 +1920,7 @@ Converter::visit(nir_intrinsic_instr *insn)
    case nir_intrinsic_load_tess_level_outer:
    case nir_intrinsic_load_vertex_id:
    case nir_intrinsic_load_workgroup_id:
+   case nir_intrinsic_load_workgroup_id_zero_base:
    case nir_intrinsic_load_work_dim: {
       const DataType dType = getDType(insn);
       SVSemantic sv = convert(op);
