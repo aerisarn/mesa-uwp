3091f5a7f412d8b7d92f96cdc0fd55cfe071f726
Asahi Lina <lina@asahilina.net>
2022-12-14 18:46:37 +0900

    asahi: Enable VS_INSTANCEID and VERTEX_ELEMENT_INSTANCE_DIVISOR caps
    These two should be functional at this point. The latter is required
    (and quite well tested) by Darwinia.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20365>

--- src/gallium/drivers/asahi/agx_pipe.c
+++ src/gallium/drivers/asahi/agx_pipe.c
@@ -1263,10 +1263,10 @@ agx_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
    case PIPE_CAP_SHADER_ARRAY_COMPONENTS:
    case PIPE_CAP_PACKED_UNIFORMS:
    case PIPE_CAP_QUADS_FOLLOW_PROVOKING_VERTEX_CONVENTION:
-      return 1;
-
    case PIPE_CAP_VS_INSTANCEID:
    case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
+      return 1;
+
    case PIPE_CAP_TEXTURE_MULTISAMPLE:
    case PIPE_CAP_SURFACE_SAMPLE_COUNT:
    case PIPE_CAP_SAMPLE_SHADING:
