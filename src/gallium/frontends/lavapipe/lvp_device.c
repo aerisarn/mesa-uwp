73db82c8160d619625ed14828178462cdbfba83b
Georg Lehmann <dadschoorse@gmail.com>
2022-12-16 11:38:55 +0100

    lavapipe: Enable VK_KHR_shader_non_semantic_info.
    Already implemented in common code.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7907
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20353>

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -127,6 +127,7 @@ static const struct vk_device_extension_table lvp_device_extensions_supported = 
    .KHR_shader_draw_parameters            = true,
    .KHR_shader_float16_int8               = true,
    .KHR_shader_integer_dot_product        = true,
+   .KHR_shader_non_semantic_info          = true,
    .KHR_shader_subgroup_extended_types    = true,
    .KHR_shader_terminate_invocation       = true,
    .KHR_spirv_1_4                         = true,
