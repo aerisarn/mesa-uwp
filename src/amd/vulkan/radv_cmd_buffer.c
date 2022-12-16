c2a4bdf1141be9f7dae81e455b5721e7f8d9c74c
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-12-16 14:25:17 +0100

    radv: dirty all dynamic states when beginning a new cmdbuf
    Sounds safer to not rely on other cmdbuf states.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20299>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -5467,38 +5467,7 @@ radv_BeginCommandBuffer(VkCommandBuffer commandBuffer, const VkCommandBufferBegi
    cmd_buffer->state.last_pa_sc_binner_cntl_0 = -1;
    cmd_buffer->usage_flags = pBeginInfo->flags;
 
-   cmd_buffer->state.dirty |= RADV_CMD_DIRTY_DYNAMIC_PRIMITIVE_TOPOLOGY |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_BIAS |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_BOUNDS |
-                              RADV_CMD_DIRTY_DYNAMIC_PRIMITIVE_RESTART_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_TEST_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_WRITE_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_COMPARE_OP |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_BOUNDS_TEST_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_STENCIL_TEST_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_STENCIL_OP |
-                              RADV_CMD_DIRTY_DYNAMIC_PATCH_CONTROL_POINTS |
-                              RADV_CMD_DIRTY_DYNAMIC_ALPHA_TO_COVERAGE_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_RASTERIZER_DISCARD_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_CLIP_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_CLIP_NEGATIVE_ONE_TO_ONE |
-                              RADV_CMD_DIRTY_DYNAMIC_CULL_MODE |
-                              RADV_CMD_DIRTY_DYNAMIC_FRONT_FACE |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_BIAS |
-                              RADV_CMD_DIRTY_DYNAMIC_POLYGON_MODE |
-                              RADV_CMD_DIRTY_DYNAMIC_PROVOKING_VERTEX_MODE |
-                              RADV_CMD_DIRTY_DYNAMIC_VIEWPORT |
-                              RADV_CMD_DIRTY_DYNAMIC_DEPTH_CLAMP_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_COLOR_WRITE_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_LINE_STIPPLE_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_CONSERVATIVE_RAST_MODE  |
-                              RADV_CMD_DIRTY_DYNAMIC_LOGIC_OP |
-                              RADV_CMD_DIRTY_DYNAMIC_LOGIC_OP_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_TESS_DOMAIN_ORIGIN |
-                              RADV_CMD_DIRTY_DYNAMIC_COLOR_BLEND_ENABLE |
-                              RADV_CMD_DIRTY_DYNAMIC_RASTERIZATION_SAMPLES;
-
-   cmd_buffer->state.dirty |= RADV_CMD_DIRTY_GUARDBAND;
+   cmd_buffer->state.dirty |= RADV_CMD_DIRTY_DYNAMIC_ALL | RADV_CMD_DIRTY_GUARDBAND;
 
    if (cmd_buffer->device->physical_device->rad_info.gfx_level >= GFX7) {
       uint32_t pred_value = 0;
