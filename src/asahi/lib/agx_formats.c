15155268de7172433c5fa7e4854e004a0d3c0efd
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-12-16 23:24:48 -0500

    asahi: Allow texturing S8 portion of combined Z/S
    Comes up in gles3.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20365>

--- src/asahi/lib/agx_formats.c
+++ src/asahi/lib/agx_formats.c
@@ -115,6 +115,9 @@ const struct agx_pixel_format_entry agx_pixel_format[PIPE_FORMAT_COUNT] = {
    AGX_FMT(Z32_FLOAT_S8X24_UINT,    R32,           FLOAT,  F, _),
    AGX_FMT(S8_UINT,                 R8,            UINT,   F, _),
 
+   /* The stencil part of Z32F + S8 is just S8 */
+   AGX_FMT(X32_S8X24_UINT,          R8,            UINT,   F, _),
+
    /* These must be lowered by u_transfer_helper to Z32F + S8 */
    AGX_FMT(Z24X8_UNORM,             R32,           FLOAT,  F, _),
    AGX_FMT(Z24_UNORM_S8_UINT,       R32,           FLOAT,  F, _),
