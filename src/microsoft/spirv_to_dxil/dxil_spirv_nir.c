c59141d3c2229aa6d8c92dfb00c7d5c038e062ec
Jesse Natalie <jenatali@microsoft.com>
2022-12-20 14:06:20 -0800

    dzn: Lower tg4 offsets
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20400>

--- src/microsoft/spirv_to_dxil/dxil_spirv_nir.c
+++ src/microsoft/spirv_to_dxil/dxil_spirv_nir.c
@@ -924,6 +924,7 @@ dxil_spirv_nir_passes(nir_shader *nir,
    nir_lower_tex_options lower_tex_options = {
       .lower_txp = UINT32_MAX,
       .lower_invalid_implicit_lod = true,
+      .lower_tg4_offsets = true,
    };
    NIR_PASS_V(nir, nir_lower_tex, &lower_tex_options);
 
