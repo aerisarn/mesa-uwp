c68530bedb00966fd8e7df982c623f5dc329e016
David Heidelberg <david.heidelberg@collabora.com>
2023-01-03 00:25:52 +0100

    postprocess: move the definition of pp_filters into **/pp_init.c
    An LTO-friendly move.
    
    Fixes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7881
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20374>

--- src/gallium/auxiliary/postprocess/pp_init.c
+++ src/gallium/auxiliary/postprocess/pp_init.c
@@ -37,6 +37,16 @@
 #include "util/u_memory.h"
 #include "cso_cache/cso_context.h"
 
+const struct pp_filter_t pp_filters[PP_FILTERS] = {
+/*    name			inner	shaders	verts	init			run                       free   */
+   { "pp_noblue",		0,	2,	1,	pp_noblue_init,		pp_nocolor,               pp_nocolor_free },
+   { "pp_nogreen",		0,	2,	1,	pp_nogreen_init,	pp_nocolor,               pp_nocolor_free },
+   { "pp_nored",		0,	2,	1,	pp_nored_init,		pp_nocolor,               pp_nocolor_free },
+   { "pp_celshade",		0,	2,	1,	pp_celshade_init,	pp_nocolor,               pp_celshade_free },
+   { "pp_jimenezmlaa",		2,	5,	2,	pp_jimenezmlaa_init,	pp_jimenezmlaa,           pp_jimenezmlaa_free },
+   { "pp_jimenezmlaa_color",	2,	5,	2,	pp_jimenezmlaa_init_color, pp_jimenezmlaa_color,  pp_jimenezmlaa_free },
+};
+
 /** Initialize the post-processing queue. */
 struct pp_queue_t *
 pp_init(struct pipe_context *pipe, const unsigned int *enabled,
