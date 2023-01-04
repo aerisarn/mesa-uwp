aerisarn/mesa-uwp

commit 0fbd82bdc45c48aa8cbc08d8bbf1319e66217d45

mesa: move calling _mesa_update_vao_derived_arrays into st_update_array
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19953>

src/mesa/main/arrayobj.c
@@ -546,8 +546,6 @@ _mesa_update_vao_derived_arrays(struct gl_context *ctx,
    /* More than 4 updates turn the VAO to dynamic. */
    if (ctx->Const.AllowDynamicVAOFastPath && ++vao->NumUpdates > 4) {
       vao->IsDynamic = true;
-      /* IsDynamic changes how vertex elements map to vertex buffers. */
-      vao->NewVertexElements = true;
       return;
    }
 

src/mesa/main/draw.c
@@ -154,8 +154,6 @@ _mesa_update_vao_state(struct gl_context *ctx, GLbitfield filter)
    bool new_vertex_buffers = false, new_vertex_elements = false;
 
    if (vao->NewVertexBuffers || vao->NewVertexElements) {
-      if (!vao->IsDynamic)
-         _mesa_update_vao_derived_arrays(ctx, vao);
       new_vertex_buffers |= vao->NewVertexBuffers;
       new_vertex_elements |= vao->NewVertexElements;
       vao->NewVertexBuffers = false;

src/mesa/state_tracker/st_atom_array.cpp
@@ -363,8 +363,12 @@ template<util_popcnt POPCNT> void ALWAYS_INLINE
 st_update_array_impl(struct st_context *st)
 {
    struct gl_context *ctx = st->ctx;
+   struct gl_vertex_array_object *vao = ctx->Array._DrawVAO;
    GLbitfield enabled_user_attribs;
    GLbitfield nonzero_divisor_attribs;
+
+   if (!vao->IsDynamic && !vao->SharedAndImmutable)
+      _mesa_update_vao_derived_arrays(ctx, vao);
 
    _mesa_get_derived_vao_masks(ctx, &enabled_user_attribs,
                                &nonzero_divisor_attribs);

commit cd15e95a114dc0adb7816a523271b2189c492553

mesa: set NewVertexElements when changing IsDynamic
Fixes: 0fbd82bdc45 - mesa: move calling _mesa_update_vao_derived_arrays into st_update_array
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7938

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20432>

src/mesa/main/arrayobj.c
@@ -546,6 +546,8 @@ _mesa_update_vao_derived_arrays(struct gl_context *ctx,
    /* More than 4 updates turn the VAO to dynamic. */
    if (ctx->Const.AllowDynamicVAOFastPath && ++vao->NumUpdates > 4) {
       vao->IsDynamic = true;
+      /* IsDynamic changes how vertex elements map to vertex buffers. */
+      ctx->Array.NewVertexElements = true;
       return;
    }
 