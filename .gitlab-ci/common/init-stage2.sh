aerisarn/mesa-uwp

commit 99a6f2a1864fe65a764e2963a41763bcbf541196

ci: Set the path to the VK drivers during HWCI_START_XORG/WESTON.
If the test is running with zink, we want zink to be able to find the
vulkan driver.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20503>

.gitlab-ci/common/init-stage2.sh
@@ -128,6 +128,7 @@ BACKGROUND_PIDS="$! $BACKGROUND_PIDS"
 if [ -n "$HWCI_START_XORG" ]; then
   echo "touch /xorg-started; sleep 100000" > /xorg-script
   env \
+    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/$(VK_DRIVER)_icd.`uname -m`.json \
     xinit /bin/sh /xorg-script -- /usr/bin/Xorg -noreset -s 0 -dpms -logfile /Xorg.0.log &
   BACKGROUND_PIDS="$! $BACKGROUND_PIDS"
 
@@ -145,7 +146,9 @@ if [ -n "$HWCI_START_WESTON" ]; then
   export XDG_RUNTIME_DIR=/run/user
   mkdir -p $XDG_RUNTIME_DIR
 
-  weston -Bheadless-backend.so --use-gl -Swayland-0 &
+  env \
+    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/$(VK_DRIVER)_icd.`uname -m`.json \
+    weston -Bheadless-backend.so --use-gl -Swayland-0 &
   export WAYLAND_DISPLAY=wayland-0
   sleep 1
 fi

commit 03a26ae3d99f76c3975903c9c4ef8280167cec42

ci: Fix VK driver setup for HWCI_START_*.
Review feedback requested a change that was incorrect, causing Xorg to
start to fail, but I forgot to retest the manual -full jobs that relied on
it.

Fixes: 99a6f2a1864f ("ci: Set the path to the VK drivers during HWCI_START_XORG/WESTON.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20523>

.gitlab-ci/common/init-stage2.sh
@@ -128,7 +128,7 @@ BACKGROUND_PIDS="$! $BACKGROUND_PIDS"
 if [ -n "$HWCI_START_XORG" ]; then
   echo "touch /xorg-started; sleep 100000" > /xorg-script
   env \
-    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/$(VK_DRIVER)_icd.`uname -m`.json \
+    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/${VK_DRIVER}_icd.`uname -m`.json \
     xinit /bin/sh /xorg-script -- /usr/bin/Xorg -noreset -s 0 -dpms -logfile /Xorg.0.log &
   BACKGROUND_PIDS="$! $BACKGROUND_PIDS"
 
@@ -147,7 +147,7 @@ if [ -n "$HWCI_START_WESTON" ]; then
   mkdir -p $XDG_RUNTIME_DIR
 
   env \
-    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/$(VK_DRIVER)_icd.`uname -m`.json \
+    VK_ICD_FILENAMES=/install/share/vulkan/icd.d/${VK_DRIVER}_icd.`uname -m`.json \
     weston -Bheadless-backend.so --use-gl -Swayland-0 &
   export WAYLAND_DISPLAY=wayland-0
   sleep 1