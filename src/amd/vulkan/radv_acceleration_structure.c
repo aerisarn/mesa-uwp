eab2c3995132f8cd1e3e9ee56eedaf17448aa157
Friedrich Vock <friedrich.vock@gmx.de>
2023-01-15 21:54:50 +0100

    radv/rt: Divide by the correct workgroup size
    Improves build performance by around 25%.
    
    Fixes: 9369b407 ("radv: Use PLOC for BVH building")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20717>

--- src/amd/vulkan/radv_acceleration_structure.c
+++ src/amd/vulkan/radv_acceleration_structure.c
@@ -908,7 +908,7 @@ ploc_build_internal(VkCommandBuffer commandBuffer, uint32_t infoCount,
                             cmd_buffer->device->meta_state.accel_struct_build.ploc_p_layout,
                             VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(consts), &consts);
       vk_common_CmdDispatch(commandBuffer,
-                            MAX2(DIV_ROUND_UP(bvh_states[i].node_count, 64), 1), 1, 1);
+                            MAX2(DIV_ROUND_UP(bvh_states[i].node_count, PLOC_WORKGROUP_SIZE), 1), 1, 1);
    }
 }
 
