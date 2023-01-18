aerisarn/mesa-uwp

commit 9c1fbc076afe5a4a7a6f534cb62bf83d3d207efc

Return 'Mesa' for GL_VENDOR for community drivers
Rather than a grab-bag of random values, return 'Mesa' as the GL_VENDOR
string for all community-supported drivers.

Drivers which are primarily developed/maintained by the hardware vendor
retain that vendor's name as the GL_VENDOR string.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16064>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -115,7 +115,7 @@ fd_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 fd_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "freedreno";
+   return "Mesa";
 }
 
 static const char *

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -126,7 +126,7 @@ etna_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 etna_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "etnaviv";
+   return "Mesa";
 }
 
 static const char *

src/gallium/drivers/lima/lima_screen.c
@@ -84,7 +84,7 @@ lima_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 lima_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "lima";
+   return "Mesa";
 }
 
 static const char *

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -103,7 +103,7 @@ static const struct debug_named_value lp_perf_flags[] = {
 static const char *
 llvmpipe_get_vendor(struct pipe_screen *screen)
 {
-   return "Mesa/X.org";
+   return "Mesa";
 }
 
 

src/gallium/drivers/nouveau/nouveau_screen.c
@@ -49,7 +49,7 @@ nouveau_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 nouveau_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "nouveau";
+   return "Mesa";
 }
 
 static const char *

src/gallium/drivers/r300/r300_screen.c
@@ -46,7 +46,7 @@
  * (Or egotistical. Yet.) */
 static const char* r300_get_vendor(struct pipe_screen* pscreen)
 {
-    return "X.Org R300 Project";
+    return "Mesa";
 }
 
 static const char* r300_get_device_vendor(struct pipe_screen* pscreen)

src/gallium/drivers/r600/r600_pipe_common.c
@@ -716,7 +716,7 @@ static const struct debug_named_value common_debug_options[] = {
 
 static const char* r600_get_vendor(struct pipe_screen* pscreen)
 {
-	return "X.Org";
+	return "Mesa";
 }
 
 static const char* r600_get_device_vendor(struct pipe_screen* pscreen)

src/gallium/drivers/softpipe/sp_screen.c
@@ -64,7 +64,7 @@ DEBUG_GET_ONCE_FLAGS_OPTION(sp_debug, "SOFTPIPE_DEBUG", sp_debug_options, 0)
 static const char *
 softpipe_get_vendor(struct pipe_screen *screen)
 {
-   return "Mesa/X.org";
+   return "Mesa";
 }
 
 

commit 6f91a5ab078820ec1cde572237c25b2a6e2702da

freedreno: Restore GL_VENDOR string
We cannot change this, as it has already been communicated to app
partners.  Also this breaks chrome's GPU quirk matching (which in some
cases is non-gpu-related, but when all you have is a hammer, everything
looks like a nail).

Fixes: 9c1fbc076af ("Return 'Mesa' for GL_VENDOR for community drivers")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20757>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -116,7 +116,7 @@ fd_screen_get_name(struct pipe_screen *pscreen)
 static const char *
 fd_screen_get_vendor(struct pipe_screen *pscreen)
 {
-   return "Mesa";
+   return "freedreno";
 }
 
 static const char *