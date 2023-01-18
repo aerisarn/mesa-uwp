b82d9b1a3d0f74fc73acc5ae77a6f51ce9ed8fc1
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-01-18 10:37:45 +0200

    nir/divergence: add missing RT intrinsinc handling
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20763>

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -686,6 +686,9 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_load_ray_hit_kind:
    case nir_intrinsic_load_ray_flags:
    case nir_intrinsic_load_cull_mask:
+   case nir_intrinsic_report_ray_intersection:
+   case nir_intrinsic_rq_proceed:
+   case nir_intrinsic_rq_load:
       is_divergent = true;
       break;
 
