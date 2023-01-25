9d89b7e4a84068f5d6fb73539a43d9782035ea81
Jesse Natalie <jenatali@microsoft.com>
2023-01-25 14:56:22 -0800

    dzn: Ensure we don't mix DSV+simultaneous-access
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20919>

--- src/microsoft/vulkan/dzn_image.c
+++ src/microsoft/vulkan/dzn_image.c
@@ -261,7 +261,7 @@ dzn_image_create(struct dzn_device *device,
    }
 
    if (pCreateInfo->sharingMode == VK_SHARING_MODE_CONCURRENT &&
-       !(usage & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT))
+       !(image->desc.Flags & D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL))
       image->desc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_SIMULTANEOUS_ACCESS;
 
    *out = dzn_image_to_handle(image);
