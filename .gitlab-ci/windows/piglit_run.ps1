1a29f3dfdb247d7f6a8bb6b3d559db497230f09d
Jesse Natalie <jenatali@microsoft.com>
2023-01-26 13:52:08 -0800

    CI/windows: Apply CI_FDO_CONCURRENT to piglit too
    Reviewed-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20924>

--- .gitlab-ci/windows/piglit_run.ps1
+++ .gitlab-ci/windows/piglit_run.ps1
@@ -4,10 +4,16 @@ Copy-Item -Path _install\bin\opengl32.dll -Destination C:\Piglit\bin\opengl32.dl
 Copy-Item -Path _install\bin\libgallium_wgl.dll -Destination C:\Piglit\bin\libgallium_wgl.dll
 Copy-Item -Path _install\bin\libglapi.dll -Destination C:\Piglit\bin\libglapi.dll
 
+$jobs = ""
+if ($null -ne $env:FDO_CI_CONCURRENT) {
+  $jobs = "--jobs", "$($env:FDO_CI_CONCURRENT)"
+}
+
 deqp-runner suite --output .\logs --suite "_install/$env:PIGLIT_SUITE" `
   --skips "_install/$env:PIGLIT_SKIPS" `
   --baseline "_install/$env:PIGLIT_BASELINE" `
-  --flakes "_install/$env:PIGLIT_FLAKES"
+  --flakes "_install/$env:PIGLIT_FLAKES" `
+  $jobs
 if (!$?) {
   Exit 1
 }
