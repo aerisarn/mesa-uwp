fb5d31c3dd17af97fc17285a01d77381e638fca1
Marek Olšák <marek.olsak@amd.com>
2023-01-30 08:35:22 -0500

    amd/surface: clean up is_dcc_supported_by_L2
    no functional change, though this removes "<=" for navi10, which was
    never true in the "<" case
    
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20967>

--- src/amd/common/ac_surface.c
+++ src/amd/common/ac_surface.c
@@ -1522,36 +1522,32 @@ static bool is_dcc_supported_by_CB(const struct radeon_info *info, unsigned sw_m
 ASSERTED static bool is_dcc_supported_by_L2(const struct radeon_info *info,
                                             const struct radeon_surf *surf)
 {
+   bool single_indep = surf->u.gfx9.color.dcc.independent_64B_blocks !=
+                       surf->u.gfx9.color.dcc.independent_128B_blocks;
+   bool valid_64b = surf->u.gfx9.color.dcc.independent_64B_blocks &&
+                    surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_64B;
+   bool valid_128b = surf->u.gfx9.color.dcc.independent_128B_blocks &&
+                     (surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_128B);
+
    if (info->gfx_level <= GFX9) {
       /* Only independent 64B blocks are supported. */
-      return surf->u.gfx9.color.dcc.independent_64B_blocks && !surf->u.gfx9.color.dcc.independent_128B_blocks &&
-             surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_64B;
+      return single_indep && valid_64b;
    }
 
    if (info->family == CHIP_NAVI10) {
       /* Only independent 128B blocks are supported. */
-      return !surf->u.gfx9.color.dcc.independent_64B_blocks && surf->u.gfx9.color.dcc.independent_128B_blocks &&
-             surf->u.gfx9.color.dcc.max_compressed_block_size <= V_028C78_MAX_BLOCK_SIZE_128B;
+      return single_indep && valid_128b;
    }
 
-   bool valid_64b = surf->u.gfx9.color.dcc.independent_64B_blocks &&
-                    surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_64B;
-   bool valid_128b = surf->u.gfx9.color.dcc.independent_128B_blocks &&
-                     surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_128B;
-
    if (info->family == CHIP_NAVI12 || info->family == CHIP_NAVI14) {
-      /* Either 64B or 128B can be used, but not both.
+      /* Either 64B or 128B can be used, but the INDEPENDENT_*_BLOCKS setting must match.
        * If 64B is used, DCC image stores are unsupported.
        */
-      return surf->u.gfx9.color.dcc.independent_64B_blocks != surf->u.gfx9.color.dcc.independent_128B_blocks &&
-             (valid_64b || valid_128b);
+      return single_indep && (valid_64b || valid_128b);
    }
 
    /* Valid settings are the same as NAVI14 + (64B && 128B && max_compressed_block_size == 64B) */
-   return (surf->u.gfx9.color.dcc.independent_64B_blocks != surf->u.gfx9.color.dcc.independent_128B_blocks &&
-           (valid_64b || valid_128b)) ||
-          (surf->u.gfx9.color.dcc.independent_64B_blocks &&
-           surf->u.gfx9.color.dcc.max_compressed_block_size == V_028C78_MAX_BLOCK_SIZE_64B);
+   return (single_indep && (valid_64b || valid_128b)) || valid_64b;
 }
 
 static bool gfx10_DCN_requires_independent_64B_blocks(const struct radeon_info *info,
