903c000055baa624354ebe3fd80c6bbd005b8f2a
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-01-30 20:11:58 -0600

    nvk: Add BO recycling to the command pool
    Command BOs (nvk_cmd_bo) are automatically mapped recyclable BOs to use
    for upload and push buffers.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

--- src/nouveau/vulkan/nvk_cmd_pool.c
+++ src/nouveau/vulkan/nvk_cmd_pool.c
@@ -1,6 +1,38 @@
 #include "nvk_cmd_pool.h"
 
 #include "nvk_device.h"
+#include "nvk_physical_device.h"
+
+static VkResult
+nvk_cmd_bo_create(struct nvk_cmd_pool *pool, struct nvk_cmd_bo **bo_out)
+{
+   struct nvk_device *dev = nvk_cmd_pool_device(pool);
+   struct nvk_cmd_bo *bo;
+
+   bo = vk_zalloc(&pool->vk.alloc, sizeof(*bo), 8,
+                  VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+   if (bo == NULL)
+      return vk_error(pool, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   uint32_t flags = NOUVEAU_WS_BO_GART | NOUVEAU_WS_BO_MAP;
+   bo->bo = nouveau_ws_bo_new_mapped(dev->pdev->dev, NVK_CMD_BO_SIZE, 0,
+                                     flags, NOUVEAU_WS_BO_WR, &bo->map);
+   if (bo->bo == NULL) {
+      vk_free(&pool->vk.alloc, bo);
+      return vk_error(pool, VK_ERROR_OUT_OF_DEVICE_MEMORY);
+   }
+
+   *bo_out = bo;
+   return VK_SUCCESS;
+}
+
+static void
+nvk_cmd_bo_destroy(struct nvk_cmd_pool *pool, struct nvk_cmd_bo *bo)
+{
+   nouveau_ws_bo_unmap(bo->bo, bo->map);
+   nouveau_ws_bo_destroy(bo->bo);
+   vk_free(&pool->vk.alloc, bo);
+}
 
 VKAPI_ATTR VkResult VKAPI_CALL
 nvk_CreateCommandPool(VkDevice _device,
@@ -23,9 +55,40 @@ nvk_CreateCommandPool(VkDevice _device,
       return result;
    }
 
+   list_inithead(&pool->free_bos);
+
    *pCmdPool = nvk_cmd_pool_to_handle(pool);
 
    return VK_SUCCESS;
+}
+
+static void
+nvk_cmd_pool_destroy_bos(struct nvk_cmd_pool *pool)
+{
+   list_for_each_entry_safe(struct nvk_cmd_bo, bo, &pool->free_bos, link)
+      nvk_cmd_bo_destroy(pool, bo);
+
+   list_inithead(&pool->free_bos);
+}
+
+VkResult
+nvk_cmd_pool_alloc_bo(struct nvk_cmd_pool *pool, struct nvk_cmd_bo **bo_out)
+{
+   if (!list_is_empty(&pool->free_bos)) {
+      struct nvk_cmd_bo *bo =
+         list_first_entry(&pool->free_bos, struct nvk_cmd_bo, link);
+      list_del(&bo->link);
+      *bo_out = bo;
+      return VK_SUCCESS;
+   }
+
+   return nvk_cmd_bo_create(pool, bo_out);
+}
+
+void
+nvk_cmd_pool_free_bo_list(struct nvk_cmd_pool *pool, struct list_head *bos)
+{
+   list_splicetail(bos, &pool->free_bos);
 }
 
 VKAPI_ATTR void VKAPI_CALL
@@ -39,6 +102,7 @@ nvk_DestroyCommandPool(VkDevice _device,
    if (!pool)
       return;
 
+   nvk_cmd_pool_destroy_bos(pool);
    vk_command_pool_finish(&pool->vk);
    vk_free2(&device->vk.alloc, pAllocator, pool);
 }
@@ -51,4 +115,5 @@ nvk_TrimCommandPool(VkDevice device,
    VK_FROM_HANDLE(nvk_cmd_pool, pool, commandPool);
 
    vk_command_pool_trim(&pool->vk, flags);
+   nvk_cmd_pool_destroy_bos(pool);
 }
