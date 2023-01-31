9b20394b896af665b10c3c12cedb4b4d783315e8
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-01-30 20:12:01 -0600

    nouveau: Add PCI information to nv_device_info
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

--- src/nouveau/nvidia-headers/nv_device_info.h
+++ src/nouveau/nvidia-headers/nv_device_info.h
@@ -2,6 +2,13 @@
 #define NV_DEVINFO_H
 
 struct nv_device_info {
+   uint16_t pci_domain;
+   uint8_t pci_bus;
+   uint8_t pci_dev;
+   uint8_t pci_func;
+   uint16_t pci_device_id;
+   uint8_t pci_revision_id;
+
    uint16_t cls_copy;
    uint16_t cls_eng2d;
    uint16_t cls_eng3d;
