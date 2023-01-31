d97397f084fbd34338f89cadb96704403ea883d0
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-01-30 20:11:53 -0600

    nvk: Use the new headers for samplers
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

--- src/nouveau/vulkan/nvk_sampler.c
+++ src/nouveau/vulkan/nvk_sampler.c
@@ -1,80 +1,136 @@
 #include "nvk_sampler.h"
 
 #include "nvk_device.h"
+#include "nouveau_context.h"
+
+#include "util/bitpack_helpers.h"
 #include "util/format_srgb.h"
 #include "vulkan/runtime/vk_sampler.h"
 
-#include "gallium/drivers/nouveau/nv50/g80_texture.xml.h"
+#include "cla097.h"
+#include "clb197.h"
+#include "cl9097tex.h"
+#include "cla097tex.h"
+#include "clb197tex.h"
+#include "drf.h"
+
+ALWAYS_INLINE static void
+__set_u32(uint32_t *o, uint32_t v, unsigned lo, unsigned hi)
+{
+   assert(lo <= hi && hi < 32);
+   *o |= util_bitpack_uint(v, lo % 32, hi % 32);
+}
+
+#define FIXED_FRAC_BITS 8
+
+ALWAYS_INLINE static void
+__set_ufixed(uint32_t *o, float v, unsigned lo, unsigned hi)
+{
+   assert(lo <= hi && hi < 32);
+   *o |= util_bitpack_ufixed_clamp(v, lo % 32, hi % 32, FIXED_FRAC_BITS);
+}
+
+ALWAYS_INLINE static void
+__set_sfixed(uint32_t *o, float v, unsigned lo, unsigned hi)
+{
+   assert(lo <= hi && hi < 32);
+   *o |= util_bitpack_sfixed_clamp(v, lo % 32, hi % 32, FIXED_FRAC_BITS);
+}
+
+ALWAYS_INLINE static void
+__set_bool(uint32_t *o, bool b, unsigned lo, unsigned hi)
+{
+   assert(lo == hi && hi < 32);
+   *o |= util_bitpack_uint(b, lo % 32, hi % 32);
+}
+
+#define MW(x) x
+
+#define SAMP_SET_U(o, NV, i, FIELD, val) \
+   __set_u32(&(o)[i], (val), DRF_LO(NV##_TEXSAMP##i##_##FIELD),\
+                             DRF_HI(NV##_TEXSAMP##i##_##FIELD))
+
+#define SAMP_SET_UF(o, NV, i, FIELD, val) \
+   __set_ufixed(&(o)[i], (val), DRF_LO(NV##_TEXSAMP##i##_##FIELD),\
+                                DRF_HI(NV##_TEXSAMP##i##_##FIELD))
+
+#define SAMP_SET_SF(o, NV, i, FIELD, val) \
+   __set_sfixed(&(o)[i], (val), DRF_LO(NV##_TEXSAMP##i##_##FIELD),\
+                                DRF_HI(NV##_TEXSAMP##i##_##FIELD))
+
+#define SAMP_SET_B(o, NV, i, FIELD, b) \
+   __set_bool(&(o)[i], (b), DRF_LO(NV##_TEXSAMP##i##_##FIELD),\
+                            DRF_HI(NV##_TEXSAMP##i##_##FIELD))
+
+#define SAMP_SET_E(o, NV, i, FIELD, E) \
+   SAMP_SET_U((o), NV, i, FIELD, NV##_TEXSAMP##i##_##FIELD##_##E)
 
 static inline uint32_t
-g80_tsc_wrap_mode(VkSamplerAddressMode addr_mode)
+vk_to_9097_address_mode(VkSamplerAddressMode addr_mode)
 {
-   switch (addr_mode) {
-   case VK_SAMPLER_ADDRESS_MODE_REPEAT:
-      return G80_TSC_WRAP_WRAP;
-   case VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT:
-      return G80_TSC_WRAP_MIRROR;
-   case VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE:
-      return G80_TSC_WRAP_CLAMP_TO_EDGE;
-   case VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER:
-      return G80_TSC_WRAP_BORDER;
-   case VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE:
-      return G80_TSC_WRAP_MIRROR_ONCE_CLAMP_TO_EDGE;
-   default:
-      unreachable("Invalid sampler address mode");
-   }
+#define MODE(VK, NV) \
+   [VK_SAMPLER_ADDRESS_MODE_##VK] = NV9097_TEXSAMP0_ADDRESS_U_##NV
+   static const uint8_t vk_to_9097[] = {
+      MODE(REPEAT,               WRAP),
+      MODE(MIRRORED_REPEAT,      MIRROR),
+      MODE(CLAMP_TO_EDGE,        CLAMP_TO_EDGE),
+      MODE(CLAMP_TO_BORDER,      BORDER),
+      MODE(MIRROR_CLAMP_TO_EDGE, MIRROR_ONCE_CLAMP_TO_EDGE),
+   };
+#undef MODE
+
+   assert(addr_mode < ARRAY_SIZE(vk_to_9097));
+   return vk_to_9097[addr_mode];
 }
 
 static uint32_t
-g80_tsc_0_depth_compare_func(VkCompareOp op)
+vk_to_9097_texsamp_compare_op(VkCompareOp op)
 {
-   switch (op) {
-   case VK_COMPARE_OP_NEVER:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_NEVER;
-   case VK_COMPARE_OP_LESS:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_LESS;
-   case VK_COMPARE_OP_EQUAL:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_EQUAL;
-   case VK_COMPARE_OP_LESS_OR_EQUAL:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_LEQUAL;
-   case VK_COMPARE_OP_GREATER:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_GREATER;
-   case VK_COMPARE_OP_NOT_EQUAL:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_NOTEQUAL;
-   case VK_COMPARE_OP_GREATER_OR_EQUAL:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_GEQUAL;
-   case VK_COMPARE_OP_ALWAYS:
-      return G80_TSC_0_DEPTH_COMPARE_FUNC_ALWAYS;
-   default:
-      unreachable("Invalid compare op");
-   }
+#define OP(VK, NV) \
+   [VK_COMPARE_OP_##VK] = NV9097_TEXSAMP0_DEPTH_COMPARE_FUNC_##NV
+   ASSERTED static const uint8_t vk_to_9097[] = {
+      OP(NEVER,            ZC_NEVER),
+      OP(LESS,             ZC_LESS),
+      OP(EQUAL,            ZC_EQUAL),
+      OP(LESS_OR_EQUAL,    ZC_LEQUAL),
+      OP(GREATER,          ZC_GREATER),
+      OP(NOT_EQUAL,        ZC_NOTEQUAL),
+      OP(GREATER_OR_EQUAL, ZC_GEQUAL),
+      OP(ALWAYS,           ZC_ALWAYS),
+   };
+#undef OP
+
+   assert(op < ARRAY_SIZE(vk_to_9097));
+   assert(op == vk_to_9097[op]);
+
+   return op;
 }
 
 static uint32_t
-g80_tsc_0_max_anisotropy(float max_anisotropy)
+vk_to_9097_max_anisotropy(float max_anisotropy)
 {
    if (max_anisotropy >= 16)
-      return G80_TSC_0_MAX_ANISOTROPY_16_TO_1;
+      return NV9097_TEXSAMP0_MAX_ANISOTROPY_ANISO_16_TO_1;
 
    if (max_anisotropy >= 12)
-      return G80_TSC_0_MAX_ANISOTROPY_12_TO_1;
+      return NV9097_TEXSAMP0_MAX_ANISOTROPY_ANISO_12_TO_1;
 
    uint32_t aniso_u32 = MAX2(0.0f, max_anisotropy);
    return (aniso_u32 >> 1) << 20;
 }
 
 static uint32_t
-g80_tsc_1_trilin_opt(float max_anisotropy)
+vk_to_9097_trilin_opt(float max_anisotropy)
 {
    /* No idea if we want this but matching nouveau */
    if (max_anisotropy >= 12)
       return 0;
 
    if (max_anisotropy >= 4)
-      return 6 << G80_TSC_1_TRILIN_OPT__SHIFT;
+      return 6;
 
    if (max_anisotropy >= 2)
-      return 4 << G80_TSC_1_TRILIN_OPT__SHIFT;
+      return 4;
 
    return 0;
 }
@@ -89,31 +145,6 @@ vk_sampler_create_reduction_mode(const VkSamplerCreateInfo *pCreateInfo)
       return VK_SAMPLER_REDUCTION_MODE_WEIGHTED_AVERAGE;
 
    return reduction->reductionMode;
-}
-
-static uint32_t
-to_sfixed(float f, unsigned int_bits, unsigned frac_bits)
-{
-   int min = -(1 << (int_bits - 1));
-   int max = (1 << (int_bits - 1)) - 1;
-   f = CLAMP(f, (float)min, (float)max);
-
-   int fixed = f * (float)(1 << frac_bits);
-
-   /* It's a uint so mask of high bits */
-   return fixed & ((1 << (int_bits + frac_bits)) - 1);
-}
-
-static uint32_t
-to_ufixed(float f, unsigned int_bits, unsigned frac_bits)
-{
-   unsigned max = (1 << int_bits) - 1;
-   f = CLAMP(f, 0.0f, (float)max);
-
-   int fixed = f * (float)(1 << frac_bits);
-
-   assert((uint32_t)fixed <= UINT32_MAX);
-   return fixed;
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL
@@ -138,30 +169,35 @@ nvk_CreateSampler(VkDevice _device,
                        "Failed to allocate image descriptor");
    }
 
-   uint32_t tsc[8] = {};
-   tsc[0] |= 0x00024000; /* Font filter? */
-   tsc[0] |= G80_TSC_0_SRGB_CONVERSION;
-   tsc[0] |= g80_tsc_wrap_mode(pCreateInfo->addressModeU)
-             << G80_TSC_0_ADDRESS_U__SHIFT;
-   tsc[0] |= g80_tsc_wrap_mode(pCreateInfo->addressModeV)
-             << G80_TSC_0_ADDRESS_V__SHIFT;
-   tsc[0] |= g80_tsc_wrap_mode(pCreateInfo->addressModeW)
-             << G80_TSC_0_ADDRESS_P__SHIFT;
+   uint32_t samp[8] = {};
+   SAMP_SET_U(samp, NV9097, 0, ADDRESS_U,
+              vk_to_9097_address_mode(pCreateInfo->addressModeU));
+   SAMP_SET_U(samp, NV9097, 0, ADDRESS_V,
+              vk_to_9097_address_mode(pCreateInfo->addressModeV));
+   SAMP_SET_U(samp, NV9097, 0, ADDRESS_P,
+              vk_to_9097_address_mode(pCreateInfo->addressModeW));
 
    if (pCreateInfo->compareEnable) {
-      tsc[0] |= G80_TSC_0_DEPTH_COMPARE;
-      tsc[0] |= g80_tsc_0_depth_compare_func(pCreateInfo->compareOp);
+      SAMP_SET_B(samp, NV9097, 0, DEPTH_COMPARE, true);
+      SAMP_SET_U(samp, NV9097, 0, DEPTH_COMPARE_FUNC,
+                 vk_to_9097_texsamp_compare_op(pCreateInfo->compareOp));
    }
 
-   tsc[0] |= g80_tsc_0_max_anisotropy(pCreateInfo->maxAnisotropy);
-   tsc[1] |= g80_tsc_1_trilin_opt(pCreateInfo->maxAnisotropy);
+   SAMP_SET_B(samp, NV9097, 0, S_R_G_B_CONVERSION, true);
+   SAMP_SET_E(samp, NV9097, 0, FONT_FILTER_WIDTH, SIZE_2);
+   SAMP_SET_E(samp, NV9097, 0, FONT_FILTER_HEIGHT, SIZE_2);
+
+   if (pCreateInfo->anisotropyEnable) {
+      SAMP_SET_U(samp, NV9097, 0, MAX_ANISOTROPY,
+                 vk_to_9097_max_anisotropy(pCreateInfo->maxAnisotropy));
+   }
 
    switch (pCreateInfo->magFilter) {
    case VK_FILTER_NEAREST:
-      tsc[1] |= G80_TSC_1_MAG_FILTER_NEAREST;
+      SAMP_SET_E(samp, NV9097, 1, MAG_FILTER, MAG_POINT);
       break;
    case VK_FILTER_LINEAR:
-      tsc[1] |= G80_TSC_1_MAG_FILTER_LINEAR;
+      SAMP_SET_E(samp, NV9097, 1, MAG_FILTER, MAG_LINEAR);
       break;
    default:
       unreachable("Invalid filter");
@@ -169,10 +205,13 @@ nvk_CreateSampler(VkDevice _device,
 
    switch (pCreateInfo->minFilter) {
    case VK_FILTER_NEAREST:
-      tsc[1] |= G80_TSC_1_MIN_FILTER_NEAREST;
+      SAMP_SET_E(samp, NV9097, 1, MIN_FILTER, MIN_POINT);
       break;
    case VK_FILTER_LINEAR:
-      tsc[1] |= G80_TSC_1_MIN_FILTER_LINEAR;
+      if (pCreateInfo->anisotropyEnable)
+         SAMP_SET_E(samp, NV9097, 1, MIN_FILTER, MIN_ANISO);
+      else
+         SAMP_SET_E(samp, NV9097, 1, MIN_FILTER, MIN_LINEAR);
       break;
    default:
       unreachable("Invalid filter");
@@ -180,53 +219,66 @@ nvk_CreateSampler(VkDevice _device,
 
    switch (pCreateInfo->mipmapMode) {
    case VK_SAMPLER_MIPMAP_MODE_NEAREST:
-      tsc[1] |= G80_TSC_1_MIP_FILTER_NEAREST;
+      SAMP_SET_E(samp, NV9097, 1, MIP_FILTER, MIP_POINT);
       break;
    case VK_SAMPLER_MIPMAP_MODE_LINEAR:
-      tsc[1] |= G80_TSC_1_MIP_FILTER_LINEAR;
+      SAMP_SET_E(samp, NV9097, 1, MIP_FILTER, MIP_LINEAR);
       break;
    default:
       unreachable("Invalid mipmap mode");
    }
 
-   tsc[1] |= GK104_TSC_1_CUBEMAP_INTERFACE_FILTERING;
-   if (pCreateInfo->unnormalizedCoordinates)
-      tsc[1] |= GK104_TSC_1_FLOAT_COORD_NORMALIZATION_FORCE_UNNORMALIZED_COORDS;
+   assert(device->ctx->eng3d.cls >= KEPLER_A);
+   SAMP_SET_E(samp, NVA097, 1, CUBEMAP_INTERFACE_FILTERING, AUTO_SPAN_SEAM);
 
-   switch (vk_sampler_create_reduction_mode(pCreateInfo)) {
-   case VK_SAMPLER_REDUCTION_MODE_WEIGHTED_AVERAGE:
-      tsc[1] |= GM204_TSC_1_REDUCTION_MODE_WEIGHTED_AVERAGE;
-      break;
-   case VK_SAMPLER_REDUCTION_MODE_MIN:
-      tsc[1] |= GM204_TSC_1_REDUCTION_MODE_MIN;
-      break;
-   case VK_SAMPLER_REDUCTION_MODE_MAX:
-      tsc[1] |= GM204_TSC_1_REDUCTION_MODE_MAX;
-      break;
-   default:
-      unreachable("Invalid reduction mode");
+   if (device->ctx->eng3d.cls >= MAXWELL_B) {
+      switch (vk_sampler_create_reduction_mode(pCreateInfo)) {
+      case VK_SAMPLER_REDUCTION_MODE_WEIGHTED_AVERAGE:
+         SAMP_SET_E(samp, NVB197, 1, REDUCTION_FILTER, RED_NONE);
+         break;
+      case VK_SAMPLER_REDUCTION_MODE_MIN:
+         SAMP_SET_E(samp, NVB197, 1, REDUCTION_FILTER, RED_MINIMUM);
+         break;
+      case VK_SAMPLER_REDUCTION_MODE_MAX:
+         SAMP_SET_E(samp, NVB197, 1, REDUCTION_FILTER, RED_MAXIMUM);
+         break;
+      default:
+         unreachable("Invalid reduction mode");
+      }
    }
 
-   tsc[1] |= to_sfixed(pCreateInfo->mipLodBias, 5, 8)
-             << G80_TSC_1_MIP_LOD_BIAS__SHIFT;
-   tsc[2] |= to_ufixed(pCreateInfo->minLod, 4, 8)
-             << G80_TSC_2_MIN_LOD_CLAMP__SHIFT;
-   tsc[2] |= to_ufixed(pCreateInfo->maxLod, 4, 8)
-             << G80_TSC_2_MAX_LOD_CLAMP__SHIFT;
+   SAMP_SET_UF(samp, NV9097, 1, MIP_LOD_BIAS, pCreateInfo->mipLodBias);
+
+   assert(device->ctx->eng3d.cls >= KEPLER_A);
+   if (pCreateInfo->unnormalizedCoordinates) {
+      SAMP_SET_E(samp, NVA097, 1, FLOAT_COORD_NORMALIZATION,
+                                  FORCE_UNNORMALIZED_COORDS);
+   } else {
+      SAMP_SET_E(samp, NVA097, 1, FLOAT_COORD_NORMALIZATION,
+                                  USE_HEADER_SETTING);
+   }
+   SAMP_SET_U(samp, NV9097, 1, TRILIN_OPT,
+              vk_to_9097_trilin_opt(pCreateInfo->maxAnisotropy));
+
+   SAMP_SET_UF(samp, NV9097, 2, MIN_LOD_CLAMP, pCreateInfo->minLod);
+   SAMP_SET_UF(samp, NV9097, 2, MAX_LOD_CLAMP, pCreateInfo->maxLod);
 
    const VkClearColorValue bc =
       vk_sampler_border_color_value(pCreateInfo, NULL);
-   uint32_t bc_srgb[3];
+   uint8_t bc_srgb[3];
    for (unsigned i = 0; i < 3; i++)
       bc_srgb[i] = util_format_linear_float_to_srgb_8unorm(bc.float32[i]);
 
-   tsc[2] |= bc_srgb[0] << G80_TSC_2_SRGB_BORDER_COLOR_R__SHIFT;
-   tsc[3] |= bc_srgb[1] << G80_TSC_3_SRGB_BORDER_COLOR_G__SHIFT;
-   tsc[3] |= bc_srgb[2] << G80_TSC_3_SRGB_BORDER_COLOR_B__SHIFT;
-   for (unsigned i = 0; i < 4; i++)
-      tsc[i + 4] = bc.uint32[i];
+   SAMP_SET_U(samp, NV9097, 2, S_R_G_B_BORDER_COLOR_R, bc_srgb[0]);
+   SAMP_SET_U(samp, NV9097, 3, S_R_G_B_BORDER_COLOR_G, bc_srgb[1]);
+   SAMP_SET_U(samp, NV9097, 3, S_R_G_B_BORDER_COLOR_B, bc_srgb[2]);
 
-   memcpy(desc_map, tsc, sizeof(tsc));
+   SAMP_SET_U(samp, NV9097, 4, BORDER_COLOR_R, bc.uint32[0]);
+   SAMP_SET_U(samp, NV9097, 5, BORDER_COLOR_G, bc.uint32[1]);
+   SAMP_SET_U(samp, NV9097, 6, BORDER_COLOR_B, bc.uint32[2]);
+   SAMP_SET_U(samp, NV9097, 7, BORDER_COLOR_A, bc.uint32[2]);
+
+   memcpy(desc_map, samp, sizeof(samp));
 
    *pSampler = nvk_sampler_to_handle(sampler);
 
