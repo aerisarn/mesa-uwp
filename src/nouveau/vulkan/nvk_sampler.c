100c7060d8423db997cbc5c2f4b54aacc614201c
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-01-30 20:11:50 -0600

    nvk: Re-format nvk_sampler.c
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

--- src/nouveau/vulkan/nvk_sampler.c
+++ src/nouveau/vulkan/nvk_sampler.c
@@ -2,15 +2,17 @@
 
 #include "nvk_device.h"
 
-VKAPI_ATTR VkResult VKAPI_CALL nvk_CreateSampler(VkDevice _device,
-   const VkSamplerCreateInfo *pCreateInfo,
-   const VkAllocationCallbacks *pAllocator,
-   VkSampler *pSampler)
+VKAPI_ATTR VkResult VKAPI_CALL
+nvk_CreateSampler(VkDevice _device,
+                  const VkSamplerCreateInfo *pCreateInfo,
+                  const VkAllocationCallbacks *pAllocator,
+                  VkSampler *pSampler)
 {
    VK_FROM_HANDLE(nvk_device, device, _device);
    struct nvk_sampler *sampler;
 
-   sampler = vk_object_zalloc(&device->vk, pAllocator, sizeof(*sampler), VK_OBJECT_TYPE_SAMPLER);
+   sampler = vk_object_zalloc(&device->vk, pAllocator, sizeof(*sampler),
+                              VK_OBJECT_TYPE_SAMPLER);
    if (!sampler)
       return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
 
@@ -19,9 +21,10 @@ VKAPI_ATTR VkResult VKAPI_CALL nvk_CreateSampler(VkDevice _device,
    return VK_SUCCESS;
 }
 
-VKAPI_ATTR void VKAPI_CALL nvk_DestroySampler(VkDevice _device,
-   VkSampler _sampler,
-   const VkAllocationCallbacks *pAllocator)
+VKAPI_ATTR void VKAPI_CALL
+nvk_DestroySampler(VkDevice _device,
+                   VkSampler _sampler,
+                   const VkAllocationCallbacks *pAllocator)
 {
    VK_FROM_HANDLE(nvk_device, device, _device);
    VK_FROM_HANDLE(nvk_sampler, sampler, _sampler);
