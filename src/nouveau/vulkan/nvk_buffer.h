1a906072db5f4d3227f0bd2417754849c40adb23
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-01-30 20:11:47 -0600

    nvk/buffer: Add a push_buffer_ref helper
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

--- src/nouveau/vulkan/nvk_buffer.h
+++ src/nouveau/vulkan/nvk_buffer.h
@@ -5,6 +5,7 @@
 #include "nvk_device_memory.h"
 
 #include "nouveau_bo.h"
+#include "nouveau_push.h"
 #include "vulkan/runtime/vk_buffer.h"
 
 struct nvk_device_memory;
@@ -16,6 +17,14 @@ struct nvk_buffer {
 };
 
 VK_DEFINE_HANDLE_CASTS(nvk_buffer, vk.base, VkBuffer, VK_OBJECT_TYPE_BUFFER)
+
+static void
+nvk_push_buffer_ref(struct nouveau_ws_push *push,
+                    const struct nvk_buffer *buffer,
+                    enum nouveau_ws_bo_map_flags flags)
+{
+   nouveau_ws_push_ref(push, buffer->mem->bo, flags);
+}
 
 static inline uint64_t
 nvk_buffer_address(struct nvk_buffer *buffer, uint64_t offset)
