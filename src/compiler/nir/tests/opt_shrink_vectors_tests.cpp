aerisarn/mesa-uwp

commit 3305c9602df65d08f586ee520de9d8f0c46b14f5

nir: fix shrinking of load_const for large vectors
Specifically when shrinking load_const with number of components
> 5, if the final number of components is not allowed (for example 8->6)
it would report false for progress even if we actually did some
reshuffling and also it would skip on the rewrite of the readers.

Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20213>

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -304,6 +304,7 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
 
    uint8_t reswizzle[NIR_MAX_VEC_COMPONENTS] = { 0 };
    unsigned num_components = 0;
+   bool progress = false;
    for (unsigned i = 0; i < def->num_components; i++) {
       if (!((mask >> i) & 0x1))
          continue;
@@ -313,6 +314,7 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
       for (j = 0; j < num_components; j++) {
          if (instr->value[i].u64 == instr->value[j].u64) {
             reswizzle[i] = j;
+            progress = true;
             break;
          }
       }
@@ -320,21 +322,20 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
       /* Otherwise, just append the value */
       if (j == num_components) {
          instr->value[num_components] = instr->value[i];
+	 if (i != num_components)
+            progress = true;
          reswizzle[i] = num_components++;
       }
    }
 
    unsigned rounded = round_up_components(num_components);
    assert(rounded <= def->num_components);
-   num_components = rounded;
 
-   if (num_components == def->num_components)
-      return false;
+   def->num_components = rounded;
+   if (progress)
+      reswizzle_alu_uses(def, reswizzle);
 
-   def->num_components = num_components;
-   reswizzle_alu_uses(def, reswizzle);
-
-   return true;
+   return progress;
 }
 
 static bool

commit 7e6acfd58772fbfbcd59404c26444939cfb84555

nir: mark progress when removing trailing unused load_const channels
When the unused channels were at the end and so no reswizzling was
needed, we wouldn't correctly mark the progress.

Fixes: 3305c960
Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21014>

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -330,12 +330,15 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
       }
    }
 
-   unsigned rounded = round_up_components(num_components);
-   assert(rounded <= def->num_components);
-
-   def->num_components = rounded;
    if (progress)
       reswizzle_alu_uses(def, reswizzle);
+
+   unsigned rounded = round_up_components(num_components);
+   assert(rounded <= def->num_components);
+   if (rounded < def->num_components)
+      progress = true;
+
+   def->num_components = rounded;
 
    return progress;
 }

src/compiler/nir/tests/opt_shrink_vectors_tests.cpp
@@ -84,6 +84,41 @@ static void check_swizzle(nir_alu_src * src, const char * swizzle)
    }
 }
 
+TEST_F(nir_opt_shrink_vectors_test, opt_shrink_vectors_load_const_trailing_component_only)
+{
+   /* Test that opt_shrink_vectors correctly removes unused trailing channels
+    * of load_const.
+    *
+    * vec4 32 ssa_1 = load_const (1.0, 2.0, 3.0, 4.0)
+    * vec1 32 ssa_2 = fmov ssa_1.x
+    *
+    * to
+    *
+    * vec1 32 ssa_1 = load_const (1.0)
+    * vec1 32 ssa_2 = fmov ssa_1.x
+    */
+
+   nir_ssa_def *imm_vec = nir_imm_vec4(&bld, 1.0, 2.0, 3.0, 4.0);
+
+   nir_ssa_def *alu_result = nir_build_alu1(&bld, nir_op_mov, imm_vec);
+   nir_alu_instr *alu_instr = nir_instr_as_alu(alu_result->parent_instr);
+   set_swizzle(&alu_instr->src[0], "x");
+   alu_result->num_components = 1;
+   alu_instr->dest.write_mask = BITFIELD_MASK(1);
+
+   nir_store_var(&bld, out_var, alu_result, 1);
+
+   ASSERT_TRUE(nir_opt_shrink_vectors(bld.shader));
+
+   nir_validate_shader(bld.shader, NULL);
+
+   ASSERT_TRUE(imm_vec->num_components == 1);
+   nir_load_const_instr * imm_vec_instr = nir_instr_as_load_const(imm_vec->parent_instr);
+   ASSERT_TRUE(nir_const_value_as_float(imm_vec_instr->value[0], 32) == 1.0);
+
+   ASSERT_FALSE(nir_opt_shrink_vectors(bld.shader));
+}
+
 TEST_F(nir_opt_shrink_vectors_test, opt_shrink_vectors_alu_trailing_component_only)
 {
    /* Test that opt_shrink_vectors correctly removes unused trailing channels