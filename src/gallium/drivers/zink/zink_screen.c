e82369d06bba824518a68d29a66c2977c42e0215
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-02-02 17:15:25 -0500

    zink: enable bindless texture with ZINK_DESCRIPTORS=db
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21085>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -774,7 +774,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return 1;
 
    case PIPE_CAP_BINDLESS_TEXTURE:
-      return zink_descriptor_mode != ZINK_DESCRIPTOR_MODE_DB && screen->info.have_EXT_descriptor_indexing;
+      return screen->info.have_EXT_descriptor_indexing;
 
    case PIPE_CAP_TEXTURE_BUFFER_OFFSET_ALIGNMENT:
       return screen->info.props.limits.minTexelBufferOffsetAlignment;
