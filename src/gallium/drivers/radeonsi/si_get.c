8b000ebbb5ec9c4ce2362d709516a0b8dedb36c4
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-02-02 10:47:04 -0500

    radeonsi: set PIPE_CAP_NULL_TEXTURES
    fixes #8163
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21072>

--- src/gallium/drivers/radeonsi/si_get.c
+++ src/gallium/drivers/radeonsi/si_get.c
@@ -178,6 +178,7 @@ static int si_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_DEVICE_RESET_STATUS_QUERY:
    case PIPE_CAP_TEXTURE_MULTISAMPLE:
    case PIPE_CAP_ALLOW_GLTHREAD_BUFFER_SUBDATA_OPT: /* TODO: remove if it's slow */
+   case PIPE_CAP_NULL_TEXTURES:
       return 1;
 
    case PIPE_CAP_TEXTURE_TRANSFER_MODES:
