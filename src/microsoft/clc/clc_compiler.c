31778ac869ef8e6d16ae029fa49f9232194b39e0
Jesse Natalie <jenatali@microsoft.com>
2023-02-07 14:14:37 -0800

    microsoft/clc: Add shader model / validator to compiler API
    Shader model 6.2 was the upper bounds of what *could* be generated
    before, but not all devices support it. And other devices support
    even more. So, let's pass in the shader model / validator that will
    be used by the API caller.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21178>

--- src/microsoft/clc/clc_compiler.c
+++ src/microsoft/clc/clc_compiler.c
@@ -975,8 +975,8 @@ clc_spirv_to_dxil(struct clc_libclc *lib,
       .disable_math_refactoring = true,
       .num_kernel_globals = num_global_inputs,
       .environment = DXIL_ENVIRONMENT_CL,
-      .shader_model_max = SHADER_MODEL_6_2,
-      .validator_version_max = DXIL_VALIDATOR_1_4,
+      .shader_model_max = conf && conf->max_shader_model ? conf->max_shader_model : SHADER_MODEL_6_2,
+      .validator_version_max = conf ? conf->validator_version : DXIL_VALIDATOR_1_4,
    };
 
    for (unsigned i = 0; i < out_dxil->kernel->num_args; i++) {
