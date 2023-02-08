067545eb9af79414054e4bef3ab48af003758663
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-02-08 15:36:51 -0500

    zink: always use NEAREST for zs blits
    LINEAR is illegal, and swapping filtering at this point still
    seems to be conformant given questionable GL spec requirements for
    LINEAR zs filtering
    
    cc: mesa-stable
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21203>

--- src/gallium/drivers/zink/zink_blit.c
+++ src/gallium/drivers/zink/zink_blit.c
@@ -265,7 +265,8 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info, bool *n
    VKCTX(CmdBlitImage)(cmdbuf, src->obj->image, src->layout,
                   dst->obj->image, dst->layout,
                   1, &region,
-                  zink_filter(info->filter));
+                  /* VUID-vkCmdBlitImage-srcImage-00232: zs formats must use NEAREST filtering */
+                  util_format_is_depth_or_stencil(info->src.format) ? VK_FILTER_NEAREST : zink_filter(info->filter));
 
    zink_cmd_debug_marker_end(ctx, marker);
 
