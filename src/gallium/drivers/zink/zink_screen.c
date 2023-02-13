2748301a09dc2d5c9e31366bbc42335e49b56750
antonino <antonino.maniscalco@collabora.com>
2023-02-13 12:50:16 +0100

    zink: advertise support for the quad primitive
    Zink now exposes the `PIPE_PRIM_QUADS` among supported primitives and
    handles them with geometry shaders.
    
    Previously, while not exposing this capability, gallium would internally
    generate an index buffer to draw them with triangles.
    
    However the information necessary to avoid drawing the diagonal line
    when using the line primitive was not preserved.
    
    fails are added for wireframe xfb quads tests
    
    xfb is expected to output tessellatated quads while showing a quad
    without a diagonal, however there is no sane way of achieving this.
    
    As part of the test quads will be rendered with and without xfb and the
    results compared.
    
    Now to avoid breaking xfb zink has to always split quads into triangles
    when xfb is enabled. This means that the test will fail.
    
    Previously the diagonal was always present so the test passed
    
    Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21238>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -520,7 +520,6 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    }
    case PIPE_CAP_SUPPORTED_PRIM_MODES: {
       uint32_t modes = BITFIELD_MASK(PIPE_PRIM_MAX);
-      modes &= ~BITFIELD_BIT(PIPE_PRIM_QUADS);
       modes &= ~BITFIELD_BIT(PIPE_PRIM_QUAD_STRIP);
       modes &= ~BITFIELD_BIT(PIPE_PRIM_POLYGON);
       modes &= ~BITFIELD_BIT(PIPE_PRIM_LINE_LOOP);
