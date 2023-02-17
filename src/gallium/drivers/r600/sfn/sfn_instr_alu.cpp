aerisarn/mesa-uwp

commit 2df023a1f1990aad6c20eca85af19c7d21a43203

r600/sfn: pre-evaluate allowed dest mask in Alu instructions
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20451>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -68,6 +68,17 @@ AluInstr::AluInstr(EAluOp opcode,
       ASSERT_OR_THROW(dest, "Write flag is set, but no destination register is given");
 
    update_uses();
+
+   if (dest && slots > 1) {
+      switch (m_opcode) {
+      case op2_dot: m_allowed_desk_mask = (1 << (4 - slots)) - 1; break;
+      default:
+         if (has_alu_flag(alu_is_cayman_trans)) {
+            m_allowed_desk_mask = (1 << slots) - 1;
+         }
+      }
+   }
+   assert(!dest || (m_allowed_desk_mask & (1 << dest->chan())));
 }
 
 AluInstr::AluInstr(EAluOp opcode):
@@ -491,20 +502,6 @@ uint8_t AluInstr::allowed_src_chan_mask() const
            mask |= 1 << i;
    }
    return mask;
-}
-
-uint8_t
-AluInstr::allowed_dest_chan_mask() const
-{
-   if (alu_slots() != 1) {
-      if (has_alu_flag(alu_is_cayman_trans))
-         return (1 << alu_slots()) - 1;
-
-      if (m_opcode == op2_dot_ieee) {
-         return (1 << (4 - alu_slots())) - 1;
-      }
-   }
-   return 0xf;
 }
 
 bool

src/gallium/drivers/r600/sfn/sfn_instr_alu.h
@@ -180,7 +180,7 @@ public:
    AluInstr *as_alu() override { return this; }
 
    uint8_t allowed_src_chan_mask() const override;
-   uint8_t allowed_dest_chan_mask() const;
+   uint8_t allowed_dest_chan_mask() const {return m_allowed_desk_mask;}
 
 private:
    friend class AluGroup;
@@ -216,6 +216,7 @@ private:
    int m_priority{0};
    std::set<PRegister, std::less<PRegister>, Allocator<PRegister>> m_extra_dependencies;
    AluGroup *m_parent_group{nullptr};
+   unsigned m_allowed_desk_mask{0xf};
 };
 
 class AluInstrVisitor : public InstrVisitor {

commit 4152e38924bda107d026dac848bf5e7ae8accf78

r600/sfn: Fix alu trans op flag setup
Fixes: commit 2df023a1f1990aad6c20eca85af19c7d21a43203
   r600/sfn: pre-evaluate allowed dest mask in Alu instructions

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21357>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -2880,6 +2880,8 @@ emit_alu_trans_op1_cayman(const nir_alu_instr& alu, EAluOp opcode, Shader& shade
 
    auto pin = pin_for_components(alu);
 
+   const std::set<AluModifiers> flags({alu_write, alu_last_instr, alu_is_cayman_trans});
+
    for (unsigned j = 0; j < nir_dest_num_components(alu.dest.dest); ++j) {
       if (alu.dest.write_mask & (1 << j)) {
          unsigned ncomp =  j == 3 ? 4 : 3;
@@ -2890,7 +2892,7 @@ emit_alu_trans_op1_cayman(const nir_alu_instr& alu, EAluOp opcode, Shader& shade
          for (unsigned i = 0; i < ncomp; ++i)
             srcs[i] = value_factory.src(src0, j);
 
-         auto ir = new AluInstr(opcode, dest, srcs, AluInstr::last_write, ncomp);
+         auto ir = new AluInstr(opcode, dest, srcs, flags, ncomp);
 
          if (alu.src[0].abs)
             ir->set_alu_flag(alu_src0_abs);
@@ -2952,6 +2954,8 @@ emit_alu_trans_op2_cayman(const nir_alu_instr& alu, EAluOp opcode, Shader& shade
 
    unsigned last_slot = 4;
 
+   const std::set<AluModifiers> flags({alu_write, alu_last_instr, alu_is_cayman_trans});
+
    for (unsigned k = 0; k < nir_dest_num_components(alu.dest.dest); ++k) {
       if (alu.dest.write_mask & (1 << k)) {
          AluInstr::SrcValues srcs(2 * last_slot);
@@ -2962,7 +2966,7 @@ emit_alu_trans_op2_cayman(const nir_alu_instr& alu, EAluOp opcode, Shader& shade
             srcs[2 * i + 1] = value_factory.src(src1, k);
          }
 
-         auto ir = new AluInstr(opcode, dest, srcs, AluInstr::last_write, last_slot);
+         auto ir = new AluInstr(opcode, dest, srcs, flags, last_slot);
 
          if (src0.negate)
             ir->set_alu_flag(alu_src0_neg);