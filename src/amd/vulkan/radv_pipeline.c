77252687fa4706b4357f1689211212147958b6a6
Georg Lehmann <dadschoorse@gmail.com>
2023-02-18 13:38:46 +0100

    amd: don't use d16 for integer loads
    D16 saturates to min/max instead of just truncating.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21404>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -3296,8 +3296,8 @@ radv_postprocess_nir(struct radv_pipeline *pipeline,
       };
       struct nir_fold_16bit_tex_image_options fold_16bit_options = {
          .rounding_mode = nir_rounding_mode_rtne,
-         .fold_tex_dest_types = nir_type_float | nir_type_uint | nir_type_int,
-         .fold_image_dest_types = nir_type_float | nir_type_uint | nir_type_int,
+         .fold_tex_dest_types = nir_type_float,
+         .fold_image_dest_types = nir_type_float,
          .fold_image_store_data = true,
          .fold_image_srcs = !radv_use_llvm_for_stage(device, stage->stage),
          .fold_srcs_options_count = separate_g16 ? 2 : 1,
