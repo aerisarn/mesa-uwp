aerisarn/mesa-uwp

commit 1026d29344192755dd340d6ac13a9674189d2d61

dri: Introduce internal Mesa DRI driver loader extension.
All DRI loaders in Mesa (EGL, GLX, gbm) now require this ext and that the
driver come from a matching build.  This will let us use Mesa-internal
types and enums across the loader-driver bounary inside of Mesa.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Acked-by: Eric Engestrom <eric@igalia.com>
LOL-YESed-by: Kristian Høgsberg <krh@bitplanet.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20069>

src/loader/loader.c
@@ -46,9 +46,11 @@
 #endif
 #include <GL/gl.h>
 #include <GL/internal/dri_interface.h>
+#include <GL/internal/mesa_interface.h>
 #include "loader.h"
 #include "util/os_file.h"
 #include "util/os_misc.h"
+#include "git_sha1.h"
 
 #ifdef HAVE_LIBDRM
 #include <xf86drm.h>
@@ -626,6 +628,18 @@ loader_bind_extensions(void *data,
                match->name, match->version);
          if (!match->optional)
             ret = false;
+      }
+
+      /* The loaders rely on the loaded DRI drivers being from the same Mesa
+       * build so that we can reference the same structs on both sides.
+       */
+      if (strcmp(match->name, __DRI_MESA) == 0) {
+         const __DRImesaCoreExtension *mesa = (const __DRImesaCoreExtension *)*field;
+         if (strcmp(mesa->version_string, MESA_INTERFACE_VERSION_STRING) != 0) {
+            log_(_LOADER_FATAL, "DRI driver not from this Mesa build ('%s' vs '%s')\n",
+                 mesa->version_string, MESA_INTERFACE_VERSION_STRING);
+            ret = false;
+         }
       }
    }
 

include/GL/internal/mesa_interface.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright © 2022 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef MESA_INTERFACE_H
+#define MESA_INTERFACE_H
+
+#include "dri_interface.h"
+
+/* Mesa-internal interface between the GLX, GBM, and EGL DRI driver loaders, and
+ * the gallium dri_util.c code.
+ */
+
+typedef struct __DRImesaCoreExtensionRec __DRImesaCoreExtension;
+
+#define __DRI_MESA "DRI_Mesa"
+#define __DRI_MESA_VERSION 1
+
+/**  Core struct that appears alongside __DRI_CORE for Mesa-internal usage.
+ * Implemented in the top-level dri/drisw/kopper extension list.
+ */
+struct __DRImesaCoreExtensionRec {
+   __DRIextension base;
+
+   /* Version string for verifying that the DRI driver is from the same build as
+    * the loader.
+    */
+#define MESA_INTERFACE_VERSION_STRING PACKAGE_VERSION MESA_GIT_SHA1
+   const char *version_string;
+};
+
+#endif /* MESA_INTERFACE_H */

src/egl/drivers/dri2/egl_dri2.c
@@ -704,12 +704,14 @@ const __DRIimageLookupExtension image_lookup_extension = {
 
 static const struct dri_extension_match dri3_driver_extensions[] = {
    { __DRI_CORE, 1, offsetof(struct dri2_egl_display, core), false },
+   { __DRI_MESA, 1, offsetof(struct dri2_egl_display, mesa), false },
    { __DRI_IMAGE_DRIVER, 1, offsetof(struct dri2_egl_display, image_driver), false },
    { __DRI_CONFIG_OPTIONS, 2, offsetof(struct dri2_egl_display, configOptions), true },
 };
 
 static const struct dri_extension_match dri2_driver_extensions[] = {
    { __DRI_CORE, 1, offsetof(struct dri2_egl_display, core), false },
+   { __DRI_MESA, 1, offsetof(struct dri2_egl_display, mesa), false },
    { __DRI_DRI2, 4, offsetof(struct dri2_egl_display, dri2), false },
    { __DRI_CONFIG_OPTIONS, 2, offsetof(struct dri2_egl_display, configOptions), true },
 };
@@ -722,6 +724,7 @@ static const struct dri_extension_match dri2_core_extensions[] = {
 
 static const struct dri_extension_match swrast_driver_extensions[] = {
    { __DRI_CORE, 1, offsetof(struct dri2_egl_display, core), false },
+   { __DRI_MESA, 1, offsetof(struct dri2_egl_display, mesa), false },
    { __DRI_SWRAST, 4, offsetof(struct dri2_egl_display, swrast), false },
    { __DRI_CONFIG_OPTIONS, 2, offsetof(struct dri2_egl_display, configOptions), true },
 };

src/egl/drivers/dri2/egl_dri2.h
@@ -59,6 +59,7 @@ struct zwp_linux_dmabuf_feedback_v1;
 
 #include <GL/gl.h>
 #include <GL/internal/dri_interface.h>
+#include <GL/internal/mesa_interface.h>
 #include "kopper_interface.h"
 
 #ifdef HAVE_DRM_PLATFORM
@@ -226,6 +227,7 @@ struct dri2_egl_display
    const __DRIconfig **driver_configs;
    void *driver;
    const __DRIcoreExtension *core;
+   const __DRImesaCoreExtension *mesa;
    const __DRIimageDriverExtension *image_driver;
    const __DRIdri2Extension *dri2;
    const __DRIswrastExtension *swrast;

src/egl/meson.build
@@ -93,6 +93,7 @@ if with_dri2
     'drivers/dri2/egl_dri2.c',
     'drivers/dri2/egl_dri2.h',
   )
+  files_egl += sha1_h
   deps_for_egl += idep_xmlconfig
   link_for_egl += libloader
   incs_for_egl += inc_loader

src/gallium/frontends/dri/dri2.c
@@ -29,7 +29,9 @@
  */
 
 #include <xf86drm.h>
+#include "git_sha1.h"
 #include "GL/mesa_glinterop.h"
+#include "GL/internal/mesa_interface.h"
 #include "util/disk_cache.h"
 #include "util/u_memory.h"
 #include "util/u_inlines.h"
@@ -2405,9 +2407,15 @@ static const struct __DRIBackendVtableExtensionRec galliumdrm_vtable = {
    .InitScreen = dri2_init_screen,
 };
 
+static const struct __DRImesaCoreExtensionRec mesaCoreExtension = {
+   .base = { __DRI_MESA, 1 },
+   .version_string = MESA_INTERFACE_VERSION_STRING,
+};
+
 /* This is the table of extensions that the loader will dlsym() for. */
 const __DRIextension *galliumdrm_driver_extensions[] = {
     &driCoreExtension.base,
+    &mesaCoreExtension.base,
     &driImageDriverExtension.base,
     &driDRI2Extension.base,
     &gallium_config_options.base,
@@ -2429,6 +2437,7 @@ static const struct __DRIBackendVtableExtensionRec dri_swrast_kms_vtable = {
 
 const __DRIextension *dri_swrast_kms_driver_extensions[] = {
     &driCoreExtension.base,
+    &mesaCoreExtension.base,
     &driImageDriverExtension.base,
     &swkmsDRI2Extension.base,
     &gallium_config_options.base,

src/gallium/frontends/dri/drisw.c
@@ -26,6 +26,8 @@
  *
  **************************************************************************/
 
+#include "GL/internal/mesa_interface.h"
+#include "git_sha1.h"
 #include "util/format/u_format.h"
 #include "util/u_memory.h"
 #include "util/u_inlines.h"
@@ -631,9 +633,15 @@ const __DRIcopySubBufferExtension driSWCopySubBufferExtension = {
    .copySubBuffer               = driswCopySubBuffer,
 };
 
+static const struct __DRImesaCoreExtensionRec mesaCoreExtension = {
+   .base = { __DRI_MESA, 1 },
+   .version_string = MESA_INTERFACE_VERSION_STRING,
+};
+
 /* This is the table of extensions that the loader will dlsym() for. */
 const __DRIextension *galliumsw_driver_extensions[] = {
     &driCoreExtension.base,
+    &mesaCoreExtension.base,
     &driSWRastExtension.base,
     &driSWCopySubBufferExtension.base,
     &gallium_config_options.base,

src/gallium/frontends/dri/kopper.c
@@ -21,6 +21,8 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+#include "GL/internal/mesa_interface.h"
+#include "git_sha1.h"
 #include "util/format/u_format.h"
 #include "util/u_memory.h"
 #include "util/u_inlines.h"
@@ -954,8 +956,14 @@ static const struct __DRIBackendVtableExtensionRec galliumvk_vtable = {
    .InitScreen = kopper_init_screen,
 };
 
+static const struct __DRImesaCoreExtensionRec mesaCoreExtension = {
+   .base = { __DRI_MESA, 1 },
+   .version_string = MESA_INTERFACE_VERSION_STRING,
+};
+
 const __DRIextension *galliumvk_driver_extensions[] = {
    &driCoreExtension.base,
+   &mesaCoreExtension.base,
    &driSWRastExtension.base,
    &driDRI2Extension.base,
    &driImageDriverExtension.base,

commit f4ce19063c2e8f2b9f4567e8b468896f957d3d8b

loader: do not check the mesa DRI_Mesa version if it was not found
Signed-off-by: Sebastian Wick <sebastian.wick@redhat.com>
Fixes: 1026d2934419 ("dri: Introduce internal Mesa DRI driver loader extension.")
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21429>

src/loader/loader.c
@@ -637,6 +637,7 @@ loader_bind_extensions(void *data,
                match->name, match->version);
          if (!match->optional)
             ret = false;
+         continue;
       }
 
       /* The loaders rely on the loaded DRI drivers being from the same Mesa