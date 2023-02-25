aerisarn/mesa-uwp

commit 9fd9f3cd10cf58549f850aeca9103d8cca5a5f81

r600/sfn: Set minimum required registers based on array allocation
In the rare case that after register allocation the highest directly
accessed register index is below the highest value used for an
indirectly accessed array we have to ensure that the shader allocates
enough registers to account for these indices that are not seen by the
assembler.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7966
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20451>

src/gallium/drivers/r600/sfn/sfn_shader.cpp
@@ -520,6 +520,7 @@ Shader::allocate_local_registers(const exec_list *registers)
 {
    if (value_factory().allocate_registers(registers))
       m_indirect_files |= 1 << TGSI_FILE_TEMPORARY;
+   m_required_registers = value_factory().next_register_index() - 1;
 }
 
 bool

src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -1029,11 +1029,13 @@ r600_shader_from_nir(struct r600_context *rctx,
                       rscreen->b.family,
                       rscreen->has_compressed_msaa_texturing);
 
+
    r600::sfn_log << r600::SfnLog::shader_info << "pipeshader->shader.processor_type = "
                  << pipeshader->shader.processor_type << "\n";
 
    pipeshader->shader.bc.type = pipeshader->shader.processor_type;
    pipeshader->shader.bc.isa = rctx->isa;
+   pipeshader->shader.bc.ngpr = shader->required_registers();
 
    r600::Assembler afs(&pipeshader->shader, *key);
    if (!afs.lower(scheduled_shader)) {

src/gallium/drivers/r600/sfn/sfn_shader.h
@@ -189,8 +189,8 @@ public:
    void set_info(nir_shader *nir);
    void get_shader_info(r600_shader *sh_info);
 
-   r600_chip_class chip_class() const { return m_chip_class; };
-   void set_chip_class(r600_chip_class cls) { m_chip_class = cls; };
+   r600_chip_class chip_class() const { return m_chip_class; }
+   void set_chip_class(r600_chip_class cls) { m_chip_class = cls; }
 
    void start_new_block(int nesting_depth);
 
@@ -205,7 +205,7 @@ public:
    void chain_scratch_read(Instr *instr);
    void chain_ssbo_read(Instr *instr);
 
-   virtual uint32_t enabled_stream_buffers_mask() const { return 0; };
+   virtual uint32_t enabled_stream_buffers_mask() const { return 0; }
 
    size_t noutputs() const { return m_outputs.size(); }
    size_t ninputs() const { return m_inputs.size(); }
@@ -244,6 +244,8 @@ public:
    PRegister emit_load_to_register(PVirtualValue src);
 
    virtual unsigned image_size_const_offset() { return 0;}
+
+   auto required_registers() const { return m_required_registers;}
 
 protected:
    enum ESlots {
@@ -350,6 +352,7 @@ private:
 
    int32_t m_ssbo_image_offset{0};
    uint32_t m_nloops{0};
+   uint32_t m_required_registers{0};
 
    class InstructionChain : public InstrVisitor {
    public:

commit 182066538f31051ebeb8216e181bec5721d886af

r600/sfn: Fix minimum required registers
Don't count the local registers, just the arrays, because local
registers might be renamed during register allocation.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8100

Fixes: 9fd9f3cd10cf58549f850aeca9103d8cca5a5f81
   r600/sfn: Set minimum required registers based on array allocation

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21523>

src/gallium/drivers/r600/sfn/sfn_shader.cpp
@@ -522,8 +522,7 @@ Shader::allocate_local_registers(const exec_list *registers)
 {
    if (value_factory().allocate_registers(registers))
       m_indirect_files |= 1 << TGSI_FILE_TEMPORARY;
-   m_required_registers = value_factory().next_register_index() ?
-                             value_factory().next_register_index() - 1 : 0;
+   m_required_registers = value_factory().array_registers();
 }
 
 bool

src/gallium/drivers/r600/sfn/sfn_valuefactory.cpp
@@ -118,6 +118,8 @@ ValueFactory::allocate_registers(const exec_list *registers)
       length = a.length;
    }
 
+   m_required_array_registers = m_next_register_index ? m_next_register_index : 0;
+
    foreach_list_typed(nir_register, reg, node, registers)
    {
       if (!reg->num_array_elems) {

src/gallium/drivers/r600/sfn/sfn_valuefactory.h
@@ -285,6 +285,8 @@ public:
 
    int next_register_index() const { return m_next_register_index; }
 
+   uint32_t array_registers() const { return m_required_array_registers; }
+
 private:
    PVirtualValue ssa_src(const nir_ssa_def& dest, int chan);
 
@@ -326,6 +328,7 @@ private:
       126, pin_chan, {0, 1, 2, 3}
    };
    ChannelCounts m_channel_counts;
+   uint32_t m_required_array_registers{0};
 };
 
 } // namespace r600