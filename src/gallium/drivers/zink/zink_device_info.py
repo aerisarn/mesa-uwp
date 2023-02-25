aerisarn/mesa-uwp

commit e02cdb397e62ae40e901568cb60a3fe0b59db706

zink: prefer vulkan_core.h over vulkan.h
There's just two places where we need any of the WSI specific vulkan
includes, the rest of Zink should do just fine with vulkan_core.h. So
let's include the win32-specific header explicitly in those two places,
and reduce the need for WSI specifics inside zink itself. Kopper
handles the rest of the WSI integration.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21441>

src/gallium/drivers/zink/zink_device_info.py
@@ -347,7 +347,12 @@ header_code = """
 
 #include "util/u_memory.h"
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <vulkan/vulkan_win32.h>
+#endif
 
 struct zink_screen;
 

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.h
@@ -26,7 +26,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #include "compiler/nir/nir.h"
 #include "compiler/shader_enums.h"

src/gallium/drivers/zink/zink_batch.h
@@ -24,7 +24,7 @@
 #ifndef ZINK_BATCH_H
 #define ZINK_BATCH_H
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 #include "zink_types.h"
 
 #include "util/list.h"

src/gallium/drivers/zink/zink_fence.c
@@ -32,6 +32,11 @@
 #include "util/set.h"
 #include "util/u_memory.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#include <vulkan/vulkan_win32.h>
+#endif
+
 static void
 destroy_fence(struct zink_screen *screen, struct zink_tc_fence *mfence)
 {

src/gallium/drivers/zink/zink_format.h
@@ -28,7 +28,7 @@
 #include "util/format/u_format.h"
 
 #include <stdbool.h>
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 union pipe_color_union;
 

src/gallium/drivers/zink/zink_instance.py
@@ -69,7 +69,7 @@ header_code = """
 
 #include "os/os_process.h"
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #if defined(__APPLE__)
 // Source of MVK_VERSION

src/gallium/drivers/zink/zink_types.h
@@ -27,7 +27,7 @@
 #ifndef ZINK_TYPES_H
 #define ZINK_TYPES_H
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #include "compiler/nir/nir.h"
 

commit 297fda6cf13c5f1672f235c65d8b437a1c64f882

zink: fix build with -Dvulkan-beta=true
Fix a build error with -Dvulkan-beta=true:

  ../src/gallium/drivers/zink/zink_screen.c: In function ‘zink_internal_create_screen’:
  ../src/gallium/drivers/zink/zink_screen.c:2764:20: error: ‘struct zink_device_info’ has no member named ‘have_KHR_portability_subset’
   2764 |    if (screen->info.have_KHR_portability_subset) {
        |                    ^
  ../src/gallium/drivers/zink/zink_screen.c:2765:60: error: ‘struct zink_device_info’ has no member named ‘portability_subset_feats’
   2765 |       screen->have_triangle_fans = (VK_TRUE == screen->info.portability_subset_feats.triangleFans);
        |                                                            ^

Fixes: e02cdb397e62 ("zink: prefer vulkan_core.h over vulkan.h")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21491>

src/gallium/drivers/zink/zink_device_info.py
@@ -349,6 +349,10 @@ header_code = """
 
 #include <vulkan/vulkan_core.h>
 
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+#include <vulkan/vulkan_beta.h>
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 #include <vulkan/vulkan_win32.h>