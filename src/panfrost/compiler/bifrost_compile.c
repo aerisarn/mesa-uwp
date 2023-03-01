cb0f4b8146fac1429a97ad341f45e405ff8adc7b
Alyssa Rosenzweig <alyssa@collabora.com>
2023-03-01 14:49:16 -0500

    pan/bi: Drop control_barrier handling
    Now unreachable.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21634>

--- src/panfrost/compiler/bifrost_compile.c
+++ src/panfrost/compiler/bifrost_compile.c
@@ -1640,11 +1640,6 @@ bi_emit_intrinsic(bi_builder *b, nir_intrinsic_instr *instr)
       bi_emit_store(b, instr, BI_SEG_WLS);
       break;
 
-   case nir_intrinsic_control_barrier:
-      assert(b->shader->stage != MESA_SHADER_FRAGMENT);
-      bi_barrier(b);
-      break;
-
    case nir_intrinsic_scoped_barrier:
       if (nir_intrinsic_execution_scope(instr) != NIR_SCOPE_NONE) {
          assert(b->shader->stage != MESA_SHADER_FRAGMENT);
