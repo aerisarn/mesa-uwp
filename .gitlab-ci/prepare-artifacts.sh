5d30d11ccceae75765b001d6cd453b4e0713f6cf
Sergi Blanch Torne <sergi.blanch.torne@collabora.com>
2023-03-03 08:53:05 +0100

    ci: include setup test environment script in the output artifacts
    In mesa/mesa !20272, some bash functions introduce a standard wait to setup
    gitlab ci sections, but the file collecting them needed to be included in the
    artifacts exported by mesa. Other projects that use tests like deqp-runner
    need to load these bash functions.
    
    Signed-off-by: Sergi Blanch Torne <sergi.blanch.torne@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21680>

--- .gitlab-ci/prepare-artifacts.sh
+++ .gitlab-ci/prepare-artifacts.sh
@@ -38,6 +38,7 @@ cp -Rp .gitlab-ci/*.txt install/
 cp -Rp .gitlab-ci/report-flakes.py install/
 cp -Rp .gitlab-ci/valve install/
 cp -Rp .gitlab-ci/vkd3d-proton install/
+cp -Rp .gitlab-ci/setup-test-env.sh install/
 cp -Rp .gitlab-ci/*-runner.sh install/
 find . -path \*/ci/\*.txt \
     -o -path \*/ci/\*.toml \
