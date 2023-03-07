aerisarn/mesa-uwp

commit b0d698c5328fbcb5e0ae3952cde1c1e411151adb

rusticl: correctly check global argument size
As the spec that is quoted in the comment says, if the argument is a
memory object, arg_size should be different than sizeof(cl_mem). The
previous verification only worked if the underlying type has the same
size as sizeof(cl_mem).

Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18985>

src/gallium/frontends/rusticl/api/kernel.rs
@@ -240,6 +240,11 @@ pub fn set_kernel_arg(
                     return Err(CL_INVALID_ARG_SIZE);
                 }
             }
+            KernelArgType::MemGlobal => {
+                if arg_size != std::mem::size_of::<cl_mem>() {
+                    return Err(CL_INVALID_ARG_SIZE);
+                }
+            }
             _ => {
                 if arg.size != arg_size {
                     return Err(CL_INVALID_ARG_SIZE);

commit 4c24ff0a3417c47fad64b587625b88c9d1fc1fd3

rusticl/kernel: Images arg sizes also have to match the host pointer size
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8267
Fixes: b0d698c5328 ("rusticl: correctly check global argument size")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21604>

src/gallium/frontends/rusticl/api/kernel.rs
@@ -240,7 +240,11 @@ pub fn set_kernel_arg(
                     return Err(CL_INVALID_ARG_SIZE);
                 }
             }
-            KernelArgType::MemGlobal | KernelArgType::MemConstant => {
+            KernelArgType::MemGlobal
+            | KernelArgType::MemConstant
+            | KernelArgType::Image
+            | KernelArgType::RWImage
+            | KernelArgType::Texture => {
                 if arg_size != std::mem::size_of::<cl_mem>() {
                     return Err(CL_INVALID_ARG_SIZE);
                 }