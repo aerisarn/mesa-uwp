aerisarn/mesa-uwp

commit 271ebdd735e4480e1a4aab107efd45d0654a3a86

zink: pass cmdbuf to debug marker begin
this is otherwise broken with cmdbuf reordering

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21583>

src/gallium/drivers/zink/zink_blit.c
@@ -71,7 +71,7 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info, bool *
    zink_batch_reference_resource_rw(batch, src, false);
    zink_batch_reference_resource_rw(batch, dst, true);
 
-   bool marker = zink_cmd_debug_marker_begin(ctx, "blit_resolve(%s->%s, %dx%d->%dx%d)",
+   bool marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "blit_resolve(%s->%s, %dx%d->%dx%d)",
                                              util_format_short_name(info->src.format),
                                              util_format_short_name(info->src.format),
                                              info->src.box.width, info->src.box.height,
@@ -181,7 +181,7 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info, bool *n
    zink_batch_reference_resource_rw(batch, src, false);
    zink_batch_reference_resource_rw(batch, dst, true);
 
-   bool marker = zink_cmd_debug_marker_begin(ctx, "blit_native(%s->%s, %dx%d->%dx%d)",
+   bool marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "blit_native(%s->%s, %dx%d->%dx%d)",
                                              util_format_short_name(info->src.format),
                                              util_format_short_name(info->src.format),
                                              info->src.box.width, info->src.box.height,
@@ -330,7 +330,7 @@ zink_blit(struct pipe_context *pctx,
    bool stencil_blit = false;
    if (!util_blitter_is_blit_supported(ctx->blitter, info)) {
       if (util_format_is_depth_or_stencil(info->src.resource->format)) {
-         bool marker = zink_cmd_debug_marker_begin(ctx, "zink_blit(Z %s->%s, %dx%d->%dx%d)",
+         bool marker = zink_cmd_debug_marker_begin(ctx, VK_NULL_HANDLE, "zink_blit(Z %s->%s, %dx%d->%dx%d)",
                                                    util_format_short_name(info->src.format),
                                                    util_format_short_name(info->dst.format),
                                                    info->src.box.width, info->src.box.height,
@@ -370,7 +370,7 @@ zink_blit(struct pipe_context *pctx,
    zink_blit_begin(ctx, ZINK_BLIT_SAVE_FB | ZINK_BLIT_SAVE_FS | ZINK_BLIT_SAVE_TEXTURES);
 
    if (stencil_blit) {
-      bool marker = zink_cmd_debug_marker_begin(ctx, "zink_blit(S fallback %s->%s, %dx%d->%dx%d)",
+      bool marker = zink_cmd_debug_marker_begin(ctx, VK_NULL_HANDLE, "zink_blit(S fallback %s->%s, %dx%d->%dx%d)",
                                                 util_format_short_name(info->src.format),
                                                 util_format_short_name(info->dst.format),
                                                 info->src.box.width, info->src.box.height,
@@ -396,7 +396,7 @@ zink_blit(struct pipe_context *pctx,
 
       zink_cmd_debug_marker_end(ctx, marker);
    } else {
-      bool marker = zink_cmd_debug_marker_begin(ctx, "zink_blit(%s->%s, %dx%d->%dx%d)",
+      bool marker = zink_cmd_debug_marker_begin(ctx, VK_NULL_HANDLE, "zink_blit(%s->%s, %dx%d->%dx%d)",
                                                 util_format_short_name(info->src.format),
                                                 util_format_short_name(info->dst.format),
                                                 info->src.box.width, info->src.box.height,

src/gallium/drivers/zink/zink_context.c
@@ -3737,7 +3737,7 @@ zink_resource_image_barrier(struct zink_context *ctx, struct zink_resource *res,
       imb.dstQueueFamilyIndex = zink_screen(ctx->base.screen)->gfx_queue;
       res->dmabuf_acquire = false;
    }
-   bool marker = zink_cmd_debug_marker_begin(ctx, "image_barrier(%s->%s)", vk_ImageLayout_to_str(res->layout), vk_ImageLayout_to_str(new_layout));
+   bool marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "image_barrier(%s->%s)", vk_ImageLayout_to_str(res->layout), vk_ImageLayout_to_str(new_layout));
    VKCTX(CmdPipelineBarrier)(
       cmdbuf,
       res->obj->access_stage ? res->obj->access_stage : VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
@@ -3792,7 +3792,7 @@ zink_resource_image_barrier2(struct zink_context *ctx, struct zink_resource *res
       1,
       &imb
    };
-   bool marker = zink_cmd_debug_marker_begin(ctx, "image_barrier(%s->%s)", vk_ImageLayout_to_str(res->layout), vk_ImageLayout_to_str(new_layout));
+   bool marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "image_barrier(%s->%s)", vk_ImageLayout_to_str(res->layout), vk_ImageLayout_to_str(new_layout));
    VKCTX(CmdPipelineBarrier2)(cmdbuf, &dep);
    zink_cmd_debug_marker_end(ctx, marker);
 
@@ -3897,7 +3897,7 @@ zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_resource *res
             idx += snprintf(&buf[idx], sizeof(buf) - idx, "%s", vk_AccessFlagBits_to_str(1ul<<bit));
             first = false;
          }
-         marker = zink_cmd_debug_marker_begin(ctx, "buffer_barrier(%s)", buf);
+         marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "buffer_barrier(%s)", buf);
       }
       VKCTX(CmdPipelineBarrier)(
          cmdbuf,
@@ -3959,7 +3959,7 @@ zink_resource_buffer_barrier2(struct zink_context *ctx, struct zink_resource *re
             idx += snprintf(&buf[idx], sizeof(buf) - idx, "%s", vk_AccessFlagBits_to_str(1ul<<bit));
             first = false;
          }
-         marker = zink_cmd_debug_marker_begin(ctx, "buffer_barrier(%s)", buf);
+         marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "buffer_barrier(%s)", buf);
       }
       VKCTX(CmdPipelineBarrier2)(cmdbuf, &dep);
       zink_cmd_debug_marker_end(ctx, marker);
@@ -5503,7 +5503,7 @@ zink_update_barriers(struct zink_context *ctx, bool is_compute,
  * execution on the GPU.
  */
 bool
-zink_cmd_debug_marker_begin(struct zink_context *ctx, const char *fmt, ...)
+zink_cmd_debug_marker_begin(struct zink_context *ctx, VkCommandBuffer cmdbuf, const char *fmt, ...)
 {
    if (!zink_tracing)
       return false;
@@ -5521,7 +5521,7 @@ zink_cmd_debug_marker_begin(struct zink_context *ctx, const char *fmt, ...)
    info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT;
    info.pLabelName = name;
 
-   VKCTX(CmdBeginDebugUtilsLabelEXT)(ctx->batch.state->cmdbuf, &info);
+   VKCTX(CmdBeginDebugUtilsLabelEXT)(cmdbuf ? cmdbuf : ctx->batch.state->cmdbuf, &info);
 
    free(name);
    return true;

src/gallium/drivers/zink/zink_context.h
@@ -253,7 +253,7 @@ struct zink_surface *
 zink_get_dummy_surface(struct zink_context *ctx, int samples_index);
 
 bool
-zink_cmd_debug_marker_begin(struct zink_context *ctx, const char *fmt, ...);
+zink_cmd_debug_marker_begin(struct zink_context *ctx, VkCommandBuffer cmdbuf, const char *fmt, ...);
 void
 zink_cmd_debug_marker_end(struct zink_context *ctx, bool emitted);
 

src/gallium/drivers/zink/zink_query.c
@@ -760,7 +760,7 @@ copy_pool_results_to_buffer(struct zink_context *ctx, struct zink_query *query, 
    if (flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
       result_size += type_size;
 
-   bool marker = zink_cmd_debug_marker_begin(ctx, "update_qbo(%s: id=%u, num_results=%d)", vk_QueryType_to_str(query->vkqtype), query_id, num_results);
+   bool marker = zink_cmd_debug_marker_begin(ctx, VK_NULL_HANDLE, "update_qbo(%s: id=%u, num_results=%d)", vk_QueryType_to_str(query->vkqtype), query_id, num_results);
 
    zink_batch_no_rp(ctx);
    /* if it's a single query that doesn't need special handling, we can copy it and be done */

commit ec513270e3dc179c1112f284b4e93a88e6cfbe97

zink: Pass the cmdbuf to the end of the marker, too.
Otherwise the end wanders off to some unrelated cmdbuf.

Fixes: 271ebdd735e4 ("zink: pass cmdbuf to debug marker begin")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21799>

src/gallium/drivers/zink/zink_blit.c
@@ -116,7 +116,7 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info, bool *
    VKCTX(CmdResolveImage)(cmdbuf, src->obj->image, src->layout,
                      dst->obj->image, dst->layout,
                      1, &region);
-   zink_cmd_debug_marker_end(ctx, marker);
+   zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
 
    return true;
 }
@@ -267,7 +267,7 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info, bool *n
                   1, &region,
                   zink_filter(info->filter));
 
-   zink_cmd_debug_marker_end(ctx, marker);
+   zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
 
    return true;
 }

src/gallium/drivers/zink/zink_context.c
@@ -3757,7 +3757,7 @@ zink_resource_image_barrier(struct zink_context *ctx, struct zink_resource *res,
       0, NULL,
       1, &imb
    );
-   zink_cmd_debug_marker_end(ctx, marker);
+   zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
 
    resource_check_defer_image_barrier(ctx, res, new_layout, pipeline);
 
@@ -3807,7 +3807,7 @@ zink_resource_image_barrier2(struct zink_context *ctx, struct zink_resource *res
    };
    bool marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "image_barrier(%s->%s)", vk_ImageLayout_to_str(res->layout), vk_ImageLayout_to_str(new_layout));
    VKCTX(CmdPipelineBarrier2)(cmdbuf, &dep);
-   zink_cmd_debug_marker_end(ctx, marker);
+   zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
 
    resource_check_defer_image_barrier(ctx, res, new_layout, pipeline);
 
@@ -3961,7 +3961,7 @@ zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_resource *res
          0, NULL,
          0, NULL
       );
-      zink_cmd_debug_marker_end(ctx, marker);
+      zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
    }
 
    resource_check_defer_buffer_barrier(ctx, res, pipeline);
@@ -4020,7 +4020,7 @@ zink_resource_buffer_barrier2(struct zink_context *ctx, struct zink_resource *re
          marker = zink_cmd_debug_marker_begin(ctx, cmdbuf, "buffer_barrier(%s)", buf);
       }
       VKCTX(CmdPipelineBarrier2)(cmdbuf, &dep);
-      zink_cmd_debug_marker_end(ctx, marker);
+      zink_cmd_debug_marker_end(ctx, cmdbuf, marker);
    }
 
    resource_check_defer_buffer_barrier(ctx, res, pipeline);
@@ -5595,8 +5595,8 @@ zink_cmd_debug_marker_begin(struct zink_context *ctx, VkCommandBuffer cmdbuf, co
 }
 
 void
-zink_cmd_debug_marker_end(struct zink_context *ctx, bool emitted)
+zink_cmd_debug_marker_end(struct zink_context *ctx, VkCommandBuffer cmdbuf, bool emitted)
 {
    if (emitted)
-      VKCTX(CmdEndDebugUtilsLabelEXT)(ctx->batch.state->cmdbuf);
+      VKCTX(CmdEndDebugUtilsLabelEXT)(cmdbuf);
 }

src/gallium/drivers/zink/zink_context.h
@@ -179,7 +179,7 @@ zink_update_barriers(struct zink_context *ctx, bool is_compute,
 bool
 zink_cmd_debug_marker_begin(struct zink_context *ctx, VkCommandBuffer cmdbuf, const char *fmt, ...);
 void
-zink_cmd_debug_marker_end(struct zink_context *ctx, bool emitted);
+zink_cmd_debug_marker_end(struct zink_context *ctx, VkCommandBuffer cmdbuf,bool emitted);
 
 #ifdef __cplusplus
 }

src/gallium/drivers/zink/zink_draw.cpp
@@ -893,7 +893,7 @@ zink_draw(struct pipe_context *pctx,
    }
 
    if (unlikely(zink_tracing && ctx->blitting))
-      zink_cmd_debug_marker_end(ctx, marker);
+      zink_cmd_debug_marker_end(ctx, batch->state->cmdbuf, marker);
 
    if (have_streamout) {
       for (unsigned i = 0; i < ctx->num_so_targets; i++) {

src/gallium/drivers/zink/zink_query.c
@@ -772,7 +772,7 @@ copy_pool_results_to_buffer(struct zink_context *ctx, struct zink_query *query, 
    res->obj->unordered_read = res->obj->unordered_write = false;
    VKCTX(CmdCopyQueryPoolResults)(batch->state->cmdbuf, pool, query_id, num_results, res->obj->buffer,
                                   offset, base_result_size, flags);
-   zink_cmd_debug_marker_end(ctx, marker);
+   zink_cmd_debug_marker_end(ctx, batch->state->cmdbuf, marker);
 }
 
 static void