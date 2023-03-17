aerisarn/mesa-uwp

commit 56108b411fd763d9fcba56bad72ddff11cb0b95a

zink: verify compressed format layer count when creating surfaces
this is illegal for some cases, and mesa/st is equipped to handle
per-layer copying here

fixes #8283

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21292>

src/gallium/drivers/zink/zink_surface.c
@@ -277,6 +277,14 @@ zink_create_surface(struct pipe_context *pctx,
    if (!res->obj->dt && pres->format != templ->format) {
       /* mutable not set by default */
       needs_mutable = !(res->base.b.bind & ZINK_BIND_MUTABLE);
+      /*
+         VUID-VkImageViewCreateInfo-image-07072
+         If image was created with the VK_IMAGE_CREATE_BLOCK_TEXEL_VIEW_COMPATIBLE_BIT flag and
+         format is a non-compressed format, the levelCount and layerCount members of
+         subresourceRange must both be 1
+       */
+      if (needs_mutable && util_format_is_compressed(pres->format) && templ->u.tex.first_layer != templ->u.tex.last_layer)
+         return NULL;
    }
 
    if (!zink_screen(pctx->screen)->threaded && needs_mutable) {

commit cf973fe2990a2dfe9c66cb427bbc37325dd1a39e

zink: fix layer check for compressed format surface creation
this is a universal check, not a check based on mutable state

Fixes: 56108b411fd ("zink: verify compressed format layer count when creating surfaces")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21881>

src/gallium/drivers/zink/zink_surface.c
@@ -283,7 +283,7 @@ zink_create_surface(struct pipe_context *pctx,
          format is a non-compressed format, the levelCount and layerCount members of
          subresourceRange must both be 1
        */
-      if (needs_mutable && util_format_is_compressed(pres->format) && templ->u.tex.first_layer != templ->u.tex.last_layer)
+      if (util_format_is_compressed(pres->format) && templ->u.tex.first_layer != templ->u.tex.last_layer)
          return NULL;
    }
 