c08c2a096396b6ea5301efe803d958559ca9ad33
Corentin Noël <corentin.noel@collabora.com>
2023-03-17 10:53:48 +0100

    ci: Uprev crosvm and virglrenderer
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21693>

--- .gitlab-ci/container/build-crosvm.sh
+++ .gitlab-ci/container/build-crosvm.sh
@@ -6,19 +6,13 @@ set -ex
 git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 
-CROSVM_VERSION=7784d7dbad81ce2e7ec448ec7e821728a2d4ae72
+CROSVM_VERSION=00af43e1b565e1ae0047ba84b970da5e089e4f48
 git clone --single-branch -b main --no-checkout https://chromium.googlesource.com/crosvm/crosvm /platform/crosvm
 pushd /platform/crosvm
 git checkout "$CROSVM_VERSION"
 git submodule update --init
 
-# Revert "minijail: remove MS_NOSYMFOLLOW def because ChromeOS is now on glibc 2.35."
-# Debian Bullseye has only glibc 2.31
-pushd third_party/minijail
-git cherry-pick 0bbf93fdc58520525c85f311c176219bb1b4e96f
-popd
-
-VIRGLRENDERER_VERSION=f0e07bfd20ac6672643dfaf5e08318d8ec803750
+VIRGLRENDERER_VERSION=fc2ad36998f8af8ea3cc68fb9c747dfec9cb4635
 rm -rf third_party/virglrenderer
 git clone --single-branch -b master --no-checkout https://gitlab.freedesktop.org/virgl/virglrenderer.git third_party/virglrenderer
 pushd third_party/virglrenderer
@@ -28,10 +22,10 @@ ninja -C build install
 popd
 
 RUSTFLAGS='-L native=/usr/local/lib' cargo install \
-  bindgen \
+  bindgen-cli \
   -j ${FDO_CI_CONCURRENT:-4} \
   --root /usr/local \
-  --version 0.60.1 \
+  --version 0.63.0 \
   $EXTRA_CARGO_ARGS
 
 RUSTFLAGS='-L native=/usr/local/lib' cargo install \
