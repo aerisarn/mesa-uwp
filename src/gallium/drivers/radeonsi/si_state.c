281126f1d65e0177e333f2be32d901981d8ca950
Marek Olšák <marek.olsak@amd.com>
2023-03-19 17:05:40 -0400

    radeonsi/gfx11: reduce MSAA samples to 8 for no-attachment framebuffer
    EQAA is unsupported
    
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22084>

--- src/gallium/drivers/radeonsi/si_state.c
+++ src/gallium/drivers/radeonsi/si_state.c
@@ -2433,7 +2433,9 @@ static bool si_is_format_supported(struct pipe_screen *screen, enum pipe_format 
       /* Chips with 1 RB don't increment occlusion queries at 16x MSAA sample rate,
        * so don't expose 16 samples there.
        */
-      const unsigned max_eqaa_samples = util_bitcount64(sscreen->info.enabled_rb_mask) <= 1 ? 8 : 16;
+      const unsigned max_eqaa_samples =
+         (sscreen->info.gfx_level >= GFX11 ||
+          util_bitcount64(sscreen->info.enabled_rb_mask) <= 1) ? 8 : 16;
       const unsigned max_samples = 8;
 
       /* MSAA support without framebuffer attachments. */
