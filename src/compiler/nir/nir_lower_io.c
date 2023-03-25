01275a1a95c1a62b3396dba257f41da67e19223f
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-03-25 13:11:16 -0500

    nir: Drop a bunch of Authors tags
    This is what git blame is for.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22120>

--- src/compiler/nir/nir_lower_io.c
+++ src/compiler/nir/nir_lower_io.c
@@ -19,11 +19,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
- *
- * Authors:
- *    Connor Abbott (cwabbott0@gmail.com)
- *    Jason Ekstrand (jason@jlekstrand.net)
- *
  */
 
 /*
