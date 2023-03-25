e001995dc5965d05fc7068a0a70316e7fa8f0b9c
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-03-25 16:21:41 -0500

    util,mesa,panfrost: Drop some author tags
    This is what git blame is for
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22120>

--- src/mesa/main/copyimage.c
+++ src/mesa/main/copyimage.c
@@ -20,9 +20,6 @@
  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
- * Authors:
- *    Jason Ekstrand <jason.ekstrand@intel.com>
  */
 
 #include "context.h"
--- src/panfrost/util/nir_mod_helpers.c
+++ src/panfrost/util/nir_mod_helpers.c
@@ -20,11 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
- *
- * Authors:
- *    Alyssa Rosenzweig <alyssa@collabora.com>
- *    Jason Ekstrand (jason@jlekstrand.net)
- *
  */
 
 #include "nir.h"
