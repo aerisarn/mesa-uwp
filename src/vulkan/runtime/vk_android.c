9d1c1379e74787f0b9feff199c6d5609ee01fe5a
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-03-25 17:58:05 -0500

    vulkan: vk_android.c should be copyright Intel
    That's where I copied+pasted it from.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22120>

--- src/vulkan/runtime/vk_android.c
+++ src/vulkan/runtime/vk_android.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Jason Ekstrand
+ * Copyright © 2022 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
