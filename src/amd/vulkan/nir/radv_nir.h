685dcecb5725eb1685b2793744bf83511725c4d1
Timur Kristóf <timur.kristof@gmail.com>
2023-03-29 19:12:06 +0200

    radv: Move radv_nir_lower_viewport_to_zero to new file.
    Also ran clang-format on the affected code.
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21971>

--- src/amd/vulkan/nir/radv_nir.h
+++ src/amd/vulkan/nir/radv_nir.h
@@ -66,6 +66,8 @@ bool radv_nir_lower_intrinsics_early(nir_shader *nir, const struct radv_pipeline
 
 bool radv_nir_lower_view_index(nir_shader *nir, bool per_primitive);
 
+bool radv_nir_lower_viewport_to_zero(nir_shader *nir);
+
 #ifdef __cplusplus
 }
 #endif
