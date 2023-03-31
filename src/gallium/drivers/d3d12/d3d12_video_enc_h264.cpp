1995762d6851b6466c791c472817718bd8811e3e
Sil Vilerino <sivileri@microsoft.com>
2023-03-31 11:12:28 -0400

    Revert "d3d12: Honor suggested driver profile/level for H264/HEVC encode"
    This reverts commit 37652da61612aadfedc23ba484b33781654d5d56.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22239>

--- src/gallium/drivers/d3d12/d3d12_video_enc_h264.cpp
+++ src/gallium/drivers/d3d12/d3d12_video_enc_h264.cpp
@@ -631,14 +631,18 @@ d3d12_video_encoder_update_current_encoder_config_state_h264(struct d3d12_video_
    }
 
    // Set profile
-   auto lastFrameProfile = pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile;
-   pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile =
-      d3d12_video_encoder_convert_profile_to_d3d12_enc_profile_h264(pD3D12Enc->base.profile);
+   auto targetProfile = d3d12_video_encoder_convert_profile_to_d3d12_enc_profile_h264(pD3D12Enc->base.profile);
+   if (pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile != targetProfile) {
+      pD3D12Enc->m_currentEncodeConfig.m_ConfigDirtyFlags |= d3d12_video_encoder_config_dirty_flag_profile;
+   }
+   pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile = targetProfile;
 
    // Set level
-   auto lastFrameLevel = pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting;
-   pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting =
-      d3d12_video_encoder_convert_level_h264(pD3D12Enc->base.level);
+   auto targetLevel = d3d12_video_encoder_convert_level_h264(pD3D12Enc->base.level);
+   if (pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting != targetLevel) {
+      pD3D12Enc->m_currentEncodeConfig.m_ConfigDirtyFlags |= d3d12_video_encoder_config_dirty_flag_level;
+   }
+   pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting = targetLevel;
 
    // Set codec config
    bool is_supported = false;
@@ -716,13 +720,7 @@ d3d12_video_encoder_update_current_encoder_config_state_h264(struct d3d12_video_
                     "mismatches UMD suggested D3D12_VIDEO_ENCODER_PROFILE_H264: %d\n",
                     pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile,
                     pD3D12Enc->m_currentEncodeCapabilities.m_encoderSuggestedProfileDesc.m_H264Profile);
-      if (!D3D12_VIDEO_ENC_HONOR_PROFILE_LEVEL_PIPE)
-         pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile =
-            pD3D12Enc->m_currentEncodeCapabilities.m_encoderSuggestedProfileDesc.m_H264Profile;
    }
-
-   if (lastFrameProfile != pD3D12Enc->m_currentEncodeConfig.m_encoderProfileDesc.m_H264Profile)
-      pD3D12Enc->m_currentEncodeConfig.m_ConfigDirtyFlags |= d3d12_video_encoder_config_dirty_flag_profile;
 
    if (pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting !=
        pD3D12Enc->m_currentEncodeCapabilities.m_encoderLevelSuggestedDesc.m_H264LevelSetting) {
@@ -730,13 +728,7 @@ d3d12_video_encoder_update_current_encoder_config_state_h264(struct d3d12_video_
                     "mismatches UMD suggested D3D12_VIDEO_ENCODER_LEVELS_H264: %d\n",
                     pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting,
                     pD3D12Enc->m_currentEncodeCapabilities.m_encoderLevelSuggestedDesc.m_H264LevelSetting);
-      if (!D3D12_VIDEO_ENC_HONOR_PROFILE_LEVEL_PIPE)
-         pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting =
-            pD3D12Enc->m_currentEncodeCapabilities.m_encoderLevelSuggestedDesc.m_H264LevelSetting;
    }
-
-   if (lastFrameLevel != pD3D12Enc->m_currentEncodeConfig.m_encoderLevelDesc.m_H264LevelSetting)
-      pD3D12Enc->m_currentEncodeConfig.m_ConfigDirtyFlags |= d3d12_video_encoder_config_dirty_flag_level;
 
    if (pD3D12Enc->m_currentEncodeCapabilities.m_MaxSlicesInOutput >
        pD3D12Enc->m_currentEncodeCapabilities.m_currentResolutionSupportCaps.MaxSubregionsNumber) {
