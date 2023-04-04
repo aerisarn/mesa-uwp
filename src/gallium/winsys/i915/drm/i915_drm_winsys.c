aerisarn/mesa-uwp

commit 853b4801f27fcb7ef15d3109d61e95397afa0282

i915: Add support for `get_screen_fd`
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21654>

src/gallium/winsys/i915/drm/i915_drm_winsys.c
@@ -44,6 +44,14 @@ i915_drm_winsys_destroy(struct i915_winsys *iws)
    FREE(idws);
 }
 
+static int
+i915_drm_winsys_get_fd(struct i915_winsys *iws)
+{
+   struct i915_drm_winsys *idws = i915_drm_winsys(iws);
+
+   return idws->fd;
+}
+
 struct i915_winsys *
 i915_drm_winsys_create(int drmFD)
 {

src/gallium/drivers/i915/i915_screen.c
@@ -639,6 +639,14 @@ i915_destroy_screen(struct pipe_screen *screen)
    FREE(is);
 }
 
+static int
+i915_screen_get_fd(struct pipe_screen *screen)
+{
+   struct i915_screen *is = i915_screen(screen);
+
+   return is->iws->get_fd(is->iws);
+}
+
 /**
  * Create a new i915_screen object
  */
@@ -681,6 +689,7 @@ i915_screen_create(struct i915_winsys *iws)
    is->base.get_name = i915_get_name;
    is->base.get_vendor = i915_get_vendor;
    is->base.get_device_vendor = i915_get_device_vendor;
+   is->base.get_screen_fd = i915_screen_get_fd;
    is->base.get_param = i915_get_param;
    is->base.get_shader_param = i915_get_shader_param;
    is->base.get_paramf = i915_get_paramf;

src/gallium/drivers/i915/i915_winsys.h
@@ -236,6 +236,11 @@ struct i915_winsys {
     * Destroy the winsys.
     */
    void (*destroy)(struct i915_winsys *iws);
+
+   /**
+    * Get FD if the winsys provides one
+    */
+   int (*get_fd)(struct i915_winsys *iws);
 };
 
 #endif

commit 59a5340f740784426bded1f8692b95d2f28789b7

i915: hook new get_fd proc for drm winsys
Fixes: 853b4801f27 ("i915: Add support for `get_screen_fd`")
Signed-off-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22294>

src/gallium/winsys/i915/drm/i915_drm_winsys.c
@@ -74,6 +74,7 @@ i915_drm_winsys_create(int drmFD)
 
    idws->base.aperture_size = i915_drm_aperture_size;
    idws->base.destroy = i915_drm_winsys_destroy;
+   idws->base.get_fd = i915_drm_winsys_get_fd;
 
    idws->gem_manager = drm_intel_bufmgr_gem_init(idws->fd, idws->max_batch_size);
    drm_intel_bufmgr_gem_enable_reuse(idws->gem_manager);