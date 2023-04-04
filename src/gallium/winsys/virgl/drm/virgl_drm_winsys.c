aerisarn/mesa-uwp

commit 2ade6917dacb0648aff33f81da4e63418374ec7e

virgl: Add support for `get_screen_fd`
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21654>

src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
@@ -1203,6 +1203,14 @@ static int virgl_init_context(int drmFD)
    return 0;
 }
 
+static int
+virgl_drm_winsys_get_fd(struct virgl_winsys *vws)
+{
+   struct virgl_drm_winsys *vdws = virgl_drm_winsys(vws);
+
+   return vdws->fd;
+}
+
 static struct virgl_winsys *
 virgl_drm_winsys_create(int drmFD)
 {

src/gallium/drivers/virgl/virgl_screen.c
@@ -1101,6 +1101,18 @@ virgl_get_compiler_options(struct pipe_screen *pscreen,
    return &vscreen->compiler_options;
 }
 
+static int
+virgl_screen_get_fd(struct pipe_screen *pscreen)
+{
+   struct virgl_screen *vscreen = virgl_screen(pscreen);
+   struct virgl_winsys *vws = vscreen->vws;
+
+   if (vws->get_fd)
+      return vws->get_fd(vws);
+   else
+      return -1;
+}
+
 struct pipe_screen *
 virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *config)
 {
@@ -1137,6 +1149,7 @@ virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *c
    screen->vws = vws;
    screen->base.get_name = virgl_get_name;
    screen->base.get_vendor = virgl_get_vendor;
+   screen->base.get_screen_fd = virgl_screen_get_fd;
    screen->base.get_param = virgl_get_param;
    screen->base.get_shader_param = virgl_get_shader_param;
    screen->base.get_video_param = virgl_get_video_param;

src/gallium/drivers/virgl/virgl_winsys.h
@@ -52,6 +52,8 @@ struct virgl_winsys {
 
    void (*destroy)(struct virgl_winsys *vws);
 
+   int (*get_fd)(struct virgl_winsys *vws);
+
    int (*transfer_put)(struct virgl_winsys *vws,
                        struct virgl_hw_res *res,
                        const struct pipe_box *box,

commit 94b3a7e3ff119ce68ec0fedcc8c76c229d715ab8

virgl: hook new get_fd proc for drm winsys
Fixes: 2ade6917dacb ("virgl: Add support for `get_screen_fd`")
Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22292>

src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
@@ -1281,6 +1281,7 @@ virgl_drm_winsys_create(int drmFD)
    qdws->base.fence_server_sync = virgl_fence_server_sync;
    qdws->base.fence_get_fd = virgl_fence_get_fd;
    qdws->base.get_caps = virgl_drm_get_caps;
+   qdws->base.get_fd = virgl_drm_winsys_get_fd;
    qdws->base.supports_fences =  drm_version >= VIRGL_DRM_VERSION_FENCE_FD;
    qdws->base.supports_encoded_transfers = 1;
 