ab3507691adee7f3542875e5fd1106f18e2e2f91
Sajeesh Sidharthan <sajeesh.sidharthan@amd.com>
2023-04-05 14:28:59 +0530

    radeonsi/vcn: optimize bitstream buffer resize logic
    bitstream buffer is unmapped, resized and mapped again if new size
    is greater than the current bitstream buffer size. This will be done
    for each input buffer. This patch will avoid that and do resize
    only once irrespective of number of input buffers. With the new logic,
    total size is calculated first and call unmap, resize and map only once.
    
    Signed-off-by: Sajeesh Sidharthan <sajeesh.sidharthan@amd.com>
    Reviewed-by: Boyuan Zhang <Boyuan.Zhang@amd.com>
    Reviewed-by: Veerabadhran Gopalakrishnan <Veerabadhran.Gopalakrishnan@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22308>

--- src/gallium/drivers/radeonsi/radeon_vcn_dec.c
+++ src/gallium/drivers/radeonsi/radeon_vcn_dec.c
@@ -2863,26 +2863,29 @@ static void radeon_dec_decode_bitstream(struct pipe_video_codec *decoder,
    if (!dec->bs_ptr)
       return;
 
-   for (i = 0; i < num_buffers; ++i) {
-      struct rvid_buffer *buf = &dec->bs_buffers[dec->cur_buffer];
-      unsigned new_size = dec->bs_size + sizes[i];
+   unsigned long total_bs_size = dec->bs_size;
+   for (i = 0; i < num_buffers; ++i)
+      total_bs_size += sizes[i];
 
-      if (new_size > buf->res->buf->size) {
-         dec->ws->buffer_unmap(dec->ws, buf->res->buf);
-         dec->bs_ptr = NULL;
-         if (!si_vid_resize_buffer(dec->screen, &dec->cs, buf, new_size, NULL)) {
-            RVID_ERR("Can't resize bitstream buffer!");
-            return;
-         }
+   struct rvid_buffer *buf = &dec->bs_buffers[dec->cur_buffer];
 
-         dec->bs_ptr = dec->ws->buffer_map(dec->ws, buf->res->buf, &dec->cs,
-                                           PIPE_MAP_WRITE | RADEON_MAP_TEMPORARY);
-         if (!dec->bs_ptr)
-            return;
-
-         dec->bs_ptr += dec->bs_size;
+   if (total_bs_size > buf->res->buf->size) {
+      dec->ws->buffer_unmap(dec->ws, buf->res->buf);
+      dec->bs_ptr = NULL;
+      if (!si_vid_resize_buffer(dec->screen, &dec->cs, buf, total_bs_size, NULL)) {
+         RVID_ERR("Can't resize bitstream buffer!");
+         return;
       }
 
+      dec->bs_ptr = dec->ws->buffer_map(dec->ws, buf->res->buf, &dec->cs,
+                                          PIPE_MAP_WRITE | RADEON_MAP_TEMPORARY);
+      if (!dec->bs_ptr)
+         return;
+
+      dec->bs_ptr += dec->bs_size;
+   }
+
+   for (i = 0; i < num_buffers; ++i) {
       memcpy(dec->bs_ptr, buffers[i], sizes[i]);
       dec->bs_size += sizes[i];
       dec->bs_ptr += sizes[i];
