83a1b74240df3afcdb0e723c5bf6514b3321b411
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-04-05 09:35:56 -0400

    zink: don't create separate shader dsls if there are no bindings
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22577>

--- src/gallium/drivers/zink/zink_descriptors.c
+++ src/gallium/drivers/zink/zink_descriptors.c
@@ -750,7 +750,8 @@ zink_descriptor_shader_init(struct zink_screen *screen, struct zink_shader *shad
       if (shader->bindless)
          dsl[screen->desc_set_id[ZINK_DESCRIPTOR_BINDLESS]] = screen->bindless_layout;
    }
-   shader->precompile.layout = zink_pipeline_layout_create(screen, dsl, num_dsl, false, VK_PIPELINE_LAYOUT_CREATE_INDEPENDENT_SETS_BIT_EXT);
+   if (num_bindings)
+      shader->precompile.layout = zink_pipeline_layout_create(screen, dsl, num_dsl, false, VK_PIPELINE_LAYOUT_CREATE_INDEPENDENT_SETS_BIT_EXT);
 }
 
 void
