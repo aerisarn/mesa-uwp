0cbab15de597704ec9eef7a580ce4f087634c118
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-04-10 17:23:27 -0500

    nak: Fold [P]Lop3 sources
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24998>

--- src/nouveau/compiler/nak_legalize.rs
+++ src/nouveau/compiler/nak_legalize.rs
@@ -18,6 +18,31 @@ fn src_is_reg(src: &Src) -> bool {
     }
 }
 
+fn src_as_lop_imm(src: &Src) -> Option<bool> {
+    let x = match src.src_ref {
+        SrcRef::Zero => false,
+        SrcRef::True => true,
+        SrcRef::False => false,
+        SrcRef::Imm32(i) => {
+            if i == 0 {
+                false
+            } else if i == !0 {
+                true
+            } else {
+                return None;
+            }
+        }
+        _ => return None,
+    };
+    Some(x ^ src.src_mod.has_not())
+}
+
+fn fold_lop_src(src: &Src, x: &mut u8) {
+    if let Some(i) = src_as_lop_imm(src) {
+        *x = if i { !0 } else { 0 };
+    }
+}
+
 impl<'a> LegalizeInstr<'a> {
     pub fn new(ssa_alloc: &'a mut SSAValueAllocator) -> LegalizeInstr {
         LegalizeInstr {
@@ -51,6 +76,68 @@ impl<'a> LegalizeInstr<'a> {
                 let [ref mut src0, ref mut src1, ref mut src2] = op.srcs;
                 self.swap_srcs_if_not_reg(src0, src1);
                 self.swap_srcs_if_not_reg(src2, src1);
+                self.mov_src_if_not_reg(src0, RegFile::GPR);
+                self.mov_src_if_not_reg(src2, RegFile::GPR);
+            }
+            Op::Lop3(op) => {
+                /* Fold constants if we can */
+                op.op = LogicOp::new_lut(&|mut x, mut y, mut z| {
+                    fold_lop_src(&op.srcs[0], &mut x);
+                    fold_lop_src(&op.srcs[1], &mut y);
+                    fold_lop_src(&op.srcs[2], &mut z);
+                    op.op.eval(x, y, z)
+                });
+                for src in &mut op.srcs {
+                    src.src_mod = SrcMod::None;
+                    if src_as_lop_imm(src).is_some() {
+                        src.src_ref = SrcRef::Zero;
+                    }
+                }
+
+                let [ref mut src0, ref mut src1, ref mut src2] = op.srcs;
+                if !src_is_reg(src0) && src_is_reg(src1) {
+                    std::mem::swap(src0, src1);
+                    op.op = LogicOp::new_lut(&|x, y, z| op.op.eval(y, x, z))
+                }
+                if !src_is_reg(src2) && src_is_reg(src1) {
+                    std::mem::swap(src2, src1);
+                    op.op = LogicOp::new_lut(&|x, y, z| op.op.eval(x, z, y))
+                }
+
+                self.mov_src_if_not_reg(src0, RegFile::GPR);
+                self.mov_src_if_not_reg(src2, RegFile::GPR);
+            }
+            Op::PLop3(op) => {
+                /* Fold constants if we can */
+                for lop in &mut op.ops {
+                    *lop = LogicOp::new_lut(&|mut x, mut y, mut z| {
+                        fold_lop_src(&op.srcs[0], &mut x);
+                        fold_lop_src(&op.srcs[1], &mut y);
+                        fold_lop_src(&op.srcs[2], &mut z);
+                        lop.eval(x, y, z)
+                    });
+                }
+                for src in &mut op.srcs {
+                    src.src_mod = SrcMod::None;
+                    if src_as_lop_imm(src).is_some() {
+                        src.src_ref = SrcRef::True;
+                    }
+                }
+
+                let [ref mut src0, ref mut src1, ref mut src2] = op.srcs;
+                if !src_is_reg(src0) && src_is_reg(src1) {
+                    std::mem::swap(src0, src1);
+                    for lop in &mut op.ops {
+                        *lop = LogicOp::new_lut(&|x, y, z| lop.eval(y, x, z));
+                    }
+                }
+                if !src_is_reg(src2) && src_is_reg(src1) {
+                    std::mem::swap(src2, src1);
+                    for lop in &mut op.ops {
+                        *lop = LogicOp::new_lut(&|x, y, z| lop.eval(x, z, y));
+                    }
+                }
+
                 self.mov_src_if_not_reg(src0, RegFile::GPR);
                 self.mov_src_if_not_reg(src2, RegFile::GPR);
             }
