52ba7e967b7ac3e4639c9c7a764ae109f58e2068
Jesse Natalie <jenatali@microsoft.com>
2023-04-10 09:59:51 -0700

    d3d12: Support creating PSOs with no attachments with MSAA without TIR
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22402>

--- src/gallium/drivers/d3d12/d3d12_screen.h
+++ src/gallium/drivers/d3d12/d3d12_screen.h
@@ -115,6 +115,9 @@ struct d3d12_screen {
    D3D12_FEATURE_DATA_D3D12_OPTIONS2 opts2;
    D3D12_FEATURE_DATA_D3D12_OPTIONS3 opts3;
    D3D12_FEATURE_DATA_D3D12_OPTIONS4 opts4;
+#if D3D12_SDK_VERSION >= 610
+   D3D12_FEATURE_DATA_D3D12_OPTIONS19 opts19;
+#endif
 
    nir_shader_compiler_options nir_options;
 
