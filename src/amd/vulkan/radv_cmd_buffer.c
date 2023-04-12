Grima04/mesa-turnip-kgsl

commit 203f60ebf2f64dab3dc77ce9c098a6ae3d2aca43

radv: emit framebuffer state from primary if secondary doesn't inherit it
Otherwise fast color/depth clears can't work because they depend
on the framebuffer.

This fixes the following CTS (when the small hint is disabled):
- dEQP-VK.geometry.layered.1d_array.secondary_cmd_buffer
- dEQP-VK.geometry.layered.2d_array.secondary_cmd_buffer
- dEQP-VK.geometry.layered.cube.secondary_cmd_buffer
- dEQP-VK.geometry.layered.cube_array.secondary_cmd_buffer

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=110810
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=107986
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_cmd_buffer.c
@@ -3724,6 +3724,15 @@ void radv_CmdExecuteCommands(
 		if (secondary->sample_positions_needed)
 			primary->sample_positions_needed = true;
 
+		if (!secondary->state.framebuffer &&
+		    (primary->state.dirty & RADV_CMD_DIRTY_FRAMEBUFFER)) {
+			/* Emit the framebuffer state from primary if secondary
+			 * has been recorded without a framebuffer, otherwise
+			 * fast color/depth clears can't work.
+			 */
+			radv_emit_framebuffer_state(primary);
+		}
+
 		primary->device->ws->cs_execute_secondary(primary->cs, secondary->cs);
 
 

aerisarn/mesa-uwp

commit 4879f309a6d366bb8bf753e165d7fceff4969419

radv: Reserve space in framebuffer emission.
In the execute secondary scenario nothing else does it for us.

Fixes: 203f60ebf2f ("radv: emit framebuffer state from primary if secondary doesn't inherit it")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22392>

src/amd/vulkan/radv_cmd_buffer.c
@@ -3402,6 +3402,9 @@ radv_emit_framebuffer_state(struct radv_cmd_buffer *cmd_buffer)
                             ? S_028C70_FORMAT_GFX11(V_028C70_COLOR_INVALID)
                             : S_028C70_FORMAT_GFX6(V_028C70_COLOR_INVALID);
 
+   ASSERTED unsigned cdw_max =
+      radeon_check_space(cmd_buffer->device->ws, cmd_buffer->cs, 48 + MAX_RTS * 70);
+
    for (i = 0; i < render->color_att_count; ++i) {
       struct radv_image_view *iview = render->color_att[i].iview;
       if (!iview) {
@@ -3556,6 +3559,8 @@ radv_emit_framebuffer_state(struct radv_cmd_buffer *cmd_buffer)
                                 S_028424_DISABLE_CONSTANT_ENCODE_REG(disable_constant_encode));
       }
    }
+
+   assert(cmd_buffer->cs->cdw <= cdw_max);
 
    cmd_buffer->state.dirty &= ~RADV_CMD_DIRTY_FRAMEBUFFER;
 }