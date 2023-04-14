aerisarn/mesa-uwp

commit 7da78ffb697bfea5c20b31dca635971d00d27b3c

zink: create/use query pools dynamically
this fits the concept of "pools are shared between queries" a bit
better and leaves the pool ownership/destruction to their users

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21534>

src/gallium/drivers/zink/zink_query.c
@@ -22,6 +22,7 @@ struct zink_query_pool {
    VkQueryPipelineStatisticFlags pipeline_stats;
    VkQueryPool query_pool;
    unsigned last_range;
+   unsigned refcount;
    bool overflow;
 };
 
@@ -54,8 +55,6 @@ struct zink_query_start {
 struct zink_query {
    struct threaded_query base;
    enum pipe_query_type type;
-
-   struct zink_query_pool *pool[2];
 
    /* Everytime the gallium query needs
     * another vulkan query, add a new start.
@@ -390,10 +389,22 @@ fail:
 }
 
 static void
-unref_vk_query(struct zink_vk_query *vkq)
+unref_vk_pool(struct zink_screen *screen, struct zink_query_pool *pool)
+{
+   if (!pool || --pool->refcount)
+      return;
+   VKSCR(DestroyQueryPool)(screen->dev, pool->query_pool, NULL);
+   if (list_is_linked(&pool->list))
+      list_del(&pool->list);
+   FREE(pool);
+}
+
+static void
+unref_vk_query(struct zink_screen *screen, struct zink_vk_query *vkq)
 {
    if (!vkq)
       return;
+   unref_vk_pool(screen, vkq->pool);
    vkq->refcount--;
    if (vkq->refcount == 0)
       FREE(vkq);
@@ -409,7 +420,7 @@ destroy_query(struct zink_screen *screen, struct zink_query *query)
    unsigned num_starts = query->starts.capacity / sizeof(struct zink_query_start);
    for (unsigned j = 0; j < num_starts; j++) {
       for (unsigned i = 0; i < PIPE_MAX_VERTEX_STREAMS; i++) {
-         unref_vk_query(starts[j].vkq[i]);
+         unref_vk_query(screen, starts[j].vkq[i]);
       }
    }
 
@@ -449,8 +460,9 @@ query_pool_get_range(struct zink_context *ctx, struct zink_query *q)
    }
    start->data = 0;
 
+   unsigned num_pools = get_num_query_pools(q);
    for (unsigned i = 0; i < num_queries; i++) {
-      int pool_idx = q->pool[1] ? i : 0;
+      int pool_idx = num_pools > 1 ? i : 0;
       /* try and find the active query for this */
       struct zink_vk_query *vkq;
       int xfb_idx = num_queries == 4 ? i : q->index;
@@ -458,8 +470,10 @@ query_pool_get_range(struct zink_context *ctx, struct zink_query *q)
            (pool_idx == 1)) && ctx->curr_xfb_queries[xfb_idx]) {
          vkq = ctx->curr_xfb_queries[xfb_idx];
          vkq->refcount++;
+         vkq->pool->refcount++;
       } else {
-         struct zink_query_pool *pool = q->pool[pool_idx];
+         struct zink_query_pool *pool = find_or_allocate_qp(ctx, q, pool_idx);
+         pool->refcount++;
          vkq = CALLOC_STRUCT(zink_vk_query);
 
          vkq->refcount = 1;
@@ -474,7 +488,7 @@ query_pool_get_range(struct zink_context *ctx, struct zink_query *q)
             pool->overflow = true;
          }
       }
-      unref_vk_query(start->vkq[i]);
+      unref_vk_query(zink_screen(ctx->base.screen), start->vkq[i]);
       start->vkq[i] = vkq;
    }
 }
@@ -511,14 +525,6 @@ zink_create_query(struct pipe_context *pctx,
       query->needs_rast_discard_workaround = !screen->info.primgen_feats.primitivesGeneratedQueryWithRasterizerDiscard;
    } else if (query_type == PIPE_QUERY_PRIMITIVES_GENERATED) {
       query->needs_rast_discard_workaround = true;
-   }
-
-   int num_pools = get_num_query_pools(query);
-   for (unsigned i = 0; i < num_pools; i++) {
-      query->pool[i] = find_or_allocate_qp(zink_context(pctx), query, i);
-
-      if (!query->pool[i])
-         goto fail;
    }
 
    if (!qbo_append(pctx->screen, query))

commit 7119a344f3db5820041dfcdf23ebcbf15121f467

zink: always defer query pool deletion
this feels dumb, but I can't think of a simpler way to do it that
would more accurately handle deletion while also guaranteeing
pool longevity

Fixes: 7da78ffb697 ("zink: create/use query pools dynamically")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22493>

src/gallium/drivers/zink/zink_batch.c
@@ -111,6 +111,9 @@ zink_reset_batch_state(struct zink_context *ctx, struct zink_batch_state *bs)
       struct zink_query *query = (void*)entry->key;
       zink_prune_query(screen, bs, query);
    }
+   util_dynarray_foreach(&bs->dead_querypools, VkQueryPool, pool)
+      VKSCR(DestroyQueryPool)(screen->dev, *pool, NULL);
+   util_dynarray_clear(&bs->dead_querypools);
 
    /* framebuffers are appended to the batch state in which they are destroyed
     * to ensure deferred deletion without destroying in-use objects
@@ -267,6 +270,7 @@ zink_batch_state_destroy(struct zink_screen *screen, struct zink_batch_state *bs
    free(bs->real_objs.objs);
    free(bs->slab_objs.objs);
    free(bs->sparse_objs.objs);
+   util_dynarray_fini(&bs->dead_querypools);
    util_dynarray_fini(&bs->swapchain_obj);
    util_dynarray_fini(&bs->zombie_samplers);
    util_dynarray_fini(&bs->dead_framebuffers);
@@ -327,6 +331,7 @@ create_batch_state(struct zink_context *ctx)
    SET_CREATE_OR_FAIL(&bs->programs);
    SET_CREATE_OR_FAIL(&bs->active_queries);
    util_dynarray_init(&bs->wait_semaphores, NULL);
+   util_dynarray_init(&bs->dead_querypools, NULL);
    util_dynarray_init(&bs->wait_semaphore_stages, NULL);
    util_dynarray_init(&bs->zombie_samplers, NULL);
    util_dynarray_init(&bs->dead_framebuffers, NULL);

src/gallium/drivers/zink/zink_query.c
@@ -387,7 +387,7 @@ unref_vk_pool(struct zink_context *ctx, struct zink_query_pool *pool)
 {
    if (!pool || --pool->refcount)
       return;
-   VKCTX(DestroyQueryPool)(zink_screen(ctx->base.screen)->dev, pool->query_pool, NULL);
+   util_dynarray_append(&ctx->batch.state->dead_querypools, VkQueryPool, pool->query_pool);
    if (list_is_linked(&pool->list))
       list_del(&pool->list);
    FREE(pool);

src/gallium/drivers/zink/zink_types.h
@@ -596,6 +596,7 @@ struct zink_batch_state {
    struct util_dynarray dead_framebuffers;
 
    struct set active_queries; /* zink_query objects which were active at some point in this batch */
+   struct util_dynarray dead_querypools;
 
    struct zink_batch_descriptor_data dd;
 