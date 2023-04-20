aerisarn/mesa-uwp

commit 5791826b1ac7085c8acf056abc279e66ca64eb55

mesa: skip draws with invalid indices offset
It's easy to misuse glDrawElements and end up with an
invalid indices offset.
Since this can cause a hang, detect this case and skip
the draw.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6625
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16662>

src/mesa/main/draw.c
@@ -1772,8 +1772,15 @@ _mesa_validated_drawrangeelements(struct gl_context *ctx, GLenum mode,
       info.index.user = indices;
       draw.start = 0;
    } else {
+      uintptr_t start = (uintptr_t) indices;
+      if (unlikely(index_bo->Size < start)) {
+         _mesa_warning(ctx, "Invalid indices offset 0x%" PRIxPTR
+                            " (indices buffer size is %ld bytes)."
+                            " Draw skipped.", start, index_bo->Size);
+         return;
+      }
       info.index.gl_bo = index_bo;
-      draw.start = (uintptr_t)indices >> index_size_shift;
+      draw.start = start >> index_size_shift;
    }
    draw.index_bias = basevertex;
 

commit a1098a1e45f4ad74d90261beb6e2594953f25540

mesa/draw: fix -Wformat warning
fixes: 5791826b
cc @pepp @mareko

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22605>

src/mesa/main/draw.c
@@ -1642,7 +1642,7 @@ _mesa_validated_drawrangeelements(struct gl_context *ctx,
          _mesa_warning(ctx, "Invalid indices offset 0x%" PRIxPTR
                             " (indices buffer size is %ld bytes)"
                             " or unallocated buffer (%u). Draw skipped.",
-                            start, index_bo->Size, !!index_bo->buffer);
+                            start, (long)index_bo->Size, !!index_bo->buffer);
          return;
       }
 