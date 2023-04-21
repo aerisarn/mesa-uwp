e3e077c9d48aaf792f7728a3f394a5e59bbc9f2b
Gert Wollny <gert.wollny@collabora.com>
2023-04-21 10:17:44 +0200

    r600/sfn: Lower tess levels to vectors in TCS
    Thanks @anholt for pointing me in the right direction
    
    Fixes: 84006587 glsl: Delete the lower_tess_level
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8884
    
    v2: replace direct call to lowering pass with nir compiler option
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22622>

--- src/gallium/drivers/r600/r600_pipe_common.c
+++ src/gallium/drivers/r600/r600_pipe_common.c
@@ -1376,7 +1376,8 @@ bool r600_common_screen_init(struct r600_common_screen *rscreen,
 		.lower_int64_options = ~0,
 		.lower_cs_local_index_to_id = true,
 		.lower_uniforms_to_ubo = true,
-		.lower_image_offset_to_range_base = 1
+		.lower_image_offset_to_range_base = 1,
+		.vectorize_tess_levels = 1
 	};
 
 	rscreen->nir_options = nir_options;
