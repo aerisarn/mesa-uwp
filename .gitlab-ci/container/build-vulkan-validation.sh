2c7734404843486f1fc13675c9a04f83efd0e8fc
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-04-24 16:07:30 -0400

    bump VVL to 1.3.248
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22672>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -3,10 +3,11 @@
 # When changing this file, you need to bump the following
 # .gitlab-ci/image-tags.yml tags:
 # DEBIAN_X86_TEST_GL_TAG
+# KERNEL_ROOTFS_TAG:
 
 set -ex
 
-VALIDATION_TAG="v1.3.238"
+VALIDATION_TAG="v1.3.248"
 
 git clone -b "$VALIDATION_TAG" --single-branch --depth 1 https://github.com/KhronosGroup/Vulkan-ValidationLayers.git
 pushd Vulkan-ValidationLayers
