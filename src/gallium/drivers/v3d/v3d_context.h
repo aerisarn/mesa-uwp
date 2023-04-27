b5a458859f4c787cef75991c35fe3d8d1912e5ad
Juan A. Suarez Romero <jasuarez@igalia.com>
2023-04-27 14:13:14 +0200

    v3d: upgrade V3D 4.1 to 4.2 version
    Some of the new features require at least V3D 4.2. And actually, 4.2 is
    the version used by the Raspberry Pi 4 hardware.
    
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22733>

--- src/gallium/drivers/v3d/v3d_context.h
+++ src/gallium/drivers/v3d/v3d_context.h
@@ -827,9 +827,9 @@ void v3d_disk_cache_store(struct v3d_context *v3d,
 
 /* Helper to call hw ver specific functions */
 #define v3d_X(devinfo, thing) ({                                \
-        __typeof(&v3d41_##thing) v3d_X_thing;                   \
-        if ((devinfo)->ver >= 41)                               \
-                v3d_X_thing = &v3d41_##thing;                   \
+        __typeof(&v3d42_##thing) v3d_X_thing;                   \
+        if ((devinfo)->ver >= 42)                               \
+                v3d_X_thing = &v3d42_##thing;                   \
         else if ((devinfo)->ver >= 33)                          \
                 v3d_X_thing = &v3d33_##thing;                   \
         else                                                    \
@@ -844,7 +844,7 @@ void v3d_disk_cache_store(struct v3d_context *v3d,
 #  include "v3dx_context.h"
 #  undef v3dX
 
-#  define v3dX(x) v3d41_##x
+#  define v3dX(x) v3d42_##x
 #  include "v3dx_context.h"
 #  undef v3dX
 #endif
