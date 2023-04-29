aerisarn/mesa-uwp

commit 4a056807bc3b813f390d194bbe382b573761649e

gallivm: break out native vector width calc for reuse
breaks dependency on lp_build_init()

cc: mesa-stable

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22600>

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -420,6 +420,15 @@ fail:
    return FALSE;
 }
 
+unsigned
+lp_build_get_native_width(void)
+{
+   // Default to 256 until we're confident llvmpipe with 512 is as correct and not slower than 256
+   unsigned vector_width = MIN2(util_get_cpu_caps()->max_vector_bits, 256);
+
+   vector_width = debug_get_num_option("LP_NATIVE_VECTOR_WIDTH", vector_width);
+   return vector_width;
+}
 
 boolean
 lp_build_init(void)
@@ -440,11 +449,7 @@ lp_build_init(void)
 
    lp_set_target_options();
 
-   // Default to 256 until we're confident llvmpipe with 512 is as correct and not slower than 256
-   lp_native_vector_width = MIN2(util_get_cpu_caps()->max_vector_bits, 256);
-
-   lp_native_vector_width = debug_get_num_option("LP_NATIVE_VECTOR_WIDTH",
-                                                 lp_native_vector_width);
+   lp_native_vector_width = lp_build_get_native_width();
 
 #if DETECT_ARCH_PPC_64
    /* Set the NJ bit in VSCR to 0 so denormalized values are handled as

src/gallium/auxiliary/gallivm/lp_bld_init.h
@@ -68,6 +68,8 @@ struct gallivm_state
    LLVMValueRef get_time_hook;
 };
 
+unsigned
+lp_build_get_native_width(void);
 
 boolean
 lp_build_init(void);

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -1113,7 +1113,7 @@ llvmpipe_create_screen(struct sw_winsys *winsys)
 
    snprintf(screen->renderer_string, sizeof(screen->renderer_string),
             "llvmpipe (LLVM " MESA_LLVM_VERSION_STRING ", %u bits)",
-            lp_native_vector_width );
+            lp_build_get_native_width() );
 
    list_inithead(&screen->ctx_list);
    (void) mtx_init(&screen->ctx_mutex, mtx_plain);

commit e9864d7f39d74a914e1d486a1a36627d296e9315

llvmpipe: fix native vector width init
this otherwise causes infinite loops in subgroup tests
and kills ci

Fixes: 4a056807bc3 ("gallivm: break out native vector width calc for reuse")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22767>

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -421,18 +421,22 @@ fail:
 }
 
 unsigned
-lp_build_get_native_width(void)
+lp_build_init_native_width(void)
 {
    // Default to 256 until we're confident llvmpipe with 512 is as correct and not slower than 256
-   unsigned vector_width = MIN2(util_get_cpu_caps()->max_vector_bits, 256);
+   lp_native_vector_width = MIN2(util_get_cpu_caps()->max_vector_bits, 256);
+   assert(lp_native_vector_width);
 
-   vector_width = debug_get_num_option("LP_NATIVE_VECTOR_WIDTH", vector_width);
-   return vector_width;
+   lp_native_vector_width = debug_get_num_option("LP_NATIVE_VECTOR_WIDTH", lp_native_vector_width);
+   assert(lp_native_vector_width);
+
+   return lp_native_vector_width;
 }
 
 boolean
 lp_build_init(void)
 {
+   lp_build_init_native_width();
    if (gallivm_initialized)
       return TRUE;
 
@@ -448,8 +452,6 @@ lp_build_init(void)
    gallivm_perf = debug_get_flags_option("GALLIVM_PERF", lp_bld_perf_flags, 0 );
 
    lp_set_target_options();
-
-   lp_native_vector_width = lp_build_get_native_width();
 
 #if DETECT_ARCH_PPC_64
    /* Set the NJ bit in VSCR to 0 so denormalized values are handled as

src/gallium/auxiliary/gallivm/lp_bld_init.h
@@ -69,7 +69,7 @@ struct gallivm_state
 };
 
 unsigned
-lp_build_get_native_width(void);
+lp_build_init_native_width(void);
 
 boolean
 lp_build_init(void);

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -1114,7 +1114,7 @@ llvmpipe_create_screen(struct sw_winsys *winsys)
 
    snprintf(screen->renderer_string, sizeof(screen->renderer_string),
             "llvmpipe (LLVM " MESA_LLVM_VERSION_STRING ", %u bits)",
-            lp_build_get_native_width() );
+            lp_build_init_native_width() );
 
    list_inithead(&screen->ctx_list);
    (void) mtx_init(&screen->ctx_mutex, mtx_plain);