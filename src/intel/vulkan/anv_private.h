1896b48f4ebe146c2ce00fc2e0d46113ab8613ff
Rohan Garg <rohan.garg@intel.com>
2023-05-03 16:21:04 +0200

    anv: use the workaround framework for WA 14013111325
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22830>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -3936,7 +3936,7 @@ anv_can_sample_mcs_with_clear(const struct intel_device_info * const devinfo,
     * See HSD 1707282275, wa_14013111325. Due to the use of
     * format-reinterpretation, a simplified workaround is implemented.
     */
-   if (devinfo->ver >= 12 &&
+   if (intel_needs_workaround(devinfo, 14013111325) &&
        isl_format_get_layout(anv_surf->isl.format)->bpb <= 16) {
       return false;
    }
