aerisarn/mesa-uwp

commit 04fa6c715b0f313af8ca7c95619e9d856c569d17

dzn: Batch command lists together
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22225>

src/microsoft/vulkan/dzn_device.c
@@ -50,6 +50,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <c99_alloca.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -2001,11 +2002,13 @@ dzn_queue_submit(struct vk_queue *q,
          return result;
    }
 
+   ID3D12CommandList **cmdlists = alloca(info->command_buffer_count * sizeof(ID3D12CommandList*));
+
    for (uint32_t i = 0; i < info->command_buffer_count; i++) {
       struct dzn_cmd_buffer *cmd_buffer =
          container_of(info->command_buffers[i], struct dzn_cmd_buffer, vk);
 
-      ID3D12CommandList *cmdlists[] = { (ID3D12CommandList *)cmd_buffer->cmdlist };
+      cmdlists[i] = (ID3D12CommandList *)cmd_buffer->cmdlist;
 
       util_dynarray_foreach(&cmd_buffer->events.wait, struct dzn_event *, evt) {
          if (FAILED(ID3D12CommandQueue_Wait(queue->cmdqueue, (*evt)->fence, 1)))
@@ -2024,8 +2027,13 @@ dzn_queue_submit(struct vk_queue *q,
          }
          mtx_unlock(&range->qpool->queries_lock);
       }
+   }
 
-      ID3D12CommandQueue_ExecuteCommandLists(queue->cmdqueue, 1, cmdlists);
+   ID3D12CommandQueue_ExecuteCommandLists(queue->cmdqueue, info->command_buffer_count, cmdlists);
+   
+   for (uint32_t i = 0; i < info->command_buffer_count; i++) {
+      struct dzn_cmd_buffer* cmd_buffer =
+         container_of(info->command_buffers[i], struct dzn_cmd_buffer, vk);
 
       util_dynarray_foreach(&cmd_buffer->events.signal, struct dzn_cmd_event_signal, evt) {
          if (FAILED(ID3D12CommandQueue_Signal(queue->cmdqueue, evt->event->fence, evt->value ? 1 : 0)))

commit be34257197afe99314883661b1e3dd6328c7e4f8

dzn: Delete queue-level event waits
These are unneeded. Events can't be used to indefinitely stall work
like you can with a semaphore or timeline semaphore. The signals
still need to happen so that execution will modify the state that
can be polled from the CPU though.

Fixes dEQP-VK.synchronization.basic.event.single_submit_multi_command_buffer

Fixes: 04fa6c71 ("dzn: Batch command lists together")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22842>

src/microsoft/vulkan/dzn_cmd_buffer.c
@@ -413,7 +413,6 @@ dzn_cmd_buffer_destroy(struct vk_command_buffer *cbuf)
    dzn_descriptor_heap_pool_finish(&cmdbuf->sampler_pool);
    dzn_descriptor_heap_pool_finish(&cmdbuf->rtvs.pool);
    dzn_descriptor_heap_pool_finish(&cmdbuf->dsvs.pool);
-   util_dynarray_fini(&cmdbuf->events.wait);
    util_dynarray_fini(&cmdbuf->events.signal);
    util_dynarray_fini(&cmdbuf->queries.reset);
    util_dynarray_fini(&cmdbuf->queries.signal);
@@ -475,7 +474,6 @@ dzn_cmd_buffer_reset(struct vk_command_buffer *cbuf, VkCommandBufferResetFlags f
    }
    cmdbuf->cur_upload_buf = NULL;
 
-   util_dynarray_clear(&cmdbuf->events.wait);
    util_dynarray_clear(&cmdbuf->events.signal);
    util_dynarray_clear(&cmdbuf->queries.reset);
    util_dynarray_clear(&cmdbuf->queries.signal);
@@ -609,7 +607,6 @@ dzn_cmd_buffer_create(const VkCommandBufferAllocateInfo *info,
    cmdbuf->state.multiview.view_mask = 1;
    for (uint32_t bucket = 0; bucket < DZN_INTERNAL_BUF_BUCKET_COUNT; ++bucket)
       list_inithead(&cmdbuf->internal_bufs[bucket]);
-   util_dynarray_init(&cmdbuf->events.wait, NULL);
    util_dynarray_init(&cmdbuf->events.signal, NULL);
    util_dynarray_init(&cmdbuf->queries.reset, NULL);
    util_dynarray_init(&cmdbuf->queries.signal, NULL);
@@ -724,18 +721,16 @@ dzn_cmd_buffer_gather_events(struct dzn_cmd_buffer *cmdbuf)
    hash_table_foreach(cmdbuf->events.ht, he) {
       enum dzn_event_state state = (uintptr_t)he->data;
 
-      if (state != DZN_EVENT_STATE_EXTERNAL_WAIT) {
-         struct dzn_cmd_event_signal signal = { (struct dzn_event *)he->key, state  == DZN_EVENT_STATE_SET };
-         struct dzn_cmd_event_signal *entry =
-            util_dynarray_grow(&cmdbuf->events.signal, struct dzn_cmd_event_signal, 1);
+      struct dzn_cmd_event_signal signal = { (struct dzn_event *)he->key, state == DZN_EVENT_STATE_SET };
+      struct dzn_cmd_event_signal *entry =
+         util_dynarray_grow(&cmdbuf->events.signal, struct dzn_cmd_event_signal, 1);
 
-         if (!entry) {
-            vk_command_buffer_set_error(&cmdbuf->vk, VK_ERROR_OUT_OF_HOST_MEMORY);
-            break;
-         }
-
-         *entry = signal;
+      if (!entry) {
+         vk_command_buffer_set_error(&cmdbuf->vk, VK_ERROR_OUT_OF_HOST_MEMORY);
+         break;
       }
+
+      *entry = signal;
    }
 
 out:
@@ -5440,22 +5435,6 @@ dzn_CmdWaitEvents(VkCommandBuffer commandBuffer,
          enum dzn_event_state state = (uintptr_t)he->data;
          assert(state != DZN_EVENT_STATE_RESET);
          flush_pipeline = state == DZN_EVENT_STATE_SET;
-      } else {
-         if (!_mesa_hash_table_insert(cmdbuf->events.ht, event,
-                                      (void *)(uintptr_t)DZN_EVENT_STATE_EXTERNAL_WAIT)) {
-            vk_command_buffer_set_error(&cmdbuf->vk, VK_ERROR_OUT_OF_HOST_MEMORY);
-            return;
-         }
-
-         struct dzn_event **entry =
-            util_dynarray_grow(&cmdbuf->events.wait, struct dzn_event *, 1);
-
-         if (!entry) {
-            vk_command_buffer_set_error(&cmdbuf->vk, VK_ERROR_OUT_OF_HOST_MEMORY);
-            return;
-         }
-
-         *entry = event;
       }
    }
 

src/microsoft/vulkan/dzn_device.c
@@ -2029,11 +2029,6 @@ dzn_queue_submit(struct vk_queue *q,
 
       cmdlists[i] = (ID3D12CommandList *)cmd_buffer->cmdlist;
 
-      util_dynarray_foreach(&cmd_buffer->events.wait, struct dzn_event *, evt) {
-         if (FAILED(ID3D12CommandQueue_Wait(queue->cmdqueue, (*evt)->fence, 1)))
-            return vk_error(device, VK_ERROR_UNKNOWN);
-      }
-
       util_dynarray_foreach(&cmd_buffer->queries.reset, struct dzn_cmd_buffer_query_range, range) {
          mtx_lock(&range->qpool->queries_lock);
          for (uint32_t q = range->start; q < range->start + range->count; q++) {

src/microsoft/vulkan/dzn_private.h
@@ -537,7 +537,6 @@ struct dzn_internal_resource {
 };
 
 enum dzn_event_state {
-   DZN_EVENT_STATE_EXTERNAL_WAIT = -1,
    DZN_EVENT_STATE_RESET = 0,
    DZN_EVENT_STATE_SET = 1,
 };
@@ -675,7 +674,6 @@ struct dzn_cmd_buffer {
 
    struct {
       struct hash_table *ht;
-      struct util_dynarray wait;
       struct util_dynarray signal;
    } events;
 