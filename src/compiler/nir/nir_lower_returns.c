5d238c0c2cd90dc175e04de6cf6b3116c440c90b
Jesse Natalie <jenatali@microsoft.com>
2023-05-08 17:45:49 -0700

    nir_lower_returns: Optimize phis before beginning the pass
    This adds a little extra work, since now dominance is computed and
    blocks that don't just have then-return or else-return are looked at.
    However it means that nir_lower_returns can now keep phis up to date
    by inserting undefs without causing some phis to become non-trivial.
    
    This ends up obviating a couple of tests for lower_returns.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22913>

--- src/compiler/nir/nir_lower_returns.c
+++ src/compiler/nir/nir_lower_returns.c
@@ -132,11 +132,11 @@ lower_returns_in_if(nir_if *if_stmt, struct lower_returns_state *state)
           */
 
          /* nir_cf_extract will not extract phis at the start of the block. In
-          * this case we know that any phis will have to have a single
-          * predecessor, so we can just replace the phi with its single source.
+          * this case we know that any phis will have to have had a single
+          * predecessor, and should've been removed by the opt_remove_phis before
+          * beginning this pass.
           */
          nir_block *succ_block = nir_after_cf_node(&if_stmt->cf_node).block;
-         nir_opt_remove_phis_block(succ_block);
          assert(nir_block_first_instr(succ_block) == NULL ||
                 nir_block_first_instr(succ_block)->type != nir_instr_type_phi);
 
@@ -298,11 +298,14 @@ nir_lower_returns_impl(nir_function_impl *impl)
 bool
 nir_lower_returns(nir_shader *shader)
 {
-   bool progress = false;
+   /* Before removing jumps and adding undef sources to otherwise single-source phis,
+    * go ahead and simplify those single-source phis.
+    */
+   bool progress = nir_opt_remove_phis(shader);
 
    nir_foreach_function(function, shader) {
       if (function->impl)
-         progress = nir_lower_returns_impl(function->impl) || progress;
+         progress |= nir_lower_returns_impl(function->impl) || progress;
    }
 
    return progress;
