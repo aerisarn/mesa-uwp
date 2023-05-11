64a595291e21d3f9962a03b37bd16876bf6c85b7
Asahi Lina <lina@asahilina.net>
2023-05-11 14:32:01 +0900

    asahi: Add some more system registers
    Core and opfifo stuff from the compute helper blob, vm_slot because it
    was the only one changing when I poked around yesterday and it hit me
    what it was ^^
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22971>

--- src/asahi/compiler/agx_opcodes.py
+++ src/asahi/compiler/agx_opcodes.py
@@ -119,6 +119,8 @@ SR = enum("sr", {
    8:  'dispatch_threads_per_threadgroup.x',
    9:  'dispatch_threads_per_threadgroup.y',
    10: 'dispatch_threads_per_threadgroup.z',
+   20: 'core_index',
+   21: 'vm_slot',
    48: 'thread_position_in_threadgroup.x',
    49: 'thread_position_in_threadgroup.y',
    50: 'thread_position_in_threadgroup.z',
@@ -132,6 +134,9 @@ SR = enum("sr", {
    80: 'thread_position_in_grid.x',
    81: 'thread_position_in_grid.y',
    82: 'thread_position_in_grid.z',
+   144: 'opfifo_cmd',
+   146: 'opfifo_data_l',
+   147: 'opfifo_data_h',
 })
 
 ATOMIC_OPC = enum("atomic_opc", {
