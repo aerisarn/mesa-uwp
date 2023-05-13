40653f078390639e75cb7a9e616a3bcc8a45e959
Konstantin Seurer <konstantin.seurer@gmail.com>
2023-05-13 19:35:14 +0200

    nir/lower_shader_calls: Remat derefs earlier
    spill_ssa_defs_and_lower_shader_calls can insert phis as well which can
    make nir_opt_shrink_stores crash.
    
    Fixes: 200e551c ("nir/lower_shader_calls: Remat derefs before lowering resumes")
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9003
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23007>

--- src/compiler/nir/nir_lower_shader_calls.c
+++ src/compiler/nir/nir_lower_shader_calls.c
@@ -1179,13 +1179,6 @@ lower_resume(nir_shader *shader, int call_idx)
    nir_function_impl *impl = nir_shader_get_entrypoint(shader);
    nir_instr *resume_instr = find_resume_instr(impl, call_idx);
 
-   /* Deref chains contain metadata information that is needed by other passes
-    * after this one. If we don't rematerialize the derefs in the blocks where
-    * they're used here, the following lowerings will insert phis which can
-    * prevent other passes from chasing deref chains.
-    */
-   nir_rematerialize_derefs_in_use_blocks_impl(impl);
-
    if (duplicate_loop_bodies(impl, resume_instr)) {
       nir_validate_shader(shader, "after duplicate_loop_bodies in "
                                   "nir_lower_shader_calls");
@@ -1940,6 +1933,13 @@ nir_lower_shader_calls(nir_shader *shader,
       if (progress)
          NIR_PASS(progress, shader, nir_opt_cse);
    }
+
+   /* Deref chains contain metadata information that is needed by other passes
+    * after this one. If we don't rematerialize the derefs in the blocks where
+    * they're used here, the following lowerings will insert phis which can
+    * prevent other passes from chasing deref chains.
+    */
+   nir_rematerialize_derefs_in_use_blocks_impl(impl);
 
    /* Save the start point of the call stack in scratch */
    unsigned start_call_scratch = shader->scratch_size;
