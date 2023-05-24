993ed1f026984eb0708c578ecfa17958a5d12031
David Heidelberg <david.heidelberg@collabora.com>
2023-05-24 12:11:51 +0200

    ci: bump libdrm from 2.4.110 to 2.4.114 present in Debian 12
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21977>

--- .gitlab-ci/container/build-libdrm.sh
+++ .gitlab-ci/container/build-libdrm.sh
@@ -3,13 +3,13 @@
 
 set -ex
 
-export LIBDRM_VERSION=libdrm-2.4.110
+export LIBDRM_VERSION=libdrm-2.4.114
 
 curl -L -O --retry 4 -f --retry-all-errors --retry-delay 60 \
     https://dri.freedesktop.org/libdrm/"$LIBDRM_VERSION".tar.xz
 tar -xvf "$LIBDRM_VERSION".tar.xz && rm "$LIBDRM_VERSION".tar.xz
 cd "$LIBDRM_VERSION"
-meson setup build -D vc4=false -D freedreno=false -D etnaviv=false $EXTRA_MESON_ARGS
+meson setup build -D vc4=disabled -D freedreno=disabled -D etnaviv=disabled $EXTRA_MESON_ARGS
 meson install -C build
 cd ..
 rm -rf "$LIBDRM_VERSION"
--- .gitlab-ci/container/debian/android_build.sh
+++ .gitlab-ci/container/debian/android_build.sh
@@ -32,7 +32,7 @@ sh .gitlab-ci/container/create-android-cross-file.sh /$ndk arm-linux-androideabi
 
 # Not using build-libdrm.sh because we don't want its cleanup after building
 # each arch.  Fetch and extract now.
-export LIBDRM_VERSION=libdrm-2.4.110
+export LIBDRM_VERSION=libdrm-2.4.114
 curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
   -O https://dri.freedesktop.org/libdrm/$LIBDRM_VERSION.tar.xz
 tar -xf $LIBDRM_VERSION.tar.xz && rm $LIBDRM_VERSION.tar.xz
@@ -48,14 +48,13 @@ for arch in \
     meson setup build-$arch \
           --cross-file=/cross_file-$arch.txt \
           --libdir=lib/$arch \
-          -Dlibkms=false \
-          -Dnouveau=false \
-          -Dvc4=false \
-          -Detnaviv=false \
-          -Dfreedreno=false \
-          -Dintel=false \
-          -Dcairo-tests=false \
-          -Dvalgrind=false
+          -Dnouveau=disabled \
+          -Dvc4=disabled \
+          -Detnaviv=disabled \
+          -Dfreedreno=disabled \
+          -Dintel=disabled \
+          -Dcairo-tests=disabled \
+          -Dvalgrind=disabled
     meson install -C build-$arch
     cd ..
 done
