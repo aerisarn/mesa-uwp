335d3074fb88c84d80351f800029b31f3963fea9
David Heidelberg <david.heidelberg@collabora.com>
2023-05-25 14:36:59 +0200

    ci: VVL uprev (temporary until new release will be published)
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21977>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -7,7 +7,7 @@
 
 set -ex
 
-VALIDATION_TAG="v1.3.248"
+VALIDATION_TAG="main"
 
 git clone -b "$VALIDATION_TAG" --single-branch --depth 1 https://github.com/KhronosGroup/Vulkan-ValidationLayers.git
 pushd Vulkan-ValidationLayers
