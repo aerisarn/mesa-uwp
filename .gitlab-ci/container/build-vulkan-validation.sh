b9dd13e1b3bdeb2ce73af93b252e1df21e2be0ad
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-05-31 14:31:31 -0400

    ci: uprev VVL to 1.3.251
    Reviewed-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23343>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -7,7 +7,7 @@
 
 set -ex
 
-VALIDATION_TAG="main"
+VALIDATION_TAG="v1.3.251"
 
 git clone -b "$VALIDATION_TAG" --single-branch --depth 1 https://github.com/KhronosGroup/Vulkan-ValidationLayers.git
 pushd Vulkan-ValidationLayers
