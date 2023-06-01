97c48edac07763a78cfeff975d1a88d5ff9a3cf8
Karol Herbst <git@karolherbst.de>
2023-06-01 21:37:49 +0200

    rusticl/device: limit MAX_PARAMETER_SIZE to 32k
    There is no good reason of having it too big and radeonsi returns an
    absurd high value through PIPE_SHADER_CAP_MAX_CONST_BUFFER0_SIZE.
    
    This speeds up the CTS testing this device limit.
    
    Signed-off-by: Karol Herbst <git@karolherbst.de>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23378>

--- src/gallium/frontends/rusticl/core/device.rs
+++ src/gallium/frontends/rusticl/core/device.rs
@@ -782,7 +782,10 @@ impl Device {
     }
 
     pub fn param_max_size(&self) -> usize {
-        self.shader_param(pipe_shader_cap::PIPE_SHADER_CAP_MAX_CONST_BUFFER0_SIZE) as usize
+        min(
+            self.shader_param(pipe_shader_cap::PIPE_SHADER_CAP_MAX_CONST_BUFFER0_SIZE),
+            32 * 1024,
+        ) as usize
     }
 
     pub fn printf_buffer_size(&self) -> usize {
