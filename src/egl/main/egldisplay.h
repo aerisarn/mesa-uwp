f85c286f51bab7d476111b781798ddb445c3f2d5
Italo Nicola <italonicola@collabora.com>
2023-06-01 07:23:49 -0300

    egl: fix comments alignment
    Signed-off-by: Italo Nicola <italonicola@collabora.com>
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20387>

--- src/egl/main/egldisplay.h
+++ src/egl/main/egldisplay.h
@@ -201,11 +201,11 @@ struct _egl_display
 
    /* options that affect how the driver initializes the display */
    struct {
-      EGLBoolean Zink; /**< Use kopper only */
+      EGLBoolean Zink;          /**< Use kopper only */
       EGLBoolean ForceSoftware; /**< Use software path only */
       EGLBoolean GalliumHud;    /**< Using gallium hud, disable buffer age */
-      EGLAttrib *Attribs;     /**< Platform-specific options */
-      int fd; /**< platform device specific, local fd */
+      EGLAttrib *Attribs;       /**< Platform-specific options */
+      int fd;                   /**< Platform device specific, local fd */
    } Options;
 
    /* these fields are set by the driver during init */
