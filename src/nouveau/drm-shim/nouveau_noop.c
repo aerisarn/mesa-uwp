b5401a4b0e84a6f24ff8e90cae8843c6763c5c23
Yonggang Luo <luoyonggang@gmail.com>
2023-06-05 04:23:38 +0800

    nouveau/drm-shim: Decouple from gallium
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24463>

--- src/nouveau/drm-shim/nouveau_noop.c
+++ src/nouveau/drm-shim/nouveau_noop.c
@@ -32,7 +32,7 @@
 #include "drm-shim/drm_shim.h"
 #include "util//u_math.h"
 
-#include "../gallium/drivers/nouveau/nv_object.xml.h"
+#include "../../gallium/drivers/nouveau/nv_object.xml.h"
 bool drm_shim_driver_prefers_first_render_node = true;
 
 struct nouveau_device {
