ed1281a17029b4c7e01163aaf3d4d36da8c20462
Yonggang Luo <luoyonggang@gmail.com>
2023-06-04 17:15:31 +0800

    util: Move PIPE_MASK_* from p_defines.h to u_formats.h
    PIPE_MASK_* is used by src/util/*, so do the move to decouple src/util/* from gallium
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19522>

--- src/util/format/u_formats.h
+++ src/util/format/u_formats.h
@@ -666,6 +666,16 @@ enum pipe_swizzle {
    PIPE_SWIZZLE_MAX, /**< Number of enums counter (must be last) */
 };
 
+#define PIPE_MASK_R  0x1
+#define PIPE_MASK_G  0x2
+#define PIPE_MASK_B  0x4
+#define PIPE_MASK_A  0x8
+#define PIPE_MASK_RGBA 0xf
+#define PIPE_MASK_Z  0x10
+#define PIPE_MASK_S  0x20
+#define PIPE_MASK_ZS 0x30
+#define PIPE_MASK_RGBAZS (PIPE_MASK_RGBA|PIPE_MASK_ZS)
+
 #ifdef __cplusplus
 }
 #endif
