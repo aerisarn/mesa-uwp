8c35537351dcb0b6181cd9c5c7ee6f065bff8594
Emma Anholt <emma@anholt.net>
2023-06-05 13:04:00 -0700

    ci: Update to vulkan-cts-1.3.5.2 (and pull in some more fixes).
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23610>

--- .gitlab-ci/container/build-deqp.sh
+++ .gitlab-ci/container/build-deqp.sh
@@ -14,7 +14,7 @@ git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 git clone \
     https://github.com/KhronosGroup/VK-GL-CTS.git \
-    -b vulkan-cts-1.3.5.1 \
+    -b vulkan-cts-1.3.5.2 \
     --depth 1 \
     /VK-GL-CTS
 pushd /VK-GL-CTS
@@ -26,16 +26,36 @@ pushd /VK-GL-CTS
 # patches.
 
 cts_commits_to_backport=(
-        # queue_transfer fix
-        8771481027d76a262195de8397d8985246bca98f
-
         # sync fix for SSBO writes
         44f1be32fe6bd2a7de7b9169fc71cc44e0b26124
+
+        # sync fix for KHR-GL46.multi_bind.dispatch_bind_image_textures
+        db6c9e295ab38054ace425cb75ff966719ccc609
+
+        # VK robustness barriers fix
+        6052f21c4d6077438d644f525c10cc58dcdf25bf
 
         # correctness fixes for zink validation fails
         1923cbc89ed3969a3afe7c6926124b51157902e1
         af3a979c49dc65f8809c27660405ae3a76c7da4a
-        6b70682c57c4ffd07fdf6bcbf1aebd7cf1123629
+
+        # GL/GLES vertex_attrib_binding.advanced-largeStrideAndOffsetsNewAndLegacyAPI fix
+        bdb456dcf85e34fced872ebdaf06f6b73451f99c
+
+        # KHR-GLES31.core.compute_shader.max fix
+        7aa3ebb49d07982f5c44edd4799edb5a894567e9
+
+        # GL arrays_of_arrays perf fix
+        b481dada59734e8e34050fe884ba6d627d9e5c54
+
+        # GL shadow samplers require depth compares fix
+        a8bc242ec234bf8d7df8b4eec1eeccab4e401288
+
+        # GL PolygonOffsetClamp fix
+        1f2feb2388da88b4e46eba55547d50856467cc20
+
+        # KHR-GL46.texture_view.view_sampling fix
+        aca29fb9553ebe28094513ce18bb46bad138cf46
 
         # video validation fails
         4cc3980a86ba5b7fe6e76b559cc1a9cb5fd1b253
@@ -43,6 +63,9 @@ cts_commits_to_backport=(
 
         # Check for robustness before testing it
         ee7138d8adf5ed3c4845e5ac2553c4f9697be9d8
+
+        # dEQP-VK.wsi.acquire_drm_display.*invalid_fd
+        98ad9402e7d94030d1689fd59135da7a2f52384c
 )
 
 for commit in "${cts_commits_to_backport[@]}"
