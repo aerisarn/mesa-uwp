dd1b92780538fca106bc443fbe228705fa4f224a
Corentin Noël <corentin.noel@collabora.com>
2023-06-05 09:40:57 +0200

    ci: uprev virglrenderer and crosvm
    Update to their latest version on time
    
    Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23427>

--- .gitlab-ci/container/build-crosvm.sh
+++ .gitlab-ci/container/build-crosvm.sh
@@ -6,13 +6,13 @@ set -ex
 git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 
-CROSVM_VERSION=b018c19e23b6f77dc2ab6258d97711a6db2c56fc
+CROSVM_VERSION=d0cbf0b23eb4bd2355b011184025c7c5d8749376
 git clone --single-branch -b main --no-checkout https://chromium.googlesource.com/crosvm/crosvm /platform/crosvm
 pushd /platform/crosvm
 git checkout "$CROSVM_VERSION"
 git submodule update --init
 
-VIRGLRENDERER_VERSION=74d35a57b3783110adee2e4a02d6f00cbc8f6810
+VIRGLRENDERER_VERSION=bcc152ffdeec05d5bfe5d42f200933123d66c7db
 rm -rf third_party/virglrenderer
 git clone --single-branch -b master --no-checkout https://gitlab.freedesktop.org/virgl/virglrenderer.git third_party/virglrenderer
 pushd third_party/virglrenderer
@@ -30,7 +30,7 @@ RUSTFLAGS='-L native=/usr/local/lib' cargo install \
   --version 0.65.1 \
   $EXTRA_CARGO_ARGS
 
-RUSTFLAGS='-L native=/usr/local/lib' cargo install \
+CROSVM_USE_SYSTEM_VIRGLRENDERER=1 RUSTFLAGS='-L native=/usr/local/lib' cargo install \
   -j ${FDO_CI_CONCURRENT:-4} \
   --locked \
   --features 'default-no-sandbox gpu x virgl_renderer virgl_renderer_next' \
