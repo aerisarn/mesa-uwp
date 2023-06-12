aerisarn/mesa-uwp

commit a4ce095bad9115c25b05c82db171171275d6e3a4

dzn: Use A4B4G4R4 instead of B4G4R4A4 when available
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22911>

src/microsoft/vulkan/dzn_image.c
@@ -276,6 +276,9 @@ dzn_image_get_dxgi_format(const struct dzn_physical_device *pdev,
 {
    enum pipe_format pfmt = vk_format_to_pipe_format(format);
 
+   if (pfmt == PIPE_FORMAT_A4R4G4B4_UNORM && !pdev->support_a4b4g4r4)
+      return DXGI_FORMAT_B4G4R4A4_UNORM;
+
    if (!vk_format_is_depth_or_stencil(format))
       return dzn_pipe_to_dxgi_format(pfmt);
 
@@ -1043,17 +1046,31 @@ dzn_image_view_prepare_srv_desc(struct dzn_image_view *iview)
 
    /* Swap components to fake B4G4R4A4 support. */
    if (iview->vk.format == VK_FORMAT_B4G4R4A4_UNORM_PACK16) {
-      static const D3D12_SHADER_COMPONENT_MAPPING bgra4_remap[] = {
-         D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_1,
-         D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_0,
-         D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_3,
-         D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_2,
-         D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_0,
-         D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_1,
-      };
+      if (pdev->support_a4b4g4r4) {
+         static const D3D12_SHADER_COMPONENT_MAPPING bgra4_remap[] = {
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_2,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_1,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_0,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_3,
+            D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_0,
+            D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_1,
+         };
 
-      for (uint32_t i = 0; i < ARRAY_SIZE(swz); i++)
-         swz[i] = bgra4_remap[swz[i]];
+         for (uint32_t i = 0; i < ARRAY_SIZE(swz); i++)
+            swz[i] = bgra4_remap[swz[i]];
+      } else {
+         static const D3D12_SHADER_COMPONENT_MAPPING bgra4_remap[] = {
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_1,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_0,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_3,
+            D3D12_SHADER_COMPONENT_MAPPING_FROM_MEMORY_COMPONENT_2,
+            D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_0,
+            D3D12_SHADER_COMPONENT_MAPPING_FORCE_VALUE_1,
+         };
+
+         for (uint32_t i = 0; i < ARRAY_SIZE(swz); i++)
+            swz[i] = bgra4_remap[swz[i]];
+      }
    } else if (iview->vk.aspects & VK_IMAGE_ASPECT_STENCIL_BIT) {
       /* D3D puts stencil in G, not R. Requests for R should be routed to G and vice versa. */
       for (uint32_t i = 0; i < ARRAY_SIZE(swz); i++) {

src/microsoft/vulkan/dzn_cmd_buffer.c
@@ -1967,15 +1967,20 @@ dzn_cmd_buffer_clear_rects_with_copy(struct dzn_cmd_buffer *cmdbuf,
 }
 
 static VkClearColorValue
-adjust_clear_color(VkFormat format, const VkClearColorValue *col)
+adjust_clear_color(struct dzn_physical_device *pdev,
+                   VkFormat format, const VkClearColorValue *col)
 {
    VkClearColorValue out = *col;
 
    // D3D12 doesn't support bgra4, so we map it to rgba4 and swizzle things
    // manually where it matters, like here, in the clear path.
    if (format == VK_FORMAT_B4G4R4A4_UNORM_PACK16) {
-      DZN_SWAP(float, out.float32[0], out.float32[1]);
-      DZN_SWAP(float, out.float32[2], out.float32[3]);
+      if (pdev->support_a4b4g4r4) {
+         DZN_SWAP(float, out.float32[0], out.float32[2]);
+      } else {
+         DZN_SWAP(float, out.float32[0], out.float32[1]);
+         DZN_SWAP(float, out.float32[2], out.float32[3]);
+      }
    }
 
    return out;
@@ -2123,6 +2128,8 @@ dzn_cmd_buffer_clear_attachment(struct dzn_cmd_buffer *cmdbuf,
 {
    struct dzn_image *image =
       container_of(view->vk.image, struct dzn_image, vk);
+   struct dzn_physical_device *pdev =
+      container_of(cmdbuf->vk.base.device->physical, struct dzn_physical_device, vk);
 
    VkImageSubresourceRange range = {
       .aspectMask = aspects,
@@ -2176,7 +2183,7 @@ dzn_cmd_buffer_clear_attachment(struct dzn_cmd_buffer *cmdbuf,
          }
       }
    } else if (aspects & VK_IMAGE_ASPECT_COLOR_BIT) {
-      VkClearColorValue color = adjust_clear_color(view->vk.format, &value->color);
+      VkClearColorValue color = adjust_clear_color(pdev, view->vk.format, &value->color);
       bool clear_with_cpy = false;
       float vals[4];
 
@@ -2245,13 +2252,15 @@ dzn_cmd_buffer_clear_color(struct dzn_cmd_buffer *cmdbuf,
                            uint32_t range_count,
                            const VkImageSubresourceRange *ranges)
 {
+   struct dzn_physical_device *pdev =
+      container_of(cmdbuf->vk.base.device->physical, struct dzn_physical_device, vk);
    if (!(image->desc.Flags & D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET) ||
        cmdbuf->type != D3D12_COMMAND_LIST_TYPE_DIRECT) {
       dzn_cmd_buffer_clear_ranges_with_copy(cmdbuf, image, layout, col, range_count, ranges);
       return;
    }
 
-   VkClearColorValue color = adjust_clear_color(image->vk.format, col);
+   VkClearColorValue color = adjust_clear_color(pdev, image->vk.format, col);
    float clear_vals[4];
 
    enum pipe_format pfmt = vk_format_to_pipe_format(image->vk.format);

src/microsoft/vulkan/dzn_device.c
@@ -475,6 +475,13 @@ dzn_physical_device_cache_caps(struct dzn_physical_device *pdev)
       pdev->options19.MaxSamplerDescriptorHeapSizeWithStaticSamplers = pdev->options19.MaxSamplerDescriptorHeapSize;
       pdev->options19.MaxViewDescriptorHeapSize = D3D12_MAX_SHADER_VISIBLE_DESCRIPTOR_HEAP_SIZE_TIER_1;
    }
+   {
+      D3D12_FEATURE_DATA_FORMAT_SUPPORT a4b4g4r4_support = {
+         .Format = DXGI_FORMAT_A4B4G4R4_UNORM
+      };
+      pdev->support_a4b4g4r4 =
+         SUCCEEDED(ID3D12Device1_CheckFeatureSupport(pdev->dev, D3D12_FEATURE_FORMAT_SUPPORT, &a4b4g4r4_support, sizeof(a4b4g4r4_support)));
+   }
 
    pdev->queue_families[pdev->queue_family_count++] = (struct dzn_queue_family) {
       .props = {
@@ -856,10 +863,16 @@ dzn_physical_device_get_format_properties(struct dzn_physical_device *pdev,
       }
    }
 
-   /* B4G4R4A4 support is required, but d3d12 doesn't support it. We map this
-    * format to R4G4B4A4 and adjust the SRV component-mapping to fake
+   /* B4G4R4A4 support is required, but d3d12 doesn't support it. The needed
+    * d3d12 format would be A4R4G4B4. We map this format to d3d12's B4G4R4A4,
+    * which is Vulkan's A4R4G4B4, and adjust the SRV component-mapping to fake
     * B4G4R4A4, but that forces us to limit the usage to sampling, which,
     * luckily, is exactly what we need to support the required features.
+    *
+    * However, since this involves swizzling the alpha channel, it can cause
+    * problems for border colors. Fortunately, d3d12 added an A4B4G4R4 format,
+    * which still isn't quite right (it'd be Vulkan R4G4B4A4), but can be
+    * swizzled by just swapping R and B, so no border color issues arise.
     */
    if (format == VK_FORMAT_B4G4R4A4_UNORM_PACK16) {
       VkFormatFeatureFlags bgra4_req_features =

src/microsoft/vulkan/dzn_private.h
@@ -223,6 +223,7 @@ struct dzn_physical_device {
    D3D12_HEAP_FLAGS heap_flags_for_mem_type[VK_MAX_MEMORY_TYPES];
    const struct vk_sync_type *sync_types[MAX_SYNC_TYPES + 1];
    float timestamp_period;
+   bool support_a4b4g4r4;
 };
 
 D3D12_FEATURE_DATA_FORMAT_SUPPORT

src/microsoft/vulkan/dzn_util.c
@@ -73,7 +73,8 @@ static const DXGI_FORMAT formats[PIPE_FORMAT_COUNT] = {
    [PIPE_FORMAT_B8G8R8X8_UNORM] = DXGI_FORMAT_B8G8R8X8_UNORM,
    [PIPE_FORMAT_B8G8R8A8_UNORM] = DXGI_FORMAT_B8G8R8A8_UNORM,
    [PIPE_FORMAT_B4G4R4A4_UNORM] = DXGI_FORMAT_B4G4R4A4_UNORM,
-   [PIPE_FORMAT_A4R4G4B4_UNORM] = DXGI_FORMAT_B4G4R4A4_UNORM,
+   [PIPE_FORMAT_A4R4G4B4_UNORM] = DXGI_FORMAT_A4B4G4R4_UNORM,
+   [PIPE_FORMAT_A4B4G4R4_UNORM] = DXGI_FORMAT_A4B4G4R4_UNORM,
    [PIPE_FORMAT_B5G6R5_UNORM] = DXGI_FORMAT_B5G6R5_UNORM,
    [PIPE_FORMAT_B5G5R5A1_UNORM] = DXGI_FORMAT_B5G5R5A1_UNORM,
 

commit b717a438261ad53bcc8c27e8a36cfd7e7a7b579f

dzn: Don't support VK R4G4B4A4_UNORM_PACK16 unless we have B4G4R4A4
Fixes: a4ce095bad9 ("dzn: Use A4B4G4R4 instead of B4G4R4A4 when available")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23602>

src/microsoft/vulkan/dzn_image.c
@@ -276,8 +276,12 @@ dzn_image_get_dxgi_format(const struct dzn_physical_device *pdev,
 {
    enum pipe_format pfmt = vk_format_to_pipe_format(format);
 
-   if (pfmt == PIPE_FORMAT_A4R4G4B4_UNORM && !pdev->support_a4b4g4r4)
-      return DXGI_FORMAT_B4G4R4A4_UNORM;
+   if (pdev && !pdev->support_a4b4g4r4) {
+      if (pfmt == PIPE_FORMAT_A4R4G4B4_UNORM)
+         return DXGI_FORMAT_B4G4R4A4_UNORM;
+      if (pfmt == PIPE_FORMAT_A4B4G4R4_UNORM)
+         return DXGI_FORMAT_UNKNOWN;
+   }
 
    if (!vk_format_is_depth_or_stencil(format))
       return dzn_pipe_to_dxgi_format(pfmt);