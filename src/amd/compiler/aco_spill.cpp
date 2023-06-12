aerisarn/mesa-uwp

commit 3d4f6a00b8e8710bbc265f8a5e3050d0550e1491

aco/spill: allow for disconnected CFG
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20853>

src/amd/compiler/aco_spill.cpp
@@ -486,7 +486,7 @@ init_live_in_vars(spill_ctx& ctx, Block* block, unsigned block_idx)
    RegisterDemand spilled_registers;
 
    /* first block, nothing was spilled before */
-   if (block_idx == 0)
+   if (block->linear_preds.empty())
       return {0, 0};
 
    /* next use distances at the beginning of the current block */
@@ -1679,7 +1679,7 @@ end_unused_spill_vgprs(spill_ctx& ctx, Block& block, std::vector<Temp>& vgpr_spi
          vgpr_spill_temps[i] = Temp();
       }
    }
-   if (temps.empty())
+   if (temps.empty() || block.linear_preds.empty())
       return;
 
    aco_ptr<Instruction> destr{create_instruction<Pseudo_instruction>(
@@ -1743,7 +1743,7 @@ assign_spill_slots(spill_ctx& ctx, unsigned spills_to_vgpr)
    unsigned last_top_level_block_idx = 0;
    for (Block& block : ctx.program->blocks) {
 
-      if (block.kind & block_kind_top_level && !block.linear_preds.empty()) {
+      if (block.kind & block_kind_top_level) {
          last_top_level_block_idx = block.index;
 
          end_unused_spill_vgprs(ctx, block, vgpr_spill_temps, slots, ctx.spills_entry[block.index]);

commit 7f3cfcc96a13e93aa9667331f9addf9a19165d21

aco: Reset scratch_rsrc on blocks without predecessors
Fixes hangs with raytracing in Hellblade: Senua's Sacrifice.

Fixes: 3d4f6a00b ('aco/spill: allow for disconnected CFG')
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23586>

src/amd/compiler/aco_spill.cpp
@@ -1755,6 +1755,11 @@ assign_spill_slots(spill_ctx& ctx, unsigned spills_to_vgpr)
          last_top_level_block_idx = block.index;
 
          end_unused_spill_vgprs(ctx, block, vgpr_spill_temps, slots, ctx.spills_entry[block.index]);
+
+         /* If the block has no predecessors (for example in RT resume shaders),
+          * we cannot reuse the current scratch_rsrc temp because its definition is unreachable */
+         if (block.linear_preds.empty())
+            ctx.scratch_rsrc = Temp();
       }
 
       std::vector<aco_ptr<Instruction>>::iterator it;