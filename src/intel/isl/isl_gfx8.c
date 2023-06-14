750744143021f3925a6e35b0a1502ef4821c8b6b
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-06-14 15:34:12 +0300

    isl: assert on gfx8 condition that should not be met
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14039>

--- src/intel/isl/isl_gfx8.c
+++ src/intel/isl/isl_gfx8.c
@@ -66,8 +66,9 @@ isl_gfx8_choose_msaa_layout(const struct isl_device *dev,
       return false;
 
    /* More obvious restrictions */
-   if (isl_surf_usage_is_display(info->usage))
-      return false;
+   assert(!isl_surf_usage_is_display(info->usage));
+   assert(tiling != ISL_TILING_LINEAR);
+
    if (!isl_format_supports_multisampling(dev->info, info->format))
       return false;
 
