aerisarn/mesa-uwp

commit cc9e958053183a9507e40be02cd7decfef1c45d0

lavapipe: fix DS3 min sample setting
if a DS3 pipeline enabling dynamic samples is not bound when samples
are set dynamically, then such a pipeline is later bound, min samples
would have been incorrectly set to 1

instead, flag the update for later and do it just before draw

cc: mesa-stable

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23368>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -434,6 +434,25 @@ static void emit_compute_state(struct rendering_state *state)
    }
 }
 
+static void
+update_min_samples(struct rendering_state *state)
+{
+   state->min_samples = 1;
+   if (state->sample_shading) {
+      state->min_samples = ceil(state->rast_samples * state->min_sample_shading);
+      if (state->min_samples > 1)
+         state->min_samples = state->rast_samples;
+      if (state->min_samples < 1)
+         state->min_samples = 1;
+   }
+   if (state->force_min_sample)
+      state->min_samples = state->rast_samples;
+   if (state->rast_samples != state->framebuffer.samples) {
+      state->framebuffer.samples = state->rast_samples;
+      state->pctx->set_framebuffer_state(state->pctx, &state->framebuffer);
+   }
+}
+
 static void emit_state(struct rendering_state *state)
 {
    if (!state->shaders[MESA_SHADER_FRAGMENT] && !state->noop_fs_bound) {
@@ -498,6 +517,7 @@ static void emit_state(struct rendering_state *state)
    }
 
    if (state->min_samples_dirty) {
+      update_min_samples(state);
       cso_set_min_samples(state->cso, state->min_samples);
       state->min_samples_dirty = false;
    }
@@ -663,21 +683,7 @@ update_samples(struct rendering_state *state, VkSampleCountFlags samples)
    state->rast_samples = samples;
    state->rs_dirty |= state->rs_state.multisample != (samples > 1);
    state->rs_state.multisample = samples > 1;
-   state->min_samples = 1;
-   if (state->sample_shading) {
-      state->min_samples = ceil(samples * state->min_sample_shading);
-      if (state->min_samples > 1)
-         state->min_samples = samples;
-      if (state->min_samples < 1)
-         state->min_samples = 1;
-   }
-   if (state->force_min_sample)
-      state->min_samples = samples;
    state->min_samples_dirty = true;
-   if (samples != state->framebuffer.samples) {
-      state->framebuffer.samples = samples;
-      state->pctx->set_framebuffer_state(state->pctx, &state->framebuffer);
-   }
 }
 
 static void

commit cb03191e82d666a222a378b25560761e4885bd23

lavapipe: more fixes for sample shading
this fixes the case where a draw without sample shading precedes
a draw with sample shading without changes to the sample mask

Fixes: cc9e9580531 ("lavapipe: fix DS3 min sample setting")

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23663>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -1060,6 +1060,7 @@ static void handle_graphics_pipeline(struct lvp_pipeline *pipeline,
       state->force_min_sample = pipeline->force_min_sample;
       state->sample_shading = ps->ms->sample_shading_enable;
       state->min_sample_shading = ps->ms->min_sample_shading;
+      state->min_samples_dirty = true;
       state->blend_dirty = true;
       if (!BITSET_TEST(ps->dynamic, MESA_VK_DYNAMIC_MS_RASTERIZATION_SAMPLES))
          update_samples(state, ps->ms->rasterization_samples);
@@ -1072,7 +1073,7 @@ static void handle_graphics_pipeline(struct lvp_pipeline *pipeline,
       if (!BITSET_TEST(ps->dynamic, MESA_VK_DYNAMIC_MS_SAMPLE_MASK)) {
          state->sample_mask_dirty = state->sample_mask != 0xffffffff;
          state->sample_mask = 0xffffffff;
-         state->min_samples_dirty = state->min_samples;
+         state->min_samples_dirty = !!state->min_samples;
          state->min_samples = 0;
       }
       state->blend_dirty |= state->blend_state.alpha_to_coverage || state->blend_state.alpha_to_one;