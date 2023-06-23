647241a770c365a77ad8354145d3f2a1f0f44ec8
Erik Faye-Lund <erik.faye-lund@collabora.com>
2023-06-23 16:43:42 +0200

    aux/pp: use unsigned instead of uint
    Reviewed-by: Yonggang Luo <luoyonggang@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24002>

--- src/gallium/auxiliary/postprocess/pp_program.c
+++ src/gallium/auxiliary/postprocess/pp_program.c
@@ -131,7 +131,7 @@ pp_init_prog(struct pp_queue_t *ppq, struct pipe_context *pipe,
       const enum tgsi_semantic semantic_names[] = { TGSI_SEMANTIC_POSITION,
          TGSI_SEMANTIC_GENERIC
       };
-      const uint semantic_indexes[] = { 0, 0 };
+      const unsigned semantic_indexes[] = { 0, 0 };
       p->passvs = util_make_vertex_passthrough_shader(p->pipe, 2,
                                                       semantic_names,
                                                       semantic_indexes, false);
