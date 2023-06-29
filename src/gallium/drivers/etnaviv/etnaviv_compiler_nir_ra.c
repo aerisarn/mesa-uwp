1faac35866a68994f4e1b1156356c39d3c57ec5b
Christian Gmeiner <cgmeiner@igalia.com>
2023-06-29 16:22:02 +0200

    etnaviv: make use nir_shader_clear_pass_flags(..)
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23931>

--- src/gallium/drivers/etnaviv/etnaviv_compiler_nir_ra.c
+++ src/gallium/drivers/etnaviv/etnaviv_compiler_nir_ra.c
@@ -124,10 +124,7 @@ etna_ra_assign(struct etna_compile *c, nir_shader *shader)
 
    nir_index_blocks(impl);
    nir_index_ssa_defs(impl);
-   nir_foreach_block(block, impl) {
-      nir_foreach_instr(instr, block)
-         instr->pass_flags = 0;
-   }
+   nir_shader_clear_pass_flags(shader);
 
    /* this gives an approximation/upper limit on how many nodes are needed
     * (some ssa values do not represent an allocated register)
