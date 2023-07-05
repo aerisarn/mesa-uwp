aerisarn/mesa-uwp

commit d4424950ace16da00cc660e0c4106d19e169b04b

asahi: Use txf for background program
More straightforward (txf instead of tex, with integer coords). No discrernible
performance difference.

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23836>

src/asahi/lib/agx_meta.c
@@ -40,15 +40,13 @@ build_background_op(nir_builder *b, enum agx_meta_op op, unsigned rt,
                     unsigned nr, bool msaa)
 {
    if (op == AGX_META_OP_LOAD) {
-      nir_ssa_def *fragcoord = nir_load_frag_coord(b);
-      nir_ssa_def *coord = nir_trim_vector(b, fragcoord, 2);
-
       nir_tex_instr *tex = nir_tex_instr_create(b->shader, msaa ? 2 : 1);
       /* The type doesn't matter as long as it matches the store */
       tex->dest_type = nir_type_uint32;
       tex->sampler_dim = msaa ? GLSL_SAMPLER_DIM_MS : GLSL_SAMPLER_DIM_2D;
-      tex->op = nir_texop_tex;
-      tex->src[0] = nir_tex_src_for_ssa(nir_tex_src_coord, coord);
+      tex->op = nir_texop_txf;
+      tex->src[0] = nir_tex_src_for_ssa(nir_tex_src_coord,
+                                        nir_u2u32(b, nir_load_pixel_coord(b)));
 
       if (msaa) {
          tex->src[1] =

commit a28f9738e1781ebdfb6e485284544905793d42d3

asahi: Use txf_ms for MSAA background programs
Fixes regression in assorted dEQP tests including:

dEQP-EGL.functional.color_clears.multi_context.gles3.rgba8888_window

Fixes: d4424950ace ("asahi: Use txf for background program")
Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23998>

src/asahi/lib/agx_meta.c
@@ -44,7 +44,7 @@ build_background_op(nir_builder *b, enum agx_meta_op op, unsigned rt,
       /* The type doesn't matter as long as it matches the store */
       tex->dest_type = nir_type_uint32;
       tex->sampler_dim = msaa ? GLSL_SAMPLER_DIM_MS : GLSL_SAMPLER_DIM_2D;
-      tex->op = nir_texop_txf;
+      tex->op = msaa ? nir_texop_txf_ms : nir_texop_txf;
       tex->src[0] = nir_tex_src_for_ssa(nir_tex_src_coord,
                                         nir_u2u32(b, nir_load_pixel_coord(b)));
 