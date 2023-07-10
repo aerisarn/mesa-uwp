7100ef4566595f73b967231d0b877781d0911de5
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-07-10 14:31:18 -0400

    glxsw: check geometry of drawables on creation
    this mimics the dri codepath and allows early rejection of invalid
    drawables to avoid subsequent breakage
    
    cc: mesa-stable
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24075>

--- src/glx/drisw_glx.c
+++ src/glx/drisw_glx.c
@@ -675,10 +675,21 @@ driswCreateDrawable(struct glx_screen *base, XID xDrawable,
 {
    struct drisw_drawable *pdp;
    __GLXDRIconfigPrivate *config = (__GLXDRIconfigPrivate *) modes;
+   unsigned depth;
    struct drisw_screen *psc = (struct drisw_screen *) base;
    const __DRIswrastExtension *swrast = psc->swrast;
    const __DRIkopperExtension *kopper = psc->kopper;
    Display *dpy = psc->base.dpy;
+
+   xcb_connection_t *conn = XGetXCBConnection(dpy);
+   xcb_generic_error_t *error;
+   xcb_get_geometry_cookie_t cookie = xcb_get_geometry(conn, xDrawable);
+   xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(conn, cookie, &error);
+   if (reply)
+      depth = reply->depth;
+   free(reply);
+   if (!reply || error)
+      return NULL;
 
    pdp = calloc(1, sizeof(*pdp));
    if (!pdp)
@@ -709,11 +720,6 @@ driswCreateDrawable(struct glx_screen *base, XID xDrawable,
 
    /* Otherwise, or if XGetVisualInfo failed, ask the server */
    if (pdp->xDepth == 0) {
-      Window root;
-      int x, y;
-      unsigned uw, uh, bw, depth;
-
-      XGetGeometry(dpy, xDrawable, &root, &x, &y, &uw, &uh, &bw, &depth);
       pdp->xDepth = depth;
    }
 
