28b07a7bd2a2afa34b6c7fb905c31e79c030e393
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2023-07-18 09:40:07 -0400

    ci: bump VVL to 1.3.257
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24214>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -7,7 +7,7 @@
 
 set -ex
 
-VALIDATION_TAG="v1.3.251"
+VALIDATION_TAG="v1.3.257"
 
 git clone -b "$VALIDATION_TAG" --single-branch --depth 1 https://github.com/KhronosGroup/Vulkan-ValidationLayers.git
 pushd Vulkan-ValidationLayers
