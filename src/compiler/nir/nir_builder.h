aerisarn/mesa-uwp

commit f534c2c539f1a4722c03c63a257e99b052e6f1a8

nir/builder: Add nir_replicate helper
Splat a scalar to all components of a vector.

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23259>

src/compiler/nir/nir_builder.h
@@ -708,6 +708,19 @@ nir_vector_insert(nir_builder *b, nir_ssa_def *vec, nir_ssa_def *scalar,
 }
 
 static inline nir_ssa_def *
+nir_replicate(nir_builder *b, nir_ssa_def *scalar, unsigned num_components)
+{
+   assert(scalar->num_components == 1);
+   assert(num_components < NIR_MAX_VEC_COMPONENTS);
+
+   nir_ssa_def *copies[NIR_MAX_VEC_COMPONENTS] = {NULL};
+   for (unsigned i = 0; i < num_components; ++i)
+      copies[i] = scalar;
+
+   return nir_vec(b, copies, num_components);
+}
+
+static inline nir_ssa_def *
 nir_iadd_imm(nir_builder *build, nir_ssa_def *x, uint64_t y)
 {
    assert(x->bit_size <= 64);

commit c2e398632645cc587072b68c0e3a97910859a6ac

nir: Fix 16-component nir_replicate.
Fixes: f534c2c539f ("nir/builder: Add nir_replicate helper")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24286>

src/compiler/nir/nir_builder.h
@@ -727,7 +727,7 @@ static inline nir_ssa_def *
 nir_replicate(nir_builder *b, nir_ssa_def *scalar, unsigned num_components)
 {
    assert(scalar->num_components == 1);
-   assert(num_components < NIR_MAX_VEC_COMPONENTS);
+   assert(num_components <= NIR_MAX_VEC_COMPONENTS);
 
    nir_ssa_def *copies[NIR_MAX_VEC_COMPONENTS] = {NULL};
    for (unsigned i = 0; i < num_components; ++i)