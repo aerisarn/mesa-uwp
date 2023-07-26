aerisarn/mesa-uwp

commit 3384f029be7e58b9c81b96c1a6250412acf41f6f

intel/compiler: rework input parameters
Use a struct for various common parameters rather than per stage
structure or arguments to stage specific entrypoints.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Felix DeGrood <felix.j.degrood@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23942>

src/intel/compiler/brw_vec4.cpp
@@ -2495,7 +2495,7 @@ vec4_visitor::run()
    bool allocated_without_spills = reg_allocate();
 
    if (!allocated_without_spills) {
-      brw_shader_perf_log(compiler, log_data,
+      brw_shader_perf_log(compiler, params->log_data,
                           "%s shader triggered register spilling.  "
                           "Try reducing the number of live vec4 values "
                           "to improve performance.\n",
@@ -2533,14 +2533,14 @@ extern "C" {
 
 const unsigned *
 brw_compile_vs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_vs_params *params)
 {
-   struct nir_shader *nir = params->nir;
+   struct nir_shader *nir = params->base.nir;
    const struct brw_vs_prog_key *key = params->key;
    struct brw_vs_prog_data *prog_data = params->prog_data;
    const bool debug_enabled =
-      brw_should_print_shader(nir, params->debug_flag ? params->debug_flag : DEBUG_VS);
+      brw_should_print_shader(nir, params->base.debug_flag ?
+                                   params->base.debug_flag : DEBUG_VS);
 
    prog_data->base.base.stage = MESA_SHADER_VERTEX;
    prog_data->base.base.ray_queries = nir->info.ray_queries;
@@ -2635,22 +2635,23 @@ brw_compile_vs(const struct brw_compiler *compiler,
    if (is_scalar) {
       prog_data->base.dispatch_mode = DISPATCH_MODE_SIMD8;
 
-      fs_visitor v(compiler, params->log_data, mem_ctx, &key->base,
+      fs_visitor v(compiler, &params->base, &key->base,
                    &prog_data->base.base, nir, 8,
-                   params->stats != NULL, debug_enabled);
+                   params->base.stats != NULL, debug_enabled);
       if (!v.run_vs()) {
-         params->error_str = ralloc_strdup(mem_ctx, v.fail_msg);
+         params->base.error_str =
+            ralloc_strdup(params->base.mem_ctx, v.fail_msg);
          return NULL;
       }
 
       prog_data->base.base.dispatch_grf_start_reg = v.payload().num_regs;
 
-      fs_generator g(compiler, params->log_data, mem_ctx,
+      fs_generator g(compiler, &params->base,
                      &prog_data->base.base, v.runtime_check_aads_emit,
                      MESA_SHADER_VERTEX);
       if (unlikely(debug_enabled)) {
          const char *debug_name =
-            ralloc_asprintf(mem_ctx, "%s vertex shader %s",
+            ralloc_asprintf(params->base.mem_ctx, "%s vertex shader %s",
                             nir->info.label ? nir->info.label :
                                "unnamed",
                             nir->info.name);
@@ -2658,7 +2659,7 @@ brw_compile_vs(const struct brw_compiler *compiler,
          g.enable_debug(debug_name);
       }
       g.generate_code(v.cfg, 8, v.shader_stats,
-                      v.performance_analysis.require(), params->stats);
+                      v.performance_analysis.require(), params->base.stats);
       g.add_const_data(nir->constant_data, nir->constant_data_size);
       assembly = g.get_assembly();
    }
@@ -2666,19 +2667,19 @@ brw_compile_vs(const struct brw_compiler *compiler,
    if (!assembly) {
       prog_data->base.dispatch_mode = DISPATCH_MODE_4X2_DUAL_OBJECT;
 
-      vec4_vs_visitor v(compiler, params->log_data, key, prog_data,
-                        nir, mem_ctx,
-                        debug_enabled);
+      vec4_vs_visitor v(compiler, &params->base, key, prog_data,
+                        nir, debug_enabled);
       if (!v.run()) {
-         params->error_str = ralloc_strdup(mem_ctx, v.fail_msg);
+         params->base.error_str =
+            ralloc_strdup(params->base.mem_ctx, v.fail_msg);
          return NULL;
       }
 
-      assembly = brw_vec4_generate_assembly(compiler, params->log_data, mem_ctx,
+      assembly = brw_vec4_generate_assembly(compiler, &params->base,
                                             nir, &prog_data->base,
                                             v.cfg,
                                             v.performance_analysis.require(),
-                                            params->stats, debug_enabled);
+                                            debug_enabled);
    }
 
    return assembly;

src/intel/compiler/brw_vec4.h
@@ -43,13 +43,11 @@ extern "C" {
 
 const unsigned *
 brw_vec4_generate_assembly(const struct brw_compiler *compiler,
-                           void *log_data,
-                           void *mem_ctx,
+                           const struct brw_compile_params *params,
                            const nir_shader *nir,
                            struct brw_vue_prog_data *prog_data,
                            const struct cfg_t *cfg,
                            const brw::performance &perf,
-                           struct brw_compile_stats *stats,
                            bool debug_enabled);
 
 #ifdef __cplusplus
@@ -66,11 +64,10 @@ class vec4_visitor : public backend_shader
 {
 public:
    vec4_visitor(const struct brw_compiler *compiler,
-                void *log_data,
+                const struct brw_compile_params *params,
                 const struct brw_sampler_prog_key_data *key,
                 struct brw_vue_prog_data *prog_data,
                 const nir_shader *shader,
-		void *mem_ctx,
                 bool no_spills,
                 bool debug_enabled);
 
@@ -345,6 +342,8 @@ private:
    const bool no_spills;
 
    unsigned last_scratch; /**< measured in 32-byte (register size) units */
+
+   const struct brw_compile_params *params;
 };
 
 } /* namespace brw */

src/gallium/drivers/crocus/crocus_program.c
@@ -1219,16 +1219,19 @@ crocus_compile_vs(struct crocus_context *ice,
    crocus_sanitize_tex_key(&key_no_ucp.base.tex);
 
    struct brw_compile_vs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = &key_no_ucp,
       .prog_data = vs_prog_data,
       .edgeflag_is_last = devinfo->ver < 6,
-      .log_data = &ice->dbg,
    };
    const unsigned *program =
-      brw_compile_vs(compiler, mem_ctx, &params);
+      brw_compile_vs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile vertex shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile vertex shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }
@@ -1414,15 +1417,18 @@ crocus_compile_tcs(struct crocus_context *ice,
    crocus_sanitize_tex_key(&key_clean.base.tex);
 
    struct brw_compile_tcs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = &key_clean,
       .prog_data = tcs_prog_data,
-      .log_data = &ice->dbg,
    };
 
-   const unsigned *program = brw_compile_tcs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_tcs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile control shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile control shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }
@@ -1555,16 +1561,19 @@ crocus_compile_tes(struct crocus_context *ice,
    crocus_sanitize_tex_key(&key_clean.base.tex);
 
    struct brw_compile_tes_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = &key_clean,
       .prog_data = tes_prog_data,
       .input_vue_map = &input_vue_map,
-      .log_data = &ice->dbg,
    };
 
-   const unsigned *program = brw_compile_tes(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_tes(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile evaluation shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile evaluation shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }
@@ -1697,15 +1706,18 @@ crocus_compile_gs(struct crocus_context *ice,
    crocus_sanitize_tex_key(&key_clean.base.tex);
 
    struct brw_compile_gs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = &key_clean,
       .prog_data = gs_prog_data,
-      .log_data = &ice->dbg,
    };
 
-   const unsigned *program = brw_compile_gs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_gs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile geometry shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile geometry shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }
@@ -1830,19 +1842,21 @@ crocus_compile_fs(struct crocus_context *ice,
    crocus_sanitize_tex_key(&key_clean.base.tex);
 
    struct brw_compile_fs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = &key_clean,
       .prog_data = fs_prog_data,
 
       .allow_spilling = true,
       .vue_map = vue_map,
-
-      .log_data = &ice->dbg,
    };
    const unsigned *program =
-      brw_compile_fs(compiler, mem_ctx, &params);
+      brw_compile_fs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile fragment shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile fragment shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }
@@ -2508,16 +2522,19 @@ crocus_compile_cs(struct crocus_context *ice,
                               num_system_values, num_cbufs, &key->base.tex);
 
    struct brw_compile_cs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = &ice->dbg,
+      },
       .key = key,
       .prog_data = cs_prog_data,
-      .log_data = &ice->dbg,
    };
 
    const unsigned *program =
-      brw_compile_cs(compiler, mem_ctx, &params);
+      brw_compile_cs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile compute shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile compute shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
       return false;
    }

src/gallium/drivers/iris/iris_program.c
@@ -1333,15 +1333,18 @@ iris_compile_vs(struct iris_screen *screen,
    struct brw_vs_prog_key brw_key = iris_to_brw_vs_key(screen, key);
 
    struct brw_compile_vs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = vs_prog_data,
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_vs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_vs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile vertex shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile vertex shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
 
       shader->compilation_failed = true;
@@ -1502,15 +1505,18 @@ iris_compile_tcs(struct iris_screen *screen,
    brw_nir_analyze_ubo_ranges(compiler, nir, NULL, prog_data->ubo_ranges);
 
    struct brw_compile_tcs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = tcs_prog_data,
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_tcs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_tcs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile control shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile control shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
 
       shader->compilation_failed = true;
@@ -1664,16 +1670,19 @@ iris_compile_tes(struct iris_screen *screen,
    struct brw_tes_prog_key brw_key = iris_to_brw_tes_key(screen, key);
 
    struct brw_compile_tes_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = tes_prog_data,
       .input_vue_map = &input_vue_map,
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_tes(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_tes(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile evaluation shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile evaluation shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
 
       shader->compilation_failed = true;
@@ -1803,15 +1812,18 @@ iris_compile_gs(struct iris_screen *screen,
    struct brw_gs_prog_key brw_key = iris_to_brw_gs_key(screen, key);
 
    struct brw_compile_gs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = gs_prog_data,
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_gs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_gs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile geometry shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile geometry shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
 
       shader->compilation_failed = true;
@@ -1939,19 +1951,21 @@ iris_compile_fs(struct iris_screen *screen,
    struct brw_wm_prog_key brw_key = iris_to_brw_fs_key(screen, key);
 
    struct brw_compile_fs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = fs_prog_data,
 
       .allow_spilling = true,
       .vue_map = vue_map,
-
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_fs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_fs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile fragment shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile fragment shader: %s\n", params.base.error_str);
       ralloc_free(mem_ctx);
 
       shader->compilation_failed = true;
@@ -2216,15 +2230,18 @@ iris_compile_cs(struct iris_screen *screen,
    struct brw_cs_prog_key brw_key = iris_to_brw_cs_key(screen, key);
 
    struct brw_compile_cs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = dbg,
+      },
       .key = &brw_key,
       .prog_data = cs_prog_data,
-      .log_data = dbg,
    };
 
-   const unsigned *program = brw_compile_cs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_cs(compiler, &params);
    if (program == NULL) {
-      dbg_printf("Failed to compile compute shader: %s\n", params.error_str);
+      dbg_printf("Failed to compile compute shader: %s\n", params.base.error_str);
 
       shader->compilation_failed = true;
       util_queue_fence_signal(&shader->ready);

src/intel/blorp/blorp.c
@@ -303,17 +303,19 @@ blorp_compile_fs(struct blorp_context *blorp, void *mem_ctx,
    }
 
    struct brw_compile_fs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = blorp->driver_ctx,
+         .debug_flag = DEBUG_BLORP,
+      },
       .key = wm_key,
       .prog_data = wm_prog_data,
 
       .use_rep_send = use_repclear,
-      .log_data = blorp->driver_ctx,
-
-      .debug_flag = DEBUG_BLORP,
    };
 
-   return brw_compile_fs(compiler, mem_ctx, &params);
+   return brw_compile_fs(compiler, &params);
 }
 
 const unsigned *
@@ -340,15 +342,17 @@ blorp_compile_vs(struct blorp_context *blorp, void *mem_ctx,
    struct brw_vs_prog_key vs_key = { 0, };
 
    struct brw_compile_vs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = blorp->driver_ctx,
+         .debug_flag = DEBUG_BLORP,
+      },
       .key = &vs_key,
       .prog_data = vs_prog_data,
-      .log_data = blorp->driver_ctx,
-
-      .debug_flag = DEBUG_BLORP,
    };
 
-   return brw_compile_vs(compiler, mem_ctx, &params);
+   return brw_compile_vs(compiler, &params);
 }
 
 static bool
@@ -398,14 +402,17 @@ blorp_compile_cs(struct blorp_context *blorp, void *mem_ctx,
               nir_metadata_block_index | nir_metadata_dominance, NULL);
 
    struct brw_compile_cs_params params = {
-      .nir = nir,
+      .base = {
+         .mem_ctx = mem_ctx,
+         .nir = nir,
+         .log_data = blorp->driver_ctx,
+         .debug_flag = DEBUG_BLORP,
+      },
       .key = cs_key,
       .prog_data = cs_prog_data,
-      .log_data = blorp->driver_ctx,
-      .debug_flag = DEBUG_BLORP,
    };
 
-   const unsigned *program = brw_compile_cs(compiler, mem_ctx, &params);
+   const unsigned *program = brw_compile_cs(compiler, &params);
 
    ralloc_free(cs_prog_data->base.param);
    cs_prog_data->base.param = NULL;

src/intel/compiler/brw_compiler.h
@@ -1754,18 +1754,10 @@ brw_prog_data_size(gl_shader_stage stage);
 unsigned
 brw_prog_key_size(gl_shader_stage stage);
 
-/**
- * Parameters for compiling a vertex shader.
- *
- * Some of these will be modified during the shader compilation.
- */
-struct brw_compile_vs_params {
+struct brw_compile_params {
+   void *mem_ctx;
+
    nir_shader *nir;
-
-   const struct brw_vs_prog_key *key;
-   struct brw_vs_prog_data *prog_data;
-
-   bool edgeflag_is_last; /* true for gallium */
 
    struct brw_compile_stats *stats;
 
@@ -1773,8 +1765,21 @@ struct brw_compile_vs_params {
 
    char *error_str;
 
-   /* If unset, DEBUG_VS is used. */
    uint64_t debug_flag;
+};
+
+/**
+ * Parameters for compiling a vertex shader.
+ *
+ * Some of these will be modified during the shader compilation.
+ */
+struct brw_compile_vs_params {
+   struct brw_compile_params base;
+
+   const struct brw_vs_prog_key *key;
+   struct brw_vs_prog_data *prog_data;
+
+   bool edgeflag_is_last; /* true for gallium */
 };
 
 /**
@@ -1784,7 +1789,6 @@ struct brw_compile_vs_params {
  */
 const unsigned *
 brw_compile_vs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_vs_params *params);
 
 /**
@@ -1793,16 +1797,10 @@ brw_compile_vs(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_tcs_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_tcs_prog_key *key;
    struct brw_tcs_prog_data *prog_data;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
 };
 
 /**
@@ -1812,7 +1810,6 @@ struct brw_compile_tcs_params {
  */
 const unsigned *
 brw_compile_tcs(const struct brw_compiler *compiler,
-                void *mem_ctx,
                 struct brw_compile_tcs_params *params);
 
 /**
@@ -1821,17 +1818,11 @@ brw_compile_tcs(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_tes_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_tes_prog_key *key;
    struct brw_tes_prog_data *prog_data;
    const struct brw_vue_map *input_vue_map;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
 };
 
 /**
@@ -1841,7 +1832,6 @@ struct brw_compile_tes_params {
  */
 const unsigned *
 brw_compile_tes(const struct brw_compiler *compiler,
-                void *mem_ctx,
                 struct brw_compile_tes_params *params);
 
 /**
@@ -1850,16 +1840,10 @@ brw_compile_tes(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_gs_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_gs_prog_key *key;
    struct brw_gs_prog_data *prog_data;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
 };
 
 /**
@@ -1869,7 +1853,6 @@ struct brw_compile_gs_params {
  */
 const unsigned *
 brw_compile_gs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_gs_params *params);
 
 /**
@@ -1905,38 +1888,26 @@ brw_compile_clip(const struct brw_compiler *compiler,
                  unsigned *final_assembly_size);
 
 struct brw_compile_task_params {
-   struct nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_task_prog_key *key;
    struct brw_task_prog_data *prog_data;
-
-   struct brw_compile_stats *stats;
-
-   char *error_str;
-   void *log_data;
 };
 
 const unsigned *
 brw_compile_task(const struct brw_compiler *compiler,
-                 void *mem_ctx,
                  struct brw_compile_task_params *params);
 
 struct brw_compile_mesh_params {
-   struct nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_mesh_prog_key *key;
    struct brw_mesh_prog_data *prog_data;
    const struct brw_tue_map *tue_map;
-
-   struct brw_compile_stats *stats;
-
-   char *error_str;
-   void *log_data;
 };
 
 const unsigned *
 brw_compile_mesh(const struct brw_compiler *compiler,
-                 void *mem_ctx,
                  struct brw_compile_mesh_params *params);
 
 /**
@@ -1945,7 +1916,7 @@ brw_compile_mesh(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_fs_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_wm_prog_key *key;
    struct brw_wm_prog_data *prog_data;
@@ -1955,15 +1926,6 @@ struct brw_compile_fs_params {
 
    bool allow_spilling;
    bool use_rep_send;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
-
-   /* If unset, DEBUG_WM is used. */
-   uint64_t debug_flag;
 };
 
 /**
@@ -1973,7 +1935,6 @@ struct brw_compile_fs_params {
  */
 const unsigned *
 brw_compile_fs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_fs_params *params);
 
 /**
@@ -1982,19 +1943,10 @@ brw_compile_fs(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_cs_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_cs_prog_key *key;
    struct brw_cs_prog_data *prog_data;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
-
-   /* If unset, DEBUG_CS is used. */
-   uint64_t debug_flag;
 };
 
 /**
@@ -2004,7 +1956,6 @@ struct brw_compile_cs_params {
  */
 const unsigned *
 brw_compile_cs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_cs_params *params);
 
 /**
@@ -2013,19 +1964,13 @@ brw_compile_cs(const struct brw_compiler *compiler,
  * Some of these will be modified during the shader compilation.
  */
 struct brw_compile_bs_params {
-   nir_shader *nir;
+   struct brw_compile_params base;
 
    const struct brw_bs_prog_key *key;
    struct brw_bs_prog_data *prog_data;
 
    unsigned num_resume_shaders;
    struct nir_shader **resume_shaders;
-
-   struct brw_compile_stats *stats;
-
-   void *log_data;
-
-   char *error_str;
 };
 
 /**
@@ -2035,7 +1980,6 @@ struct brw_compile_bs_params {
  */
 const unsigned *
 brw_compile_bs(const struct brw_compiler *compiler,
-               void *mem_ctx,
                struct brw_compile_bs_params *params);
 
 /**

commit d62e494b370084298761f2e110b511c5c7e0f6a6

intel/vec4: fix log_data pointer
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 3384f029be ("intel/compiler: rework input parameters")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9421
Acked-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24307>

src/intel/compiler/brw_vec4.cpp
@@ -2495,7 +2495,7 @@ vec4_visitor::run()
    bool allocated_without_spills = reg_allocate();
 
    if (!allocated_without_spills) {
-      brw_shader_perf_log(compiler, params->log_data,
+      brw_shader_perf_log(compiler, log_data,
                           "%s shader triggered register spilling.  "
                           "Try reducing the number of live vec4 values "
                           "to improve performance.\n",

src/intel/compiler/brw_vec4.h
@@ -342,8 +342,6 @@ private:
    const bool no_spills;
 
    unsigned last_scratch; /**< measured in 32-byte (register size) units */
-
-   const struct brw_compile_params *params;
 };
 
 } /* namespace brw */