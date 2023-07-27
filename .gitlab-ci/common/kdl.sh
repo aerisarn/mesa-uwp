38d1e73868c51fd73c8a9348b221f8ab0175588e
David Heidelberg <david.heidelberg@collabora.com>
2023-07-27 16:16:48 +0300

    ci/kdl: remove extra-verbose ls command
    Not needed for non-debugging builds.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9422
    
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24348>

--- .gitlab-ci/common/kdl.sh
+++ .gitlab-ci/common/kdl.sh
@@ -14,14 +14,10 @@ trap terminate SIGTERM
 
 if [ -f /ci-kdl.venv/bin/activate ]; then
   source /ci-kdl.venv/bin/activate
-  echo -e "Launch ci-kdl"
   /ci-kdl.venv/bin/python /ci-kdl.venv/bin/ci-kdl | tee -a /results/kdl.log &
   child=$!
   wait $child
-  ls -l
   mv kdl_*.json /results/kdl.json
-  echo -e "ci-kdl json file moved to /results"
-  ls -ls /results
 else
   echo -e "Not possible to activate ci-kdl virtual environment"
 fi
