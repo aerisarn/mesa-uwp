aerisarn/mesa-uwp

commit 9ce717ab31f24faf0a15ba09e8f3efcd78929508

ci_run_n_monitor: add ability to specify the pipeline to use, instead of auto-detecting it
The auto-detection code currently looks for a repo called "mesa" in the
current user's fork (ie. the user providing the api token), which is great for
the common use case, but sometimes needs to be able to be overridden, such as
when running a pipeline in another fork than one's own, when working with
someone else in their fork.

Signed-off-by: Eric Engestrom <eric@igalia.com>
Acked-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23230>

bin/ci/ci_run_n_monitor.py
@@ -26,6 +26,8 @@ from colorama import Fore, Style
 from gitlab_common import get_gitlab_project, read_token, wait_for_pipeline
 from gitlab_gql import GitlabGQL, create_job_needs_dag, filter_dag, print_dag
 
+GITLAB_URL = "https://gitlab.freedesktop.org"
+
 REFRESH_WAIT_LOG = 10
 REFRESH_WAIT_JOBS = 6
 
@@ -240,6 +242,10 @@ def parse_args() -> None:
         "--rev", metavar="revision", help="repository git revision (default: HEAD)"
     )
     parser.add_argument(
+        "--pipeline-url",
+        help="URL of the pipeline to use, instead of auto-detecting it.",
+    )
+    parser.add_argument(
         "--token",
         metavar="token",
         help="force GitLab token, otherwise it's read from ~/.config/gitlab-token",
@@ -277,18 +283,30 @@ if __name__ == "__main__":
 
         token = read_token(args.token)
 
-        gl = gitlab.Gitlab(url="https://gitlab.freedesktop.org",
+        gl = gitlab.Gitlab(url=GITLAB_URL,
                            private_token=token,
                            retry_transient_errors=True)
-
-        cur_project = get_gitlab_project(gl, "mesa")
 
         REV: str = args.rev
         if not REV:
             REV = check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
         print(f"Revision: {REV}")
-        pipe = wait_for_pipeline(cur_project, REV)
+
+        if args.pipeline_url:
+            assert args.pipeline_url.startswith(GITLAB_URL)
+            url_path = args.pipeline_url[len(GITLAB_URL):]
+            url_path_components = url_path.split("/")
+            project_name = "/".join(url_path_components[1:3])
+            assert url_path_components[3] == "-"
+            assert url_path_components[4] == "pipelines"
+            pipeline_id = int(url_path_components[5])
+            cur_project = gl.projects.get(project_name)
+            pipe = cur_project.pipelines.get(pipeline_id)
+        else:
+            cur_project = get_gitlab_project(gl, "mesa")
+            pipe = wait_for_pipeline(cur_project, REV)
         print(f"Pipeline: {pipe.web_url}")
+
         deps = set()
         if args.target:
             print("🞋 job: " + Fore.BLUE + args.target + Style.RESET_ALL)

commit 42021b4d13fe849bbe0c2986329d899c9f993bde

bin/ci_run_n_monitor: get git sha from pipeline if specified, instead of requiring --rev to match
Fixes: 9ce717ab31f24faf0a15 ("ci_run_n_monitor: add ability to specify the pipeline to use, instead of auto-detecting it")
Signed-off-by: Eric Engestrom <eric@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24366>

bin/ci/ci_run_n_monitor.py
@@ -240,13 +240,6 @@ def parse_args() -> None:
     )
     parser.add_argument("--target", metavar="target-job", help="Target job")
     parser.add_argument(
-        "--rev", metavar="revision", help="repository git revision (default: HEAD)"
-    )
-    parser.add_argument(
-        "--pipeline-url",
-        help="URL of the pipeline to use, instead of auto-detecting it.",
-    )
-    parser.add_argument(
         "--token",
         metavar="token",
         help="force GitLab token, otherwise it's read from ~/.config/gitlab-token",
@@ -255,6 +248,16 @@ def parse_args() -> None:
         "--force-manual", action="store_true", help="Force jobs marked as manual"
     )
     parser.add_argument("--stress", action="store_true", help="Stresstest job(s)")
+
+    mutex_group1 = parser.add_mutually_exclusive_group()
+    mutex_group1.add_argument(
+        "--rev", metavar="revision", help="repository git revision (default: HEAD)"
+    )
+    mutex_group1.add_argument(
+        "--pipeline-url",
+        help="URL of the pipeline to use, instead of auto-detecting it.",
+    )
+
     return parser.parse_args()
 
 
@@ -289,9 +292,6 @@ if __name__ == "__main__":
                            retry_transient_errors=True)
 
         REV: str = args.rev
-        if not REV:
-            REV = check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
-        print(f"Revision: {REV}")
 
         if args.pipeline_url:
             assert args.pipeline_url.startswith(GITLAB_URL)
@@ -303,9 +303,14 @@ if __name__ == "__main__":
             pipeline_id = int(url_path_components[5])
             cur_project = gl.projects.get(project_name)
             pipe = cur_project.pipelines.get(pipeline_id)
+            REV = pipe.sha
         else:
             cur_project = get_gitlab_project(gl, "mesa")
+            if not REV:
+                REV = check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
             pipe = wait_for_pipeline(cur_project, REV)
+
+        print(f"Revision: {REV}")
         print(f"Pipeline: {pipe.web_url}")
 
         deps = set()