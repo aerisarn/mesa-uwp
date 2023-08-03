ced762a8d30a8916fad93b41f8607f1eae2a4511
David Heidelberg <david.heidelberg@collabora.com>
2023-08-03 08:11:07 +0300

    bin/ci: handle errors more gracefully in update_traces_checksum script
    UnicodeDecodeError is present very often, since GitLab logs are scary.
    
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24444>

--- bin/ci/update_traces_checksum.py
+++ bin/ci/update_traces_checksum.py
@@ -70,7 +70,7 @@ def gather_results(
 
                 # parse artifact
                 results_json_bz2 = cur_job.artifact(path="results/results.json.bz2", streamed=False)
-                results_json = bz2.decompress(results_json_bz2).decode("utf-8")
+                results_json = bz2.decompress(results_json_bz2).decode("utf-8", errors="replace")
                 results = json.loads(results_json)
 
                 for _, value in results["tests"].items():
