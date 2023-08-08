c4874b4cee6925f3faa2d68e780c7ea0d16c7ba8
Emma Anholt <emma@anholt.net>
2023-08-08 09:53:16 -0700

    freedreno/a5xx: Set num_sp_cores and set PC/VFD_POWER_CNTL accordingly.
    Based on libwrap tracing of the blob.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24358>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -232,8 +232,36 @@ add_gpus([
         GPUId(506),
         GPUId(508),
         GPUId(509),
+    ], GPUInfo(
+        CHIP.A5XX,
+        gmem_align_w = 64,  gmem_align_h = 32,
+        tile_align_w = 64,  tile_align_h = 32,
+        tile_max_w   = 1024, # max_bitfield_val(7, 0, 5)
+        tile_max_h   = max_bitfield_val(16, 9, 5),
+        num_vsc_pipes = 16,
+        cs_shared_mem_size = 32 * 1024,
+        num_sp_cores = 1,
+        wave_granularity = 2,
+        fibers_per_sp = 64 * 16, # Lowest number that didn't fault on spillall fs-varying-array-mat4-col-row-rd.
+    ))
+
+add_gpus([
         GPUId(510),
         GPUId(512),
+    ], GPUInfo(
+        CHIP.A5XX,
+        gmem_align_w = 64,  gmem_align_h = 32,
+        tile_align_w = 64,  tile_align_h = 32,
+        tile_max_w   = 1024, # max_bitfield_val(7, 0, 5)
+        tile_max_h   = max_bitfield_val(16, 9, 5),
+        num_vsc_pipes = 16,
+        cs_shared_mem_size = 32 * 1024,
+        num_sp_cores = 2,
+        wave_granularity = 2,
+        fibers_per_sp = 64 * 16, # Lowest number that didn't fault on spillall fs-varying-array-mat4-col-row-rd.
+    ))
+
+add_gpus([
         GPUId(530),
         GPUId(540),
     ], GPUInfo(
@@ -244,7 +272,7 @@ add_gpus([
         tile_max_h   = max_bitfield_val(16, 9, 5),
         num_vsc_pipes = 16,
         cs_shared_mem_size = 32 * 1024,
-        num_sp_cores = 0, # TODO
+        num_sp_cores = 4,
         wave_granularity = 2,
         fibers_per_sp = 0, # TODO
     ))
