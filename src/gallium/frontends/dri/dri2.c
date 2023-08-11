aerisarn/mesa-uwp

commit 26e3be513dc855f235ee12c8ebc0e7d1a82cd4cf

gallium/st: add support for PIPE_FORMAT_NV21 and PIPE_FORMAT_G8_B8R8_420
Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21219>

src/gallium/frontends/dri/dri2.c
@@ -859,6 +859,16 @@ static const struct dri2_format_mapping r8_g8b8_mapping = {
      { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } }
 };
 
+static const struct dri2_format_mapping g8_b8r8_mapping = {
+   DRM_FORMAT_NV21,
+   __DRI_IMAGE_FORMAT_NONE,
+   __DRI_IMAGE_COMPONENTS_Y_UV,
+   PIPE_FORMAT_G8_B8R8_420_UNORM,
+   2,
+   { { 0, 0, 0, __DRI_IMAGE_FORMAT_R8 },
+     { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } }
+};
+
 static const struct dri2_format_mapping r8g8_r8b8_mapping = {
    DRM_FORMAT_YUYV,
    __DRI_IMAGE_FORMAT_NONE,
@@ -905,6 +915,14 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
        pscreen->is_format_supported(pscreen, PIPE_FORMAT_R8_G8B8_420_UNORM,
                                     screen->target, 0, 0, PIPE_BIND_SAMPLER_VIEW)) {
       map = &r8_g8b8_mapping;
+      tex_usage |= PIPE_BIND_SAMPLER_VIEW;
+   }
+
+   /* For NV21, see if we have support for sampling g8_b8r8 */
+   if (!tex_usage && map->pipe_format == PIPE_FORMAT_NV21 &&
+       pscreen->is_format_supported(pscreen, PIPE_FORMAT_G8_B8R8_420_UNORM,
+                                    screen->target, 0, 0, PIPE_BIND_SAMPLER_VIEW)) {
+      map = &g8_b8r8_mapping;
       tex_usage |= PIPE_BIND_SAMPLER_VIEW;
    }
 

src/mesa/state_tracker/st_atom_sampler.c
@@ -269,6 +269,11 @@ update_shader_samplers(struct st_context *st,
             /* no additional views needed */
             break;
          FALLTHROUGH;
+      case PIPE_FORMAT_NV21:
+         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM)
+            /* no additional views needed */
+            break;
+         FALLTHROUGH;
       case PIPE_FORMAT_P010:
       case PIPE_FORMAT_P012:
       case PIPE_FORMAT_P016:

src/mesa/state_tracker/st_atom_texture.c
@@ -186,6 +186,18 @@ st_get_sampler_views(struct st_context *st,
          sampler_views[extra] =
                pipe->create_sampler_view(pipe, stObj->pt->next, &tmpl);
          break;
+      case PIPE_FORMAT_NV21:
+         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM)
+            /* no additional views needed */
+            break;
+
+         /* we need one additional R8G8 view: */
+         tmpl.format = PIPE_FORMAT_RG88_UNORM;
+         tmpl.swizzle_g = PIPE_SWIZZLE_Y;   /* tmpl from Y plane is R8 */
+         extra = u_bit_scan(&free_slots);
+         sampler_views[extra] =
+               pipe->create_sampler_view(pipe, stObj->pt->next, &tmpl);
+         break;
       case PIPE_FORMAT_P010:
       case PIPE_FORMAT_P012:
       case PIPE_FORMAT_P016:

src/mesa/state_tracker/st_cb_eglimage.c
@@ -63,6 +63,7 @@ is_format_supported(struct pipe_screen *screen, enum pipe_format format,
                                                  nr_storage_samples, usage);
          break;
       case PIPE_FORMAT_NV12:
+      case PIPE_FORMAT_NV21:
          supported = screen->is_format_supported(screen, PIPE_FORMAT_R8_UNORM,
                                                  PIPE_TEXTURE_2D, nr_samples,
                                                  nr_storage_samples, usage) &&
@@ -149,6 +150,17 @@ is_nv12_as_r8_g8b8_supported(struct pipe_screen *screen, struct st_egl_image *ou
    if (out->format == PIPE_FORMAT_NV12 &&
        out->texture->format == PIPE_FORMAT_R8_G8B8_420_UNORM &&
        screen->is_format_supported(screen, PIPE_FORMAT_R8_G8B8_420_UNORM,
+                                   PIPE_TEXTURE_2D,
+                                   out->texture->nr_samples,
+                                   out->texture->nr_storage_samples,
+                                   usage)) {
+      *native_supported = false;
+      return true;
+   }
+
+   if (out->format == PIPE_FORMAT_NV21 &&
+       out->texture->format == PIPE_FORMAT_G8_B8R8_420_UNORM &&
+       screen->is_format_supported(screen, PIPE_FORMAT_G8_B8R8_420_UNORM,
                                    PIPE_TEXTURE_2D,
                                    out->texture->nr_samples,
                                    out->texture->nr_storage_samples,
@@ -301,7 +313,11 @@ st_bind_egl_image(struct gl_context *ctx,
    if (!native_supported) {
       switch (stimg->format) {
       case PIPE_FORMAT_NV12:
+      case PIPE_FORMAT_NV21:
          if (stimg->texture->format == PIPE_FORMAT_R8_G8B8_420_UNORM) {
+            texFormat = MESA_FORMAT_R8G8B8X8_UNORM;
+            texObj->RequiredTextureImageUnits = 1;
+         } else if (stimg->texture->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
             texFormat = MESA_FORMAT_R8G8B8X8_UNORM;
             texObj->RequiredTextureImageUnits = 1;
          } else {

src/mesa/state_tracker/st_program.h
@@ -48,6 +48,7 @@ extern "C" {
 struct st_external_sampler_key
 {
    GLuint lower_nv12;             /**< bitmask of 2 plane YUV samplers */
+   GLuint lower_nv21;
    GLuint lower_iyuv;             /**< bitmask of 3 plane YUV samplers */
    GLuint lower_xy_uxvx;          /**< bitmask of 2 plane YUV samplers */
    GLuint lower_yx_xuxv;          /**< bitmask of 2 plane YUV samplers */
@@ -91,6 +92,13 @@ st_get_external_sampler_key(struct st_context *st, struct gl_program *prog)
       case PIPE_FORMAT_P016:
       case PIPE_FORMAT_P030:
          key.lower_nv12 |= (1 << unit);
+         break;
+      case PIPE_FORMAT_NV21:
+         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
+            key.lower_yuv |= (1 << unit);
+            break;
+         }
+         key.lower_nv21 |= (1 << unit);
          break;
       case PIPE_FORMAT_IYUV:
          key.lower_iyuv |= (1 << unit);

src/mesa/state_tracker/st_sampler_view.c
@@ -386,6 +386,12 @@ st_get_sampler_view_format(const struct st_context *st,
          break;
       }
       FALLTHROUGH;
+   case PIPE_FORMAT_NV21:
+      if (texObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
+         format = PIPE_FORMAT_G8_B8R8_420_UNORM;
+         break;
+      }
+      FALLTHROUGH;
    case PIPE_FORMAT_IYUV:
       format = PIPE_FORMAT_R8_UNORM;
       break;

src/gallium/auxiliary/vl/vl_video_buffer.c
@@ -76,6 +76,7 @@ vl_video_buffer_plane_order(enum pipe_format format)
       return const_resource_plane_order_YVU;
 
    case PIPE_FORMAT_NV12:
+   case PIPE_FORMAT_NV21:
    case PIPE_FORMAT_Y8_U8_V8_444_UNORM:
    case PIPE_FORMAT_R8G8B8A8_UNORM:
    case PIPE_FORMAT_R8G8B8X8_UNORM:

src/gallium/frontends/dri/dri_helpers.c
@@ -577,6 +577,10 @@ static const struct dri2_format_mapping dri2_format_table[] = {
         __DRI_IMAGE_COMPONENTS_Y_UV,      PIPE_FORMAT_NV12, 2,
         { { 0, 0, 0, __DRI_IMAGE_FORMAT_R8 },
           { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } } },
+      { DRM_FORMAT_NV21,          __DRI_IMAGE_FORMAT_NONE,
+        __DRI_IMAGE_COMPONENTS_Y_UV,      PIPE_FORMAT_NV21, 2,
+        { { 0, 0, 0, __DRI_IMAGE_FORMAT_R8 },
+          { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } } },
 
       { DRM_FORMAT_P010,          __DRI_IMAGE_FORMAT_NONE,
         __DRI_IMAGE_COMPONENTS_Y_UV,      PIPE_FORMAT_P010, 2,

commit 2dc883eb37f04eb0bfbbb3cda62687a18b9235de

gallium/st: lower NV21 to R8_B8G8 instead of G8_B8R8
When NV21 lowering with hardware sampling and shader CSC was added, the
incorrect PIPE_FORMAT_G8_B8R8_UNORM was used. That format is supposed to
represent vulkan NV12 instead.

This commit introduces PIPE_FORMAT_R8_B8G8_UNORM, which correctly describes the
gallium mapping for YUV CSC, with R as Y, instead of G as Y.

Fixes: 26e3be513dc ("gallium/st: add support for PIPE_FORMAT_NV21 and PIPE_FORMAT_G8_B8R8_420")
Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24266>

src/gallium/frontends/dri/dri2.c
@@ -890,11 +890,11 @@ static const struct dri2_format_mapping r8_g8b8_mapping = {
      { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } }
 };
 
-static const struct dri2_format_mapping g8_b8r8_mapping = {
+static const struct dri2_format_mapping r8_b8g8_mapping = {
    DRM_FORMAT_NV21,
    __DRI_IMAGE_FORMAT_NONE,
    __DRI_IMAGE_COMPONENTS_Y_UV,
-   PIPE_FORMAT_G8_B8R8_420_UNORM,
+   PIPE_FORMAT_R8_B8G8_420_UNORM,
    2,
    { { 0, 0, 0, __DRI_IMAGE_FORMAT_R8 },
      { 1, 1, 1, __DRI_IMAGE_FORMAT_GR88 } }
@@ -959,7 +959,7 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
                                     PIPE_BIND_SAMPLER_VIEW))
       tex_usage |= PIPE_BIND_SAMPLER_VIEW;
 
-   /* For NV12, see if we have support for sampling r8_b8g8 */
+   /* For NV12, see if we have support for sampling r8_g8b8 */
    if (!tex_usage && map->pipe_format == PIPE_FORMAT_NV12 &&
        pscreen->is_format_supported(pscreen, PIPE_FORMAT_R8_G8B8_420_UNORM,
                                     screen->target, 0, 0, PIPE_BIND_SAMPLER_VIEW)) {
@@ -967,11 +967,11 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
       tex_usage |= PIPE_BIND_SAMPLER_VIEW;
    }
 
-   /* For NV21, see if we have support for sampling g8_b8r8 */
+   /* For NV21, see if we have support for sampling r8_b8g8 */
    if (!tex_usage && map->pipe_format == PIPE_FORMAT_NV21 &&
-       pscreen->is_format_supported(pscreen, PIPE_FORMAT_G8_B8R8_420_UNORM,
+       pscreen->is_format_supported(pscreen, PIPE_FORMAT_R8_B8G8_420_UNORM,
                                     screen->target, 0, 0, PIPE_BIND_SAMPLER_VIEW)) {
-      map = &g8_b8r8_mapping;
+      map = &r8_b8g8_mapping;
       tex_usage |= PIPE_BIND_SAMPLER_VIEW;
    }
 

src/mesa/state_tracker/st_atom_sampler.c
@@ -270,7 +270,7 @@ update_shader_samplers(struct st_context *st,
             break;
          FALLTHROUGH;
       case PIPE_FORMAT_NV21:
-         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM)
+         if (stObj->pt->format == PIPE_FORMAT_R8_B8G8_420_UNORM)
             /* no additional views needed */
             break;
          FALLTHROUGH;

src/mesa/state_tracker/st_atom_texture.c
@@ -187,7 +187,7 @@ st_get_sampler_views(struct st_context *st,
                pipe->create_sampler_view(pipe, stObj->pt->next, &tmpl);
          break;
       case PIPE_FORMAT_NV21:
-         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM)
+         if (stObj->pt->format == PIPE_FORMAT_R8_B8G8_420_UNORM)
             /* no additional views needed */
             break;
 

src/mesa/state_tracker/st_cb_eglimage.c
@@ -181,8 +181,8 @@ is_nv12_as_r8_g8b8_supported(struct pipe_screen *screen, struct st_egl_image *ou
    }
 
    if (out->format == PIPE_FORMAT_NV21 &&
-       out->texture->format == PIPE_FORMAT_G8_B8R8_420_UNORM &&
-       screen->is_format_supported(screen, PIPE_FORMAT_G8_B8R8_420_UNORM,
+       out->texture->format == PIPE_FORMAT_R8_B8G8_420_UNORM &&
+       screen->is_format_supported(screen, PIPE_FORMAT_R8_B8G8_420_UNORM,
                                    PIPE_TEXTURE_2D,
                                    out->texture->nr_samples,
                                    out->texture->nr_storage_samples,
@@ -366,10 +366,8 @@ st_bind_egl_image(struct gl_context *ctx,
       switch (stimg->format) {
       case PIPE_FORMAT_NV12:
       case PIPE_FORMAT_NV21:
-         if (stimg->texture->format == PIPE_FORMAT_R8_G8B8_420_UNORM) {
-            texFormat = MESA_FORMAT_R8G8B8X8_UNORM;
-            texObj->RequiredTextureImageUnits = 1;
-         } else if (stimg->texture->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
+         if (stimg->texture->format == PIPE_FORMAT_R8_G8B8_420_UNORM ||
+             stimg->texture->format == PIPE_FORMAT_R8_B8G8_420_UNORM) {
             texFormat = MESA_FORMAT_R8G8B8X8_UNORM;
             texObj->RequiredTextureImageUnits = 1;
          } else {

src/mesa/state_tracker/st_program.h
@@ -97,7 +97,7 @@ st_get_external_sampler_key(struct st_context *st, struct gl_program *prog)
          key.lower_nv12 |= (1 << unit);
          break;
       case PIPE_FORMAT_NV21:
-         if (stObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
+         if (stObj->pt->format == PIPE_FORMAT_R8_B8G8_420_UNORM) {
             key.lower_yuv |= (1 << unit);
             break;
          }

src/mesa/state_tracker/st_sampler_view.c
@@ -387,8 +387,8 @@ st_get_sampler_view_format(const struct st_context *st,
       }
       FALLTHROUGH;
    case PIPE_FORMAT_NV21:
-      if (texObj->pt->format == PIPE_FORMAT_G8_B8R8_420_UNORM) {
-         format = PIPE_FORMAT_G8_B8R8_420_UNORM;
+      if (texObj->pt->format == PIPE_FORMAT_R8_B8G8_420_UNORM) {
+         format = PIPE_FORMAT_R8_B8G8_420_UNORM;
          break;
       }
       FALLTHROUGH;

src/panfrost/lib/pan_format.c
@@ -193,7 +193,7 @@ const struct panfrost_format GENX(panfrost_pipe_format)[PIPE_FORMAT_COUNT] = {
    FMT_YUV(R8B8_R8G8_UNORM, YUYV8, VYUA, NO_SWAP, CO_SITED, _T__),
    FMT_YUV(B8R8_G8R8_UNORM, VYUY8, VUYA, SWAP,    CO_SITED, _T__),
    FMT_YUV(R8_G8B8_420_UNORM, Y8_UV8_420, YUVA, NO_SWAP, CO_SITED, _T__),
-   FMT_YUV(G8_B8R8_420_UNORM, Y8_UV8_420, YVUA, NO_SWAP, CO_SITED, _T__),
+   FMT_YUV(R8_B8G8_420_UNORM, Y8_UV8_420, YVUA, NO_SWAP, CO_SITED, _T__),
    FMT_YUV(R8_G8_B8_420_UNORM, Y8_U8_V8_420, YUVA, NO_SWAP, CO_SITED, _T__),
    FMT_YUV(R8_B8_G8_420_UNORM, Y8_U8_V8_420, YVUA, NO_SWAP, CO_SITED, _T__),
 #endif

src/util/format/u_format_table.py
@@ -113,6 +113,7 @@ def has_access(format):
         'y16_u16v16_422_unorm',
         'y16_u16_v16_444_unorm',
         'r8_g8b8_420_unorm',
+        'r8_b8g8_420_unorm',
         'g8_b8r8_420_unorm',
         'r8_g8_b8_420_unorm',
         'r8_b8_g8_420_unorm',

src/util/format/u_formats.h
@@ -511,6 +511,7 @@ enum pipe_format {
    PIPE_FORMAT_XYUV,
 
    PIPE_FORMAT_R8_G8B8_420_UNORM,
+   PIPE_FORMAT_R8_B8G8_420_UNORM,
    PIPE_FORMAT_G8_B8R8_420_UNORM,
    PIPE_FORMAT_R8_G8_B8_420_UNORM,
    PIPE_FORMAT_R8_B8_G8_420_UNORM,