c0d804685d241637f7231538050b5740be0b2c63
Sylvain Munaut <tnt@246tNt.com>
2023-08-11 14:02:31 +0200

    glx: Remove MESA_depth_float_bit from enum
    This is not used anywhere. Been there from the beginning of this
    file but with no related code in glxextensions.c
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>
    Acked-by: Antonio Gomes <antoniospg100@gmail.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24573>

--- src/glx/glxextensions.h
+++ src/glx/glxextensions.h
@@ -60,7 +60,6 @@ enum
    ATI_pixel_format_float_bit,
    INTEL_swap_event_bit,
    MESA_copy_sub_buffer_bit,
-   MESA_depth_float_bit,
    MESA_query_renderer_bit,
    MESA_swap_control_bit,
    MESA_swap_frame_usage_bit,
