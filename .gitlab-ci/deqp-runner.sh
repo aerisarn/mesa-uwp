aerisarn/mesa-uwp

commit d110299d77c9663bf3d91a50d447f8db4f64acb0

ci/deqp-runner: compress results.csv before uploading it to GitLab
The results.csv file can get ridiculously big for a vkcts run (up to
135MB). Given how seldomly this file is being used, and the fact that
it cannot be viewed directly in gitlab's artifact page anyway.

Let's compress the file using zstd, and enjoy a ~95% reduction in size
at the cost of probably less than 1 second of compression time on even
the slowest of the devices in CI (which would use sharing), and about
150ms on the CI machines in the Valve farm.

Suggested-by: Daniel Stone <daniels@collabora.com>
Acked-by: David Heidelberg <david.heidelberg@collabora.com>
Signed-off-by: Martin Roukala (né Peres) <martin.roukala@mupuf.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20669>

.gitlab-ci/deqp-runner.sh
@@ -230,6 +230,11 @@ if [ -n "$FLAKES_CHANNEL" ]; then
          --branch-title "${CI_MERGE_REQUEST_TITLE:-$CI_COMMIT_TITLE}"
 fi
 
+# Compress results.csv to save on bandwidth during the upload of artifacts to
+# GitLab. This reduces the size in a VKCTS run from 135 to 7.6MB, and takes
+# 0.17s on a Ryzen 5950X (16 threads, 0.95s when limited to 1 thread).
+zstd --rm -T0 -8qc $RESULTS/results.csv -o $RESULTS/results.csv.zst
+
 echo -e "\e[0Ksection_end:$(date +%s):test_post_process\r\e[0K"
 
 exit $DEQP_EXITCODE

commit 9bf104f9348d5e7a04268399a5d68523e886d487

ci/deqp: really remove the uncompressed results.csv file
Arguments are parsed in order, so the `-c` here causes that first `--rm`
won't get applied and then it gets overriden by `-o` which writes the file.

Sadly zstd won't report this as a issue, but that's being reported in [1],
so maybe in future versions zstd will warn about it.

[1] https://github.com/facebook/zstd/issues/3719

Fixes: d110299d77c9 ("ci/deqp-runner: compress results.csv before uploading it to GitLab")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24651>

.gitlab-ci/deqp-runner.sh
@@ -235,7 +235,7 @@ fi
 # Compress results.csv to save on bandwidth during the upload of artifacts to
 # GitLab. This reduces the size in a VKCTS run from 135 to 7.6MB, and takes
 # 0.17s on a Ryzen 5950X (16 threads, 0.95s when limited to 1 thread).
-zstd --rm -T0 -8qc $RESULTS/results.csv -o $RESULTS/results.csv.zst
+zstd --rm -T0 -8q "$RESULTS/results.csv" -o "$RESULTS/results.csv.zst"
 
 section_end test_post_process
 