aerisarn/mesa-uwp

commit b2dc63ed8ce95622312d7fa211def60f3673facd

aux/trace: Add pipe_context::set_hw_atomic_buffers
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20453>

src/gallium/auxiliary/driver_trace/tr_context.c
@@ -2300,6 +2300,28 @@ static void trace_context_set_global_binding(struct pipe_context *_pipe,
    trace_dump_call_end();
 }
 
+static void
+trace_context_set_hw_atomic_buffers(struct pipe_context *_pipe,
+                                    unsigned start_slot, unsigned count,
+                                    const struct pipe_shader_buffer *buffers)
+{
+   struct trace_context *tr_ctx = trace_context(_pipe);
+   struct pipe_context *pipe = tr_ctx->pipe;
+
+   trace_dump_call_begin("pipe_context", "set_global_binding");
+   trace_dump_arg(ptr, pipe);
+   trace_dump_arg(uint, start_slot);
+   trace_dump_arg(uint, count);
+
+   trace_dump_arg_begin("buffers");
+   trace_dump_struct_array(shader_buffer, buffers, count);
+   trace_dump_arg_end();
+
+   pipe->set_hw_atomic_buffers(pipe, start_slot, count, buffers);
+
+   trace_dump_call_end();
+}
+
 struct pipe_context *
 trace_context_create(struct trace_screen *tr_scr,
                      struct pipe_context *pipe)
@@ -2434,6 +2456,8 @@ trace_context_create(struct trace_screen *tr_scr,
    TR_CTX_INIT(set_context_param);
    TR_CTX_INIT(set_debug_callback);
    TR_CTX_INIT(set_global_binding);
+   TR_CTX_INIT(set_hw_atomic_buffers);
+
 
 #undef TR_CTX_INIT
 

commit 9d1da9ec20217c21338dc6f9e1077cf19ac2fb61

aux/trace: fix set_hw_atomic_buffers method name
Fixes: b2dc63ed8ce ("aux/trace: Add pipe_context::set_hw_atomic_buffers")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24482>

src/gallium/auxiliary/driver_trace/tr_context.c
@@ -2309,7 +2309,7 @@ trace_context_set_hw_atomic_buffers(struct pipe_context *_pipe,
    struct trace_context *tr_ctx = trace_context(_pipe);
    struct pipe_context *pipe = tr_ctx->pipe;
 
-   trace_dump_call_begin("pipe_context", "set_global_binding");
+   trace_dump_call_begin("pipe_context", "set_hw_atomic_buffers");
    trace_dump_arg(ptr, pipe);
    trace_dump_arg(uint, start_slot);
    trace_dump_arg(uint, count);