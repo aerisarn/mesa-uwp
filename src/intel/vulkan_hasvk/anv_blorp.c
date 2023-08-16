9231f24be1f5f52a1d64f2089808620dc7a146d2
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-08-16 11:53:24 +0300

    hasvk: add state cache invalidation back before fast clears
    Prior to 87149cc545, blorp added a state cache invalidation prior to
    fast clears. This got dropped on Hasvk.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: 87149cc545 ("blorp: update and move fast clear PIPE_CONTROLs to drivers")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24719>

--- src/intel/vulkan_hasvk/anv_blorp.c
+++ src/intel/vulkan_hasvk/anv_blorp.c
@@ -1824,6 +1824,12 @@ anv_image_mcs_op(struct anv_cmd_buffer *cmd_buffer,
                              ANV_PIPE_END_OF_PIPE_SYNC_BIT,
                              "before fast clear mcs");
 
+   if (!blorp_address_is_null(surf.clear_color_addr)) {
+      anv_add_pending_pipe_bits(cmd_buffer,
+                                ANV_PIPE_STATE_CACHE_INVALIDATE_BIT,
+                                "before blorp clear color edit");
+   }
+
    switch (mcs_op) {
    case ISL_AUX_OP_FAST_CLEAR:
       blorp_fast_clear(&batch, &surf, format, swizzle,
@@ -1911,6 +1917,12 @@ anv_image_ccs_op(struct anv_cmd_buffer *cmd_buffer,
                              ANV_PIPE_PSS_STALL_SYNC_BIT |
                              ANV_PIPE_END_OF_PIPE_SYNC_BIT,
                              "before fast clear ccs");
+
+   if (!blorp_address_is_null(surf.clear_color_addr)) {
+      anv_add_pending_pipe_bits(cmd_buffer,
+                                ANV_PIPE_STATE_CACHE_INVALIDATE_BIT,
+                                "before blorp clear color edit");
+   }
 
    switch (ccs_op) {
    case ISL_AUX_OP_FAST_CLEAR:
