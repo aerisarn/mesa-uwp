yuq/mesa-lima

commit b5f9820d905a275bc01bbffa9b4927ec11286f8d

mesa_glinterop: remove unneeded GLAPI/GLAPIENTRY/APIENTRYP symbols
These come from windows.h, gl.h, glcorearb.h and/or glext.h.

The interop interface is aimed at non-Windows platforms while the macros
are used/derived due to Windows specifics. Thus we can safely remove
them.

Strictly speaking there should be GLXAPIENTRY/EGLAPIENTRY and alike
macros, although a) there is no GLX ones and b) this brings us even
further from decoupling the file from the GLX/EGL header dependency.

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Tested-by: Tom Stellard <thomas.stellard@amd.com>

include/GL/mesa_glinterop.h
@@ -229,7 +229,7 @@ typedef struct _mesa_glinterop_export_out {
  *
  * \return MESA_GLINTEROP_SUCCESS or MESA_GLINTEROP_* != 0 on error
  */
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropGLXQueryDeviceInfo(Display *dpy, GLXContext context,
                                 mesa_glinterop_device_info *out);
 
@@ -238,7 +238,7 @@ MesaGLInteropGLXQueryDeviceInfo(Display *dpy, GLXContext context,
  * Same as MesaGLInteropGLXQueryDeviceInfo except that it accepts EGLDisplay
  * and EGLContext.
  */
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropEGLQueryDeviceInfo(EGLDisplay dpy, EGLContext context,
                                 mesa_glinterop_device_info *out);
 
@@ -254,7 +254,7 @@ MesaGLInteropEGLQueryDeviceInfo(EGLDisplay dpy, EGLContext context,
  *
  * \return MESA_GLINTEROP_SUCCESS or MESA_GLINTEROP_* != 0 on error
  */
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropGLXExportObject(Display *dpy, GLXContext context,
                              const mesa_glinterop_export_in *in,
                              mesa_glinterop_export_out *out);
@@ -264,22 +264,22 @@ MesaGLInteropGLXExportObject(Display *dpy, GLXContext context,
  * Same as MesaGLInteropGLXExportObject except that it accepts
  * EGLDisplay and EGLContext.
  */
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropEGLExportObject(EGLDisplay dpy, EGLContext context,
                              const mesa_glinterop_export_in *in,
                              mesa_glinterop_export_out *out);
 
 
-typedef int (APIENTRYP PFNMESAGLINTEROPGLXQUERYDEVICEINFOPROC)(Display *dpy, GLXContext context,
-                                                               mesa_glinterop_device_info *out);
-typedef int (APIENTRYP PFNMESAGLINTEROPEGLQUERYDEVICEINFOPROC)(EGLDisplay dpy, EGLContext context,
-                                                               mesa_glinterop_device_info *out);
-typedef int (APIENTRYP PFNMESAGLINTEROPGLXEXPORTOBJECTPROC)(Display *dpy, GLXContext context,
-                                                            const mesa_glinterop_export_in *in,
-                                                            mesa_glinterop_export_out *out);
-typedef int (APIENTRYP PFNMESAGLINTEROPEGLEXPORTOBJECTPROC)(EGLDisplay dpy, EGLContext context,
-                                                            const mesa_glinterop_export_in *in,
-                                                            mesa_glinterop_export_out *out);
+typedef int (PFNMESAGLINTEROPGLXQUERYDEVICEINFOPROC)(Display *dpy, GLXContext context,
+                                                     mesa_glinterop_device_info *out);
+typedef int (PFNMESAGLINTEROPEGLQUERYDEVICEINFOPROC)(EGLDisplay dpy, EGLContext context,
+                                                     mesa_glinterop_device_info *out);
+typedef int (PFNMESAGLINTEROPGLXEXPORTOBJECTPROC)(Display *dpy, GLXContext context,
+                                                  const mesa_glinterop_export_in *in,
+                                                  mesa_glinterop_export_out *out);
+typedef int (PFNMESAGLINTEROPEGLEXPORTOBJECTPROC)(EGLDisplay dpy, EGLContext context,
+                                                  const mesa_glinterop_export_in *in,
+                                                  mesa_glinterop_export_out *out);
 
 #ifdef __cplusplus
 }

src/egl/main/eglapi.c
@@ -1937,7 +1937,7 @@ _eglLockDisplayInterop(EGLDisplay dpy, EGLContext context,
    return MESA_GLINTEROP_SUCCESS;
 }
 
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropEGLQueryDeviceInfo(EGLDisplay dpy, EGLContext context,
                                 mesa_glinterop_device_info *out)
 {
@@ -1959,7 +1959,7 @@ MesaGLInteropEGLQueryDeviceInfo(EGLDisplay dpy, EGLContext context,
    return ret;
 }
 
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropEGLExportObject(EGLDisplay dpy, EGLContext context,
                              const mesa_glinterop_export_in *in,
                              mesa_glinterop_export_out *out)

src/glx/glxcmds.c
@@ -2695,7 +2695,7 @@ __glXGetUST(int64_t * ust)
 
 #if defined(GLX_DIRECT_RENDERING) && !defined(GLX_USE_APPLEGL)
 
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropGLXQueryDeviceInfo(Display *dpy, GLXContext context,
                                 mesa_glinterop_device_info *out)
 {
@@ -2719,7 +2719,7 @@ MesaGLInteropGLXQueryDeviceInfo(Display *dpy, GLXContext context,
    return ret;
 }
 
-GLAPI int GLAPIENTRY
+int
 MesaGLInteropGLXExportObject(Display *dpy, GLXContext context,
                              const mesa_glinterop_export_in *in,
                              mesa_glinterop_export_out *out)

aerisarn/mesa-uwp

commit 798a7ef698bfafccf0d345aaca907ee8eb63b714

include: Fix the PFN declarations to be pointers as they should
Broken by b5f9820d905a275bc01bbffa9b4927ec11286f8d back in 2016.

Signed-off-by: Sylvain Munaut <tnt@246tNt.com>
Acked-by: Antonio Gomes <antoniospg100@gmail.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Reviewed-by: Eric Engestrom <eric@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24573>

include/GL/mesa_glinterop.h
@@ -372,30 +372,30 @@ wglMesaGLInteropFlushObjects(HDC dpy, HGLRC context,
                              GLsync *sync);
 
 
-typedef int (PFNMESAGLINTEROPGLXQUERYDEVICEINFOPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
-                                                     struct mesa_glinterop_device_info *out);
-typedef int (PFNMESAGLINTEROPEGLQUERYDEVICEINFOPROC)(EGLDisplay dpy, EGLContext context,
-                                                     struct mesa_glinterop_device_info *out);
-typedef int (PFNWGLMESAGLINTEROPQUERYDEVICEINFOPROC)(HDC dpy, HGLRC context,
-                                                     struct mesa_glinterop_device_info *out);
-typedef int (PFNMESAGLINTEROPGLXEXPORTOBJECTPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
-                                                  struct mesa_glinterop_export_in *in,
-                                                  struct mesa_glinterop_export_out *out);
-typedef int (PFNMESAGLINTEROPEGLEXPORTOBJECTPROC)(EGLDisplay dpy, EGLContext context,
-                                                  struct mesa_glinterop_export_in *in,
-                                                  struct mesa_glinterop_export_out *out);
-typedef int (PFNWGLMESAGLINTEROPEXPORTOBJECTPROC)(HDC dpy, HGLRC context,
-                                                  struct mesa_glinterop_export_in *in,
-                                                  struct mesa_glinterop_export_out *out);
-typedef int (PFNMESAGLINTEROPGLXFLUSHOBJECTSPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
-                                                  unsigned count, struct mesa_glinterop_export_in *resources,
-                                                  GLsync *sync);
-typedef int (PFNMESAGLINTEROPEGLFLUSHOBJECTSPROC)(EGLDisplay dpy, EGLContext context,
-                                                  unsigned count, struct mesa_glinterop_export_in *resources,
-                                                  GLsync *sync);
-typedef int (PFNWGLMESAGLINTEROPFLUSHOBJECTSPROC)(HDC dpy, HGLRC context,
-                                                  unsigned count, struct mesa_glinterop_export_in *resources,
-                                                  GLsync *sync);
+typedef int (*PFNMESAGLINTEROPGLXQUERYDEVICEINFOPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
+                                                      struct mesa_glinterop_device_info *out);
+typedef int (*PFNMESAGLINTEROPEGLQUERYDEVICEINFOPROC)(EGLDisplay dpy, EGLContext context,
+                                                      struct mesa_glinterop_device_info *out);
+typedef int (*PFNWGLMESAGLINTEROPQUERYDEVICEINFOPROC)(HDC dpy, HGLRC context,
+                                                      struct mesa_glinterop_device_info *out);
+typedef int (*PFNMESAGLINTEROPGLXEXPORTOBJECTPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
+                                                   struct mesa_glinterop_export_in *in,
+                                                   struct mesa_glinterop_export_out *out);
+typedef int (*PFNMESAGLINTEROPEGLEXPORTOBJECTPROC)(EGLDisplay dpy, EGLContext context,
+                                                   struct mesa_glinterop_export_in *in,
+                                                   struct mesa_glinterop_export_out *out);
+typedef int (*PFNWGLMESAGLINTEROPEXPORTOBJECTPROC)(HDC dpy, HGLRC context,
+                                                   struct mesa_glinterop_export_in *in,
+                                                   struct mesa_glinterop_export_out *out);
+typedef int (*PFNMESAGLINTEROPGLXFLUSHOBJECTSPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
+                                                   unsigned count, struct mesa_glinterop_export_in *resources,
+                                                   GLsync *sync);
+typedef int (*PFNMESAGLINTEROPEGLFLUSHOBJECTSPROC)(EGLDisplay dpy, EGLContext context,
+                                                   unsigned count, struct mesa_glinterop_export_in *resources,
+                                                   GLsync *sync);
+typedef int (*PFNWGLMESAGLINTEROPFLUSHOBJECTSPROC)(HDC dpy, HGLRC context,
+                                                   unsigned count, struct mesa_glinterop_export_in *resources,
+                                                   GLsync *sync);
 
 #ifdef __cplusplus
 }