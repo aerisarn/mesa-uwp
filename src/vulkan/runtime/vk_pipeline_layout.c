054188bff821f490b157dc0a4740d72793b97212
Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
2023-08-19 23:33:10 +0300

    vulkan: Fix pipeline layout allocation scope
    The lifetime of Mesa's vk_pipeline_layout may exceed that of the
    VkPipelineLayout object as other objects on the device may hold references
    to it. In other places in vk_pipeline_layout and vk_descriptor_set_layout,
    the device allocation scope is used with this pattern, but there was an
    inconsistency in vk_pipeline_layout_zalloc, which is fixed by this commit.
    
    Reviewed-by: Karmjit Mahil <Karmjit.Mahil@imgtec.com>
    Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24794>

--- src/vulkan/runtime/vk_pipeline_layout.c
+++ src/vulkan/runtime/vk_pipeline_layout.c
@@ -66,7 +66,7 @@ vk_pipeline_layout_zalloc(struct vk_device *device, size_t size,
     * their own object.
     */
    struct vk_pipeline_layout *layout =
-      vk_zalloc(&device->alloc, size, 8, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+      vk_zalloc(&device->alloc, size, 8, VK_SYSTEM_ALLOCATION_SCOPE_DEVICE);
    if (layout == NULL)
       return NULL;
 
