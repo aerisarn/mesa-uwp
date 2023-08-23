aerisarn/mesa-uwp

commit 37fcbb375cb14d0cddd003874b4ddd45c115c6df

blorp: Disable unaligned partial HIZ fast clears for HIZ_CCS too.
Fixes MSAA scissored fast clears under zink and ANGLE.

Fixes: e488773b29d9 ("anv: Fast clear depth/stencil surface in vkCmdClearAttachments")
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24225>

src/intel/blorp/blorp_clear.c
@@ -968,7 +968,7 @@ blorp_can_hiz_clear_depth(const struct intel_device_info *devinfo,
        */
       if (x0 % 8 || y0 % 4 || x1 % 8 || y1 % 4)
          return false;
-   } else if (aux_usage == ISL_AUX_USAGE_HIZ_CCS_WT) {
+   } else if (isl_aux_usage_has_ccs(aux_usage)) {
       /* We have to set the WM_HZ_OP::FullSurfaceDepthandStencilClear bit
        * whenever we clear an uninitialized HIZ buffer (as some drivers
        * currently do). However, this bit seems liable to clear 16x8 pixels in

commit 839b03cc06d63739b702e00a6f29472e7a0c058a

blorp: Drop unnecessary assertions in blorp_can_hiz_clear_depth
We already checks for the alignment and the multislice surface, we don't
need to add assertions around those two.

fixes: 37fcbb375cb ("blorp: Disable unaligned partial HIZ fast clears for HIZ_CCS too")
closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9684

Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Tested-by: Mark Janes <markjanes@swizzler.org>
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24837>

src/intel/blorp/blorp_clear.c
@@ -972,13 +972,10 @@ blorp_can_hiz_clear_depth(const struct intel_device_info *devinfo,
       /* We have to set the WM_HZ_OP::FullSurfaceDepthandStencilClear bit
        * whenever we clear an uninitialized HIZ buffer (as some drivers
        * currently do). However, this bit seems liable to clear 16x8 pixels in
-       * the ZCS on Gfx12 - greater than the slice alignments for depth
+       * the ZCS on Gfx12 - greater than the slice alignments of many depth
        * buffers.
-       */
-      assert(surf->image_alignment_el.w % 16 != 0 ||
-             surf->image_alignment_el.h % 8 != 0);
-
-      /* This is the hypothesis behind some corruption that was seen with the
+       *
+       * This is the hypothesis behind some corruption that was seen with the
        * amd_vertex_shader_layer-layered-depth-texture-render piglit test.
        *
        * From the Compressed Depth Buffers section of the Bspec, under the
@@ -1001,7 +998,6 @@ blorp_can_hiz_clear_depth(const struct intel_device_info *devinfo,
                                    surf->dim == ISL_SURF_DIM_3D ? 0 : layer,
                                    surf->dim == ISL_SURF_DIM_3D ? layer: 0,
                                    &slice_x0, &slice_y0, &slice_z0, &slice_a0);
-      assert(slice_z0 == 0 && slice_a0 == 0);
       const bool max_x1_y1 =
          x1 == u_minify(surf->logical_level0_px.width, level) &&
          y1 == u_minify(surf->logical_level0_px.height, level);