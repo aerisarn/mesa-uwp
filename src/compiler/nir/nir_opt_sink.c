561df40211b70f14b2fa407bad9a0ac3503c2e5c
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2023-08-24 07:14:28 -0400

    nir/opt_sink: Do not move derivatives
    At the moment, this does nothing. It will prevent problems from the next patch,
    however.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24833>

--- src/compiler/nir/nir_opt_sink.c
+++ src/compiler/nir/nir_opt_sink.c
@@ -44,10 +44,19 @@ nir_can_move_instr(nir_instr *instr, nir_move_options options)
       return options & nir_move_const_undef;
    }
    case nir_instr_type_alu: {
-      if (nir_op_is_vec_or_mov(nir_instr_as_alu(instr)->op) ||
-          nir_instr_as_alu(instr)->op == nir_op_b2i32)
+      nir_alu_instr *alu = nir_instr_as_alu(instr);
+
+      /* Derivatives cannot be moved into non-uniform control flow, including
+       * past a discard_if in the same block. Even if they could, sinking
+       * derivatives extends the lifetime of helper invocations which may be
+       * worse than the register pressure decrease. Bail on derivatives.
+       */
+      if (nir_op_is_derivative(alu->op))
+         return false;
+
+      if (nir_op_is_vec_or_mov(alu->op) || alu->op == nir_op_b2i32)
          return options & nir_move_copies;
-      if (nir_alu_instr_is_comparison(nir_instr_as_alu(instr)))
+      if (nir_alu_instr_is_comparison(alu))
          return options & nir_move_comparisons;
       return false;
    }
