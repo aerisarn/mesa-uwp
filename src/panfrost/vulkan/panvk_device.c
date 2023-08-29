78ec03b3ba65d221c78930e87616eef53f0fcf6c
David Heidelberg <david.heidelberg@collabora.com>
2023-08-29 14:02:03 +0200

    panvk: catch unsupported arch in the panvk_physical_device_init
    Suggested-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24201>

--- src/panfrost/vulkan/panvk_device.c
+++ src/panfrost/vulkan/panvk_device.c
@@ -449,7 +449,7 @@ panvk_physical_device_init(struct panvk_physical_device *device,
    panfrost_open_device(NULL, fd, &device->pdev);
    fd = -1;
 
-   if (device->pdev.arch <= 5) {
+   if (device->pdev.arch <= 5 || device->pdev.arch >= 8) {
       result = vk_errorf(instance, VK_ERROR_INCOMPATIBLE_DRIVER,
                          "%s not supported", device->pdev.model->name);
       goto fail;
