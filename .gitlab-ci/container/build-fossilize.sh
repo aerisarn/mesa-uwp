a48beddb564cc84663ad7f4b6bc814e1996bcea4
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2023-08-29 15:13:18 +0200

    ci: add comment explaining which image tags to update for Fossilize
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24923>

--- .gitlab-ci/container/build-fossilize.sh
+++ .gitlab-ci/container/build-fossilize.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# When changing this file, you need to bump the following
+# .gitlab-ci/image-tags.yml tags:
+# DEBIAN_X86_64_TEST_VK_TAG
+# KERNEL_ROOTFS_TAG
+
 set -ex
 
 git clone https://github.com/ValveSoftware/Fossilize.git
