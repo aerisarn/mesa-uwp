2a530cb1889628f51b849e919c7beeb8dc2d52e2
Vignesh Raman <vignesh.raman@collabora.com>
2023-08-30 10:15:13 +0530

    ci: copy logging script to install
    Copy structured_logger.py and custom_logger.py to install,
    so that the ci jobs can use these scripts for logging.
    
    Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25179>

--- .gitlab-ci/prepare-artifacts.sh
+++ .gitlab-ci/prepare-artifacts.sh
@@ -42,6 +42,8 @@ cp -Rp .gitlab-ci/valve install/
 cp -Rp .gitlab-ci/vkd3d-proton install/
 cp -Rp .gitlab-ci/setup-test-env.sh install/
 cp -Rp .gitlab-ci/*-runner.sh install/
+cp -Rp .gitlab-ci/bin/structured_logger.py install/
+cp -Rp .gitlab-ci/bin/custom_logger.py install/
 find . -path \*/ci/\*.txt \
     -o -path \*/ci/\*.toml \
     -o -path \*/ci/\*traces\*.yml \
