0df0980fc4e044c48c917c8d7d1666d892a08bad
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2023-09-02 09:26:41 -0400

    agx: Enable sinking ALU
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24833>

--- src/asahi/compiler/agx_compile.c
+++ src/asahi/compiler/agx_compile.c
@@ -2306,7 +2306,8 @@ agx_optimize_nir(nir_shader *nir, unsigned *preamble_size)
    /* Cleanup optimizations */
    nir_move_options move_all = nir_move_const_undef | nir_move_load_ubo |
                                nir_move_load_input | nir_move_comparisons |
-                               nir_move_copies | nir_move_load_ssbo;
+                               nir_move_copies | nir_move_load_ssbo |
+                               nir_move_alu;
 
    NIR_PASS_V(nir, nir_opt_sink, move_all);
    NIR_PASS_V(nir, nir_opt_move, move_all);
