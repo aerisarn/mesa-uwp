Grima04/mesa-turnip-kgsl

commit 97d6ceaf04d6e2fe2c6764f0a6a415f07c956851

intel: Remove GEN_IS_HASWELL macro
Use GEN_VERSIONx10 == 75 check in place of GEN_IS_HASWELL macro.
GEN_GEN and GEN_VERSIONx10 macros provide a consistent way to do platform
version checks. We can avoid platform specific macros.

Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9608>

src/intel/blorp/blorp_genX_exec.h
@@ -227,7 +227,7 @@ emit_urb_config(struct blorp_batch *batch,
                         false, false, entry_size,
                         entries, start, deref_block_size, &constrained);
 
-#if GEN_GEN == 7 && !GEN_IS_HASWELL
+#if GEN_GEN == 7 && GEN_VERSIONx10 != 75
    /* From the IVB PRM Vol. 2, Part 1, Section 3.2.1:
     *
     *    "A PIPE_CONTROL with Post-Sync Operation set to 1h and a depth stall
@@ -958,7 +958,7 @@ blorp_emit_ps_config(struct blorp_batch *batch,
       ps.MaximumNumberofThreads =
          batch->blorp->isl_dev->info->max_wm_threads - 1;
 
-#if GEN_IS_HASWELL
+#if GEN_VERSIONx10 == 75
       ps.SampleMask = 1;
 #endif
 

src/intel/common/mi_builder.h
@@ -95,7 +95,7 @@ struct mi_value {
       uint32_t reg;
    };
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    bool invert;
 #endif
 };
@@ -130,7 +130,7 @@ mi_adjust_reg_num(uint32_t reg)
 struct mi_builder {
    __gen_user_data *user_data;
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    uint32_t gprs;
    uint8_t gpr_refs[MI_BUILDER_NUM_ALLOC_GPRS];
 
@@ -145,7 +145,7 @@ mi_builder_init(struct mi_builder *b, __gen_user_data *user_data)
    memset(b, 0, sizeof(*b));
    b->user_data = user_data;
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    b->gprs = 0;
    b->num_math_dwords = 0;
 #endif
@@ -154,7 +154,7 @@ mi_builder_init(struct mi_builder *b, __gen_user_data *user_data)
 static inline void
 mi_builder_flush_math(struct mi_builder *b)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    if (b->num_math_dwords == 0)
       return;
 
@@ -172,7 +172,7 @@ mi_builder_flush_math(struct mi_builder *b)
 /* The actual hardware limit on GPRs */
 #define _MI_BUILDER_NUM_HW_GPRS 16
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 
 static inline bool
 mi_value_is_reg(struct mi_value val)
@@ -221,7 +221,7 @@ mi_new_gpr(struct mi_builder *b)
       .reg = _MI_BUILDER_GPR_BASE + gpr * 8,
    };
 }
-#endif /* GEN_GEN >= 8 || GEN_IS_HASWELL */
+#endif /* GEN_GEN >= 8 || GEN_VERSIONx10 == 75 */
 
 /** Take a reference to a mi_value
  *
@@ -236,7 +236,7 @@ mi_new_gpr(struct mi_builder *b)
 static inline struct mi_value
 mi_value_ref(struct mi_builder *b, struct mi_value val)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    if (_mi_value_is_allocated_gpr(val)) {
       unsigned gpr = _mi_value_as_gpr(val);
       assert(gpr < MI_BUILDER_NUM_ALLOC_GPRS);
@@ -244,7 +244,7 @@ mi_value_ref(struct mi_builder *b, struct mi_value val)
       assert(b->gpr_refs[gpr] < UINT8_MAX);
       b->gpr_refs[gpr]++;
    }
-#endif /* GEN_GEN >= 8 || GEN_IS_HASWELL */
+#endif /* GEN_GEN >= 8 || GEN_VERSIONx10 == 75 */
 
    return val;
 }
@@ -256,7 +256,7 @@ mi_value_ref(struct mi_builder *b, struct mi_value val)
 static inline void
 mi_value_unref(struct mi_builder *b, struct mi_value val)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    if (_mi_value_is_allocated_gpr(val)) {
       unsigned gpr = _mi_value_as_gpr(val);
       assert(gpr < MI_BUILDER_NUM_ALLOC_GPRS);
@@ -265,7 +265,7 @@ mi_value_unref(struct mi_builder *b, struct mi_value val)
       if (--b->gpr_refs[gpr] == 0)
          b->gprs &= ~(1u << gpr);
    }
-#endif /* GEN_GEN >= 8 || GEN_IS_HASWELL */
+#endif /* GEN_GEN >= 8 || GEN_VERSIONx10 == 75 */
 }
 
 static inline struct mi_value
@@ -284,7 +284,7 @@ mi_reg32(uint32_t reg)
       .type = MI_VALUE_TYPE_REG32,
       .reg = reg,
    };
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    assert(!_mi_value_is_allocated_gpr(val));
 #endif
    return val;
@@ -297,7 +297,7 @@ mi_reg64(uint32_t reg)
       .type = MI_VALUE_TYPE_REG64,
       .reg = reg,
    };
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    assert(!_mi_value_is_allocated_gpr(val));
 #endif
    return val;
@@ -360,7 +360,7 @@ static inline void
 _mi_copy_no_unref(struct mi_builder *b,
                   struct mi_value dst, struct mi_value src)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    /* TODO: We could handle src.invert by emitting a bit of math if we really
     * wanted to.
     */
@@ -450,7 +450,7 @@ _mi_copy_no_unref(struct mi_builder *b,
             cmm.DestinationMemoryAddress = dst.addr;
             cmm.SourceMemoryAddress = src.addr;
          }
-#elif GEN_IS_HASWELL
+#elif GEN_VERSIONx10 == 75
          {
             struct mi_value tmp = mi_new_gpr(b);
             _mi_copy_no_unref(b, tmp, src);
@@ -506,7 +506,7 @@ _mi_copy_no_unref(struct mi_builder *b,
 
       case MI_VALUE_TYPE_REG32:
       case MI_VALUE_TYPE_REG64:
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
          if (src.reg != dst.reg) {
             mi_builder_emit(b, GENX(MI_LOAD_REGISTER_REG), lrr) {
                struct mi_reg_num reg = mi_adjust_reg_num(src.reg);
@@ -536,7 +536,7 @@ _mi_copy_no_unref(struct mi_builder *b,
    }
 }
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 static inline struct mi_value
 mi_resolve_invert(struct mi_builder *b, struct mi_value src);
 #endif
@@ -552,7 +552,7 @@ mi_resolve_invert(struct mi_builder *b, struct mi_value src);
 static inline void
 mi_store(struct mi_builder *b, struct mi_value dst, struct mi_value src)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    src = mi_resolve_invert(b, src);
 #endif
    _mi_copy_no_unref(b, dst, src);
@@ -564,7 +564,7 @@ static inline void
 mi_memset(struct mi_builder *b, __gen_address_type dst,
           uint32_t value, uint32_t size)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    assert(b->num_math_dwords == 0);
 #endif
 
@@ -582,7 +582,7 @@ static inline void
 mi_memcpy(struct mi_builder *b, __gen_address_type dst,
           __gen_address_type src, uint32_t size)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    assert(b->num_math_dwords == 0);
 #endif
 
@@ -592,7 +592,7 @@ mi_memcpy(struct mi_builder *b, __gen_address_type dst,
    for (uint32_t i = 0; i < size; i += 4) {
       struct mi_value dst_val = mi_mem32(__gen_address_offset(dst, i));
       struct mi_value src_val = mi_mem32(__gen_address_offset(src, i));
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       mi_store(b, dst_val, src_val);
 #else
       /* IVB does not have a general purpose register for command streamer
@@ -609,7 +609,7 @@ mi_memcpy(struct mi_builder *b, __gen_address_type dst,
  * MI_MATH Section.  Only available on Haswell+
  */
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 
 /**
  * Perform a predicated store (assuming the condition is already loaded

src/intel/common/tests/mi_builder_test.cpp
@@ -55,7 +55,7 @@ __gen_address_offset(address addr, uint64_t offset)
    return addr;
 }
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 #define RSVD_TEMP_REG 0x2678 /* MI_ALU_REG15 */
 #else
 #define RSVD_TEMP_REG 0x2430 /* GEN7_3DPRIM_START_VERTEX */
@@ -194,7 +194,7 @@ mi_builder_test::SetUp()
                             (void *)&getparam), 0) << strerror(errno);
 
          ASSERT_TRUE(gen_get_device_info_from_pci_id(device_id, &devinfo));
-         if (devinfo.gen != GEN_GEN || devinfo.is_haswell != GEN_IS_HASWELL) {
+         if (devinfo.gen != GEN_GEN || devinfo.is_haswell != (GEN_VERSIONx10 == 75)) {
             close(fd);
             fd = -1;
             continue;
@@ -412,7 +412,7 @@ TEST_F(mi_builder_test, imm_mem)
 }
 
 /* mem -> mem copies are only supported on HSW+ */
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 TEST_F(mi_builder_test, mem_mem)
 {
    const uint64_t value = 0x0123456789abcdef;
@@ -532,7 +532,7 @@ TEST_F(mi_builder_test, memcpy)
 }
 
 /* Start of MI_MATH section */
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 
 #define EXPECT_EQ_IMM(x, imm) EXPECT_EQ(x, mi_value_to_u64(imm))
 
@@ -953,7 +953,7 @@ TEST_F(mi_builder_test, store_if)
    EXPECT_EQ(*(uint32_t *)(output + 12), (uint32_t)canary);
 }
 
-#endif /* GEN_GEN >= 8 || GEN_IS_HASWELL */
+#endif /* GEN_GEN >= 8 || GEN_VERSIONx10 == 75 */
 
 #if GEN_VERSIONx10 >= 125
 

src/intel/genxml/gen_macros.h
@@ -31,7 +31,7 @@
  *
  * You can do pseudo-runtime checks in your function such as
  *
- * if (GEN_GEN > 8 || GEN_IS_HASWELL) {
+ * if (GEN_GEN > 8 || GEN_VERSIONx10 == 75) {
  *    // Do something
  * }
  *
@@ -41,7 +41,7 @@
  * For places where you really do have a compile-time conflict, you can
  * use preprocessor logic:
  *
- * #if (GEN_GEN > 8 || GEN_IS_HASWELL)
+ * #if (GEN_GEN > 8 || GEN_VERSIONx10 == 75)
  *    // Do something
  * #endif
  *
@@ -57,7 +57,6 @@
 #endif
 
 #define GEN_GEN ((GEN_VERSIONx10) / 10)
-#define GEN_IS_HASWELL ((GEN_VERSIONx10) == 75)
 #define GEN_IS_G4X ((GEN_VERSIONx10) == 45)
 
 /* Prefixing macros */

src/intel/isl/isl_emit_depth_stencil.c
@@ -169,7 +169,7 @@ isl_genX(emit_depth_stencil_hiz_s)(const struct isl_device *dev, void *batch,
       sb.StencilCompressionEnable =
          info->stencil_aux_usage == ISL_AUX_USAGE_STC_CCS;
       sb.ControlSurfaceEnable = sb.StencilCompressionEnable;
-#elif GEN_GEN >= 8 || GEN_IS_HASWELL
+#elif GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       sb.StencilBufferEnable = true;
 #endif
       sb.SurfaceBaseAddress = info->stencil_address;

src/intel/isl/isl_surface_state.c
@@ -324,7 +324,7 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
    assert(info->write_disables == 0);
 #endif
 
-#if GEN_IS_HASWELL
+#if GEN_VERSIONx10 == 75
    s.IntegerSurfaceFormat =
       isl_format_has_int_channel((enum isl_format) s.SurfaceFormat);
 #endif
@@ -363,7 +363,7 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
        *
        * This restriction appears to exist only on Ivy Bridge.
        */
-      if (GEN_GEN == 7 && !GEN_IS_HASWELL && !ISL_DEV_IS_BAYTRAIL(dev) &&
+      if (GEN_GEN == 7 && GEN_VERSIONx10 != 75 && !ISL_DEV_IS_BAYTRAIL(dev) &&
           (info->view->usage & ISL_SURF_USAGE_TEXTURE_BIT) &&
           info->surf->samples > 1)
          assert(info->view->base_array_layer == 0);
@@ -529,7 +529,7 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
 #endif
 #endif
 
-#if (GEN_GEN >= 8 || GEN_IS_HASWELL)
+#if (GEN_GEN >= 8 || GEN_VERSIONx10 == 75)
    if (info->view->usage & ISL_SURF_USAGE_RENDER_TARGET_BIT)
       assert(isl_swizzle_supports_rendering(dev->info, info->view->swizzle));
 
@@ -917,7 +917,7 @@ isl_genX(buffer_fill_state_s)(const struct isl_device *dev, void *state,
    s.MOCS = info->mocs;
 #endif
 
-#if (GEN_GEN >= 8 || GEN_IS_HASWELL)
+#if (GEN_GEN >= 8 || GEN_VERSIONx10 == 75)
    s.ShaderChannelSelectRed = (enum GENX(ShaderChannelSelect)) info->swizzle.r;
    s.ShaderChannelSelectGreen = (enum GENX(ShaderChannelSelect)) info->swizzle.g;
    s.ShaderChannelSelectBlue = (enum GENX(ShaderChannelSelect)) info->swizzle.b;

src/intel/vulkan/gen7_cmd_buffer.c
@@ -33,7 +33,7 @@
 #include "genxml/gen_macros.h"
 #include "genxml/genX_pack.h"
 
-#if GEN_GEN == 7 && !GEN_IS_HASWELL
+#if GEN_GEN == 7 && GEN_VERSIONx10 != 75
 static int64_t
 clamp_int64(int64_t x, int64_t min, int64_t max)
 {
@@ -163,7 +163,7 @@ void genX(CmdBindIndexBuffer)(
    ANV_FROM_HANDLE(anv_buffer, buffer, _buffer);
 
    cmd_buffer->state.gfx.dirty |= ANV_CMD_DIRTY_INDEX_BUFFER;
-   if (GEN_IS_HASWELL)
+   if (GEN_VERSIONx10 == 75)
       cmd_buffer->state.restart_index = restart_index_for_type(indexType);
    cmd_buffer->state.gfx.gen7.index_buffer = buffer;
    cmd_buffer->state.gfx.gen7.index_type = vk_to_gen_index_type(indexType);
@@ -312,7 +312,7 @@ genX(cmd_buffer_flush_dynamic_state)(struct anv_cmd_buffer *cmd_buffer)
       struct anv_buffer *buffer = cmd_buffer->state.gfx.gen7.index_buffer;
       uint32_t offset = cmd_buffer->state.gfx.gen7.index_offset;
 
-#if GEN_IS_HASWELL
+#if GEN_VERSIONx10 == 75
       anv_batch_emit(&cmd_buffer->batch, GEN75_3DSTATE_VF, vf) {
          vf.IndexedDrawCutIndexEnable  = pipeline->primitive_restart;
          vf.CutIndex                   = cmd_buffer->state.restart_index;
@@ -320,7 +320,7 @@ genX(cmd_buffer_flush_dynamic_state)(struct anv_cmd_buffer *cmd_buffer)
 #endif
 
       anv_batch_emit(&cmd_buffer->batch, GENX(3DSTATE_INDEX_BUFFER), ib) {
-#if !GEN_IS_HASWELL
+#if GEN_VERSIONx10 != 75
          ib.CutIndexEnable        = pipeline->primitive_restart;
 #endif
          ib.IndexFormat           = cmd_buffer->state.gfx.gen7.index_type;

src/intel/vulkan/genX_cmd_buffer.c
@@ -759,7 +759,7 @@ set_image_fast_clear_state(struct anv_cmd_buffer *cmd_buffer,
 /* This is only really practical on haswell and above because it requires
  * MI math in order to get it correct.
  */
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 static void
 anv_cmd_compute_resolve_predicate(struct anv_cmd_buffer *cmd_buffer,
                                   const struct anv_image *image,
@@ -840,7 +840,7 @@ anv_cmd_compute_resolve_predicate(struct anv_cmd_buffer *cmd_buffer,
       mip.CompareOperation = COMPARE_SRCS_EQUAL;
    }
 }
-#endif /* GEN_GEN >= 8 || GEN_IS_HASWELL */
+#endif /* GEN_GEN >= 8 || GEN_VERSIONx10 == 75 */
 
 #if GEN_GEN <= 8
 static void
@@ -931,7 +931,7 @@ anv_cmd_predicated_mcs_resolve(struct anv_cmd_buffer *cmd_buffer,
    assert(aspect == VK_IMAGE_ASPECT_COLOR_BIT);
    assert(resolve_op == ISL_AUX_OP_PARTIAL_RESOLVE);
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    anv_cmd_compute_resolve_predicate(cmd_buffer, image,
                                      aspect, 0, array_layer,
                                      resolve_op, fast_clear_supported);
@@ -1000,7 +1000,7 @@ init_fast_clear_color(struct anv_cmd_buffer *cmd_buffer,
    } else {
       anv_batch_emit(&cmd_buffer->batch, GENX(MI_STORE_DATA_IMM), sdi) {
          sdi.Address = addr;
-         if (GEN_GEN >= 8 || GEN_IS_HASWELL) {
+         if (GEN_GEN >= 8 || GEN_VERSIONx10 == 75) {
             /* Pre-SKL, the dword containing the clear values also contains
              * other fields, so we need to initialize those fields to match the
              * values that would be in a color attachment.
@@ -1682,7 +1682,7 @@ genX(BeginCommandBuffer)(
       cmd_buffer->state.gfx.dirty |= ANV_CMD_DIRTY_RENDER_TARGETS;
    }
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    if (cmd_buffer->level == VK_COMMAND_BUFFER_LEVEL_SECONDARY) {
       const VkCommandBufferInheritanceConditionalRenderingInfoEXT *conditional_rendering_info =
          vk_find_struct_const(pBeginInfo->pInheritanceInfo->pNext, COMMAND_BUFFER_INHERITANCE_CONDITIONAL_RENDERING_INFO_EXT);
@@ -1801,7 +1801,7 @@ genX(CmdExecuteCommands)(
       assert(secondary->level == VK_COMMAND_BUFFER_LEVEL_SECONDARY);
       assert(!anv_batch_has_error(&secondary->batch));
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       if (secondary->state.conditional_render_enabled) {
          if (!primary->state.conditional_render_enabled) {
             /* Secondary buffer is constructed as if it will be executed
@@ -2144,7 +2144,7 @@ genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
       if (bits & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
          bits &= ~(ANV_PIPE_RENDER_TARGET_BUFFER_WRITES);
 
-      if (GEN_IS_HASWELL) {
+      if (GEN_VERSIONx10 == 75) {
          /* Haswell needs addition work-arounds:
           *
           * From Haswell PRM, volume 2, part 1, "End-of-Pipe Synchronization":
@@ -2345,7 +2345,7 @@ cmd_buffer_alloc_push_constants(struct anv_cmd_buffer *cmd_buffer)
 
 #if GEN_GEN >= 8
    const unsigned push_constant_kb = 32;
-#elif GEN_IS_HASWELL
+#elif GEN_VERSIONx10 == 75
    const unsigned push_constant_kb = cmd_buffer->device->info.gt == 3 ? 32 : 16;
 #else
    const unsigned push_constant_kb = 16;
@@ -3053,7 +3053,7 @@ cmd_buffer_emit_push_constant(struct anv_cmd_buffer *cmd_buffer,
          c.MOCS = isl_mocs(&cmd_buffer->device->isl_dev, 0, false);
 #endif
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
          /* The Skylake PRM contains the following restriction:
           *
           *    "The driver must ensure The following case does not occur
@@ -3076,7 +3076,7 @@ cmd_buffer_emit_push_constant(struct anv_cmd_buffer *cmd_buffer,
             /* For Ivy Bridge, make sure we only set the first range (actual
              * push constants)
              */
-            assert((GEN_GEN >= 8 || GEN_IS_HASWELL) || i == 0);
+            assert((GEN_GEN >= 8 || GEN_VERSIONx10 == 75) || i == 0);
 
             c.ConstantBody.ReadLength[i + shift] = range->length;
             c.ConstantBody.Buffer[i + shift] =
@@ -3780,7 +3780,7 @@ void genX(CmdDrawIndirectByteCountEXT)(
     uint32_t                                    counterOffset,
     uint32_t                                    vertexStride)
 {
-#if GEN_IS_HASWELL || GEN_GEN >= 8
+#if GEN_VERSIONx10 == 75 || GEN_GEN >= 8
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
    ANV_FROM_HANDLE(anv_buffer, counter_buffer, counterBuffer);
    struct anv_graphics_pipeline *pipeline = cmd_buffer->state.gfx.pipeline;
@@ -3838,7 +3838,7 @@ void genX(CmdDrawIndirectByteCountEXT)(
    }
 
    update_dirty_vbs_for_gen8_vb_flush(cmd_buffer, SEQUENTIAL);
-#endif /* GEN_IS_HASWELL || GEN_GEN >= 8 */
+#endif /* GEN_VERSIONx10 == 75 || GEN_GEN >= 8 */
 }
 
 static void
@@ -3855,7 +3855,7 @@ load_indirect_parameters(struct anv_cmd_buffer *cmd_buffer,
    struct mi_value instance_count = mi_mem32(anv_address_add(addr, 4));
    unsigned view_count = anv_subpass_view_count(cmd_buffer->state.subpass);
    if (view_count > 1) {
-#if GEN_IS_HASWELL || GEN_GEN >= 8
+#if GEN_VERSIONx10 == 75 || GEN_GEN >= 8
       instance_count = mi_imul_imm(&b, instance_count, view_count);
 #else
       anv_finishme("Multiview + indirect draw requires MI_MATH; "
@@ -3987,7 +3987,7 @@ prepare_for_draw_count_predicate(struct anv_cmd_buffer *cmd_buffer,
    struct mi_value ret = mi_imm(0);
 
    if (conditional_render_enabled) {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       ret = mi_new_gpr(b);
       mi_store(b, mi_value_ref(b, ret), mi_mem32(count_address));
 #endif
@@ -4032,7 +4032,7 @@ emit_draw_count_predicate(struct anv_cmd_buffer *cmd_buffer,
    }
 }
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 static void
 emit_draw_count_predicate_with_conditional_render(
                           struct anv_cmd_buffer *cmd_buffer,
@@ -4094,7 +4094,7 @@ void genX(CmdDrawIndirectCount)(
    for (uint32_t i = 0; i < maxDrawCount; i++) {
       struct anv_address draw = anv_address_add(buffer->address, offset);
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       if (cmd_state->conditional_render_enabled) {
          emit_draw_count_predicate_with_conditional_render(
             cmd_buffer, &b, i, mi_value_ref(&b, max));
@@ -4165,7 +4165,7 @@ void genX(CmdDrawIndexedIndirectCount)(
    for (uint32_t i = 0; i < maxDrawCount; i++) {
       struct anv_address draw = anv_address_add(buffer->address, offset);
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
       if (cmd_state->conditional_render_enabled) {
          emit_draw_count_predicate_with_conditional_render(
             cmd_buffer, &b, i, mi_value_ref(&b, max));
@@ -4674,7 +4674,7 @@ void genX(CmdDispatchIndirect)(
       mip.CompareOperation = COMPARE_FALSE;
    }
 
-#if GEN_IS_HASWELL
+#if GEN_VERSIONx10 == 75
    if (cmd_buffer->state.conditional_render_enabled) {
       /* predicate &= !(conditional_rendering_predicate == 0); */
       mi_store(&b, mi_reg32(MI_PREDICATE_SRC0),
@@ -6071,7 +6071,7 @@ void genX(CmdEndRenderPass2)(
 void
 genX(cmd_emit_conditional_render_predicate)(struct anv_cmd_buffer *cmd_buffer)
 {
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
    struct mi_builder b;
    mi_builder_init(&b, &cmd_buffer->batch);
 
@@ -6087,7 +6087,7 @@ genX(cmd_emit_conditional_render_predicate)(struct anv_cmd_buffer *cmd_buffer)
 #endif
 }
 
-#if GEN_GEN >= 8 || GEN_IS_HASWELL
+#if GEN_GEN >= 8 || GEN_VERSIONx10 == 75
 void genX(CmdBeginConditionalRenderingEXT)(
    VkCommandBuffer                             commandBuffer,
    const VkConditionalRenderingBeginInfoEXT*   pConditionalRenderingBegin)

aerisarn/mesa-uwp

commit ca7ae1a53f83e71a8a117f12a7079cd57485166b

blorp: drop undefined macro
Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 97d6ceaf04 ("intel: Remove GEN_IS_HASWELL macro")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25011>

src/intel/blorp/blorp_genX_exec.h
@@ -2257,7 +2257,7 @@ blorp_exec_compute(struct blorp_batch *batch, const struct blorp_params *params)
       .SharedLocalMemorySize = encode_slm_size(GFX_VER,
                                                prog_data->total_shared),
       .BarrierEnable = cs_prog_data->uses_barrier,
-#if GFX_VER >= 8 || GEN_IS_HASWELL
+#if GFX_VER >= 8 || GFX_VERx10 == 75
       .CrossThreadConstantDataReadLength =
          cs_prog_data->push.cross_thread.regs,
 #endif