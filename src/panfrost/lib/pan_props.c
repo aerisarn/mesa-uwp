a4f7f9975aedcdb35eb2c7c4ac43d1351267a146
Carsten Haitzler <carsten.haitzler@foss.arm.com>
2023-09-04 10:05:03 +0100

    panfrost: Add GPU variant of G57 to the set of known ids
    There already is a G57 ID, but this is for a MTK variant that is not
    actually the original design. The original design is 0x9091 not
    0x9093.
    
    Signed-off-by: Carsten Haitzler <carsten.haitzler@foss.arm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25026>

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -69,6 +69,7 @@ const struct panfrost_model panfrost_model_list[] = {
         MODEL(0x7211, "G76",    "TNOx", HAS_ANISO,         16384, {}),
         MODEL(0x7212, "G52",    "TGOx", HAS_ANISO,         16384, {}),
         MODEL(0x7402, "G52 r1", "TGOx", HAS_ANISO,         16384, {}),
+        MODEL(0x9091, "G57",    "TNAx", HAS_ANISO,         16384, {}),
         MODEL(0x9093, "G57",    "TNAx", HAS_ANISO,         16384, {}),
 };
 /* clang-format on */
