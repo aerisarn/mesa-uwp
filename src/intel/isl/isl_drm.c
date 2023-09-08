1b8a07095d487aa288947ab0c525b893fa6e99ba
Jianxun Zhang <jianxun.zhang@intel.com>
2023-09-08 10:04:57 -0700

    intel/isl: Add Gfx 12.x RC_CCS_CC into modifier scores
    Add RC_CCS_CC drm modifiers of TGL, DG2 and MTL into
    the list with a higher score than RC_CCS modifiers.
    
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
    Acked-by: Rohan Garg <rohan.garg@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25651>

--- src/intel/isl/isl_drm.c
+++ src/intel/isl/isl_drm.c
@@ -231,6 +231,14 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
          return 0;
 
       return 4;
+   case I915_FORMAT_MOD_Y_TILED_GEN12_RC_CCS_CC:
+      if (devinfo->verx10 != 120)
+         return 0;
+
+      if (INTEL_DEBUG(DEBUG_NO_CCS) || INTEL_DEBUG(DEBUG_NO_FAST_CLEAR))
+         return 0;
+
+      return 5;
    case I915_FORMAT_MOD_4_TILED:
       /* Gfx12.5 introduces Tile4. */
       if (devinfo->verx10 < 125)
@@ -245,6 +253,14 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
          return 0;
 
       return 4;
+   case I915_FORMAT_MOD_4_TILED_DG2_RC_CCS_CC:
+      if (!intel_device_info_is_dg2(devinfo))
+         return 0;
+
+      if (INTEL_DEBUG(DEBUG_NO_CCS) || INTEL_DEBUG(DEBUG_NO_FAST_CLEAR))
+         return 0;
+
+      return 5;
    case I915_FORMAT_MOD_4_TILED_MTL_RC_CCS:
       if (!intel_device_info_is_mtl(devinfo))
          return 0;
@@ -253,6 +269,14 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
          return 0;
 
       return 4;
+   case I915_FORMAT_MOD_4_TILED_MTL_RC_CCS_CC:
+      if (!intel_device_info_is_mtl(devinfo))
+         return 0;
+
+      if (INTEL_DEBUG(DEBUG_NO_CCS) || INTEL_DEBUG(DEBUG_NO_FAST_CLEAR))
+         return 0;
+
+      return 5;
    }
 }
 
