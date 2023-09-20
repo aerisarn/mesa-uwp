b975d4e8004a68c821d8592adbd0b1402adbbd03
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2023-09-20 16:09:37 +0200

    radv: enable meshShaderQueries on GFX10.3
    GFX11 support will come later.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25950>

--- src/amd/vulkan/radv_physical_device.c
+++ src/amd/vulkan/radv_physical_device.c
@@ -929,7 +929,7 @@ radv_physical_device_get_features(const struct radv_physical_device *pdevice, st
       .taskShader = taskmesh_en,
       .multiviewMeshShader = taskmesh_en,
       .primitiveFragmentShadingRateMeshShader = taskmesh_en,
-      .meshShaderQueries = false,
+      .meshShaderQueries = pdevice->rad_info.gfx_level == GFX10_3,
 
       /* VK_VALVE_descriptor_set_host_mapping */
       .descriptorSetHostMapping = true,
