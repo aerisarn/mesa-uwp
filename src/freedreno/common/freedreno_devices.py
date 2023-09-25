b4d111ab6c88573e2b54d1b0460e474306d5e2eb
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-09-25 18:28:27 +0200

    freedreno: Include speedbin fallback in 690 chipid to fix probing
    The kernel exposes CHIP_ID, which consists of:
    
    [63:48] const 0 / reserved
    [47:32] speedbin (default 0xffff)
    [31:0 ] actual gpu chip ID
    
    Fix the 690 chip id to make it probe correctly.
    Keep the existing entry for the downstream KGSL driver.
    
    This is essentially the same as Commit 6067aba9e6db ("freedreno: Include
    speedbin fallback in 740 chipid to fix probing"), except for the older
    core.
    
    Fixes: 210c6c11cc61 ("freedreno+tu: Add a690 support")
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25408>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -656,6 +656,7 @@ add_gpus([
 
 add_gpus([
         GPUId(690),
+        GPUId(chip_id=0xffff06090000, name="FD690"), # Default no-speedbin fallback
     ], A6xxGPUInfo(
         CHIP.A6XX,
         a6xx_gen4,
