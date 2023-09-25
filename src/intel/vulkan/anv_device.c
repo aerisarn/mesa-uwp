31883b1f5ea3446d193fcd1a69a963a0f851a9e3
Tapani Pälli <tapani.palli@intel.com>
2023-09-25 15:15:05 +0300

    anv: fix a leak of fp64_nir shader
    Fixes: 8c4c4c3ee1a2 ("anv: Add softtp64 workaround")
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9846
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25372>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -3692,6 +3692,8 @@ void anv_DestroyDevice(
       anv_queue_finish(&device->queues[i]);
    vk_free(&device->vk.alloc, device->queues);
 
+   ralloc_free(device->fp64_nir);
+
    anv_device_destroy_context_or_vm(device);
 
    if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS)) {
