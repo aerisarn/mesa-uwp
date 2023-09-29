57decad9768a445de23d093cc8e004269a352b50
Francisco Jerez <currojerez@riseup.net>
2023-09-29 14:11:39 -0700

    intel/xehp: Enable TBIMR by default.
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25493>

--- src/gallium/drivers/iris/driinfo_iris.h
+++ src/gallium/drivers/iris/driinfo_iris.h
@@ -12,7 +12,7 @@ DRI_CONF_SECTION_END
 DRI_CONF_SECTION_PERFORMANCE
    DRI_CONF_ADAPTIVE_SYNC(true)
    DRI_CONF_OPT_E(bo_reuse, 1, 0, 1, "Buffer object reuse",)
-   DRI_CONF_OPT_B(intel_tbimr, false, "Enable TBIMR tiled rendering")
+   DRI_CONF_OPT_B(intel_tbimr, true, "Enable TBIMR tiled rendering")
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_QUALITY
--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -88,7 +88,7 @@ static const driOptionDescription anv_dri_options[] = {
       DRI_CONF_ANV_QUERY_COPY_WITH_SHADER_THRESHOLD(6)
       DRI_CONF_ANV_FORCE_INDIRECT_DESCRIPTORS(false)
       DRI_CONF_SHADER_SPILLING_RATE(0)
-      DRI_CONF_OPT_B(intel_tbimr, false, "Enable TBIMR tiled rendering")
+      DRI_CONF_OPT_B(intel_tbimr, true, "Enable TBIMR tiled rendering")
    DRI_CONF_SECTION_END
 
    DRI_CONF_SECTION_DEBUG
