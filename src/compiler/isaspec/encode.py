aerisarn/mesa-uwp

commit 9e56f69edf5e1755e04b1461272e3a08051d25a3

isaspec: encode: handle special fieldname properties
Without this change a fieldname like '{DST::align=12}' was not used for
encoding. Change the regex to include such fieldnames and remove the fieldname
property in a later step.

Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20462>

src/compiler/isaspec/encode.py
@@ -133,8 +133,11 @@ class Case(object):
         self.expr = None
         if case.expr is not None:
             self.expr = isa.expressions[case.expr]
-        self.fieldnames = re.findall(r"{([a-zA-Z0-9_]+)}", case.display)
+        self.fieldnames = re.findall(r"{([a-zA-Z0-9_:]+)}", case.display)
         self.append_forced(bitset)
+
+        # remove special fieldname properties e.g. :align=
+        self.fieldnames = list(map(lambda name: name.split(':')[0], self.fieldnames))
 
     # Handle fields which don't appear in display template but have
     # force="true"

commit ef602e77f676ba71606f99c99df4a389d9fb65dc

isaspec: encode: Correct used regex
The current regex misses the = sign and therefore fails to match
DST:align=16.

Fixes: 9e56f69edf5 ("isaspec: encode: handle special fieldname properties")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25451>

src/compiler/isaspec/encode.py
@@ -134,7 +134,7 @@ class Case(object):
         self.expr = None
         if case.expr is not None:
             self.expr = bitset.isa.expressions[case.expr]
-        self.fieldnames = re.findall(r"{([a-zA-Z0-9_:]+)}", case.display)
+        self.fieldnames = re.findall(r"{([a-zA-Z0-9_:=]+)}", case.display)
         self.append_forced(bitset)
 
         # remove special fieldname properties e.g. :align=