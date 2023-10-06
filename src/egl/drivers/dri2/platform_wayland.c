aerisarn/mesa-uwp

commit 74451ed3f0832694c8c1fb8fbfe42b4ed2688d4a

egl/wayland: wait for compositor to release shm buffers
Some Wayland compositors, notably Exo, do not always release buffers
fast enough, and not in sync with their frame callbacks, to guarantee
that a free buffer is available the next time a client calls
`eglSwapBuffers()`.

This currently leads to a crash in `dri2_wl_swrast_get_backbuffer_data()`
with the swrast backend. To avoid this, simply block until the
compositor releases a buffer eventually.

While arguably compositors should release buffers they don't need any
more for the next frame, this can be quite complex depending on
the architecture - notably multi-process/IPC in case of Exo.

cc: mesa-stable

Signed-off-by: Robert Mader <robert.mader@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24091>

src/egl/drivers/dri2/platform_wayland.c
@@ -2454,7 +2454,7 @@ swrast_update_buffers(struct dri2_egl_surface *dri2_surf)
    }
 
    /* else choose any another free location */
-   if (!dri2_surf->back) {
+   while (!dri2_surf->back) {
       for (int i = 0; i < ARRAY_SIZE(dri2_surf->color_buffers); i++) {
          if (!dri2_surf->color_buffers[i].locked) {
             dri2_surf->back = &dri2_surf->color_buffers[i];
@@ -2472,11 +2472,15 @@ swrast_update_buffers(struct dri2_egl_surface *dri2_surf)
             break;
          }
       }
-   }
 
-   if (!dri2_surf->back) {
-      _eglError(EGL_BAD_ALLOC, "failed to find free buffer");
-      return -1;
+      /* wait for the compositor to release a buffer */
+      if (!dri2_surf->back) {
+         if (wl_display_dispatch_queue(dri2_dpy->wl_dpy, dri2_surf->wl_queue) ==
+             -1) {
+            _eglError(EGL_BAD_ALLOC, "waiting for a free buffer failed");
+            return -1;
+         }
+      }
    }
 
    dri2_surf->back->locked = true;

commit 8aa43d70e117376ddc1c8d766437b02e1f8fdedd

egl/wayland: don't block in swrast when updating buffers for zink
this is broken, let vulkan wsi handle buffer management

Fixes: 74451ed3f08 ("egl/wayland: wait for compositor to release shm buffers")

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24700>

src/egl/drivers/dri2/platform_wayland.c
@@ -2416,8 +2416,8 @@ swrast_update_buffers(struct dri2_egl_surface *dri2_surf)
 
       /* wait for the compositor to release a buffer */
       if (!dri2_surf->back) {
-         if (wl_display_dispatch_queue(dri2_dpy->wl_dpy, dri2_surf->wl_queue) ==
-             -1) {
+         bool error = zink || wl_display_dispatch_queue(dri2_dpy->wl_dpy, dri2_surf->wl_queue) == -1;
+         if (error) {
             _eglError(EGL_BAD_ALLOC, "waiting for a free buffer failed");
             return -1;
          }