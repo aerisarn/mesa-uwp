a65ba651083f158a4add47a235ec3239d555825a
Vignesh Raman <vignesh.raman@collabora.com>
2023-10-06 10:44:35 +0530

    ci: Uprev crosvm
    Recent commit in linux kernel 6.6 rc3 broke booting
    in crosvm. Latest crosvm contains a fix for this issue.
    So bump the crosvm version to latest.
    
    https://issuetracker.google.com/issues/303128596
    https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4906858
    
    Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25582>

--- .gitlab-ci/container/build-crosvm.sh
+++ .gitlab-ci/container/build-crosvm.sh
@@ -6,7 +6,7 @@ set -ex
 git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 
-CROSVM_VERSION=d0cbf0b23eb4bd2355b011184025c7c5d8749376
+CROSVM_VERSION=e3815e62d675ef436956a992e0ed58b7309c759d
 git clone --single-branch -b main --no-checkout https://chromium.googlesource.com/crosvm/crosvm /platform/crosvm
 pushd /platform/crosvm
 git checkout "$CROSVM_VERSION"
