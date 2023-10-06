d01ca323dd0dbc470d76ebcb0c3a2b58c7e2b17b
Erik Faye-Lund <erik.faye-lund@collabora.com>
2023-10-06 11:33:34 +0200

    docs: mark some redirects as allowed
    These redirects are all fine. I don't think we want to apply them,
    because they are more about implementation details on the target site.
    
    So let's just mark them as OK. They cause no harm.
    
    There's also some redirects that are all about authentication. We also
    don't want to apply these, because they would break the links.
    
    Reviewed-by: Corentin Noël <corentin.noel@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25585>

--- docs/conf.py
+++ docs/conf.py
@@ -139,6 +139,20 @@ linkcheck_ignore = [
 ]
 linkcheck_exclude_documents = [r'relnotes/.*']
 
+linkcheck_allowed_redirects = {
+    # Pages that forward the front-page to a wiki or some explore-page
+    'https://www.freedesktop.org': 'https://www.freedesktop.org/wiki/',
+    'https://x.org': 'https://x.org/wiki/',
+    'https://perf.wiki.kernel.org/': 'https://perf.wiki.kernel.org/index.php/Main_Page',
+    'https://dri.freedesktop.org/': 'https://dri.freedesktop.org/wiki/',
+    'https://gitlab.freedesktop.org/': 'https://gitlab.freedesktop.org/explore/groups',
+    'https://www.sphinx-doc.org/': 'https://www.sphinx-doc.org/en/master/',
+
+    # Pages that requires authentication
+    'https://gitlab.freedesktop.org/admin/runners': 'https://gitlab.freedesktop.org/users/sign_in',
+    'https://gitlab.freedesktop.org/profile/personal_access_tokens': 'https://gitlab.freedesktop.org/users/sign_in',
+}
+
 
 # -- Options for HTMLHelp output ------------------------------------------
 
