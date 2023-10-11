aerisarn/mesa-uwp

commit a5f0f7d4b162c04878fb9d505d55ebdd05c5c773

turnip,ir3: Implement A7XX push consts load via preamble
New push consts loading consist of:
- Push consts are set for the entire pipeline via HLSQ_SHARED_CONSTS_IMM
  array which could fit up to 256b of push consts.
- For each shader stage that uses push consts READ_IMM_SHARED_CONSTS
  should be set in HLSQ_*_CNTL, otherwise push consts may get overwritten
  by new push consts that are set after the draw.
- Push consts are loaded into consts reg file in a shader preamble via
  stsc at the very start of the preamble.

OPC_PUSH_CONSTS_LOAD_MACRO is used instead of directly translating NIR
intrinsic into stsc because: we don't want to teach legalize pass how
to set (ss) between stores and loads of consts reg file, don't want for
stsc to be reordered, etc.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25086>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -2678,6 +2678,16 @@ emit_intrinsic(struct ir3_context *ctx, nir_intrinsic_instr *intr)
       array_insert(b, b->keeps, stc);
       break;
    }
+   case nir_intrinsic_copy_push_const_to_uniform_ir3: {
+      struct ir3_instruction *load =
+         ir3_instr_create(ctx->block, OPC_PUSH_CONSTS_LOAD_MACRO, 0, 0);
+      array_insert(b, b->keeps, load);
+
+      load->push_consts.dst_base = nir_src_as_uint(intr->src[0]);
+      load->push_consts.src_base = nir_intrinsic_base(intr);
+      load->push_consts.src_size = nir_intrinsic_range(intr);
+      break;
+   }
    default:
       ir3_context_error(ctx, "Unhandled intrinsic type: %s\n",
                         nir_intrinsic_infos[intr->intrinsic].name);

src/compiler/nir/nir_intrinsics.py
@@ -1329,6 +1329,10 @@ store("uniform_ir3", [], indices=[BASE])
 # vec4's.
 intrinsic("copy_ubo_to_uniform_ir3", [1, 1], indices=[BASE, RANGE])
 
+# IR3-specific intrinsic for stsc. Loads from push consts to constant file
+# Should be used in the shader preamble.
+intrinsic("copy_push_const_to_uniform_ir3", [1], indices=[BASE, RANGE])
+
 # Intrinsics used by the Midgard/Bifrost blend pipeline. These are defined
 # within a blend shader to read/write the raw value from the tile buffer,
 # without applying any format conversion in the process. If the shader needs

src/freedreno/common/freedreno_dev_info.h
@@ -191,6 +191,10 @@ struct fd_dev_info {
    } a6xx;
 
    struct {
+      /* stsc may need to be done twice for the same range to workaround
+       * _something_, observed in blob's disassembly.
+       */
+      bool stsc_duplication_quirk;
    } a7xx;
 };
 

src/freedreno/common/freedreno_devices.py
@@ -704,7 +704,9 @@ add_gpus([
 
 a7xx_730 = A7XXProps()
 
-a7xx_740 = A7XXProps()
+a7xx_740 = A7XXProps(
+        stsc_duplication_quirk = True,
+    )
 
 add_gpus([
         GPUId(chip_id=0x07030001, name="FD730"), # KGSL, no speedbin data

src/freedreno/ir3/disasm-a3xx.c
@@ -194,6 +194,7 @@ static const struct opc_info {
    OPC(1, OPC_SWZ_SHARED_MACRO, swz_shared.macro),
    OPC(1, OPC_SCAN_MACRO, scan.macro),
    OPC(1, OPC_SHPS_MACRO, shps.macro),
+   OPC(1, OPC_PUSH_CONSTS_LOAD_MACRO, push_consts_load.macro),
 
    /* category 2: */
    OPC(2, OPC_ADD_F,        add.f),

src/freedreno/ir3/instr-a3xx.h
@@ -131,6 +131,11 @@ typedef enum {
    /* Macros that expand to a loop */
    OPC_SCAN_MACRO      = _OPC(1, 58),
 
+   /* Macros that expand to an stsc at the start of the preamble.
+    * It loads into const file and should not be optimized in any way.
+    */
+   OPC_PUSH_CONSTS_LOAD_MACRO = _OPC(1, 59),
+
    /* category 2: */
    OPC_ADD_F           = _OPC(2, 0),
    OPC_MIN_F           = _OPC(2, 1),
@@ -406,7 +411,7 @@ typedef enum {
    /*
     * A manually encoded opcode
     */
-   OPC_META_RAW = _OPC(OPC_META, 7)
+   OPC_META_RAW = _OPC(OPC_META, 7),
 } opc_t;
 /* clang-format on */
 

src/freedreno/ir3/ir3.h
@@ -446,6 +446,10 @@ struct ir3_instruction {
          gl_system_value sysval;
       } input;
       struct {
+         unsigned src_base, src_size;
+         unsigned dst_base;
+      } push_consts;
+      struct {
          uint64_t value;
       } raw;
    };
@@ -2485,6 +2489,7 @@ INSTR1(QUAD_SHUFFLE_VERT)
 INSTR1(QUAD_SHUFFLE_DIAG)
 INSTR2NODST(LDC_K)
 INSTR2NODST(STC)
+INSTR2NODST(STSC)
 #ifndef GPU
 #elif GPU >= 600
 INSTR3NODST(STIB);

src/freedreno/ir3/ir3_compiler.c
@@ -202,13 +202,13 @@ ir3_compiler_create(struct fd_device *dev, const struct fd_dev_id *dev_id,
          compiler->shared_consts_size = 8;
          compiler->geom_shared_consts_size_quirk = 16;
       } else {
-         /* A7XX TODO: properly use new shared consts mechanism */
          compiler->shared_consts_base_offset = -1;
          compiler->shared_consts_size = 0;
          compiler->geom_shared_consts_size_quirk = 0;
       }
 
       compiler->has_fs_tex_prefetch = dev_info->a6xx.has_fs_tex_prefetch;
+      compiler->stsc_duplication_quirk = dev_info->a7xx.stsc_duplication_quirk;
    } else {
       compiler->max_const_pipeline = 512;
       compiler->max_const_geom = 512;

commit 5ad78e61d08479b58d6ef100b7ec14ddd0ab8171

tu: Count a whole push consts range in constlen for PREAMBLE push consts
Either none push consts are loaded via preamble or all of them even if
shader uses a single push const.

Fixes misrenderings with Zink. Zink unconditionally declares push
consts, regardless of whether they are actually used in the shader.

Fixes: a5f0f7d4b162c04878fb9d505d55ebdd05c5c773
("turnip,ir3: Implement A7XX push consts load via preamble")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25638>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -2686,6 +2686,11 @@ emit_intrinsic(struct ir3_context *ctx, nir_intrinsic_instr *intr)
       load->push_consts.dst_base = nir_src_as_uint(intr->src[0]);
       load->push_consts.src_base = nir_intrinsic_base(intr);
       load->push_consts.src_size = nir_intrinsic_range(intr);
+
+      ctx->so->constlen =
+         MAX2(ctx->so->constlen,
+              DIV_ROUND_UP(
+                 load->push_consts.dst_base + load->push_consts.src_size, 4));
       break;
    }
    default: