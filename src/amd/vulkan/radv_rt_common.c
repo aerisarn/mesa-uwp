aerisarn/mesa-uwp

commit e034ba1c44ac662699026bdadfa68af61bb6c1be

radv/rt: Miss rays that hit the triangle's v edge
The hardware seems to do this as well. Avoids invoking hit shaders twice
at shared edges.
Fixes the fails in watertightness tests on emulated RT.

Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24063>

src/amd/vulkan/radv_rt_common.c
@@ -253,40 +253,6 @@ intersect_ray_amd_software_tri(struct radv_device *device, nir_builder *b, nir_s
    nir_ssa_def *v = nir_fsub(b, nir_fmul(b, ax, cy), nir_fmul(b, ay, cx));
    nir_ssa_def *w = nir_fsub(b, nir_fmul(b, bx, ay), nir_fmul(b, by, ax));
 
-   nir_variable *u_var = nir_variable_create(b->shader, nir_var_shader_temp, glsl_float_type(), "u");
-   nir_variable *v_var = nir_variable_create(b->shader, nir_var_shader_temp, glsl_float_type(), "v");
-   nir_variable *w_var = nir_variable_create(b->shader, nir_var_shader_temp, glsl_float_type(), "w");
-   nir_store_var(b, u_var, u, 0x1);
-   nir_store_var(b, v_var, v, 0x1);
-   nir_store_var(b, w_var, w, 0x1);
-
-   /* Fallback to testing edges with double precision...
-    *
-    * The Vulkan spec states it only needs single precision watertightness
-    * but we fail dEQP-VK.ray_tracing_pipeline.watertightness.closedFan2.1024 with
-    * failures = 1 without doing this. :( */
-   nir_ssa_def *cond_retest =
-      nir_ior(b, nir_ior(b, nir_feq_imm(b, u, 0.0f), nir_feq_imm(b, v, 0.0f)), nir_feq_imm(b, w, 0.0f));
-
-   nir_push_if(b, cond_retest);
-   {
-      ax = nir_f2f64(b, ax);
-      ay = nir_f2f64(b, ay);
-      bx = nir_f2f64(b, bx);
-      by = nir_f2f64(b, by);
-      cx = nir_f2f64(b, cx);
-      cy = nir_f2f64(b, cy);
-
-      nir_store_var(b, u_var, nir_f2f32(b, nir_fsub(b, nir_fmul(b, cx, by), nir_fmul(b, cy, bx))), 0x1);
-      nir_store_var(b, v_var, nir_f2f32(b, nir_fsub(b, nir_fmul(b, ax, cy), nir_fmul(b, ay, cx))), 0x1);
-      nir_store_var(b, w_var, nir_f2f32(b, nir_fsub(b, nir_fmul(b, bx, ay), nir_fmul(b, by, ax))), 0x1);
-   }
-   nir_pop_if(b, NULL);
-
-   u = nir_load_var(b, u_var);
-   v = nir_load_var(b, v_var);
-   w = nir_load_var(b, w_var);
-
    /* Perform edge tests. */
    nir_ssa_def *cond_back =
       nir_ior(b, nir_ior(b, nir_flt_imm(b, u, 0.0f), nir_flt_imm(b, v, 0.0f)), nir_flt_imm(b, w, 0.0f));
@@ -295,6 +261,13 @@ intersect_ray_amd_software_tri(struct radv_device *device, nir_builder *b, nir_s
       nir_ior(b, nir_ior(b, nir_fgt_imm(b, u, 0.0f), nir_fgt_imm(b, v, 0.0f)), nir_fgt_imm(b, w, 0.0f));
 
    nir_ssa_def *cond = nir_inot(b, nir_iand(b, cond_back, cond_front));
+
+   /* If the ray is exactly on the edge where v is 0, consider it a miss.
+    * This seems to correspond to what the hardware is doing.
+    * Also, it avoids invoking hit shaders twice on a shared edge, which is
+    * discouraged by the spec.
+    */
+   cond = nir_iand(b, cond, nir_fneu(b, v, nir_imm_float(b, 0.0f)));
 
    nir_push_if(b, cond);
    {

commit f8eec4c4e3a11de7e4617d9e7d8c9a28105ca645

radv/rt: Reject hits within 10ULP of previous hits in emulated RT
This is an alternative workaround that fixes double hits on shared edges
failing some watertightness CTS tests.

Fixes: e034ba1c44 ("radv/rt: Miss rays that hit the triangle's v edge")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24093>

src/amd/vulkan/radv_rt_common.c
@@ -258,13 +258,6 @@ intersect_ray_amd_software_tri(struct radv_device *device, nir_builder *b, nir_d
 
    nir_def *cond = nir_inot(b, nir_iand(b, cond_back, cond_front));
 
-   /* If the ray is exactly on the edge where v is 0, consider it a miss.
-    * This seems to correspond to what the hardware is doing.
-    * Also, it avoids invoking hit shaders twice on a shared edge, which is
-    * discouraged by the spec.
-    */
-   cond = nir_iand(b, cond, nir_fneu(b, v, nir_imm_float(b, 0.0f)));
-
    nir_push_if(b, cond);
    {
       nir_def *det = nir_fadd(b, u, nir_fadd(b, v, w));
@@ -373,7 +366,9 @@ insert_traversal_triangle_case(struct radv_device *device, nir_builder *b, const
    nir_def *div = nir_channel(b, result, 1);
    intersection.t = nir_fdiv(b, intersection.t, div);
 
-   nir_push_if(b, nir_flt(b, intersection.t, nir_load_deref(b, args->vars.tmax)));
+   nir_def *tmax = nir_load_deref(b, args->vars.tmax);
+
+   nir_push_if(b, nir_flt(b, intersection.t, tmax));
    {
       intersection.frontface = nir_fgt_imm(b, div, 0);
       nir_def *switch_ccw =
@@ -407,6 +402,20 @@ insert_traversal_triangle_case(struct radv_device *device, nir_builder *b, const
          {
             nir_def *divs[2] = {div, div};
             intersection.barycentrics = nir_fdiv(b, nir_channels(b, result, 0xc), nir_vec(b, divs, 2));
+
+            nir_def *hit_t = intersection.t;
+            /* t values within 10 ULP of the current hit t are most likely duplicate hits along shared edges, which
+             * might occur with emulated RT. The Vulkan spec discourages double-hits along shared-edges, so reject them
+             * here by subtracting 10 ULP from t.
+             */
+            if (radv_emulate_rt(device->physical_device)) {
+               nir_def *abs_t = nir_fabs(b, hit_t);
+               nir_def *sign_t = nir_fsign(b, hit_t);
+
+               nir_def *tm1 = nir_iadd(b, hit_t, nir_imul_imm(b, nir_f2i32(b, sign_t), -10));
+               nir_def *tm2 = nir_fmul(b, nir_isub_imm(b, 10, abs_t), nir_fneg(b, sign_t));
+               intersection.t = nir_bcsel(b, nir_ige_imm(b, abs_t, 10), tm1, tm2);
+            }
 
             args->triangle_cb(b, &intersection, args, ray_flags);
          }