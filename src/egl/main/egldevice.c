c7e522a9f3e86f16877a9a09d9b273e945247650
Tapani Pälli <tapani.palli@intel.com>
2023-10-23 08:01:31 +0300

    egl: fix leaking drmDevicePtr in _eglFindDevice
    Fixes: 3a9e1014e13 ("egl: Rename _eglAddDevice() to _eglFindDevice()")
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25524>

--- src/egl/main/egldevice.c
+++ src/egl/main/egldevice.c
@@ -185,12 +185,15 @@ _eglFindDevice(int fd, bool software)
 
       if (_eglDeviceSupports(dev, _EGL_DEVICE_DRM) &&
           drmDevicesEqual(device, dev->device) != 0) {
-         goto out;
+         goto cleanup_drm;
       }
    }
 
    /* Couldn't find an EGLDevice for the device. */
    dev = NULL;
+
+cleanup_drm:
+   drmFreeDevice(&device);
 
 #else
    _eglLog(_EGL_FATAL,
