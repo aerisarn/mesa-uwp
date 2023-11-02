04ffef15da1903c7f2497e24d96f742e476b3e8d
Connor Abbott <cwabbott0@gmail.com>
2023-11-02 12:03:35 +0100

    ir3/ra: Don't swap killed sources for early-clobber destination
    We have an optimization to try to swap regular live intervals with
    killed sources when evicting them fails in order to make a contiguous
    space for the destination to fit in, but this doesn't work when the
    destination is early-clobber.
    
    Fixes
    dEQP-GLES31.functional.synchronization.inter_invocation.image_atomic_read_write
    on a650+.
    
    Fixes: d4b5d2a ("ir3/ra: Use killed sources in register eviction")
    Closes: #8886
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26004>

--- src/freedreno/ir3/ir3_ra.c
+++ src/freedreno/ir3/ir3_ra.c
@@ -879,9 +879,13 @@ try_evict_regs(struct ra_ctx *ctx, struct ra_file *file,
       if (evicted)
          continue;
 
-      /* If we couldn't evict this range, we may be able to swap it with a
-       * killed range to acheive the same effect.
+      /* If we couldn't evict this range, but the register we're allocating is
+       * allowed to overlap with a killed range, then we may be able to swap it
+       * with a killed range to acheive the same effect.
        */
+      if (is_early_clobber(reg) || is_source)
+         return false;
+
       foreach_interval (killed, file) {
          if (!killed->is_killed)
             continue;
