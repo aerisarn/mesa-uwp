aerisarn/mesa-uwp

commit 8616c0a52c776ecc7d0e946207ab35213b5ba985

rusticl/event: flush queues from dependencies
We have to flush all event dependencies inside clWaitForEvents and
clFinish implicitly otherwise applications might wait forever.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9052
Fixes: ebfffa1daec ("rusticl/event: wrong but non crashing impl of clWaitForEvents")
Cc: mesa-stable
Signed-off-by: Karol Herbst <git@karolherbst.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23110>

src/gallium/frontends/rusticl/api/queue.rs
@@ -195,7 +195,13 @@ pub fn flush_queue(command_queue: cl_command_queue) -> CLResult<()> {
 
 pub fn finish_queue(command_queue: cl_command_queue) -> CLResult<()> {
     // CL_INVALID_COMMAND_QUEUE if command_queue is not a valid host command-queue.
-    command_queue.get_ref()?.flush(true)
+    let q = command_queue.get_ref()?;
+
+    for q in q.dependencies_for_pending_events() {
+        q.flush(false)?;
+    }
+
+    q.flush(true)
 }
 
 pub fn release_command_queue(command_queue: cl_command_queue) -> CLResult<()> {

src/gallium/frontends/rusticl/core/queue.rs
@@ -7,6 +7,7 @@ use crate::impl_cl_type_trait;
 use mesa_rust_util::properties::*;
 use rusticl_opencl_gen::*;
 
+use std::collections::HashSet;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -92,6 +93,14 @@ impl Queue {
             }
         }
         Ok(())
+    }
+
+    pub fn dependencies_for_pending_events(&self) -> HashSet<Arc<Queue>> {
+        let p = self.pending.lock().unwrap();
+
+        let mut queues = Event::deep_unflushed_queues(&p);
+        queues.remove(self);
+        queues
     }
 }
 

src/gallium/frontends/rusticl/api/event.rs
@@ -74,13 +74,14 @@ pub fn wait_for_events(num_events: cl_uint, event_list: *const cl_event) -> CLRe
         return Err(CL_INVALID_CONTEXT);
     }
 
-    // TODO better impl
+    // find all queues we have to flush
+    for q in Event::deep_unflushed_queues(&evs) {
+        q.flush(false)?;
+    }
+
+    // now wait on all events and check if we got any errors
     let mut err = false;
     for e in evs {
-        if let Some(q) = &e.queue {
-            q.flush(false)?;
-        }
-
         err |= e.wait() < 0;
     }
 

src/gallium/frontends/rusticl/core/event.rs
@@ -9,6 +9,7 @@ use mesa_rust::pipe::fence::*;
 use mesa_rust_util::static_assert;
 use rusticl_opencl_gen::*;
 
+use std::collections::HashSet;
 use std::os::raw::c_void;
 use std::slice;
 use std::sync::Arc;
@@ -184,6 +185,39 @@ impl Event {
         } else {
             status
         }
+    }
+
+    fn deep_unflushed_deps_impl<'a>(&'a self, result: &mut HashSet<&'a Event>) {
+        if self.status() <= CL_SUBMITTED as i32 {
+            return;
+        }
+
+        // only scan dependencies if it's a new one
+        if result.insert(self) {
+            for e in &self.deps {
+                e.deep_unflushed_deps_impl(result);
+            }
+        }
+    }
+
+    /// does a deep search and returns a list of all dependencies including `events` which haven't
+    /// been flushed out yet
+    pub fn deep_unflushed_deps(events: &[Arc<Event>]) -> HashSet<&Event> {
+        let mut result = HashSet::new();
+
+        for e in events {
+            e.deep_unflushed_deps_impl(&mut result);
+        }
+
+        result
+    }
+
+    /// does a deep search and returns a list of all queues which haven't been flushed yet
+    pub fn deep_unflushed_queues(events: &[Arc<Event>]) -> HashSet<Arc<Queue>> {
+        Event::deep_unflushed_deps(events)
+            .iter()
+            .filter_map(|e| e.queue.clone())
+            .collect()
     }
 }
 

commit 8cbb84dc428ff805abc514d810faebe64bb03cdb

rusticl/queue: fix implicit flushing of queue dependencies
Needed by Davinci Resolve.

There are two parts to this fix:
1. flush dependencies also on flush, not just finish
2. move the dependency checking logic into Queue::flush as otherwise we
   miss required implicit flushes.

Fixes: 8616c0a52c7 ("rusticl/event: flush queues from dependencies")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Reviewed-by: @LingMan <18294-LingMan@users.noreply.gitlab.freedesktop.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26053>

src/gallium/frontends/rusticl/api/queue.rs
@@ -212,13 +212,7 @@ fn flush(command_queue: cl_command_queue) -> CLResult<()> {
 #[cl_entrypoint]
 fn finish(command_queue: cl_command_queue) -> CLResult<()> {
     // CL_INVALID_COMMAND_QUEUE if command_queue is not a valid host command-queue.
-    let q = command_queue.get_ref()?;
-
-    for q in q.dependencies_for_pending_events() {
-        q.flush(false)?;
-    }
-
-    q.flush(true)
+    command_queue.get_ref()?.flush(true)
 }
 
 #[cl_entrypoint]

src/gallium/frontends/rusticl/core/queue.rs
@@ -9,7 +9,6 @@ use mesa_rust::pipe::context::PipeContext;
 use mesa_rust_util::properties::*;
 use rusticl_opencl_gen::*;
 
-use std::collections::HashSet;
 use std::mem;
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -133,6 +132,7 @@ impl Queue {
     pub fn flush(&self, wait: bool) -> CLResult<()> {
         let mut state = self.state.lock().unwrap();
         let events = mem::take(&mut state.pending);
+        let mut queues = Event::deep_unflushed_queues(&events);
 
         // Update last if and only if we get new events, this prevents breaking application code
         // doing things like `clFlush(q); clFinish(q);`
@@ -146,21 +146,26 @@ impl Queue {
                 .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
         }
 
-        if wait {
+        let last = wait.then(|| state.last.clone());
+
+        // We have to unlock before actually flushing otherwise we'll run into dead locks when a
+        // queue gets flushed concurrently.
+        drop(state);
+
+        // We need to flush out other queues implicitly and this _has_ to happen after taking the
+        // pending events, otherwise we'll risk dead locks when waiting on events.
+        queues.remove(self);
+        for q in queues {
+            q.flush(false)?;
+        }
+
+        if let Some(last) = last {
             // Waiting on the last event is good enough here as the queue will process it in order
             // It's not a problem if the weak ref is invalid as that means the work is already done
             // and waiting isn't necessary anymore.
-            state.last.upgrade().map(|e| e.wait());
+            last.upgrade().map(|e| e.wait());
         }
         Ok(())
-    }
-
-    pub fn dependencies_for_pending_events(&self) -> HashSet<Arc<Queue>> {
-        let state = self.state.lock().unwrap();
-
-        let mut queues = Event::deep_unflushed_queues(&state.pending);
-        queues.remove(self);
-        queues
     }
 
     pub fn is_profiling_enabled(&self) -> bool {