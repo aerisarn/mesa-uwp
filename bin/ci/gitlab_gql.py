aerisarn/mesa-uwp

commit b87e092489037daf45a54641e655b6838536ec9f

ci/bin: Fix gitlab_gql methods that uses needs DAG
Some gitlab_gql.py features like `--print-dag` were affected by recent
changes. Update those functions with the refactored data.

Fixes: c7b67d86191659c4c5b1adc0306e2a1381400178
Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25940>

bin/ci/gitlab_gql.py
@@ -460,6 +460,12 @@ def get_job_final_definition(job_name, merged_yaml, project_path, sha):
         print(image)
 
 
+def from_sha_to_pipeline_iid(gl_gql: GitlabGQL, params) -> str:
+    result = gl_gql.query("pipeline_utils.gql", params)
+
+    return result["project"]["pipelines"]["nodes"][0]["iid"]
+
+
 def parse_args() -> Namespace:
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
@@ -499,10 +505,11 @@ def parse_args() -> Namespace:
 def main():
     args = parse_args()
     gl_gql = GitlabGQL(token=args.gitlab_token)
+    args.iid = from_sha_to_pipeline_iid(gl_gql, {"projectPath": args.project_path, "sha": args.sha})
 
     if args.print_dag:
-        dag, jobs = create_job_needs_dag(
-            gl_gql, {"projectPath": args.project_path, "sha": args.sha}
+        dag = create_job_needs_dag(
+            gl_gql, {"projectPath": args.project_path, "iid": args.iid}, disable_cache=True
         )
 
         if args.regex:

bin/ci/pipeline_utils.gql
@@ -1,0 +1,9 @@
+query sha2pipelineIID($projectPath: ID!, $sha: String!) {
+  project(fullPath: $projectPath) {
+    pipelines(last: 1, sha:$sha){
+      nodes {
+        iid
+      }
+    }
+  }
+}

commit f4f4d80096c360f86a2594f84034c086fb498b99

bin/gitlab_gql: only get the pipeline when a pipeline is needed
Otherwise, options like --print-merged-yaml are broken when their
commit's push didn't create a pipeline.

Fixes: b87e092489037daf45a5 ("ci/bin: Fix gitlab_gql methods that uses needs DAG")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26142>

bin/ci/gitlab_gql.py
@@ -509,11 +509,10 @@ def main():
 
     sha = check_output(['git', 'rev-parse', args.sha]).decode('ascii').strip()
 
-    args.iid = from_sha_to_pipeline_iid(gl_gql, {"projectPath": args.project_path, "sha": sha})
-
     if args.print_dag:
+        iid = from_sha_to_pipeline_iid(gl_gql, {"projectPath": args.project_path, "sha": sha})
         dag = create_job_needs_dag(
-            gl_gql, {"projectPath": args.project_path, "iid": args.iid}, disable_cache=True
+            gl_gql, {"projectPath": args.project_path, "iid": iid}, disable_cache=True
         )
 
         if args.regex: