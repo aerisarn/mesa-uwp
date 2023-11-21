d0c355601129fb0fcfb6039eee69217bc9597c77
Sil Vilerino <sivileri@microsoft.com>
2023-11-21 11:08:15 -0500

    meson: Add all, all_free (default) options for video-codecs option.
    - Adds new 'all' value to the video-codecs option
    - Adds 'all_free' value to video-codecs and sets
       it as default value for non patent-encumbered
       codecs, restoring the behavior for these codecs
       before existing as options in commit 7b22dd8bfdc
    
    Fixes: 7b22dd8bfdc ("meson: add vp9 and av1 codec support options")
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26320>

--- .gitlab-ci/windows/mesa_build.ps1
+++ .gitlab-ci/windows/mesa_build.ps1
@@ -50,7 +50,7 @@ meson setup `
 -Dvulkan-drivers="swrast,amd,microsoft-experimental" `
 -Dgallium-drivers="swrast,d3d12,zink" `
 -Dgallium-va=enabled `
--Dvideo-codecs="h264dec,h264enc,h265dec,h265enc,vc1dec,av1dec,av1enc,vp9dec" `
+-Dvideo-codecs="all" `
 -Dshared-glapi=enabled `
 -Dgles1=enabled `
 -Dgles2=enabled `
