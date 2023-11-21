aerisarn/mesa-uwp

commit c47488341ec39527af9e153aaaf918edcabd801d

nak: Use load_interpolated_input for frag_coord
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24998>

src/nouveau/compiler/nak_nir.c
@@ -426,11 +426,25 @@ nak_nir_lower_system_value_instr(nir_builder *b, nir_instr *instr, void *data)
    nir_def *val;
    nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
    switch (intrin->intrinsic) {
+   case nir_intrinsic_load_frag_coord:
    case nir_intrinsic_load_sample_pos: {
+      nir_def *bary = nir_load_barycentric_sample(b, 32,
+         .interp_mode = INTERP_MODE_SMOOTH);
       const uint32_t addr = nak_sysval_attr_addr(SYSTEM_VALUE_FRAG_COORD);
-      val = nir_load_input(b, 2, 32, nir_imm_int(b, 0), .base = addr,
-                           .dest_type = nir_type_float32);
-      val = nir_ffract(b, val);
+      nir_def *coord_in =
+         nir_load_interpolated_input(b, 4, 32, bary, nir_imm_int(b, 0),
+                                     .base = addr,
+                                     .dest_type = nir_type_float32);
+
+      /* gl_FragCoord = (x, y, z, 1/w) */
+      val = nir_fdiv(b, nir_vec4(b, nir_channel(b, coord_in, 0),
+                                    nir_channel(b, coord_in, 1),
+                                    nir_channel(b, coord_in, 2),
+                                    nir_imm_float(b, 1.0f)),
+                        nir_channel(b, coord_in, 3));
+
+      if (intrin->intrinsic == nir_intrinsic_load_sample_pos)
+         val = nir_ffract(b, nir_trim_vector(b, val, 2));
       break;
    }
 
@@ -442,7 +456,6 @@ nak_nir_lower_system_value_instr(nir_builder *b, nir_instr *instr, void *data)
       break;
    }
 
-   case nir_intrinsic_load_frag_coord:
    case nir_intrinsic_load_point_coord:
    case nir_intrinsic_load_tess_coord:
    case nir_intrinsic_load_instance_id:

commit 354cf99869262e64e7c9800c17a864e845e0f7d2

nak: gl_FragCoord and gl_PointCoord are screen-space interpolated
This matches what the NVIDIA proprietary driver does.  It also fixes
rendering corruptions in Grand Theft Auto 3.

Fixes: c47488341ec3 ("nak: Use load_interpolated_input for frag_coord")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10156
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26321>

src/nouveau/compiler/nak_nir.c
@@ -567,7 +567,7 @@ load_frag_w(nir_builder *b, enum nak_interp_loc interp_loc, nir_def *offset)
       nak_sysval_attr_addr(SYSTEM_VALUE_FRAG_COORD) + 12;
 
    const struct nak_nir_ipa_flags flags = {
-      .interp_mode = NAK_INTERP_MODE_PERSPECTIVE,
+      .interp_mode = NAK_INTERP_MODE_SCREEN_LINEAR,
       .interp_freq = NAK_INTERP_FREQ_PASS,
       .interp_loc = interp_loc,
    };
@@ -689,22 +689,11 @@ lower_fs_input_intrin(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
          nak_sysval_attr_addr(SYSTEM_VALUE_POINT_COORD) :
          nak_sysval_attr_addr(SYSTEM_VALUE_FRAG_COORD);
 
-      nir_def *w = load_frag_w(b, interp_loc, NULL);
       nir_def *coord = load_interpolated_input(b, intrin->def.num_components,
                                                addr,
-                                               NAK_INTERP_MODE_PERSPECTIVE,
-                                               interp_loc, nir_frcp(b, w),
-                                               NULL, ctx->nak);
-
-      switch (intrin->intrinsic) {
-      case nir_intrinsic_load_frag_coord:
-         coord = nir_vector_insert_imm(b, coord, w, 3);
-         break;
-      case nir_intrinsic_load_point_coord:
-         break;
-      default:
-         unreachable("Unknown intrinsic");
-      }
+                                               NAK_INTERP_MODE_SCREEN_LINEAR,
+                                               interp_loc, NULL, NULL,
+                                               ctx->nak);
 
       nir_def_rewrite_uses(&intrin->def, coord);
       nir_instr_remove(&intrin->instr);