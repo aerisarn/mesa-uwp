f0cb8852a460545a2a43013c1c3d3381d806d279
Marek Olšák <marek.olsak@amd.com>
2023-11-24 04:14:34 -0500

    ac: move the IB parsers into ac_parse_ib.c
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26349>

--- src/amd/common/ac_debug.h
+++ src/amd/common/ac_debug.h
@@ -24,6 +24,8 @@
 extern "C" {
 #endif
 
+struct si_reg;
+
 struct ac_wave_info {
    unsigned se; /* shader engine */
    unsigned sh; /* shader array */
@@ -40,10 +42,21 @@ struct ac_wave_info {
 
 typedef void *(*ac_debug_addr_callback)(void *data, uint64_t addr);
 
+/* ac_debug.c */
+const struct si_reg *ac_find_register(enum amd_gfx_level gfx_level, enum radeon_family family,
+                                      unsigned offset);
 const char *ac_get_register_name(enum amd_gfx_level gfx_level, enum radeon_family family,
                                  unsigned offset);
 bool ac_register_exists(enum amd_gfx_level gfx_level, enum radeon_family family,
                         unsigned offset);
+bool ac_vm_fault_occurred(enum amd_gfx_level gfx_level, uint64_t *old_dmesg_timestamp,
+                         uint64_t *out_addr);
+unsigned ac_get_wave_info(enum amd_gfx_level gfx_level, const struct radeon_info *info,
+                          struct ac_wave_info waves[AC_MAX_WAVES_PER_CHIP]);
+void ac_print_gpuvm_fault_status(FILE *output, enum amd_gfx_level gfx_level,
+                                 uint32_t status);
+
+/* ac_parse_ib.c */
 void ac_dump_reg(FILE *file, enum amd_gfx_level gfx_level, enum radeon_family family,
                  unsigned offset, uint32_t value, uint32_t field_mask);
 void ac_parse_ib_chunk(FILE *f, uint32_t *ib, int num_dw, const int *trace_ids,
@@ -53,15 +66,6 @@ void ac_parse_ib_chunk(FILE *f, uint32_t *ib, int num_dw, const int *trace_ids,
 void ac_parse_ib(FILE *f, uint32_t *ib, int num_dw, const int *trace_ids, unsigned trace_id_count,
                  const char *name, enum amd_gfx_level gfx_level, enum radeon_family family,
                  enum amd_ip_type ip_type, ac_debug_addr_callback addr_callback, void *addr_callback_data);
-
-bool ac_vm_fault_occurred(enum amd_gfx_level gfx_level, uint64_t *old_dmesg_timestamp,
-                         uint64_t *out_addr);
-
-unsigned ac_get_wave_info(enum amd_gfx_level gfx_level, const struct radeon_info *info,
-                          struct ac_wave_info waves[AC_MAX_WAVES_PER_CHIP]);
-
-void ac_print_gpuvm_fault_status(FILE *output, enum amd_gfx_level gfx_level,
-                                 uint32_t status);
 
 #ifdef __cplusplus
 }
