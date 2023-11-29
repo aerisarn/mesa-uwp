a8b68badd58487838aef13926bf9ddd2359676f4
Erik Faye-Lund <erik.faye-lund@collabora.com>
2023-11-29 21:56:59 +0100

    mesa/st: use _mesa_is_zero_size_texture-helper
    No need to repeat the logic.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26418>

--- src/mesa/state_tracker/st_cb_texture.c
+++ src/mesa/state_tracker/st_cb_texture.c
@@ -2355,7 +2355,7 @@ st_TexImage(struct gl_context * ctx, GLuint dims,
 
    prep_teximage(ctx, texImage, format, type);
 
-   if (texImage->Width == 0 || texImage->Height == 0 || texImage->Depth == 0)
+   if (_mesa_is_zero_size_texture(texImage))
       return;
 
    /* allocate storage for texture data */
