b16017d080745973578211489516b2870b38f9d2
Qiang Yu <yuq825@gmail.com>
2023-11-30 21:14:27 +0800

    nir: fix load layer id system_values_read info gather
    Otherwise radeons+ACO compilation fail.
    
    Fixes: 32ee6376ad3 ("nir: add lowering from FS LAYER input to LAYER_ID sysval")
    Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Signed-off-by: Qiang Yu <yuq825@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26422>

--- src/compiler/nir/nir_gather_info.c
+++ src/compiler/nir/nir_gather_info.c
@@ -681,6 +681,7 @@ gather_intrinsic_info(nir_intrinsic_instr *instr, nir_shader *shader,
    case nir_intrinsic_load_gs_header_ir3:
    case nir_intrinsic_load_tcs_header_ir3:
    case nir_intrinsic_load_ray_triangle_vertex_positions:
+   case nir_intrinsic_load_layer_id:
       BITSET_SET(shader->info.system_values_read,
                  nir_system_value_from_intrinsic(instr->intrinsic));
       break;
