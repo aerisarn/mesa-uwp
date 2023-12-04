7db3cc7502913e368d78cc5632f5a842c3eedb66
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-12-04 14:17:47 -0600

    nak/nir: Don't use nir_lower_bit_size on 64-bit values
    It works by casting values to a larger type and doing the operation
    there so it makes no sense to use it for 64-bit values.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26246>

--- src/nouveau/compiler/nak_nir.c
+++ src/nouveau/compiler/nak_nir.c
@@ -150,7 +150,7 @@ lower_bit_size_cb(const nir_instr *instr, void *_data)
       switch (intrin->intrinsic) {
       case nir_intrinsic_vote_ieq:
          if (intrin->src[0].ssa->bit_size != 1 &&
-             intrin->src[0].ssa->bit_size != 32)
+             intrin->src[0].ssa->bit_size < 32)
             return 32;
          return 0;
 
@@ -168,7 +168,7 @@ lower_bit_size_cb(const nir_instr *instr, void *_data)
       case nir_intrinsic_reduce:
       case nir_intrinsic_inclusive_scan:
       case nir_intrinsic_exclusive_scan:
-         if (intrin->src[0].ssa->bit_size != 32)
+         if (intrin->src[0].ssa->bit_size < 32)
             return 32;
          return 0;
 
@@ -179,7 +179,7 @@ lower_bit_size_cb(const nir_instr *instr, void *_data)
 
    case nir_instr_type_phi: {
       nir_phi_instr *phi = nir_instr_as_phi(instr);
-      if (phi->def.bit_size != 32 && phi->def.bit_size != 1)
+      if (phi->def.bit_size < 32 && phi->def.bit_size != 1)
          return 32;
       return 0;
    }
