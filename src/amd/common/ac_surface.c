Grima04/mesa-turnip-kgsl

commit 87ecfdfbf0a8448d1475e6da15175e68bdeb933b

ac/surface: adapt surf_size when modifying surf_pitch
Otherwise we might get VM_L2_PROTECTION_FAULT_STATUS errors.

Fixes: 8275dc1ed57 ("ac/surface: fix epitch when modifying surf_pitch")
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5841>

src/amd/common/ac_surface.c
@@ -1464,18 +1464,27 @@ static int gfx9_compute_miptree(struct ac_addrlib *addrlib,
 
 	surf->u.gfx9.surf_slice_size = out.sliceSize;
 	surf->u.gfx9.surf_pitch = out.pitch;
+	surf->u.gfx9.surf_height = out.height;
+	surf->surf_size = out.surfSize;
+	surf->surf_alignment = out.baseAlign;
+
 	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch &&
 	    surf->u.gfx9.surf.swizzle_mode == ADDR_SW_LINEAR) {
-		/* Adjust surf_pitch to be in elements units,
-		 * not in pixels */
+		/* Adjust surf_pitch to be in elements units not in pixels */
 		surf->u.gfx9.surf_pitch =
 			align(surf->u.gfx9.surf_pitch / surf->blk_w, 256 / surf->bpe);
 		surf->u.gfx9.surf.epitch = MAX2(surf->u.gfx9.surf.epitch,
 						surf->u.gfx9.surf_pitch * surf->blk_w - 1);
+		/* The surface is really a surf->bpe bytes per pixel surface even if we
+		 * use it as a surf->bpe bytes per element one.
+		 * Adjust surf_slice_size and surf_size to reflect the change
+		 * made to surf_pitch.
+		 */
+		surf->u.gfx9.surf_slice_size = MAX2(
+			surf->u.gfx9.surf_slice_size,
+			surf->u.gfx9.surf_pitch * out.height * surf->bpe * surf->blk_w);
+		surf->surf_size = surf->u.gfx9.surf_slice_size * in->numSlices;
 	}
-	surf->u.gfx9.surf_height = out.height;
-	surf->surf_size = out.surfSize;
-	surf->surf_alignment = out.baseAlign;
 
 	if (in->swizzleMode == ADDR_SW_LINEAR) {
 		for (unsigned i = 0; i < in->numMipLevels; i++) {

aerisarn/mesa-uwp

commit 115b61e51f619df0b8d920b8ee572b56e7be575f

ac/surface: don't oversize surf_size
Yet another iteration on the same YUV surfaces.

The change from 87ecfdfbf0a has 2 odd things:
* it's using MAX2(original value, new value) but the point of updating
  surf_slice_size / surf_size is to make it correct relative to the new
  value of surf_pitch
* it's multiplying surf_pitch (= number of elements per row) by height (ok)
  by surf->bpe (= number of bytes per element) by surf->blk_w (= number of
  "horizontal" pixels in an element) so the end unit doesn't make sense.

Fix this by computing a reasonnable value based on unit: the surf_slice_size
is the number of elements per row (surf_pitch) x number of bytes per element
(bpe) x number of rows.

This makes the expected size correct and thus fixes users of eglCreateImageKHR,
like the issue #6131.

I tested a bunch of gst pipelines and ffmpeg scripts on various files I have
and didn't notice any issues (on gfx10.3 and gfx9).

Fixes: 87ecfdfbf0a ("ac/surface: adapt surf_size when modifying surf_pitch")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6131
Acked-by: Marek Olšák <marek.olsak@amd.com>
Tested-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26693>

src/amd/common/ac_surface.c
@@ -1889,14 +1889,8 @@ static int gfx9_compute_miptree(struct ac_addrlib *addrlib, const struct radeon_
                                       linear_alignment);
       surf->u.gfx9.epitch =
          MAX2(surf->u.gfx9.epitch, surf->u.gfx9.surf_pitch * surf->blk_w - 1);
-      /* The surface is really a surf->bpe bytes per pixel surface even if we
-       * use it as a surf->bpe bytes per element one.
-       * Adjust surf_slice_size and surf_size to reflect the change
-       * made to surf_pitch.
-       */
-      surf->u.gfx9.surf_slice_size =
-         MAX2(surf->u.gfx9.surf_slice_size,
-              (uint64_t)surf->u.gfx9.surf_pitch * out.height * surf->bpe * surf->blk_w);
+       /* Adjust surf_slice_size and surf_size to reflect the change made to surf_pitch. */
+      surf->u.gfx9.surf_slice_size = (uint64_t)surf->u.gfx9.surf_pitch * out.height * surf->bpe;
       surf->surf_size = surf->u.gfx9.surf_slice_size * in->numSlices;
 
       for (unsigned i = 0; i < in->numMipLevels; i++) {