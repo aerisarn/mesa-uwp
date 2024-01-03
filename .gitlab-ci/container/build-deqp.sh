e1935daa195c4de9e7f23c44875d0b096865e270
Eric Engestrom <eric@igalia.com>
2024-01-03 23:22:35 +0000

    ci: convert several `find | xargs` to `find -exec`
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26896>

--- .gitlab-ci/container/build-deqp.sh
+++ .gitlab-ci/container/build-deqp.sh
@@ -148,8 +148,7 @@ rm -rf /deqp/external/openglcts/modules/cts-runner
 rm -rf /deqp/modules/internal
 rm -rf /deqp/execserver
 rm -rf /deqp/framework
-# shellcheck disable=SC2038,SC2185 # TODO: rewrite find
-find -iname '*cmake*' -o -name '*ninja*' -o -name '*.o' -o -name '*.a' | xargs rm -rf
+find . -depth \( -iname '*cmake*' -o -name '*ninja*' -o -name '*.o' -o -name '*.a' \) -exec rm -rf {} \;
 ${STRIP_CMD:-strip} external/vulkancts/modules/vulkan/deqp-vk
 ${STRIP_CMD:-strip} external/openglcts/modules/glcts
 ${STRIP_CMD:-strip} modules/*/deqp-*
