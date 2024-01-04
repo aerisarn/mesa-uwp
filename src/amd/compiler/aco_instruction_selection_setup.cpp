3009dcd1023a5c29cc61fcc576feac132653c4ae
Rhys Perry <pendingchaos02@gmail.com>
2024-01-04 15:14:56 +0000

    aco: correctly set min/max_subgroup_size for wave32-as-wave64
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26894>

--- src/amd/compiler/aco_instruction_selection_setup.cpp
+++ src/amd/compiler/aco_instruction_selection_setup.cpp
@@ -293,12 +293,8 @@ init_context(isel_context* ctx, nir_shader* shader)
 
    /* Init NIR range analysis. */
    ctx->range_ht = _mesa_pointer_hash_table_create(NULL);
-   ctx->ub_config.min_subgroup_size = 64;
-   ctx->ub_config.max_subgroup_size = 64;
-   if (ctx->shader->info.stage == MESA_SHADER_COMPUTE && ctx->program->info.cs.subgroup_size) {
-      ctx->ub_config.min_subgroup_size = ctx->program->info.cs.subgroup_size;
-      ctx->ub_config.max_subgroup_size = ctx->program->info.cs.subgroup_size;
-   }
+   ctx->ub_config.min_subgroup_size = ctx->program->wave_size;
+   ctx->ub_config.max_subgroup_size = ctx->program->wave_size;
    ctx->ub_config.max_workgroup_invocations = 2048;
    ctx->ub_config.max_workgroup_count[0] = 65535;
    ctx->ub_config.max_workgroup_count[1] = 65535;
