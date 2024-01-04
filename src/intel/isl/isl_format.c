590fe58ef63e4f1ec44c3f406dfa66473be4fdbc
Mark Janes <markjanes@swizzler.org>
2024-01-04 15:24:25 -0800

    intel: remove MTL a0 workarounds
    Meteorlake shipped with the b0 stepping.  Remove fixes for hardware
    bugs that were corrected prior to the platform release.
    
    Acked-by: Caio Oliveira <caio.oliveira@intel.com>
    Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Acked-by: Tapani Pälli <tapani.palli@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26898>

--- src/intel/isl/isl_format.c
+++ src/intel/isl/isl_format.c
@@ -882,10 +882,6 @@ bool
 isl_format_supports_ccs_e(const struct intel_device_info *devinfo,
                           enum isl_format format)
 {
-   /* Disable compression on MTL until B0 */
-   if (intel_needs_workaround(devinfo, 14017240301))
-      return false;
-
    if (!format_info_exists(format))
       return false;
 
